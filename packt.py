6396229e923a416fbc9c3744c2acf663cb8facc5
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-13 14:42:03 -0400

    Update packt.py

--- packt.py
+++ packt.py
@@ -1,5 +1,27 @@
 import sopel.module
+import requests
+from lxml import html
+from fake_useragent import UserAgent
 
 @sopel.module.commands('packt')
 def packt(bot, trigger):
+    title = getPacktTitle()
     bot.say('Packt Free Book (daily)     https://www.packtpub.com/packt/offers/free-learning')
+    bot.say("Today's free book is: " + title)
+
+def getPacktTitle():
+        url = 'https://www.packtpub.com/packt/offers/free-learning'
+
+        ua = UserAgent()
+        header = {'User-Agent':str(ua.chrome)}
+        page = requests.get(url,headers = header)
+
+        if page.status_code == 200:
+                tree = html.fromstring(page.content)
+                title = str(tree.xpath('//*[@id="deal-of-the-day"]/div/div/div[2]/div[2]/h2/text()'))
+                title = title.replace("\\t","")
+                title = title.replace("\\n","")
+                return title
+        else:
+                title = 'Could not get Title. The status code was : ' + page.status_code
+        return title
