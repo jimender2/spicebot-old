02e42b02aedc77ff830b3226dfbed4d30a891e71
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-13 09:42:14 -0400

    Update asimov.py

--- asimov.py
+++ asimov.py
@@ -2,6 +2,6 @@ import sopel.module
 
 @sopel.module.commands('asimov')
 def asimov(bot, trigger):
-    bot.say('1.  A robot may not injure a human being or, through inaction, allow a human being to come to harm.')
-    bot.say('2.  A robot must obey orders given it by human beings except where such orders would conflict with the First Law.')
-    bot.say('3.  A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.')
+    bot.action('may not injure a human being or, through inaction, allow a human being to come to harm.')
+    bot.action('must obey orders given it by human beings except where such orders would conflict with the First Law.')
+    bot.action('must protect its own existence as long as such protection does not conflict with the First or Second Law.')
