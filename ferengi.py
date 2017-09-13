19cdd0a84e86dc512f8f7664d68ef02fa2c69085
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-13 17:49:14 -0400

    Update ferengi.py

--- ferengi.py
+++ ferengi.py
@@ -1,13 +1,11 @@
 import sopel.module
 import random
-import urllib2.urlopen
+import urllib
 
-data = urllib.request.urlopen('https://gist.githubusercontent.com/deathbybandaid/cf1faedb87098224943dc04453bc3f79/raw/5ef59ed209f580bf0a7885945e816445aea178e3/gistfile1.txt')
-lines = open(data).read().splitlines()
+htmlfile=urllib.urlopen("https://gist.githubusercontent.com/deathbybandaid/cf1faedb87098224943dc04453bc3f79/raw/5ef59ed209f580bf0a7885945e816445aea178e3/gistfile1.txt")
+lines=htmlfile.read().splitlines()
 myline =random.choice(lines)
-
-
-@sopel.module.commands('ferengi')
 
 def ferengi(bot, trigger):
     bot.say(myline)
+@sopel.module.commands('ferengi')
