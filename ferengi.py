e70222be4e258e9bc241c8eef9d2c80b00affcc8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-13 18:01:27 -0400

    Update ferengi.py

--- ferengi.py
+++ ferengi.py
@@ -5,7 +5,7 @@ import urllib
 fra='https://gist.githubusercontent.com/deathbybandaid/cf1faedb87098224943dc04453bc3f79/raw/5ef59ed209f580bf0a7885945e816445aea178e3/gistfile1.txt'
 htmlfile=urllib.urlopen(fra)
 lines=htmlfile.read().splitlines()
-myline =random.choice(lines)
+myline=random.choice(lines)
 
 def ferengi(bot, trigger):
     bot.say(myline)
