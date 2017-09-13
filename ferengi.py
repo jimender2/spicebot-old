57e07bd51c6514b7dd32c94507973e0b61052f3f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-13 17:45:05 -0400

    Update ferengi.py

--- ferengi.py
+++ ferengi.py
@@ -1,6 +1,6 @@
 import sopel.module
 import random
-import urllib.request
+import urllib2.urlopen
 
 data = urllib.request.urlopen('https://gist.githubusercontent.com/deathbybandaid/cf1faedb87098224943dc04453bc3f79/raw/5ef59ed209f580bf0a7885945e816445aea178e3/gistfile1.txt')
 lines = open(data).read().splitlines()
