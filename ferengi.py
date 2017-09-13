d4e49bdbd1fb0c32312865bbd999f49600d82457
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-13 17:36:18 -0400

    Update ferengi.py

--- ferengi.py
+++ ferengi.py
@@ -1,5 +1,12 @@
 import sopel.module
+import random
+
+fra='https://gist.githubusercontent.com/deathbybandaid/cf1faedb87098224943dc04453bc3f79/raw/5ef59ed209f580bf0a7885945e816445aea178e3/gistfile1.txt'
+lines = open(fra).read().splitlines()
+myline =random.choice(lines)
+
 
 @sopel.module.commands('ferengi')
+
 def ferengi(bot, trigger):
-    bot.say('Ferengi Rules of Acquisition     http://memory-alpha.wikia.com/wiki/Rules_of_Acquisition')
+    bot.say(myline)
