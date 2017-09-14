89bf482b8c088e566c2cb0ae4641a7e743c1248a
zsutton92 <zsutton92@gmail.com>
2017-09-14 08:48:07 -0500

    Update points.py
    Changed line 13 - concatenation and cast rando to str type
    changed line 14 - concatenation

--- points.py
+++ points.py
@@ -5,6 +5,10 @@ from random import randint
 @sopel.module.commands('points','pants')
 def points(bot, trigger):
         rando = randint(1, 666)
+        # Not real sure if this next line is necessary, may leave it out and see if it works. - Z
+        #rando = str(rando).encode('ascii','ignore').decode('ascii')
         winner = trigger.group(2)
-        randopoints = ('is awarded' , rando , 'points from')
-        bot.say(winner , randopoints , trigger.nick)
+        #randopoints = ('is awarded' , rando , 'points from')
+        #bot.say(winner , randopoints , trigger.nick)
+        randopoints = (' is awarded ' + str(rando) + ' points from ' )
+        bot.say(winner + ' ' + randopoints + ' ' + trigger.nick)
