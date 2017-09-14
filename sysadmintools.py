c2e6ffe2518cfb293bb6ae9512bf1a8818387206
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-14 19:06:26 -0400

    Delete sysadmintools.py

--- sysadmintools.py
+++
@@ -1,6 +0,0 @@
-import sopel.module
-
-@sopel.module.commands('sysadmintools')
-def sysadmintools(bot, trigger):
-    bot.say('https://sysadmin.it-landscape.info/')
-    bot.say('https://github.com/srikanthchandika/awesome-sysadmin')
