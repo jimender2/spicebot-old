7a9222953da47452e9b8cc0f52554d977003d028
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-15 13:46:00 -0400

    Update reload.sh

--- reload.sh
+++ reload.sh
@@ -2,10 +2,15 @@
 ## Simply reload the bot
 
 ## vars
+GITREPOSITORYURL=
 REPODIR=$(dirname $0)
+COMPLETEFOLDERPATH=$(realpath $REPODIR)
+
+## stop
+sudo service sopel stop
 
 ## pull modules
-git -C $REPODIR"/" pull
+git -C $COMPLETEFOLDERPATH pull
 
 ## restart service
 sudo service sopel restart
