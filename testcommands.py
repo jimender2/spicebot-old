7f8953f796fc69de11cb39e3dbedfb728ddcd448
zsutton92 <zsutton92@gmail.com>
2017-09-15 15:53:15 -0500

    Update testcommands.py

--- testcommands.py
+++ testcommands.py
@@ -4,3 +4,7 @@ import sopel.module
 def getChannels(bot,trigger):
 	for c in bot.channels:
 		bot.say("You can find me in " + c)
+@sopel.module.commands('getmembers')
+def getMembers(bot,trigger):
+    for m in bot.channels[1].members:
+        bot.say(m + " is in here.")
