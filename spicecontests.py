0077f1186d01df1349f1774d7eca17e86c4a348e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-15 11:31:33 -0400

    Update spicecontests.py

--- spicecontests.py
+++ spicecontests.py
@@ -10,6 +10,7 @@ rel_path = "swContestsLastBuild.txt"
 abs_file_path = os.path.join(script_dir, rel_path)
 
 @interval(3)
+@sopel.module.commands('swcontests')
 def getSWContests(bot):
     url = 'https://community.spiceworks.com/feed/forum/1550.rss'
     ua = UserAgent()
