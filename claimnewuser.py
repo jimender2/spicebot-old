a15a5355f6b8e7f6d576cb421c16efedf626a0fc
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-15 14:27:53 -0400

    Update claimnewuser.py

--- claimnewuser.py
+++ claimnewuser.py
@@ -2,4 +2,5 @@ import sopel.module
 
 @sopel.module.commands('pee')
 def pee(bot, trigger):
-    bot.say(trigger.nick + ' urinates on new user! Claimed!')
+    bot.say(trigger.nick + ' urinates on new user!')
+    bot.say('Claimed!')
