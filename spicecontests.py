e7b3f36a3cc80dbda78dd375ca3a7a09e7739177
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-16 10:06:41 -0400

    Update spicecontests.py

--- spicecontests.py
+++ spicecontests.py
@@ -9,6 +9,7 @@ script_dir = os.path.dirname(__file__)
 rel_path = "swContestsLastBuild.txt"
 abs_file_path = os.path.join(script_dir, rel_path)
 
+def getChannels(bot):
 for c in bot.channels:
 	channel_name = c
 
