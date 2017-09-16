cff2cc261eae63b7ceadf3b9f4c88efbb7ea4a63
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-16 14:57:57 -0400

    Update dave.py

--- dave.py
+++ dave.py
@@ -2,7 +2,7 @@ import sopel.module
 
 @sopel.module.commands('dave','daveb')
 def sorry(bot, trigger):
-    if trigger.nick == Dave or trigger.nick == dave:
+    if trigger.nick == 'Dave' or trigger.nick == 'dave':
                 bot.say("Is that really you, Dave?")
                 bot.say('Im sorry, ' + trigger.nick + 'but I cannot help you.')
     else:
