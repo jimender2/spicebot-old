404a4a1afd7688e21f802117c4a1818888027c57
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-16 14:12:39 -0400

    Update rulessnew.py

--- rulessnew.py
+++ rulessnew.py
@@ -5,12 +5,12 @@ rulesurl = 'https://pastebin.com/raw/Vrq9bHBD'
 
 @sopel.module.commands('rulesa','rulea')
 def rules(bot, trigger):
+        rulenumber = trigger.group(2)
         if not trigger.group(2):
                 bot.say('Chat Rules:     https://pastebin.com/Vrq9bHBD')
         else:
-                rulenumber = str(trigger.group(2))
                 htmlfile=urllib.urlopen(rulesurl)
                 lines=htmlfile.readlines()
-                myline=lines[rulenumber]
+                myline=lines[3]
                 bot.say(myline)
                 bot.say(rulenumber)
