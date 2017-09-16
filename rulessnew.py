4a1e87df0a5568fbfa1cea7d868049f61fdce08b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-16 14:21:49 -0400

    Update rulessnew.py

--- rulessnew.py
+++ rulessnew.py
@@ -5,13 +5,12 @@ rulesurl = 'https://pastebin.com/raw/Vrq9bHBD'
 
 @sopel.module.commands('rulesa','rulea')
 def rules(bot, trigger):
-        rulenumber = trigger.group(2)
-        actualrulenumber = rulenumber - 1
+        rulenumber = int(trigger.group(2))
         if not trigger.group(2):
                 bot.say('Chat Rules:     https://pastebin.com/Vrq9bHBD')
         else:
                 htmlfile=urllib.urlopen(rulesurl)
                 lines=htmlfile.readlines()
-                myline=lines[3]
+                myline=lines[rulenumber-1]
                 bot.say(myline)
-                bot.say(actualrulenumber)
+                bot.say(rulenumber)
