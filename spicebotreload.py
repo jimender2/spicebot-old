d671d4046854b5fd0f87fd62b00054070e8a0d52
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-18 14:55:36 -0400

    Create spicebotreload.py

---
+++ spicebotreload.py
@@ -0,0 +1,14 @@
+import sopel.module
+import os
+import sys
+from os.path import exists
+
+script_dir = os.path.dirname(__file__)
+
+@sopel.module.require_admin
+@sopel.module.commands('spicebotreload')
+def spicebotreload(bot, trigger):
+  bot.say('Pulling From Github')
+  os.system("sudo git -C " + script_dir +" pull")
+  bot.say('Restarting Service')
+  os.system("sudo service sopel restart")
