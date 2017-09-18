d255b165ca98fd53d007bc85d79fb05a67d1446d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-18 13:39:43 -0400

    Update letsbefriends.py

--- letsbefriends.py
+++ letsbefriends.py
@@ -1,5 +1,5 @@
 import sopel.module
 
-@sopel.module.commands('fuckyouspicebot','fuckspicebot','banspicebot','kickspicebot','hatespicebot')
+@sopel.module.commands('fuckyouspicebot','fuckspicebot','banspicebot','kickspicebot','hatespicebot','fuckoffspicebot')
 def apology(bot, trigger):
     bot.say(trigger.nick + ", I'm sorry I offended you. Lets try to be friends.")
