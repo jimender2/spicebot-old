737c76a78031be740866e459d048a29918c93fce
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-18 13:19:02 -0400

    Update unknown.py

--- unknown.py
+++ unknown.py
@@ -1,5 +1,5 @@
 import sopel.module
 
-@sopel.module.event(ERR_UNKNOWNCOMMAND)
+@sopel.module.event(event.ERR_UNKNOWNCOMMAND)
 def davie(bot):
 	bot.say(trigger.nick + " , I haven't been programmed for that yet.")
