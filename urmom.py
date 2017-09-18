b8ad68a237fedff42ab116c0ef00a6a8fb21fc6c
zsutton92 <zsutton92@gmail.com>
2017-09-18 11:53:01 -0500

    Create urmom.py

---
+++ urmom.py
@@ -0,0 +1,19 @@
+import sopel.module
+import requests
+import json
+
+@sopel.module.commands('urmom')
+def sayJoke(bot,trigger):
+    joke = getJoke()
+    if joke:
+        bot.say('Hey, ' + trigger.group(2) + '!' + joke)
+    else:
+        bot.say('Please leave the mothers out of it.')
+
+def getJoke():
+    url = 'http://api.yomomma.info/'
+    page = requests.get(url,headers = {'Accept':'text/plain'})
+    result = page.content
+    if result:
+        _joke = json.load(result)
+        joke = _joke['joke']
