c20a4d9d9fbf027bff309859b42fec10c5532f28
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-18 14:28:26 -0400

    Update spicebotreload.py

--- spicebotreload.py
+++ spicebotreload.py
@@ -2,10 +2,22 @@ import sopel.module
 import os
 import sys
 from os.path import exists
+import git
+import shutil
 
-script_dir = os.path.dirname(__file__)
+DIR_NAME = os.path.dirname(__file__)
+repo = git.Repo.init(script_dir)
+REMOTE_URL = "https://github.com/deathbybandaid/sopel-modules.git"
 
 @sopel.module.require_admin
 @sopel.module.commands('spicebotreload')
 def spicebotreload(bot, trigger):
-  os.system("sudo git -C " , script_dir , " pull && sudo service sopel restart")
+  bot.say('Pulling From Github')
+  if os.path.isdir(DIR_NAME):
+    origin = repo.create_remote('origin',REMOTE_URL)
+    origin.pull(origin.refs[0].remote_head)
+  else
+    bot.say('Git directory is NOT present.')
+  
+  bot.say('Restarting Service')
+  os.system("sudo service sopel restart")
