40b839c20daa57aaa91a479674f0e2ecde23b48a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-18 13:37:14 -0400

    Update dave.py

--- dave.py
+++ dave.py
@@ -9,6 +9,6 @@ def sorry(bot, trigger):
     else:
                 bot.say('Im sorry, ' + trigger.nick + ', but I cannot help you.')
 
-@module.event('421')
+@sopel.tools.event('421')
 def davie(bot):
 	bot.say(trigger.nick + " , I haven't been programmed for that yet.")
