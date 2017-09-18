c45620cf40dd6f2c2de55d8be64187560747deca
zsutton92 <zsutton92@gmail.com>
2017-09-18 12:06:30 -0500

    Update urmom.py

--- urmom.py
+++ urmom.py
@@ -5,15 +5,16 @@ import json
 @sopel.module.commands('urmom')
 def sayJoke(bot,trigger):
     joke = getJoke()
-    #if joke:
-        #bot.say('Hey, ' + trigger.group(2) + '!' + joke)
-    #else:
-        #bot.say('Please leave the mothers out of it.')
-    bot.say('Hey, ' + trigger.group(2) + '! ' + joke)
+    if joke:
+        bot.say('Hey, ' + trigger.group(2) + '! ' + joke)
+    else:
+        bot.say('Please leave the mothers out of it.')
+    
 
 def getJoke():
     url = 'http://api.yomomma.info'
-    page = requests.get(url,headers = {'Accept':'text/plain'})
+    page = requests.get(url)
     result = page.content
     jsonjoke = json.loads(result)
     joke = jsonjoke['joke']
+    return joke
