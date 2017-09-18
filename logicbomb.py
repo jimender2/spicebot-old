cd7394f89d4ef1ed2914d82cd7930f9e97074f20
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-18 14:47:15 -0400

    Update logicbomb.py

--- logicbomb.py
+++ logicbomb.py
@@ -2,4 +2,4 @@ import sopel.module
 
 @sopel.module.commands('logicbomb')
 def logicbomb(bot, trigger):
-    bot.say('I must... but I can't... But I must...')
+    bot.say("I must... but I can't... But I must...")
