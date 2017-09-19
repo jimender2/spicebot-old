0fabc0b05b438c03d047c5ccac7e671901c102ec
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-19 10:54:41 -0400

    Update bowel.py

--- bowel.py
+++ bowel.py
@@ -3,13 +3,13 @@ import sopel.module
 @sopel.module.commands('poop','poops')
 def pee(bot, trigger):
     if not trigger.group(2):
-        winner = "in the designated corner"
+        myline = "in the designated corner"
     else:
-        winner = trigger.group(2).strip()
-    if winner == "in the designated corner":
-        bot.say(trigger.nick + ' poops ' + winner + '!')
+        myline = trigger.group(2).strip()
+    if myline == "in the designated corner":
+        bot.say(trigger.nick + ' poops ' + myline + '!')
     else:
         if myline.endswith('desk'):
-            bot.say(trigger.nick + ' poops on ' + winner + ', maintaining eye contact the entire time!')
+            bot.say(trigger.nick + ' poops on ' + myline + ', maintaining eye contact the entire time!')
         else:
-            bot.say(trigger.nick + ' poops on ' + winner + ' desk, maintaining eye contact the entire time!')
+            bot.say(trigger.nick + ' poops on ' + myline + ' desk, maintaining eye contact the entire time!')
