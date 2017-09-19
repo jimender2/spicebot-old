a8f91b3ca19bb8aa03a502f081a6d943fc64b1d7
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-19 11:22:50 -0400

    Update dayoftheweek.py

--- dayoftheweek.py
+++ dayoftheweek.py
@@ -2,28 +2,28 @@ from sopel import module
 
 def monday(bot, input):
     bot.say('monday')
-rules.commands = ['monday']
+monday.commands = ['monday']
 
 def tuesday(bot, input):
     bot.say('tuesday')
-rules.commands = ['tuesday']
+tuesday.commands = ['tuesday']
 
 def wednesday(bot, input):
     bot.say('wednesday')
-rules.commands = ['wednesday']
+wednesday.commands = ['wednesday']
 
 def thursday(bot, input):
     bot.say('thursday')
-rules.commands = ['thursday']
+thursday.commands = ['thursday']
 
 def friday(bot, input):
     bot.say('friday')
-rules.commands = ['friday']
+friday.commands = ['friday']
 
 def saturday(bot, input):
     bot.say('saturday')
-rules.commands = ['saturday']
+saturday.commands = ['saturday']
 
 def sunday(bot, input):
     bot.say('sunday')
-rules.commands = ['sunday']
+sunday.commands = ['sunday']
