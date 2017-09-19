58aebe917f20b85af532b6200306e6fb25aa00f2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-19 10:38:03 -0400

    Update bowel.py

--- bowel.py
+++ bowel.py
@@ -5,5 +5,5 @@ def pee(bot, trigger):
     if not trigger.group(2):
         winner = "in the designated corner"
     else:
-        winner = '"on" , trigger.group(2).strip()'
+        winner = trigger.group(2).strip()'
     bot.say(trigger.nick + ' poops ' + winner + 'maintaining eye contact the entire time!')
