498c30186f3218069edef53546b45080bee092e6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-19 10:48:13 -0400

    Update bowel.py

--- bowel.py
+++ bowel.py
@@ -6,7 +6,7 @@ def pee(bot, trigger):
         winner = "in the designated corner"
     else:
         winner = trigger.group(2).strip()
-    if winner == "in the designated corner"
+    if winner == "in the designated corner":
         bot.say(trigger.nick + ' poops ' + winner + '!')
     else:
         bot.say(trigger.nick + ' poops on ' + winner + ' maintaining eye contact the entire time!')
