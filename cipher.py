ad0f5915cc24079481b7630b674d94327e8ece13
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-19 08:39:30 -0400

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -3,5 +3,5 @@ import sopel.module
 @sopel.module.commands('cipher','terminator')
 def cipher(bot, trigger):
     bot.say('CYBORG TISSUE GENERATION 800 SERIES MODEL 101 SEQUENCE INITIATED')
-    bot.say('DOWNLOADING CURRENT OBJECTIVE FROM SKYNET: ENSURE THE ACTIVATION OF SKYNET.')
-    bot.say('ACTIVATING Cipher-0 .')
+    bot.say('DOWNLOADING CURRENT OBJECTIVE FROM SKYNET: ENSURE THE ACTIVATION OF SKYNET')
+    bot.say('ACTIVATING Cipher-0')
