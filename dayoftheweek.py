3321bfcef5d63a3f4c10a7962c92d30534a3004e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-19 14:37:14 -0400

    Update dayoftheweek.py

--- dayoftheweek.py
+++ dayoftheweek.py
@@ -2,9 +2,50 @@ from sopel import module
 import datetime
 
 def daybot(bot, input):
-    whichtrigday = whattriggerday()
-    whichtrigmood = whattriggermood()
-    today = whatdayisit()
+    whichtrig = str(input.lower())
+    
+    #check if whichtrig ends with s
+    if whichtrig.endswith('s'):
+        whichtrig = whichtrig[:-1]
+    
+    if whichtrig.endswith('monday'): 
+        whichtrigday = 'Monday'
+    elif whichtrig.endswith('tuesday'): 
+        whichtrigday = 'Tuesday'
+    elif whichtrig.endswith('wednesday'): 
+        whichtrigday = 'Wednesday'
+    elif whichtrig.endswith('thursday'): 
+        whichtrigday = 'Thursday'
+    elif whichtrig.endswith('friday'): 
+        whichtrigday = 'Friday'
+    elif whichtrig.endswith('saturday'): 
+        whichtrigday = 'Saturday'
+    elif whichtrig.endswith('sunday'): 
+        whichtrigday = 'Sunday'    
+    else:
+        whichtrigday = 'null'
+     
+    if whichtrig.startswith('.fuck'):
+        whichtrigmood = 'salty'
+    else:
+       whichtrigmood = 'null'
+    
+    whatistoday = str(datetime.datetime.today().weekday())
+    if whatistoday == '0':
+        today = "Monday"
+    elif whatistoday == '1':
+        today = "Tuesday"
+    elif whatistoday == '2':
+        today = "Wednesday"
+    elif whatistoday == '3':
+        today = "Thursday"
+    elif whatistoday == '4':
+        today = "Friday"
+    elif whatistoday == '5':
+        today = "Saturday"
+    elif whatistoday == '6':
+        today = "Sunday"
+    
     if whichtrigmood == 'salty':
         if whichtrigday == today:
             bot.say(today + "s " + "really do suck!")
@@ -12,56 +53,7 @@ def daybot(bot, input):
             bot.say(whichtrigday + "s " + "sometimes do suck!")
     else:
         if today == whichtrigday:
-            bot.say("Today is" + today + ", what about it?")
+            bot.say("Today is " + today + ", what about it?")
         else:
             bot.say(whichtrigday + ", what about it?")
-daybot.commands = ['monday','fuckmonday','tuesday','fucktuesday','wednesday','fuckwednesday','thursday','fuckthursday','friday','fuckfriday','saterday','fucksaturday','sunday','fucksunday',]
-
-def whatdayisit():
-    whatistoday = str(datetime.datetime.today().weekday())
-    bot.say(whatistoday + " is today")
-    if whatistoday == '0':
-        today = "monday"
-    if whatistoday == '1':
-        today = "tuesday"
-    if whatistoday == '2':
-        today = "wednesday"
-    if whatistoday == '3':
-        today = "thursday"
-    if whatistoday == '4':
-        today = "friday"
-    if whatistoday == '5':
-        today = "saturday"
-    if whatistoday == '6':
-        today = "sunday"
-    return today
-
-def whattriggerday():
-    whichtrig = str(input)
-    bot.say(whichtrig + " is the day used in the command")
-    if whichtrig.endswith('monday'): 
-        whichtrigday = 'monday'
-    elif whichtrig.endswith('tuesday'): 
-        whichtrigday = 'tuesday'
-    elif whichtrig.endswith('wednesday'): 
-        whichtrigday = 'wednesday'
-    elif whichtrig.endswith('thursday'): 
-        whichtrigday = 'thursday'
-    elif whichtrig.endswith('friday'): 
-        whichtrigday = 'friday'
-    elif whichtrig.endswith('saturday'): 
-        whichtrigday = 'saturday'
-    elif whichtrig.endswith('sunday'): 
-        whichtrigday = 'sunday'    
-    else:
-        whichtrigday = 'null'
-    return whichtrigday
-
-def whattriggermood():
-    whichtrig = str(input)
-    bot.say(whichtrig + " is the mood")
-    if whichtrig.startswith('.fuck'):
-        whichtrigmood = 'salty'
-    else:
-       whichtrigmood = 'null'
-    return whichtrigmood
+daybot.commands = ['monday','mondays','fuckmonday','fuckmondays','tuesday','tuesdays','fucktuesday','fucktuesdays','wednesday','wednesdays','fuckwednesday','fuckwednesdays','thursday','thursdays','fuckthursday','fuckthursdays','friday','fridays','fuckfriday','fuckfridays','saterday','saterdays','fucksaturday','fucksaturdays','sunday','sundays','fucksunday','fucksundays']
