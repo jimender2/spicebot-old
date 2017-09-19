391eeea2ffe2bded13708e4ad98c6aa93ebb8b7b
zsutton92 <zsutton92@gmail.com>
2017-09-19 15:18:06 -0500

    Update testcommands.py

--- testcommands.py
+++ testcommands.py
@@ -2,16 +2,19 @@ import sopel.module
 import sqlite3
 from sopel.tools.target import User, Channel
 
+@sopel.module.require_admin
 @sopel.module.commands('getchannels')
 def getChannels(bot,trigger):
 	for c in bot.channels:
 		bot.say("You can find me in " + c)
+@sopel.module.require_admin
 @sopel.module.commands('getmembers')
 def getMembers(bot,trigger):
     #users = str(bot.channels[trigger.sender].users)
     #bot.say(users)
     for u in bot.channels[trigger.sender].users:
         bot.say(u)
+@sopel.module.require_admin
 @sopel.module.commands('dbtest')
 def get_tables(bot, trigger):
     con = sqlite3.connect('sopel.db')
