0d9d1771530bf84ebfe7421201860a655f2680c1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-19 08:16:38 -0400

    Update isitaholiday.py

--- isitaholiday.py
+++ isitaholiday.py
@@ -4,13 +4,9 @@ import json
 
 @sopel.module.commands('isitaholiday')
 def isitaholiday(bot,trigger):
-    holiday = getholiday()
-    bot.say(holiday)
-
-def getholiday():
     url = 'http://isitaholiday.herokuapp.com/api/v2/holidays/today/'
     page = requests.get(url)
     result = page.content
     jsonholiday = json.loads(result)
     holiday = jsonholiday['status']
-    return holiday
+    bot.say(holiday)
