bc3c182088933206f7d97c69a363e83458d7f8c3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-19 14:07:01 -0400

    Update dayoftheweek.py

--- dayoftheweek.py
+++ dayoftheweek.py
@@ -3,42 +3,9 @@ import datetime
 
 @sopel.module.commands('monday','fuckmonday','tuesday','fucktuesday','wednesday','fuckwednesday','thursday','fuckthursday','friday','fuckfriday','saterday','fucksaturday','sunday','fucksunday')
 def daybot(bot, trigger):
-    whichtrigday = whattriggerday()
-    whichtrigmood = whattriggermood()
-    today = whatdayisit()
-    if whichtrigmood == 'salty':
-        if whichtrigday == today:
-            bot.say(today + "s " + "really do suck!")
-        else:
-            bot.say(whichtrigday + "s " + "sometimes do suck!")
-    else:
-        if today == whichtrigday:
-            bot.say("Today is" + today + ", what about it?")
-        else:
-            bot.say(whichtrigday + ", what about it?")
-
-def whatdayisit(bot):
-    whatistoday = str(datetime.datetime.today().weekday())
-    bot.say(whatistoday + " is today")
-    if whatistoday == '0':
-        today = "monday"
-    if whatistoday == '1':
-        today = "tuesday"
-    if whatistoday == '2':
-        today = "wednesday"
-    if whatistoday == '3':
-        today = "thursday"
-    if whatistoday == '4':
-        today = "friday"
-    if whatistoday == '5':
-        today = "saturday"
-    if whatistoday == '6':
-        today = "sunday"
-    return today
-
-def whattriggerday(bot):
     whichtrig = str(input)
-    bot.say(whichtrig + " is the day used in the command")
+    bot.say(whichtrig + " is the command")
+    
     if whichtrig.endswith('monday'): 
         whichtrigday = 'monday'
     elif whichtrig.endswith('tuesday'): 
@@ -55,13 +22,40 @@ def whattriggerday(bot):
         whichtrigday = 'sunday'    
     else:
         whichtrigday = 'null'
-    return whichtrigday
-
-def whattriggermood(bot):
-    whichtrig = str(input)
-    bot.say(whichtrig + " is the mood")
+    bot.say(whichtrigday + " is the day used in the command")
+    
+    
     if whichtrig.startswith('.fuck'):
         whichtrigmood = 'salty'
     else:
        whichtrigmood = 'null'
-    return whichtrigmood
+    bot.say(whichtrigmood + " is the mood")
+    
+    whatistoday = str(datetime.datetime.today().weekday())
+    bot.say(whatistoday + " is todays number")
+    if whatistoday == '0':
+        today = "monday"
+    if whatistoday == '1':
+        today = "tuesday"
+    if whatistoday == '2':
+        today = "wednesday"
+    if whatistoday == '3':
+        today = "thursday"
+    if whatistoday == '4':
+        today = "friday"
+    if whatistoday == '5':
+        today = "saturday"
+    if whatistoday == '6':
+        today = "sunday"
+    bot.say(whatistoday + " is today")
+    
+    if whichtrigmood == 'salty':
+        if whichtrigday == today:
+            bot.say(today + "s " + "really do suck!")
+        else:
+            bot.say(whichtrigday + "s " + "sometimes do suck!")
+    else:
+        if today == whichtrigday:
+            bot.say("Today is" + today + ", what about it?")
+        else:
+            bot.say(whichtrigday + ", what about it?")
