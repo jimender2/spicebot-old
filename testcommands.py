ea2defb00c754f6d65dc8bd88e7ee536177290e1
zsutton92 <zsutton92@gmail.com>
2017-09-19 15:40:16 -0500

    Update testcommands.py

--- testcommands.py
+++ testcommands.py
@@ -22,7 +22,7 @@ def get_tables(bot, trigger):
     cursor.execute("SELECT * FROM nick_ids;")
     bot.say(str(cursor.fetchall()))
 
- @sopel.module.commands('getcmd')
+@sopel.module.commands('getcmd')
 def get_tables(bot, trigger):
     bot.say(trigger.group(1)
    
