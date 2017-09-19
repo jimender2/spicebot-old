f529d14c1d7f7d85cfa2c406c1623c17cbc6c67b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-19 16:30:23 -0400

    Update points.py

--- points.py
+++ points.py
@@ -4,13 +4,14 @@ from random import randint
 
 @sopel.module.commands('points','pants')
 def points(bot, trigger):
-        rando = randint(1, 666)
-        if not trigger.group(2):
-                winner = "Everybody"
-        else:
-                winner = trigger.group(2).strip()
-        randopoints = ('is awarded ' + str(rando) + ' points from' )
-        if winner == trigger.nick:
-                bot.say("You can't give yourself points!")
-        else:
-                bot.say(winner + ' ' + randopoints + ' ' + trigger.nick)
+    whichtrig = str(input.lower())    
+    rando = randint(1, 666)
+    if not trigger.group(2):
+        winner = "Everybody"
+    else:
+        winner = trigger.group(2).strip()
+    randopoints = ('is awarded ' + str(rando) + whichtrig ' from' )
+    if winner == trigger.nick:
+        bot.say("You can't give yourself points!")
+    else:
+        bot.say(winner + ' ' + randopoints + ' ' + trigger.nick)
