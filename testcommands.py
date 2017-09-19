98f6c74558aa0725122f9bb58d27309b8f04fa7a
zsutton92 <zsutton92@gmail.com>
2017-09-19 13:41:45 -0500

    Update testcommands.py

--- testcommands.py
+++ testcommands.py
@@ -11,4 +11,9 @@ def getMembers(bot,trigger):
     #bot.say(users)
     for u in bot.channels[trigger.sender].users:
         bot.say(u)
-
+@sopel.module.commands('dbtest')
+def get_duels(bot, nick):
+    wins = bot.db.get_nick_value(nick, 'duel_wins') or 0
+    losses = bot.db.get_nick_value(nick, 'duel_losses') or 0
+    bot.say(wins)
+    bot.say(losses)
