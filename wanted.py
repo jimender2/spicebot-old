d1e462c2e3d76eab6c265cb6b4d772ac64c6db6a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-20 18:44:45 -0400

    Update wanted.py

--- wanted.py
+++ wanted.py
@@ -2,4 +2,5 @@ import sopel.module
 
 @sopel.module.commands('wanted')
 def wanted(bot,trigger):
-	bot.say(trigger.nick + " was never wanted as a child, but now is wanted in 37 states!")
+    if trigger.group(2):
+        bot.say(trigger.group(2).strip() + " was never wanted as a child, but now is wanted in 37 states!")
