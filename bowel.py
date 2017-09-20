34617c79b7dd57b48985f25977f86332c410499e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-20 12:49:07 -0400

    Update bowel.py

--- bowel.py
+++ bowel.py
@@ -4,12 +4,14 @@ import sopel.module
 def pee(bot, trigger):
     if not trigger.group(2):
         myline = "in the designated corner"
-    elif trigger.group(2) == 'all':
-        myline = everyone
+    elif trigger.group(2) == 'all' or trigger.group(2) == 'everyone':
+        myline = 'everyone'
     else:
         myline = trigger.group(2).strip()
     if myline == "in the designated corner":
         bot.say(trigger.nick + ' poops ' + myline + '!')
+    elif myline == "everyone":
+        bot.say(trigger.nick + ' poops on ' + myline + '\'s desk, one at a a time!')
     else:
         if myline.endswith('desk'):
             bot.say(trigger.nick + ' poops on ' + myline + ', maintaining eye contact the entire time!')
