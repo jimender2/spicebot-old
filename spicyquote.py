f033a8af0f754fc72c0e3dfa82a4d02880af9c89
zsutton92 <zsutton92@gmail.com>
2017-09-20 09:05:04 -0500

    Create spicyquote.py

---
+++ spicyquote.py
@@ -0,0 +1,25 @@
+from BeautifulSoup import BeautifulSoup
+
+@sopel.module.commands('spicyquote')
+def spicyQuote(bot,trigger):
+    qNum = str(trigger.group(2))
+    if qNum != "":
+        quote = getQuote(qNum)
+        bot.say('Spice quoute #' + qNum + ' coming up!')
+        bot.say(quote)
+    else:
+        bot.say('Please provide a quote number and try again!')
+
+
+
+
+def getQuote(qNum):
+    urlsuffix = 'http://spice.dussed.com/?'
+    quotenum = qNum
+    url = urlsuffix + qNum
+    soup = BeautifulSoup(urlib2.urlopen(url).read())
+    txt = soup.find('td',{'class':'body'}).text
+    txt = txt.replace("&lt;","<")
+    txt = txt.replace("&gt;",">")
+    return quote
+
