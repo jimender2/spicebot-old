b35930e7c300f75651258dafe8a38eb7fd02d440
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-20 15:46:07 -0400

    Update warn.py

--- warn.py
+++ warn.py
@@ -2,7 +2,7 @@ import sopel.module
 
 @sopel.module.commands('warn','warning')
 def warning(bot,trigger):
-	if not trigger.group(2):
-      bot.say("This is just a warning. Overuse of the bot, can get you kicked or banned by an operator. If you want to purely play with the bot, go to ##test, or send Spicebot a PrivateMessage.")
-  else:
-      bot.say(trigger.group(2).strip() + ", This is just a warning. Overuse of the bot, can get you kicked or banned by an operator. If you want to purely play with the bot, go to ##test, or send Spicebot a PrivateMessage.")
+    if not trigger.group(2):
+        bot.say("This is just a warning. Overuse of the bot, can get you kicked or banned by an operator. If you want to purely play with the bot, go to ##test, or send Spicebot a PrivateMessage.")
+    else:
+        bot.say(trigger.group(2).strip() + ", This is just a warning. Overuse of the bot, can get you kicked or banned by an operator. If you want to purely play with the bot, go to ##test, or send Spicebot a PrivateMessage.")
