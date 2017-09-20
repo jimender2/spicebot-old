44d0038291b5a62fdad16ae2b7bc712c25c9e0b2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-20 15:00:01 -0400

    Create matrix.py

---
+++ matrix.py
@@ -0,0 +1,13 @@
+from sopel import module
+
+def matrix(bot, input):
+    bot.say('You have two choices. Red Pill Or Blue Pill')
+matrix.commands = ['matrix']
+
+def redpill(bot, input):
+    bot.say('You take the blue pill, the story ends. You wake up in your bed and believe whatever you want to believe.')
+redpill.commands = ['redpill']
+
+def bluepill(bot, input):
+    bot.say('You take the red pill, you stay in Wonderland, and I show you how deep the rabbit hole goes.')
+bluepill.commands = ['bluepill']
