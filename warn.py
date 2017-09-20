446134d9eb47e9cec61bbf6a2a46504852963490
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-20 15:45:32 -0400

    Update warn.py

--- warn.py
+++ warn.py
@@ -5,4 +5,4 @@ def warning(bot,trigger):
 	if not trigger.group(2):
       bot.say("This is just a warning. Overuse of the bot, can get you kicked or banned by an operator. If you want to purely play with the bot, go to ##test, or send Spicebot a PrivateMessage.")
   else:
-      bot.say(trigger.group(2) + ", This is just a warning. Overuse of the bot, can get you kicked or banned by an operator. If you want to purely play with the bot, go to ##test, or send Spicebot a PrivateMessage.")
+      bot.say(trigger.group(2).strip() + ", This is just a warning. Overuse of the bot, can get you kicked or banned by an operator. If you want to purely play with the bot, go to ##test, or send Spicebot a PrivateMessage.")
