59685be788724623f4b38d8f2a323fbf2b6e3645
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-21 14:54:49 -0400

    Update SpiceBot_Get_cmd.py

--- SpiceBot_Get_cmd.py
+++ SpiceBot_Get_cmd.py
@@ -3,6 +3,7 @@ import sqlite3
 from sopel.tools.target import User, Channel
 from random import randint
 
+@sopel.module.require_admin
 @sopel.module.commands('getcmd')
 def get_tables(bot, trigger):
     bot.say(trigger.group(1))
