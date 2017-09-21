5fd0bb7a4eb24c0aeb56992aa8a7f842566b1703
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-21 18:49:55 -0400

    Update Ferengi_Rules_Of_Acquisition.py

--- Ferengi_Rules_Of_Acquisition.py
+++ Ferengi_Rules_Of_Acquisition.py
@@ -6,9 +6,7 @@ import urllib
 def ferengi(bot, trigger):
     fra='https://raw.githubusercontent.com/deathbybandaid/sopel-modules/dev/otherfiles/ferengi_rules.txt'
     if not trigger.group(2):
-        htmlfile=urllib.urlopen(fra)
-        lines=htmlfile.read().splitlines()
-        myline=random.choice(lines)
+        myline = randomfra()
     else:
         rulenumber = int(trigger.group(2))
         htmlfile=urllib.urlopen(fra)
@@ -21,3 +19,10 @@ def ferengi(bot, trigger):
     if not myline or myline == '\n':
         myline = 'There is no cannonized rule tied to this number.'
     bot.say(myline)
+
+# random rule
+def randomfra():
+    htmlfile=urllib.urlopen(fra)
+    lines=htmlfile.read().splitlines()
+    myline=random.choice(lines)
+    return myline
