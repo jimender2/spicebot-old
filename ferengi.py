b1f7abe88479220a4633968538232972e476e391
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-21 13:50:44 -0400

    Update ferengi.py

--- ferengi.py
+++ ferengi.py
@@ -4,7 +4,7 @@ import urllib
 
 @sopel.module.commands('ferengi')
 def ferengi(bot, trigger):
-    fra='https://gist.githubusercontent.com/deathbybandaid/cf1faedb87098224943dc04453bc3f79/raw/5ef59ed209f580bf0a7885945e816445aea178e3/gistfile1.txt'
+    fra='https://raw.githubusercontent.com/deathbybandaid/sopel-modules/dev/otherfiles/ferengi_rules.txt'
     if not trigger.group(2):
         htmlfile=urllib.urlopen(fra)
         lines=htmlfile.read().splitlines()
