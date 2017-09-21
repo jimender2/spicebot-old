81432f232e6dc3c7e7764adb2e43d8f1c6718e98
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-21 18:27:46 -0400

    Update TechSupport.py

--- TechSupport.py
+++ TechSupport.py
@@ -1,7 +1,8 @@
 import sopel.module
+import random
 
 @sopel.module.commands('techsupport')
 def techsupport(bot, trigger):
-    bot.say('Did you try turning it off and on again?')
-    bot.say('Did you try licking the mouse? Double-lick?')
-    bot.say('Did your try replacing all the ones with zeros?')
+    messages = ["Did you try turning it off and on again?","Did you try licking the mouse? Double-lick?","Did your try replacing all the ones with zeros?"]
+    answer = random.randint(0,len(messages) - 1)
+    bot.say(messages[answer]);
