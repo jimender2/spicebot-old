99a850d78343b76a9a71b81e38a1d597832ddaab
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-21 12:17:38 -0400

    Update trust.py

--- trust.py
+++ trust.py
@@ -5,4 +5,4 @@ def trust(bot,trigger):
     if trigger.group(2):
         bot.say("I just can't ever bring myself to trust " + trigger.group(2) + " again. I can never forgive them for the death of my boy.")
     else:
-        bot.say('Trust Doesn't Rust.')
+        bot.say("Trust Doesn't Rust.")
