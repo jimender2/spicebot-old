464c0f7e1ad31111055b35e7b532c8385f72aa11
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-21 15:16:11 -0400

    Create No_Time.py

---
+++ No_Time.py
@@ -0,0 +1,5 @@
+import sopel.module
+
+@sopel.module.commands('notime')
+def notime(bot,trigger):
+    bot.say("Well I woke up to get me a cold pop and then I thought somebody was barbequing. I said oh lord Jesus it’s a fire. Then I ran out, I didn’t grab no shoes or nothin’ Jesus, I ran for my life. And then the smoke got me, I got bronchitis ain’t nobody got time for that.")
