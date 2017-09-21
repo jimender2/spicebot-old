4a4c4a60cb2086dbcbdd39a3322c11827bb381a2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-21 14:19:54 -0400

    Create Is_It_Friday_Yet.py

---
+++ Is_It_Friday_Yet.py
@@ -0,0 +1,18 @@
+from sopel import module
+import datetime
+import sys
+
+def fridaybot(bot,trigger):
+    whatistoday = str(datetime.datetime.today().weekday())        
+    fridaynumber = '4'
+    if whatistoday == fridaynumber:
+        bot.say("TGIF! It's finally here!!!")
+    elif whatistoday == '5' or whatistoday == '6':
+        bot.say("It's the Weekend. It should be better than Friday!")
+    else:
+        daysmath = int(fridaynumber) - int(whatistoday)
+        if daysmath == int('1'):
+            bot.say("Unfortunately Friday is " + str(daysmath) + " day away. I'm sure we'll make it there!")
+        else:
+            bot.say("Unfortunately Friday is " + str(daysmath) + " days away. I'm sure we'll make it there!")
+fridaybot.commands = ['isitfriday','isitfridayyet']
