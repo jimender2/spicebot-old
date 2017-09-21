7288b6018694823cb879e41c968381c3981aa429
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-21 14:24:59 -0400

    Update and rename halp.py to Halp_Not_Help.py

--- Halp_Not_Help.py
+++ Halp_Not_Help.py
@@ -2,4 +2,4 @@ import sopel.module
 
 @sopel.module.commands('halp')
 def halp(bot,trigger):
-	bot.say("if you need help using help you are truly lost")
+    bot.say("if you need help using help you are truly lost")
