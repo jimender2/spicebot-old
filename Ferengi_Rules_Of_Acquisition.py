a69fa16c35245e96bea213a5d6ef5107ed75272a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-21 19:05:57 -0400

    Update Ferengi_Rules_Of_Acquisition.py

--- Ferengi_Rules_Of_Acquisition.py
+++ Ferengi_Rules_Of_Acquisition.py
@@ -3,14 +3,21 @@ import random
 import urllib
 
 fra='https://raw.githubusercontent.com/deathbybandaid/sopel-modules/dev/otherfiles/ferengi_rules.txt'
-rulenumber = int(trigger.group(2))
 
 @sopel.module.commands('ferengi')
 def ferengi(bot, trigger):
     if not trigger.group(2):
         myline = randomfra()
     else:
-        myline = specificrule()
+        rulenumber = int(trigger.group(2))
+        htmlfile=urllib.urlopen(fra)
+        lines=htmlfile.readlines()
+        try:
+            myline = str(lines[rulenumber-1])
+        except IndexError:
+            myline = 'That doesnt appear to be a rule number.'
+        if not myline or myline == '\n':
+            myline = 'There is no cannonized rule tied to this number.'
     bot.say(myline)
 
 # random rule
@@ -21,14 +28,3 @@ def randomfra():
     if not myline or myline == '\n':
         myline = randomfra()
     return myline
-
-# rule number
-def specificrule():
-    htmlfile=urllib.urlopen(fra)
-    lines=htmlfile.readlines()
-    try:
-        myline = str(lines[rulenumber-1])
-    except IndexError:
-        myline = 'That doesnt appear to be a rule number.'
-    if not myline or myline == '\n':
-        myline = 'There is no cannonized rule tied to this number.'
