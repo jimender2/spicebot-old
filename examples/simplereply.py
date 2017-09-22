947785b9873e15db82799db3e3f03e6dd4aa40c8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-22 12:32:40 -0400

    Create simplereply.py

---
+++ examples/simplereply.py
@@ -0,0 +1,20 @@
+import sopel.module
+
+@sopel.module.commands('yourcommandhere')
+def simplereply(bot,trigger):
+    bot.say('')
+
+# trigger.nick is the person asking command
+# trigger.group(1) is the command
+# trigger.group(2) is the text after the command if any
+
+# example
+# command in example is
+# .fights klingons
+
+#@sopel.module.commands('fight','fights')
+#def simplereply(bot,trigger):
+#    bot.say(trigger.nick + trigger.group(1) + trigger.group(2) + 'at the bar')
+
+# this will say:
+# deathbybandaid fights klingons at the bar
