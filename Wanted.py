886ac0268b76f88a9bb65530cfb6019a6acb0e41
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-22 15:51:17 -0400

    Update Wanted.py

--- Wanted.py
+++ Wanted.py
@@ -5,7 +5,7 @@ from random import randint
 @sopel.module.commands('wanted')
 def wanted(bot,trigger):
     rando = randint(2, 50)
-    if trigger.group(2):
-        bot.say(trigger.group(2).strip() + ' was never wanted as a child, but now is wanted in ' + str(rando) + ' states!')
-    else:
+    if not trigger.group(2):
         bot.say('You must choose a Person.')
+    elif not trigger.group(2) == bot.nick:
+        bot.say(trigger.group(2).strip() + ' was never wanted as a child, but now is wanted in ' + str(rando) + ' states!')
