cbd6a91cc9e29183f302825fbb304f284c1966a7
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-22 13:26:05 -0400

    Update Meraki_Free.py

--- Meraki_Free.py
+++ Meraki_Free.py
@@ -2,6 +2,12 @@ import sopel.module
 
 @sopel.module.commands('meraki','freemeraki')
 def meraki(bot, trigger):
-    bot.say('MX     https://meraki.cisco.com/tc/freemx')
-    bot.say('Switch     https://meraki.cisco.com/tc/freeswitch')
-    bot.say('AP     https://meraki.cisco.com/tc/freeap')
+    if trigger.group(2):
+        if trigger.group(2) == 'mx':
+            bot.say('MX     https://meraki.cisco.com/tc/freemx')
+        if trigger.group(2) == 'switch':
+            bot.say('Switch     https://meraki.cisco.com/tc/freeswitch')
+        if trigger.group(2) == 'ap':
+            bot.say('AP     https://meraki.cisco.com/tc/freeap')
+    else:
+        bot.say('Please specify which product. Choices are MX , AP , or switch .')
