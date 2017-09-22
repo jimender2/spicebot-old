b369d2ae5bbb75fb86681e4e01cb98b91c00c0ae
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-22 15:59:21 -0400

    Update Slap_With_Fish.py

--- Slap_With_Fish.py
+++ Slap_With_Fish.py
@@ -2,5 +2,6 @@ import sopel.module
 
 @sopel.module.commands('fish')
 def slapwithfish(bot,trigger):
-    if not trigger.group(2) == bot.nick:
-        bot.say(trigger.nick + " slaps " + trigger.group(2) + " with a fish.")
+    if trigger.group(2):
+        if not trigger.group(2) == bot.nick:
+            bot.say(trigger.nick + " slaps " + trigger.group(2) + " with a fish.")
