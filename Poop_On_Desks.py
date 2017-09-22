be9e8954cd516584d52af47f2019279407f23b38
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-22 15:27:56 -0400

    Update Poop_On_Desks.py

--- Poop_On_Desks.py
+++ Poop_On_Desks.py
@@ -1,23 +1,18 @@
 import sopel.module
 
 @sopel.module.commands('poop','poops')
-def pee(bot, trigger):
+def poop(bot, trigger):
     if not trigger.group(2):
-        myline = "in the designated corner"
-    elif trigger.group(2) == 'all' or trigger.group(2) == 'everyone':
-        myline = 'everyone'
-    elif trigger.group(2) == 'group':
-        myline = 'poopgroup'
+        bot.say(trigger.nick + ' poops in the designated corner!')
     else:
-        myline = trigger.group(2).strip()
-    if myline == "in the designated corner":
-        bot.say(trigger.nick + ' poops ' + myline + '!')
-    elif myline == "everyone":
-        bot.say(trigger.nick + ' poops on ' + myline + '\'s desk, one at a a time!')
-    elif myline == "poopgroup":
-        bot.say(trigger.nick + ', get your poop in a group.')
-    else:
-        if myline.endswith('desk'):
-            bot.say(trigger.nick + ' poops on ' + myline + ', maintaining eye contact the entire time!')
-        else:
-            bot.say(trigger.nick + ' poops on ' + myline + '\'s desk, maintaining eye contact the entire time!')
+        if trigger.group(2) = 'group':
+            bot.say(trigger.nick + ', get your poop in a group.')
+        elif not trigger.group(2) == bot.nick"
+            if trigger.group(2) == 'all' or trigger.group(2) == 'everyone' or trigger.group(2) == 'everyones':
+                bot.say(trigger.nick + " poops on everyone's desk, one at a time!")
+            else:
+                myline = trigger.group(2).strip()
+                if myline.endswith('desk'):
+                    bot.say(trigger.nick + ' poops on ' + myline + ', maintaining eye contact the entire time!')
+                else:
+                    bot.say(trigger.nick + ' poops on ' + myline + '\'s desk, maintaining eye contact the entire time!')
