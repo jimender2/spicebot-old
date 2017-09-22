d0eabf1db461afea9385eb35139d6011d475d82e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-22 15:01:12 -0400

    Update My_Name.py

--- My_Name.py
+++ My_Name.py
@@ -1,11 +1,11 @@
 import sopel.module
 
-botnick = getbotnick(bot)
+def getbotnick():
+    botnick = bot.nick
+    return botnick
 
 @sopel.module.commands(botnick)
 def name(bot,trigger):
     bot.say("That's my name. Don't wear it out!")
 
-def getbotnick(bot):
-    botnick = bot.nick
-    return botnick
+
