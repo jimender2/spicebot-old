09979d21679b79c5b91cad2f14d2cb549c8f940a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-21 21:46:55 -0400

    Update SpiceBot_Reload.py

--- SpiceBot_Reload.py
+++ SpiceBot_Reload.py
@@ -12,6 +12,6 @@ def spicebotreload(bot, trigger):
   bot.say('Pulling From Github')
   os.system("sudo git -C " + script_dir + " pull")
   bot.say('Cleaning Directory.')
-  os.system("sudo rm" + script_dir + "/*.pyc")
+  os.system("sudo rm " + script_dir + "/*.pyc")
   bot.say('Restarting Service')
   os.system("sudo service sopel restart")
