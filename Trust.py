0f6588d4c568062e15d1773524e8a07bf28ab8cf
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-22 15:50:08 -0400

    Update Trust.py

--- Trust.py
+++ Trust.py
@@ -2,7 +2,7 @@ import sopel.module
 
 @sopel.module.commands('trust')
 def trust(bot,trigger):
-    if trigger.group(2):
-        bot.say("I just can't ever bring myself to trust " + trigger.group(2).strip() + " again. I can never forgive " + trigger.group(2).strip() + " for the death of my boy.")
-    else:
+    if  not trigger.group(2):
         bot.say("Trust Doesn't Rust.")
+    elif not trigger.group(2) == bot.nick:
+        bot.say("I just can't ever bring myself to trust " + trigger.group(2).strip() + " again. I can never forgive " + trigger.group(2).strip() + " for the death of my boy.")
