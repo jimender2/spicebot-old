6a0e72f2a7f6f464f54146b7cdcd5afa4d00413a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-22 15:37:42 -0400

    Update Poop_On_Desks.py

--- Poop_On_Desks.py
+++ Poop_On_Desks.py
@@ -4,14 +4,13 @@ import sopel.module
 def poop(bot, trigger):
     if not trigger.group(2):
         bot.say(trigger.nick + ' poops in the designated corner!')
-    elif not trigger.group(2) == bot.nick:
-        if trigger.group(2) = 'group':
+    elif trigger.group(2) = 'group':
             bot.say(trigger.nick + ', get your poop in a group.')
-        elif trigger.group(2) == 'all' or trigger.group(2) == 'everyone' or trigger.group(2) == 'everyones':
+    elif trigger.group(2) == 'all' or trigger.group(2) == 'everyone' or trigger.group(2) == 'everyones':
             bot.say(trigger.nick + " poops on everyone's desk, one at a time!")
+    elif not trigger.group(2) == bot.nick:
+        myline = trigger.group(2).strip()
+        if myline.endswith('desk'):
+            bot.say(trigger.nick + ' poops on ' + myline + ', maintaining eye contact the entire time!')
         else:
-            myline = trigger.group(2).strip()
-            if myline.endswith('desk'):
-                bot.say(trigger.nick + ' poops on ' + myline + ', maintaining eye contact the entire time!')
-            else:
-                bot.say(trigger.nick + ' poops on ' + myline + '\'s desk, maintaining eye contact the entire time!')
+             ot.say(trigger.nick + ' poops on ' + myline + '\'s desk, maintaining eye contact the entire time!')
