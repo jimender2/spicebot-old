33ab33d105fc32296be4c6324871a4c1c2336a3f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-22 16:11:53 -0400

    Create Blackjack_And_Hookers.py

---
+++ Blackjack_And_Hookers.py
@@ -0,0 +1,7 @@
+import sopel.module
+
+@sopel.module.commands('myown')
+def bender(bot,trigger):
+    if trigger.group(2):
+        if not trigger.group(2) == bot.nick:
+            bot.say("Fine! I'll start my own " + trigger.group(2) + " with blackjack and hookers!")
