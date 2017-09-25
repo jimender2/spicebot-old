14ca7933e435a8274c8d0aa23b2c90bd1874a4df
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 12:06:27 -0400

    Update Python_pyjokes.py

--- Python_pyjokes.py
+++ Python_pyjokes.py
@@ -1,9 +1,9 @@
+import sopel.module
 import random
 import pyjokes
-from sopel import module
 
 @sopel.module.rate(120)
-@module.commands('pyjoke')
+@sopel.module.commands('pyjoke')
 def pyjoke(bot, trigger):
     j1 = pyjokes.get_jokes(category='neutral')
     j2 = pyjokes.get_jokes(category='adult')
