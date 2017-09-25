c435bc39263b026faf1a783aafb9392570d14d83
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 15:22:33 -0400

    Update Is_Op.py

--- Is_Op.py
+++ Is_Op.py
@@ -1,5 +1,5 @@
 import sopel.module
-from sopel.module import OP
+from sopel.module import OP, Channel
 
 @sopel.module.commands('isop')
 def optest(bot,trigger):
@@ -9,7 +9,8 @@ def optest(bot,trigger):
         elif bot.privileges[trigger.sender][trigger.nick] < OP:
             bot.say(trigger.nick + ', you are not op.')
     else:
-        if bot.privileges[trigger.sender][trigger.group(2)] == OP:
-            bot.say(trigger.group(2) + ' is op.')
-        elif bot.privileges[trigger.sender][trigger.group(2)] < OP:
-            bot.say(trigger.group(2) + ' is not op.')
+        if trigger.group(2) not in bot.privileges[channel.lower()]:
+            if bot.privileges[trigger.sender][trigger.group(2)] == OP:
+                bot.say(trigger.group(2) + ' is op.')
+            elif bot.privileges[trigger.sender][trigger.group(2)] < OP:
+                bot.say(trigger.group(2) + ' is not op.')
