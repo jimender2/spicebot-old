98f43920dbc19bb8a84c0d86761e32f17f94b215
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 13:53:35 -0400

    Update operatoronly.py

--- operatoronly.py
+++ operatoronly.py
@@ -3,9 +3,9 @@ from sopel.module import rule, event, commands, example, OP
 
 @sopel.module.commands('optest')
 def optest(bot,trigger):
-    if trigger.privileges == OP:
+    if trigger.nick == OP:
         bot.say('you are op')
-    elif not trigger.privileges == OP:
+    elif not trigger.nick == OP:
         bot.say('you are not op')
     else:
         bot.say("this does not work")
