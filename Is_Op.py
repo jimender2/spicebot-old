9a7584aa417061ec8e350735bf80f21d19202a9b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 16:01:25 -0400

    Update Is_Op.py

--- Is_Op.py
+++ Is_Op.py
@@ -10,8 +10,7 @@ def isop(bot,trigger):
         elif bot.privileges[trigger.sender][trigger.nick] < OP:
             bot.say(trigger.nick + ', you are not op.')
     else:
-        tagged=trigger.group(2)
-        if bot.privileges[trigger.sender][tagged] == OP:
+        if bot.privileges[trigger.sender][trigger.group(2)] == OP:
             bot.say(trigger.group(2) + ' is op.')
-        elif bot.privileges[trigger.sender][tagged] < OP:
+        elif bot.privileges[trigger.sender][trigger.group(2)] < OP:
             bot.say(trigger.group(2) + ' is not op.')
