7f8d3885c9ac49ab6f60c53433b58996786c4b03
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 11:55:14 -0400

    Update TechSupport.py

--- TechSupport.py
+++ TechSupport.py
@@ -1,6 +1,7 @@
 import sopel.module
 import random
 
+@sopel.module.rate(120)
 @sopel.module.commands('techsupport')
 def techsupport(bot, trigger):
     messages = ["Have you tried flinging feces at it?","Have you tried chewing the cable?","Did you try turning it off and on again?","Did you try licking the mouse? Double-lick?","Did your try replacing all the ones with zeros?"]
