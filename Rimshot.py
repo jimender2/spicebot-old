91d2eb552d5457219eaa1cb1d084adeed01ed6c8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 11:52:21 -0400

    Update Rimshot.py

--- Rimshot.py
+++ Rimshot.py
@@ -1,5 +1,6 @@
 import sopel.module
 
+@sopel.module.rate(120)
 @sopel.module.commands('rimshot')
 def rimshot(bot, trigger):
     #bot.action('uses drumsticks to hit both the head and the rim of the drum, then the cymbal.')
