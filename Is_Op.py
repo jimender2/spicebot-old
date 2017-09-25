8f171e7934cc8d6c3c1734ffce75cc62e0982ff8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 15:04:39 -0400

    Update and rename operatoronly.py to Is_Op.py

---
+++ Is_Op.py
@@ -0,0 +1,10 @@
+import sopel.module
+
+@sopel.module.commands('isop')
+def optest(bot,trigger):
+    if bot.privileges[trigger.sender][trigger.nick] == OP:
+        bot.say('you are op')
+    elif bot.privileges[trigger.sender][trigger.nick] < OP:
+        bot.say('you are not op')
+    else:
+        bot.say("this does not work")
