6f497be92b86c31793e37ce49bdb306ffc385760
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 14:04:11 -0400

    Delete Thats_My_Name.py

--- Thats_My_Name.py
+++
@@ -1,7 +0,0 @@
-import sopel.module
-from sopel.module import bot
-
-@sopel.module.rate(120)
-@sopel.module.commands(bot.nick)
-def airbiscuit(bot,trigger):
-    bot.say("That's my name, don't wear it out.")
