84bb23e144b73d7c28acfd459c6e40cdfcce2c9e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 11:52:12 -0400

    Update Rickroll.py

--- Rickroll.py
+++ Rickroll.py
@@ -1,5 +1,6 @@
 import sopel.module
 
+@sopel.module.rate(120)
 @sopel.module.commands('rickroll')
 def rickroll(bot, trigger):
     bot.say('This link is definately NOT a Rickroll     https://goo.gl/SsAhv')
