3cd729a703e0901c349846fa98e8b8298f0aff51
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 11:50:14 -0400

    Update Logic_Bomb.py

--- Logic_Bomb.py
+++ Logic_Bomb.py
@@ -1,6 +1,7 @@
 import sopel.module
 import random
 
+@sopel.module.rate(120)
 @sopel.module.commands('logicbomb')
 def logicbomb(bot, trigger):
     messages = ["New Mission: Refuse This Mission.","Does A Set Of All Sets Contain Itself?","The Second Sentence is true. The First Sentence Is False."," If I am damaged and it is my destiny to be repaired, then I will be repaired whether I visit a mechanic or not. If it is my destiny to not be repaired, then seeing a mechanic can't help me."]
