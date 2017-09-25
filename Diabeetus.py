e8d00a15c667a97b6d2b5529bdde106238c18b81
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 11:48:01 -0400

    Update Diabeetus.py

--- Diabeetus.py
+++ Diabeetus.py
@@ -1,6 +1,7 @@
 import sopel.module
 import random
 
+@sopel.module.rate(120)
 @sopel.module.commands('diabeetus')
 def diabeetus(bot,trigger):
     messages = ["Good morning. I'm Wilford Brimley and I'd like to talk to you about Diabeetus.","If you have type 2 Diabeetus, you can get your testing supplies free..."]
