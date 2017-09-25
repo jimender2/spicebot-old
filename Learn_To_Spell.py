d81808ff148580971f28f11b40e91142c7850c80
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 11:49:57 -0400

    Update Learn_To_Spell.py

--- Learn_To_Spell.py
+++ Learn_To_Spell.py
@@ -1,5 +1,6 @@
 import sopel.module
 
+@sopel.module.rate(120)
 @sopel.module.commands('devescuse','devesxuse')
 def learntospell(bot, trigger):
     bot.say('You typed .' + trigger.group(1) + ' , You may need to learn to spell.')
