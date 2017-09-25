cff245e407451deba598de3651b7f735208bea35
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 11:54:51 -0400

    Update SysAdmin_Tools.py

--- SysAdmin_Tools.py
+++ SysAdmin_Tools.py
@@ -1,5 +1,6 @@
 import sopel.module
 
+@sopel.module.rate(120)
 @sopel.module.commands('sysadmintools')
 def sysadmintools(bot, trigger):
     bot.say('https://sysadmin.it-landscape.info/     https://sysadmin.libhunt.com/     https://github.com/n1trux/awesome-sysadmin')
