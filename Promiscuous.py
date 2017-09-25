16179cac6fd71f98da87ceb3a2fe4db48b7352f2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 11:51:38 -0400

    Update Promiscuous.py

--- Promiscuous.py
+++ Promiscuous.py
@@ -2,6 +2,7 @@ import sopel.module
 from random import random
 from random import randint
 
+@sopel.module.rate(120)
 @sopel.module.commands('sexbot','cockbot','fuckbot')
 def promiscuous(bot,trigger):
     rando = randint(2, 666)
