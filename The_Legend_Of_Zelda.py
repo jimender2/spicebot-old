e340be38a7ead3b477316893840b2dcb5e4ed257
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 11:55:24 -0400

    Update The_Legend_Of_Zelda.py

--- The_Legend_Of_Zelda.py
+++ The_Legend_Of_Zelda.py
@@ -1,5 +1,6 @@
 import sopel.module
 
+@sopel.module.rate(120)
 @sopel.module.commands('sword','link','zelda')
 def canYouSeeMe(bot,trigger):
 	bot.say("it's dangerous to go alone take this 0==\======>")
