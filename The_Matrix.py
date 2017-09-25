a83df3569f9e24885477b3679ab7e832666d8164
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 12:01:08 -0400

    Update The_Matrix.py

--- The_Matrix.py
+++ The_Matrix.py
@@ -1,15 +1,16 @@
-from sopel import module
+import sopel.module
 
 @sopel.module.rate(120)
-
+@sopel.module.commands('matrix') 
 def matrix(bot, input):
     bot.say('You have two choices. .redpill Or .bluepill')
-matrix.commands = ['matrix']
 
+@sopel.module.rate(120)
+@sopel.module.commands('redpill') 
 def redpill(bot, input):
     bot.say('You take the red pill, you stay in Wonderland, and I show you how deep the rabbit hole goes.')
-redpill.commands = ['redpill']
 
+@sopel.module.rate(120)
+@sopel.module.commands('bluepill')
 def bluepill(bot, input):
     bot.say('You take the blue pill, the story ends. You wake up in your bed and believe whatever you want to believe.')
-bluepill.commands = ['bluepill']
