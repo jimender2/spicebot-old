39e61f3ed330eb56d568b58a3a962575411f01ec
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 13:24:15 -0400

    Update operatoronly.py

--- operatoronly.py
+++ operatoronly.py
@@ -3,7 +3,7 @@ from sopel.module import commands, example, OP
 
 @sopel.module.commands('optest')
 def optest(bot,trigger):
-    if trigger.nick < OP:
+    if trigger.nick == OP:
         bot.say('you are op')
     else:
         bot.say("you are not op")
