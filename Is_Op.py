66a1965237f738044eb90c37d69ee63a02aab771
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 15:55:57 -0400

    Update Is_Op.py

--- Is_Op.py
+++ Is_Op.py
@@ -4,7 +4,13 @@ from sopel.tools.target import User, Channel
 
 @sopel.module.commands('isop')
 def optest(bot,trigger):
-    if bot.privileges[trigger.sender][trigger.nick] == OP:
-        bot.say(trigger.nick + ', you are op.')
-    elif bot.privileges[trigger.sender][trigger.nick] < OP:
-        bot.say(trigger.nick + ', you are not op.')
+    if not trigger.group(2):
+        if bot.privileges[trigger.sender][trigger.nick] == OP:
+            bot.say(trigger.nick + ', you are op.')
+        elif bot.privileges[trigger.sender][trigger.nick] < OP:
+            bot.say(trigger.nick + ', you are not op.')
+    else:
+        if bot.privileges[trigger.sender][trigger.group(2)] == OP:
+            bot.say(trigger.group(2) + ' is op.')
+        elif bot.privileges[trigger.sender][trigger.group(2)] < OP:
+            bot.say(trigger.group(2) + ' is not op.')
