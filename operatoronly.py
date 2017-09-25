d64ee7fdb7d7574cb5eeb121003821c29a56c403
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 13:02:27 -0400

    Update operatoronly.py

--- operatoronly.py
+++ operatoronly.py
@@ -3,5 +3,5 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('optest')
 def optest(bot,trigger):
-    if not trigger.operator:
+    if not trigger.op:
         bot.say('you are not op')
