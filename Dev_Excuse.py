0568a0327b10adddbe17df796e321047bc747f7b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 11:58:52 -0400

    Update Dev_Excuse.py

--- Dev_Excuse.py
+++ Dev_Excuse.py
@@ -1,12 +1,12 @@
+import sopel.module
 import urllib
 from xml.dom.minidom import parseString
-from sopel import module
 
 @sopel.module.rate(120)
+@sopel.module.commands('devexcuse')
 def devexcuse(bot, input):
     bot.say(parseString(
         urllib.urlopen('http://developerexcuses.com').read().replace('&', '')).
         getElementsByTagName('body')[0].getElementsByTagName('div')[0].
         getElementsByTagName('center')[0].getElementsByTagName('a')[0].
         childNodes[0].nodeValue)
-devexcuse.commands = ['devexcuse']
