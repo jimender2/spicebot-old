c0afba896e6e1cdd7b56a759f4ce92288144d27a
zsutton92 <zsutton92@gmail.com>
2017-09-25 15:17:14 -0500

    Update Is_Op.py

--- Is_Op.py
+++ Is_Op.py
@@ -5,12 +5,19 @@ from sopel.tools.target import User, Channel
 @sopel.module.commands('isop')
 def isop(bot,trigger):
     if not trigger.group(2):
-        if bot.privileges[trigger.sender][trigger.nick] == OP:
-            bot.say(trigger.nick + ', you are op.')
-        elif bot.privileges[trigger.sender][trigger.nick] < OP:
-            bot.say(trigger.nick + ', you are not op.')
+        nick = trigger.nick
     else:
-        if bot.privileges[trigger.sender][trigger.group(2)] == OP:
-            bot.say(trigger.group(2) + ' is op.')
-        elif bot.privileges[trigger.sender][trigger.group(2)] < OP:
-            bot.say(trigger.group(2).strip() + ' is not op.')
+        nick = trigger.group(2)
+    if bot.privileges[trigger.sender][nick] == OP:
+        bot.say(trigger.nick + ', is an op.')
+    else: 
+        bot.say(trigger.nick + ', is not an op.')
+        #if bot.privileges[trigger.sender][trigger.nick] == OP:
+         #   bot.say(trigger.nick + ', you are op.')
+        #elif bot.privileges[trigger.sender][trigger.nick] < OP:
+         #   bot.say(trigger.nick + ', you are not op.')
+    #else:
+       # if bot.privileges[trigger.sender][trigger.group(2)] == OP:
+        #    bot.say(trigger.group(2) + ' is op.')
+        #elif bot.privileges[trigger.sender][trigger.group(2)] < OP:
+         #   bot.say(trigger.group(2).strip() + ' is not op.')
