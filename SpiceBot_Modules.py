6e5681f74ad2073a3f41ccf1cd33b9cc47748702
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 11:53:32 -0400

    Update SpiceBot_Modules.py

--- SpiceBot_Modules.py
+++ SpiceBot_Modules.py
@@ -1,5 +1,6 @@
 import sopel.module
 
+@sopel.module.rate(120)
 @sopel.module.commands('spicebotmodules')
 def spicemodules(bot, trigger):
     bot.say('Spiceworks IRC Modules     https://github.com/deathbybandaid/sopel-modules')
