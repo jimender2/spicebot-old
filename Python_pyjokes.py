bac316ca201c83942455265fc0ac883f9141564a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 11:51:51 -0400

    Update Python_pyjokes.py

--- Python_pyjokes.py
+++ Python_pyjokes.py
@@ -2,6 +2,7 @@ import random
 import pyjokes
 from sopel import module
 
+@sopel.module.rate(120)
 @module.commands('pyjoke')
 def pyjoke(bot, trigger):
     j1 = pyjokes.get_jokes(category='neutral')
