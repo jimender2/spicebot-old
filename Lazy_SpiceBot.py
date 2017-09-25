7f2d7ba64db47b5347ae2b4f7917b40f1a61113d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 11:49:49 -0400

    Update Lazy_SpiceBot.py

--- Lazy_SpiceBot.py
+++ Lazy_SpiceBot.py
@@ -1,5 +1,6 @@
 import sopel.module
 
+@sopel.module.rate(120)
 @sopel.module.commands('lazy','lazyfuckingspicebot','fuckinglazyspicebot','lazyspicebot')
 def lazy(bot, trigger):
     bot.say('I do not tell you how to do your job, ' + trigger.nick + '!!')
