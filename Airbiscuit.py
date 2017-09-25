4a73e7622a768c6b0e049676c6c97f49cd9c3354
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-25 11:21:31 -0400

    Update Airbiscuit.py

--- Airbiscuit.py
+++ Airbiscuit.py
@@ -1,6 +1,6 @@
 import sopel.module
 
-@sopel.module.rate(60)
+@sopel.module.rate(6)
 @sopel.module.commands('airbiscuit','float','floats')
 def airbiscuit(bot,trigger):
     bot.say(trigger.nick + " floats an air biscuit.")
