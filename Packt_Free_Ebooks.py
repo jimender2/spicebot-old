b5df99de0be4ed24a33ae4d84f2972177326a3d0
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-26 13:57:30 -0400

    Update Packt_Free_Ebooks.py

--- Packt_Free_Ebooks.py
+++ Packt_Free_Ebooks.py
@@ -14,10 +14,9 @@ def packt(bot, trigger):
 
 @sopel.module.interval(60)
 def getpackt(bot):
-    now = datetime.now()
-    now_time = now.time()
+    now_UTC = datetime.utcnow()
     for channel in bot.channels:
-        if now_time == time(14,00):
+        if(now_UTC.hour == 14):
             title = getPacktTitle()
             bot.say('Packt Free Book (daily)     https://www.packtpub.com/packt/offers/free-learning')
             bot.say("Today's free book is: " + title)
