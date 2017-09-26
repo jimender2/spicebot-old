a39684ad9ec35614f402ba44b7533a21272452a8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-26 09:37:38 -0400

    Update Is_Admin.py

--- Is_Admin.py
+++ Is_Admin.py
@@ -9,19 +9,9 @@ def isadmin(bot,trigger):
     else:
         nick = trigger.group(2).lower()
     try:    
-        if bot.privileges[trigger.sender][nick]:
+        if bot.privileges[trigger.sender][nick] == admin:
             bot.say(nick + ' is an admin.')
         else: 
             bot.say(nick + ' is not an admin.')
     except KeyError:
         bot.say(nick + ' is not here right now!')
-
-@sopel.module.commands('getpriv')
-def getpriv(bot,trigger):
-    if not trigger.group(2):
-        nick = trigger.nick.lower()
-    else:
-        nick = trigger.group(2).lower()
-    channel_privs = bot.privileges[trigger.sender][nick]
-    allowed = channel_privs.get(nick, 0) >= level
-    bot.say(allowed)
