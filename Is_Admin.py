24990e4fba9fc87c86326d32fd7e952a4a5e7c7f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-26 09:08:10 -0400

    Create Is_Admin.py

---
+++ Is_Admin.py
@@ -0,0 +1,17 @@
+import sopel.module
+from sopel.module import ADMIN
+from sopel.tools.target import User, Channel
+
+@sopel.module.commands('isadmin')
+def isadmin(bot,trigger):
+    if not trigger.group(2):
+        nick = trigger.nick.lower()
+    else:
+        nick = trigger.group(2).lower()
+    try:    
+        if bot.privileges[trigger.sender][nick] == ADMIN:
+            bot.say(nick + ' is an admin.')
+        else: 
+            bot.say(nick + ' is not an admin.')
+    except KeyError:
+        bot.say(nick + ' is not here right now!')
