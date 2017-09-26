4cf8f2741b586b948751a89596f1266f77bc7e33
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-26 17:49:41 -0400

    Update The_Matrix.py

--- The_Matrix.py
+++ The_Matrix.py
@@ -3,14 +3,17 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('matrix') 
 def matrix(bot, input):
-    bot.say('You have two choices. .redpill Or .bluepill')
-
-@sopel.module.rate(120)
-@sopel.module.commands('redpill') 
-def redpill(bot, input):
-    bot.say('You take the red pill, you stay in Wonderland, and I show you how deep the rabbit hole goes.')
-
-@sopel.module.rate(120)
-@sopel.module.commands('bluepill')
-def bluepill(bot, input):
-    bot.say('You take the blue pill, the story ends. You wake up in your bed and believe whatever you want to believe.')
+    if trigger.group(2):
+        if trigger.group(2) == 'redpill':
+            bot.say('You take the red pill, you stay in Wonderland, and I show you how deep the rabbit hole goes.')
+        elif trigger.group(2) == 'bluepill':
+            bot.say('You take the blue pill, the story ends. You wake up in your bed and believe whatever you want to believe.')
+        else:
+            normalrun='true'
+    else:
+        normalrun='true'
+    try:
+        if normalrun:
+            bot.say('You have two choices. redpill Or bluepill')        
+    except UnboundLocalError:
+        return
