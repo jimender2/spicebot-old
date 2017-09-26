ac2e7e4b55defd6fad9df4d73144ee712abf8516
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-26 11:10:21 -0400

    Update Is_Admin.py

--- Is_Admin.py
+++ Is_Admin.py
@@ -4,9 +4,9 @@ from sopel.tools.target import User, Channel
 
 @sopel.module.commands('isadmin')
 def isadmin(bot,trigger):
-    bot.say(trigger)
     if not trigger.group(2):
-        if trigger.admin:
+        nickame = trigger.nick
+        if nickame.admin:
             bot.say(trigger.nick + ' is an admin')
         else:
             bot.say(trigger.nick + ' is not an admin')
