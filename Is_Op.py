e8fc1c367b3dc2afc42f20498df0ecc259e4742b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-26 10:03:58 -0400

    Update Is_Op.py

--- Is_Op.py
+++ Is_Op.py
@@ -15,12 +15,3 @@ def isop(bot,trigger):
             bot.say(nick + ' is not an op.')
     except KeyError:
         bot.say(nick + ' is not here right now!')
-
-@sopel.module.commands('getpriv')
-def getpriv(bot,trigger):
-    if not trigger.group(2):
-        nick = trigger.nick.lower()
-    else:
-        nick = trigger.group(2).lower()
-    priv = str(bot.privileges[trigger.sender][nick])
-    bot.say(priv)
