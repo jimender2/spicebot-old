4bc54a1804fb4503707da7674fd5a17326974907
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-26 15:35:20 -0400

    Update Packt_Free_Ebooks.py

--- Packt_Free_Ebooks.py
+++ Packt_Free_Ebooks.py
@@ -9,8 +9,8 @@ import datetime
 def packt(bot, trigger):
     title = getPacktTitle()
     packttimediff = getpackttimediff()
-    bot.say('Packt Free Book (daily)     https://www.packtpub.com/packt/offers/free-learning   Time Left: Less than ' + str(packttimediff) + ' hours remaining')
-    bot.say("Today's free book is: " + title)
+    bot.say('Packt Free Book (daily)     https://www.packtpub.com/packt/offers/free-learning ')
+    bot.say("Today's free book is: " + title + '     Time Left: Less than ' + str(packttimediff) + ' hours remaining')
 
 # new book is 23:00 UTC
 @sopel.module.interval(60)
