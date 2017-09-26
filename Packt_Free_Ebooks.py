5f1784a0c2428d976fb43485ad56f7d7c1f05f89
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-26 19:23:56 -0400

    Update Packt_Free_Ebooks.py

--- Packt_Free_Ebooks.py
+++ Packt_Free_Ebooks.py
@@ -57,7 +57,7 @@ def getpackttimediff():
         hourcompare = int(packthour) - int(now.hour)
     else:
         hourcomparea = str(24 - int(now.hour))
-        hourcompare = int(hourcomparea) + int(packthour)
+        hourcompare = str(int(hourcomparea) + 23)
     
     if int(now.minute) > '0':
         hourcompare = str(int(hourcompare) - 1)
