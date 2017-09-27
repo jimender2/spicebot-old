4f47159a1477f48f727bd43eef687b4d39bb5f27
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-27 14:53:20 -0400

    Update and rename SpiceWorks_Webby.new to SpiceWorks_Webby.py

---
+++ SpiceWorks_Webby.py
@@ -0,0 +1,52 @@
+#import sopel.module
+import requests
+from lxml import html
+
+@sopel.module.commands('packt')
+def webbymanual(bot, trigger):
+    if not trigger.group(2):
+        bot.say('stay tuned')
+    else:
+        if trigger.group(2) == 'bonus'
+            webbybonus = getwebbybonus()
+            bot.say(webbybonus)
+        else:
+            bot.say('stay tuned')
+
+#@sopel.module.interval(60)
+#def webbyauto(bot):
+    #for channel in bot.channels:
+        #now = datetime.datetime.utcnow()
+        #webbyhour = getwebbyhour()
+        #if now.hour == webbyhour:
+            #if now.minute == '45'
+                #webbytitle = getwebbytitle()
+                #webbylink = getwebbylink()
+                #webbybonus = getwebbybonus()
+                #bot.msg(channel, webbybonus)
+            
+#def getwebbyhour():
+    ## Parse The hour
+    # //*[@id="primary"]/div/ul/li[1]/div[2]/h4[2]/span
+    # return webbyhour
+
+#def getwebbytitle():
+    ## Parse the title
+    #return webbytitle
+
+#def getwebbylink():
+    ## Parse the url out,,, don't need anything after ShowKey
+    #return webbylink
+
+def getwebbybonus():
+    url = 'https://community.spiceworks.com/calendar'
+    page = requests.get(url,headers = None)
+    if page.status_code == 200:
+        tree= html.fromstring(page.content)
+        webbybonus = str(tree.xpath('//*[@id="primary"]/div/ul/li[1]/div[2]/div[2]/p/text()'))
+        webbybonus = str(webbybonus.split("BONUS: ", 1)[1])
+        for r in (("\\r", ""), ("\\n", ""), ("]",""), ('"','')):
+            webbybonus = webbybonus.replace(*r)
+    if not webbybonus:
+        webbybonus = 'No Bonus Available'
+    return webbybonus
