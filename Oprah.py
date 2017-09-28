e53e2b2691df6cce61abb216706de70371b13951
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-28 12:00:08 -0400

    Update Oprah.py

--- Oprah.py
+++ Oprah.py
@@ -4,4 +4,9 @@ import sopel.module
 @sopel.module.commands('oprah')
 def trust(bot,trigger):
     if trigger.group(2):
-        bot.say("You get a " + trigger.group(2).strip() + "! And You get a " + trigger.group(2).strip() + "! Everyone gets a "+ trigger.group(2).strip() + "!")
+        item = trigger.group(2).strip()
+        if item.startswith('a') or item.startswith('e') or item.startswith('i') or item.startswith('o') or item.startswith('u'):
+            item = str('an' + item)
+        else:
+            item = str('a' + item)
+        bot.say("You get " + item + "! And You get " + item + "! Everyone gets "+ item + "!")
