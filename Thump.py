6053b03801cf4a7829e45f0d4d9ed73ed17dbe24
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-28 14:50:17 -0400

    Update Thump.py

--- Thump.py
+++ Thump.py
@@ -5,5 +5,5 @@ import sopel.module
 def thump(bot, trigger):
     if not trigger.group(2):
         bot.say("Did you mean to thump somebody?")
-    elif not trigger.group(2) == bot.nick:
+    elif not trigger.group(2).strip() == bot.nick:
         bot.action('thumps ' + trigger.group(2).strip() + ' on behalf of ' + trigger.nick)
