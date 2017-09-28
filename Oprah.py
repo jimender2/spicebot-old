098f00239390a66df40e35933d5484042c5407ce
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-28 12:01:02 -0400

    Update Oprah.py

--- Oprah.py
+++ Oprah.py
@@ -6,7 +6,7 @@ def trust(bot,trigger):
     if trigger.group(2):
         item = trigger.group(2).strip()
         if item.startswith('a') or item.startswith('e') or item.startswith('i') or item.startswith('o') or item.startswith('u'):
-            item = str('an' + item)
+            item = str('an ' + item)
         else:
-            item = str('a' + item)
+            item = str('a ' + item)
         bot.say("You get " + item + "! And You get " + item + "! Everyone gets "+ item + "!")
