29bd3e558fbc9691fe1699d0cc24e843874d4ae9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-29 08:46:38 -0400

    Update SpiceBot_Reload.py

--- SpiceBot_Reload.py
+++ SpiceBot_Reload.py
@@ -12,12 +12,12 @@ service = os.path.split(os.getcwd())[1]
 @sopel.module.require_privmsg
 @sopel.module.commands('spicebotreload')
 def spicebotreload(bot, trigger):
-    bot.say(service)
+    bot.say(str(service))
     bot.say('Pulling From Github...')
     os.system("sudo git -C " + script_dir + " pull")
     bot.say('Cleaning Directory...')
     os.system("sudo rm " + script_dir + "/*.pyc")
     bot.say('Restarting Service...')
-    os.system("sudo service " + service + " restart")
+    os.system("sudo service " + str(service) + " restart")
     bot.say(' ')
     bot.say('If you see this, the service is hanging. Run Command Again.')
