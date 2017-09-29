5a347240b53348db9a2a8c1366f2649f70b4473c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-29 14:34:49 -0400

    Update SpiceBot_Debug.py

--- SpiceBot_Debug.py
+++ SpiceBot_Debug.py
@@ -4,15 +4,16 @@ import sys
 from os.path import exists
 
 script_dir = os.path.dirname(__file__)
-rel_path = "templog.txt"
+rel_path = "data/templog.txt"
 abs_file_path = os.path.join(script_dir, rel_path)
 
 @sopel.module.require_admin
 @sopel.module.require_privmsg
 @sopel.module.commands('debug')
 def debug(bot, trigger):
+    service = bot.nick.lower()
     bot.action('Is Copying Log')
-    os.system("sudo journalctl -u sopel >> " + abs_file_path)
+    os.system("sudo journalctl -u " + service + " >> " + abs_file_path)
     bot.action('Is Filtering Log')
     os.system("sudo sed -i '/Starting Sopel IRC bot/h;//!H;$!d;x;' " + abs_file_path)
     os.system("sudo sed -i '/sudo/d; /COMMAND/d' " + abs_file_path)
