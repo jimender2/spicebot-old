d55a94ad6462e141daac845290f38801893f8591
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-29 14:09:39 -0400

    Update Cipher_The_Terminator.py

--- Cipher_The_Terminator.py
+++ Cipher_The_Terminator.py
@@ -1,7 +1,7 @@
 import sopel.module
 
 @sopel.module.rate(120)
-@sopel.module.commands('cipher','terminator')
+@sopel.module.commands('cipher','terminator','ciphertheterminator')
 def cipher(bot, trigger):
     bot.say('CYBORG TISSUE GENERATION 800 SERIES MODEL 101 SEQUENCE INITIATED')
     bot.say('DOWNLOADING CURRENT OBJECTIVE FROM SKYNET: ENSURE THE ACTIVATION OF SKYNET')
