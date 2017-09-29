71073b972ca9a5a0d3ff90e076dcb04abfe76e3c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-29 10:54:42 -0400

    Update TechSupport.py

--- TechSupport.py
+++ TechSupport.py
@@ -2,7 +2,7 @@ import sopel.module
 import random
 
 @sopel.module.rate(120)
-@sopel.module.commands('techsupport')
+@sopel.module.commands('techsupport','itsupport')
 def techsupport(bot, trigger):
     messages = ["YOU MUST CONSTRUCT ADDITIONAL PYLONS!","Have you tried flinging feces at it?","Have you tried chewing the cable?","Did you try turning it off and on again?","Did you try licking the mouse? Double-lick?","Did your try replacing all the ones with zeros?"]
     answer = random.randint(0,len(messages) - 1)
