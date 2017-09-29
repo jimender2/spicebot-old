abf82505aba88a72bed47683e41a25349dfaa1d7
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-29 09:27:47 -0400

    Update SpiceBot_Reload.py

--- SpiceBot_Reload.py
+++ SpiceBot_Reload.py
@@ -6,12 +6,12 @@ from inspect import currentframe, getframeinfo
 from pathlib import Path
 
 script_dir = os.path.dirname(__file__)
-service = os.path.split(os.getcwd())[1]
 
 @sopel.module.require_admin
 @sopel.module.require_privmsg
 @sopel.module.commands('spicebotreload')
 def spicebotreload(bot, trigger):
+    service = bot.nick.lower
     bot.say(service)
     bot.say('Pulling From Github...')
     os.system("sudo git -C " + script_dir + " pull")
