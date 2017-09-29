cd1fa06f09b9392b41cc899fdfa80d72f052cbcf
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-29 14:00:14 -0400

    Update Red_Alert.py

--- Red_Alert.py
+++ Red_Alert.py
@@ -3,4 +3,4 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('redalert')
 def redalert(bot, trigger):
-    bot.say('Shields Up, Captain ' + trigger.nick)
+    bot.say('Shields Up, Captain ' + trigger.nick + '!!')
