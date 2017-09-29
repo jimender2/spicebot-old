053d3f00eace8938ab6afb906ff0772384dcdab5
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-28 20:17:22 -0400

    Update SpiceBot_Reload.py

--- SpiceBot_Reload.py
+++ SpiceBot_Reload.py
@@ -4,14 +4,23 @@ import sys
 from os.path import exists
 
 script_dir = os.path.dirname(__file__)
+filename = getframeinfo(currentframe()).filename
+script_dirname = str(str(Path(filename).resolve().parent).split("/", 1)[1]).split("/", 1)[1]
 
 @sopel.module.require_admin
 @sopel.module.require_privmsg
 @sopel.module.commands('spicebotreload')
 def spicebotreload(bot, trigger):
-  bot.say('Pulling From Github')
+  if str(script_dirname) == 'github':
+    version = 'master'
+    service = 'spicebot'
+  elif str(script_dirname) == 'githubdev':
+    version = 'dev'
+    service = 'spicebotdev'
+  
+  bot.say('Pulling From Github ' + version)
   os.system("sudo git -C " + script_dir + " pull")
   bot.say('Cleaning Directory.')
   os.system("sudo rm " + script_dir + "/*.pyc")
   bot.say('Restarting Service')
-  os.system("sudo service sopel restart")
+  os.system("sudo service " service + " restart")
