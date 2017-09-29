36fbb09c014792e6c4b9ff397bb1d78fdf7fe502
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-29 14:01:05 -0400

    Update Halp_Not_Help.py

--- Halp_Not_Help.py
+++ Halp_Not_Help.py
@@ -3,4 +3,4 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('halp')
 def halp(bot,trigger):
-    bot.say("if you need help using help you are truly lost")
+    bot.say("If you need help using help you are truly lost.")
