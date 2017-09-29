edc85f36d27607fd8a603e6f504a45adc04a6828
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-29 15:52:55 -0400

    Update SpiceWorks_Webby.py

--- SpiceWorks_Webby.py
+++ SpiceWorks_Webby.py
@@ -83,7 +83,7 @@ def getwebbytimeuntil():
     else:
         if str(timetilyears) > '0':
             timetilyears = str(int(timetilyears) - 1)
-        if str(webbytime.month) > str(now.month) and str(now.year) == str(webbytime.year):
+        if str(webbytime.month) > str(now.month):
             timetilmonths = str(int(webbytime.month) - int(now.month))
         else:
             timetilmonths = str(12 - int(now.month) + int(webbytime.month))
@@ -93,7 +93,7 @@ def getwebbytimeuntil():
     else:
         if str(timetilmonths) > '0':
             timetilmonths = str(int(timetilmonths) - 1)
-        if str(webbytime.day) > str(now.day) and str(now.month) == str(webbytime.month):
+        if str(webbytime.day) > str(now.day):
             timetildays = str(int(webbytime.day) - int(now.day))
         else:
             daysthismonth = calendar.monthrange(now.year, now.month)[1]
@@ -104,7 +104,7 @@ def getwebbytimeuntil():
     else:
         if str(timetildays) > '0':
             timetildays = str(int(timetildays) - 1)
-        if str(webbytime.hour) > str(now.hour) and str(now.day) == str(webbytime.day):
+        if str(webbytime.hour) > str(now.hour):
             timetilhours = str(int(webbytime.hour) - int(now.hour))
         else:
             timetilhours = str(24 - int(now.hour) + int(webbytime.hour))
