0391117432a0b1122cd9e32bd496ce53078ed73d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-29 22:19:22 -0400

    Update SpiceWorks_Webby.py

--- SpiceWorks_Webby.py
+++ SpiceWorks_Webby.py
@@ -75,7 +75,7 @@ def getwebbytimeuntil():
     webbytime = getwebbytime()
     a = arrow.get(now)
     b = arrow.get(webbytime)
-    timecompare = (b.humanize(a, granularity=('year','month','day','hour','minute')))
+    timecompare = (b.humanize(a, granularity='minute'))
     #'second'minute''hour''day''month''year'
     webbytimeuntil = str(timecompare)
     return webbytimeuntil
