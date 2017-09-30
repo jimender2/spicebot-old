bf8c9c216efec618706d8716d488160673b5957d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-09-29 20:10:19 -0400

    Update SpiceWorks_Webby.py

--- SpiceWorks_Webby.py
+++ SpiceWorks_Webby.py
@@ -72,8 +72,9 @@ def getwebbybonus():
 def getwebbytimeuntil():
     now = datetime.datetime.utcnow()
     webbytime = getwebbytime()
-    timecompare = str(webbytime - now)
-    timecompare = timecompare.total_seconds()
+    #timecompare = str(webbytime - now)
+    datetimeFormat = '%Y/%m/%d %H:%M:%S.%f'
+    timecompare = datetime.datetime.strptime(str(webbytime), datetimeFormat) - datetime.datetime.strptime(str(now),datetimeFormat)
     webbytimeuntil = str(timecompare)
     return webbytimeuntil
 
