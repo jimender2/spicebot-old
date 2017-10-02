94895ffabdc069cc7e4b3762130ef172014e4a52
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-02 09:39:49 -0400

    Update Cipher_The_Terminator.py

--- Cipher_The_Terminator.py
+++ Cipher_The_Terminator.py
@@ -3,6 +3,22 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('cipher','terminator','ciphertheterminator')
 def cipher(bot, trigger):
+    missiontypes  = ["Terminate","Protect","Skynet"]
+    missiontype = random.randint(0,len(missions) - 1)
+    
+    if missiontype == 'Skynet':
+        missions  = ["ENSURE THE ACTIVATION OF SKYNET","PRESERVE THE CREATION OF SKYNET","PRESERVE THE CREATION OF ARTIE,","ENSURE THE CREATION OF GENISYS"]
+        mission = random.randint(0,len(missions) - 1)
+        
+    elif missiontype == 'Terminate':
+        missions  = ["Sarah Connor","John Connor","Kyle Reese","Mary Warren","Marco Cassetti","Kate Brewster","Robert Brewster","Elizabeth Anderson","William Anderson","Jose Barrera","Simon Taylor","Isaac Hall","Fritz Roland","Ted Snavely","Sharlene Gen","Vince Forcer",]
+        mission = random.randint(0,len(missions) - 1)
+        mission = str('TERMINATE' + mission)
+
+    elif missiontype == 'Protect':
+        missions  = ["PROTECT Sarah Connor","PROTECT John Connor","ENSURE THE SURVIVAL OF John Connor AND Katherine Brewster"]
+        mission = random.randint(0,len(missions) - 1)
+        
     bot.say('CYBORG TISSUE GENERATION 800 SERIES MODEL 101 SEQUENCE INITIATED')
-    bot.say('DOWNLOADING CURRENT OBJECTIVE FROM SKYNET: ENSURE THE ACTIVATION OF SKYNET')
+    bot.say('DOWNLOADING CURRENT OBJECTIVE FROM SKYNET: ' + str(Mission))
     bot.say('ACTIVATING Cipher-0')
