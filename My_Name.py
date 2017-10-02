8b25dda5c3f7b58a5de0008e764dffd98cbda180
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-02 15:41:30 -0400

    Update My_Name.py

--- My_Name.py
+++ My_Name.py
@@ -1,6 +1,6 @@
 import sopel.module
 
 @sopel.module.rate(120)
-@sopel.module.commands('spicebot','spicebotdev','spicebot ','spicebotdev ')
+@sopel.module.commands('spicebot','spicebotdev')
 def name(bot,trigger):
     bot.say("That's my name. Don't wear it out!")
