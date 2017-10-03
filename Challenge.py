41eba115eef6f9651743ec3395b6b77037990065
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-03 17:53:06 -0400

    Update Challenge.py

--- Challenge.py
+++ Challenge.py
@@ -73,9 +73,9 @@ def removeweapons():
         if str(weapondel) in open(weaponslocker).read():
             os.system('sudo sed -i "/' + weapondel + '/d ' + weaponslocker)
             if str(weapondel) not in open(weaponslocker).read():
-                bot.say(weapondel + ' has been removed from the weapons locker.')
+                #bot.say(weapondel + ' has been removed from the weapons locker.')
         else:
-            bot.say(weapondel + " is not in the weapons locker.")
+            #bot.say(weapondel + " is not in the weapons locker.")
           
 def weaponofchoice():
     checkweapons()
