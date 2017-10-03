96f5a204e1ce6a82b409195bc4121c7f70608f44
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-03 13:30:15 -0400

    Delete Challenge.py

--- Challenge.py
+++
@@ -1,7 +0,0 @@
-import sopel.module
-
-@sopel.module.rate(120)
-@sopel.module.commands('challenge')
-def challenge(bot,trigger):
-    if trigger.group(2):
-        bot.say(trigger.nick + " challenges " + trigger.group(2) + " to a duel.")
