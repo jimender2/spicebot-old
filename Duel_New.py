ff198a20dde75eb1fc0f63a26a8c9b521c2df13f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-03 14:50:03 -0400

    Update Duel_New.py

--- Duel_New.py
+++ Duel_New.py
@@ -2,12 +2,7 @@ import sopel
 from sopel import module, tools
 import random
 
-#@sopel.module.commands('challenge')
-#def challenge(bot,trigger):
-#    if trigger.group(2):
-#        return duel(bot, trigger.sender, trigger.nick, trigger.group(3) or '', is_admin=trigger.admin)
-#    else:
-#        bot.say(trigger.nick + ", Who did you want to duel?")
+## Enforce challenges. if challenge is not accepted, don't duel
  
 #@module.rule('^(?:challenges|(?:fi(?:ght|te)|duel)s(?:\s+with)?)\s+([a-zA-Z0-9\[\]\\`_\^\{\|\}-]{1,32}).*')
 #@module.intent('ACTION')
@@ -22,24 +17,22 @@ def duel_cmd(bot, trigger):
 def duel(bot, channel, instigator, target, is_admin=False, warn_nonexistent=True):
     target = tools.Identifier(target or '')
     if not target:
-        bot.say(instigator + ", Who did you want to duel?")
+        bot.say(instigator + ", Who did you want to fight?")
     else:
         if target == bot.nick:
-            bot.say("I refuse to duel with the yeller-bellied likes of you!")
+            bot.say("I refuse to fight a biological entity!")
         elif target == instigator:
-            bot.say("You can't duel yourself, you coward!")
+            bot.say("If you want to duel yorself, please find a mirror!")
         else:
-            bot.say(instigator + " versus " + target + ", loser's a yeller belly!")
-            contestants  = [instigator , target]
-            winner = random.randint(0,len(contestants) - 1)
-            winner = str(contestants [winner])
-            if winner == instigator:
-                loser = target
-            else:
-                loser = instigator
+            bot.say(instigator + " versus " + target)
+            combatants = sorted([instigator, target])
+            random.shuffle(combatants)
+            winner = combatants.pop()
+            loser = combatants.pop()
             bot.say(winner + " wins!")
-            bot.say(winner + " done killed ya, " + loser)
+
 
         
 
 #def weaponofchoice():
+#def loserisa()"
