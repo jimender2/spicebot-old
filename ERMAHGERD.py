4698f56b8c8ebdd76cf9603c0126516d135e8a1e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-03 10:16:04 -0400

    Update ERMAHGERD.py

--- ERMAHGERD.py
+++ ERMAHGERD.py
@@ -1,6 +1,6 @@
 import sopel.module
 import sys, re
-from word2number import w2n
+from num2words import num2words
 
 @sopel.module.rate(120)
 @sopel.module.commands('ERMAHGERD')
@@ -27,7 +27,7 @@ def ermergerd(w):
         return derctshernerer[w].upper()
     else:
         if w[0].isdigit():
-            w = w2n.word_to_num(w)
+            w = num2words(w)
         w = re.sub(r"[\.,/;:!@#$%^&*\?]+", '', w) # punctuation is hard. another day. 
         w = re.sub(r"tion", "shun", w)
         pat = r"[aeiouy]+"
