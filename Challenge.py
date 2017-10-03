e3b53a05d23578f39a1368c6a1486a3c6180aacf
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-03 19:07:40 -0400

    Update Challenge.py

--- Challenge.py
+++ Challenge.py
@@ -71,8 +71,7 @@ def removeweapons(bot, trigger):
     else:
         weapondel = str(trigger.group(2))
         if str(weapondel) in open(weaponslocker).read():
-            #oscommand = str('sudo sed -i "/' + str(weapondel) + '/d" ' + weaponslocker)
-            os.system('sudo sed -i "/' + str(weapondel) + '/d" ' + weaponslocker)
+            os.system('sudo sed -i "/' + str(weapondel) + '/d; /^$/d " ' + weaponslocker)
             if str(weapondel) not in open(weaponslocker).read():
                 bot.say(weapondel + ' has been removed from the weapons locker.')
         else:
