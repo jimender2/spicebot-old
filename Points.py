4ce4abf8e063afb9481a5f62409a445f498ac008
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-04 08:49:33 -0400

    Update Points.py

--- Points.py
+++ Points.py
@@ -6,23 +6,22 @@ from random import randint
 @sopel.module.commands('points','pants')
 def points(bot, trigger):
     whichtrig = trigger.group(1)    
-    rando = randint(1, 666)
     if not trigger.group(2):
-        winner = "Everybody"
+        loser = "Everybody"
     else:
-        winner = trigger.group(2).strip()
+        loser = trigger.group(2).strip()
         if trigger.group(2) == 'all':
-            winner = "Everybody"
-    if winner == trigger.nick:
+            loser = "Everybody"
+    if loser == trigger.nick:
         bot.say('You can\'t give yourself ' + whichtrig + '!')
     else:
+        rando = randint(1, 666)
         randopoints = ('is awarded ' + str(rando) + ' ' + whichtrig + ' from' )
-        bot.say(winner + ' ' + randopoints + ' ' + trigger.nick)
+        bot.say(loser + ' ' + randopoints + ' ' + trigger.nick)
 
 @sopel.module.rate(120)
 @sopel.module.commands('takepoints','takepants','minuspants','minuspoints')
 def takepoints(bot, trigger):   
-    rando = randint(1, 666)
     if not trigger.group(2):
         winner = "Everybody"
     else:
@@ -32,6 +31,7 @@ def takepoints(bot, trigger):
     if winner == trigger.nick:
         bot.say('You can\'t take your own points away!')
     else:
+        rando = randint(1, 666)
         randopoints = ('loses ' + str(rando) + ' points')
         bot.say(winner + ' ' + randopoints)
 
