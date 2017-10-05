89b6a1b11d462362550b01df0d11922691b1be67
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-04 20:08:53 -0400

    Update Points.py

--- Points.py
+++ Points.py
@@ -61,9 +61,11 @@ def pointstask(bot, channel, instigator, target, giveortake, tofrom, addminus, p
 def update_points(bot, nick, rando, addminus):
     points = get_points(bot, nick)
     if addminus == 'up':
-        bot.db.set_nick_value(nick, 'points_points', int(points) + int(rando))
+        bot.db.set_nick_value(nick, 'points_points', points + rando)
+    elif addminus == 'down':
+        bot.db.set_nick_value(nick, 'points_points', points - rando)
     else:
-        bot.db.set_nick_value(nick, 'points_points', int(points) - int(rando))
+        bot.say('fart knockers')
         
 def get_points(bot, nick):
     points = bot.db.get_nick_value(nick, 'points_points') or 0
