f62fd49c25b4fcd7d304d58bea5d569e9b8a7385
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-16 12:27:30 -0400

    Update Challenge.py

--- Challenge.py
+++ Challenge.py
@@ -54,9 +54,9 @@ def challenge(bot, channel, instigator, target):
             bot.say("I'm not sure who that is.")
         
         ## Check Opt-in Status
-        elif instigatordisenable:
+        elif not instigatordisenable:
             bot.say(instigator + ', It looks like you have disabled Challenges. Run .challengeon to re-enable.')
-        elif targetdisenable:
+        elif not targetdisenable:
             bot.say(instigator + ', It looks like ' + target + ' has disabled Challenges.')
         
         ## Enforce Timeout, unless in dev-channel
