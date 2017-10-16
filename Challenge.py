33882111a51b4df3bafdb724a3431bacd92821bc
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-16 13:54:36 -0400

    Update Challenge.py

--- Challenge.py
+++ Challenge.py
@@ -45,7 +45,9 @@ def challenge(bot, channel, instigator, target):
         ## Bot opt-out
         instigatorspicebotdisenable = get_spicebotdisenable(bot, instigator)
         targetspicebotdisenable = get_spicebotdisenable(bot, target)
-        if not instigatorspicebotdisenable or not targetspicebotdisenable:
+        if not instigatorspicebotdisenable:
+            sys.exit()
+        if not targetspicebotdisenable:
             sys.exit()
         
         ## People can opt out of playing
