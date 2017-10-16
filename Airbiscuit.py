515d9df1db9952407070a6afd1c718c12faa4484
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-16 13:37:06 -0400

    Update Airbiscuit.py

--- Airbiscuit.py
+++ Airbiscuit.py
@@ -3,4 +3,13 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('airbiscuit','float','floats')
 def airbiscuit(bot,trigger):
-    bot.say(trigger.nick + " floats an air biscuit.")
+    target = trigger.group(3) or trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        if target == trigger.nick:
+            bot.say(trigger.nick + " floats an air biscuit.")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
