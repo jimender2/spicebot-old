0dec808b23b1db168d60a750b93d6ad2d7a6826c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-16 15:20:10 -0400

    Update Asimov_Laws_Of_Robotics.py

--- Asimov_Laws_Of_Robotics.py
+++ Asimov_Laws_Of_Robotics.py
@@ -3,7 +3,15 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('asimov')
 def asimov(bot, trigger):
-    bot.action('may not injure a human being or, through inaction, allow a human being to come to harm.')
-    bot.action('must obey orders given it by human beings except where such orders would conflict with the First Law.')
-    bot.action('must protect its own existence as long as such protection does not conflict with the First or Second Law.')
-    bot.action('must comply with all chatroom rules.')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.action('may not injure a human being or, through inaction, allow a human being to come to harm.')
+        bot.action('must obey orders given it by human beings except where such orders would conflict with the First Law.')
+        bot.action('must protect its own existence as long as such protection does not conflict with the First or Second Law.')
+        bot.action('must comply with all chatroom rules.')
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
