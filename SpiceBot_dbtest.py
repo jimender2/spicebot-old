e4ae0e3b96d2be729706e75b4b56634b40ccc053
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:36:23 -0400

    Update SpiceBot_dbtest.py

--- SpiceBot_dbtest.py
+++ SpiceBot_dbtest.py
@@ -6,7 +6,15 @@ from random import randint
 @sopel.module.require_admin
 @sopel.module.commands('dbtest')
 def get_tables(bot, trigger):
-    con = bot.db.connect()
-    cursor = con.cursor()
-    cursor.execute("SELECT * FROM nick_ids;")
-    bot.say(str(cursor.fetchall()))
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        con = bot.db.connect()
+        cursor = con.cursor()
+        cursor.execute("SELECT * FROM nick_ids;")
+        bot.say(str(cursor.fetchall()))
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
