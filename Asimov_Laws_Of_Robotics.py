342eb41ccbc15b39307712bb3ac1addc65d14301
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 11:28:59 -0400

    Update Asimov_Laws_Of_Robotics.py

--- Asimov_Laws_Of_Robotics.py
+++ Asimov_Laws_Of_Robotics.py
@@ -10,7 +10,10 @@ def asimov(bot, trigger):
         bot.action('must obey orders given it by human beings except where such orders would conflict with the First Law.')
         bot.action('must protect its own existence as long as such protection does not conflict with the First or Second Law.')
         bot.action('must comply with all chatroom rules.')
-
+    else:
+        instigator = trigger.nick
+        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        
 ## Check Status of Opt In
 def get_disenable(bot, nick):
     disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
