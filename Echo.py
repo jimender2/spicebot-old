f887c9b3e198ef9ff6efadd3f8901df60d1553c8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:20:52 -0400

    Update Echo.py

--- Echo.py
+++ Echo.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('echo','echo','echo')
 def echo(bot,trigger):
-    bot.say(".echo")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.say(".echo")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
