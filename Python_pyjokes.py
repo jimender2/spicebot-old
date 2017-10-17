701f0c6299ef6ff24f071bf00421c20a7af2b68e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:30:44 -0400

    Update Python_pyjokes.py

--- Python_pyjokes.py
+++ Python_pyjokes.py
@@ -5,6 +5,14 @@ import pyjokes
 @sopel.module.rate(120)
 @sopel.module.commands('pyjoke')
 def pyjoke(bot, trigger):
-    j1 = pyjokes.get_jokes(category='neutral')
-    j2 = pyjokes.get_jokes(category='adult')
-    bot.say(random.choice(j1 + j2))
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        j1 = pyjokes.get_jokes(category='neutral')
+        j2 = pyjokes.get_jokes(category='adult')
+        bot.say(random.choice(j1 + j2))
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
