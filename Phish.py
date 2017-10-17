f959b1dd86052c6bb4987b829fe568ca7a835557
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:28:56 -0400

    Update Phish.py

--- Phish.py
+++ Phish.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('phish','catphish')
 def phis(bot,trigger):
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
 	bot.say("A/S/L ??")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
