d330064bd04f07f0d51434ae50fcb394772a5dfa
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 11:42:12 -0400

    Update Urban_Dictionary.py

--- Urban_Dictionary.py
+++ Urban_Dictionary.py
@@ -67,7 +67,10 @@ def urban(bot, input):
                 print msg
 
         urban(Sopel(), re.match('()(.*)', sys.argv[1]))
-
+    else:
+        instigator = trigger.nick
+        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        
 ## Check Status of Opt In
 def get_disenable(bot, nick):
     disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
