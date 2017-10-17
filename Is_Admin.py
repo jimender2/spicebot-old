afbe3c73cbad8969f946ee1088b6502f12341f6d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 11:33:09 -0400

    Update Is_Admin.py

--- Is_Admin.py
+++ Is_Admin.py
@@ -12,7 +12,10 @@ def isadmin(bot,trigger):
                 bot.say(trigger.nick + ' is an admin')
             else:
                 bot.say(trigger.nick + ' is not an admin')
-
+    else:
+        instigator = trigger.nick
+        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        
 ## Check Status of Opt In
 def get_disenable(bot, nick):
     disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
