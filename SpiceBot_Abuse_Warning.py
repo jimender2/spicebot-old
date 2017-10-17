69945fe10a7efc587eb8c5cf5fb24ce8c56af3ae
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:32:04 -0400

    Update SpiceBot_Abuse_Warning.py

--- SpiceBot_Abuse_Warning.py
+++ SpiceBot_Abuse_Warning.py
@@ -3,7 +3,15 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('warn','warning')
 def warning(bot,trigger):
-    if not trigger.group(2):
-        bot.say("This is just a warning. Overuse of the bot, can get you kicked or banned by an operator. If you want to purely play with the bot, go to ##SpiceBotTest, or send Spicebot a PrivateMessage.")
-    elif not trigger.group(2) == bot.nick:
-        bot.say(trigger.group(2).strip() + ", This is just a warning. Overuse of the bot, can get you kicked or banned by an operator. If you want to purely play with the bot, go to ##SpiceBotTest, or send Spicebot a PrivateMessage.")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        if not trigger.group(2):
+            bot.say("This is just a warning. Overuse of the bot, can get you kicked or banned by an operator. If you want to purely play with the bot, go to ##SpiceBotTest, or send Spicebot a PrivateMessage.")
+        elif not trigger.group(2) == bot.nick:
+            bot.say(trigger.group(2).strip() + ", This is just a warning. Overuse of the bot, can get you kicked or banned by an operator. If you want to purely play with the bot, go to ##SpiceBotTest, or send Spicebot a PrivateMessage.")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
