ed3c151d263a531c2fb14fe734c9d85a80765309
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 14:36:29 -0400

    Update Airbiscuit.py

--- Airbiscuit.py
+++ Airbiscuit.py
@@ -12,14 +12,18 @@ def airbiscuit(bot,trigger):
             bot.say(trigger.nick + " floats an air biscuit.")
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
-
-## Check Status of Opt In
-def get_disenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if warned:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+        else:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
 
 def update_usertotal(bot, nick):
     usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
     bot.say(str(usertotal))
     bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
+    
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
