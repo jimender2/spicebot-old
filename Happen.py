1295fd8a9cd93dc9e84476da2ebab3655b7b3d51
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:23:33 -0400

    Update Happen.py

--- Happen.py
+++ Happen.py
@@ -3,5 +3,13 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('happen')
 def happen(bot,trigger):
-    if trigger.group(2):
-        bot.say("Stop trying to make " + trigger.group(2) + " happen. It's not going to happen")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        if trigger.group(2):
+            bot.say("Stop trying to make " + trigger.group(2) + " happen. It's not going to happen")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
