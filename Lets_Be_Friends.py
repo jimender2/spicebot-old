54a8ca5168f0fdf253664640db90a1a4a5c3747c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:26:14 -0400

    Update Lets_Be_Friends.py

--- Lets_Be_Friends.py
+++ Lets_Be_Friends.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('fuckyouspicebot','fuckspicebot','banspicebot','kickspicebot','hatespicebot','fuckoffspicebot')
 def apology(bot, trigger):
-    bot.say(trigger.nick + ", I'm sorry I offended you. Lets try to be friends.")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.say(trigger.nick + ", I'm sorry I offended you. Lets try to be friends.")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
