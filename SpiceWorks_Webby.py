982de516ea487003384152b971d770363391b757
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:33:52 -0400

    Update SpiceWorks_Webby.py

--- SpiceWorks_Webby.py
+++ SpiceWorks_Webby.py
@@ -11,7 +11,9 @@ url = 'https://community.spiceworks.com/calendar'
 
 @sopel.module.commands('spicewebby')
 def webbymanual(bot, trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         page = requests.get(url,headers = None)
@@ -25,7 +27,15 @@ def webbymanual(bot, trigger):
             bot.say('BONUS: ' + webbybonus)
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 @sopel.module.interval(60)
 def webbyauto(bot):
