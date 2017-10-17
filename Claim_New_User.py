85615d86457c19040abffb4ba78d74cf990d3cff
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:15:21 -0400

    Update Claim_New_User.py

--- Claim_New_User.py
+++ Claim_New_User.py
@@ -3,12 +3,15 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('pee','claim','urinate')
 def pee(bot, trigger):
-    if not trigger.group(2):
-        claimed = "new user"
-    else:
-        claimed = trigger.group(2).strip()
-    if not claimed == bot.nick and not claimed == trigger.nick:
-        if trigger.nick == 'IT_Sean':
-            bot.say(trigger.nick + ' releases the contents of his bladder on ' + claimed + '! All should recognize this profound claim of ownership upon ' + claimed +'!')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        if not trigger.group(2):
+            claimed = "new user"
         else:
-            bot.say(trigger.nick + ' urinates on ' + claimed + '! Claimed!')
+            claimed = trigger.group(2).strip()
+        if not claimed == bot.nick and not claimed == trigger.nick:
+            if trigger.nick == 'IT_Sean':
+                bot.say(trigger.nick + ' releases the contents of his bladder on ' + claimed + '! All should recognize this profound claim of ownership upon ' + claimed +'!')
+            else:
+                bot.say(trigger.nick + ' urinates on ' + claimed + '! Claimed!')
