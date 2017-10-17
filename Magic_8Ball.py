e245a3d2a0f92fa8eaa2279fcbdbf0a72a8fe2aa
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:28:39 -0400

    Update Magic_8Ball.py

--- Magic_8Ball.py
+++ Magic_8Ball.py
@@ -4,7 +4,9 @@ import random
 @sopel.module.rate(120)
 @sopel.module.commands('8ball')
 def ball(bot, trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         bot.say("Let me dig deep into the waters of life, and find your answer")
@@ -13,7 +15,15 @@ def ball(bot, trigger):
         bot.say(responses [reply]);
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 ## Check Status of Opt In
 def get_disenable(bot, nick):
