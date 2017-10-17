52f174a0e2323a4f3151280766701ca89108f1f8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 11:36:48 -0400

    Update Promiscuous.py

--- Promiscuous.py
+++ Promiscuous.py
@@ -10,7 +10,10 @@ def promiscuous(bot,trigger):
     if targetdisenable:
         rando = randint(2, 666)
         bot.say("Please insert " + str(rando) + " bitcoins, for that kind of service.")
-
+    else:
+        instigator = trigger.nick
+        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        
 ## Check Status of Opt In
 def get_disenable(bot, nick):
     disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
