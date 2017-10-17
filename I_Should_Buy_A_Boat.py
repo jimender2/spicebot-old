29e09b31d04e94eca0a57899e35145b4c2643701
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:24:35 -0400

    Update I_Should_Buy_A_Boat.py

--- I_Should_Buy_A_Boat.py
+++ I_Should_Buy_A_Boat.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('boat')
 def shouldbuyaboat(bot,trigger):
-    bot.say(trigger.nick + " should buy a boat.")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.say(trigger.nick + " should buy a boat.")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
