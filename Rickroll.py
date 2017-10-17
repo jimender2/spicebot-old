bc6aad5c4187055712213aaf0542612f1b9604df
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:31:03 -0400

    Update Rickroll.py

--- Rickroll.py
+++ Rickroll.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('rickroll')
 def rickroll(bot, trigger):
-    bot.say('This link is definately NOT a Rickroll     https://goo.gl/SsAhv')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.say('This link is definately NOT a Rickroll     https://goo.gl/SsAhv')
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
