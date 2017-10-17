41c0589a31d826af4e4d7321ac7bc66d1b7fcb03
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:37:42 -0400

    Update SpiceWorks_IRC_Rules.py

--- SpiceWorks_IRC_Rules.py
+++ SpiceWorks_IRC_Rules.py
@@ -7,31 +7,39 @@ rulesurl = 'https://pastebin.com/raw/Vrq9bHBD'
 @sopel.module.rate(120)
 @sopel.module.commands('rules','rule')
 def rules(bot, trigger):
-    if not trigger.group(2):
-        myline='Chat Rules:     https://pastebin.com/Vrq9bHBD'
-    else:
-        rulenumber = trigger.group(2)
-        if not rulenumber[0].isdigit():
-            rulenumber = w2n.word_to_num(str(rulenumber))
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        if not trigger.group(2):
+            myline='Chat Rules:     https://pastebin.com/Vrq9bHBD'
         else:
-            rulenumber = int(rulenumber)
+            rulenumber = trigger.group(2)
+            if not rulenumber[0].isdigit():
+                rulenumber = w2n.word_to_num(str(rulenumber))
+            else:
+                rulenumber = int(rulenumber)
         
-        htmlfile=urllib.urlopen(rulesurl)
-        lines=htmlfile.readlines()
-        try:
-            if str(rulenumber) != '0':
-                myline=lines[rulenumber-1]
-            else:
-                myline='Rule Zero (read the rules):     https://pastebin.com/Vrq9bHBD'
-        except IndexError or TypeError:
-            if rulenumber == 69:
-                myline='giggles'
-            elif rulenumber == 34:
-                myline='If it exists, there is porn of it.'
-            else:
-                myline= 'That doesnt appear to be a rule number.'
+            htmlfile=urllib.urlopen(rulesurl)
+            lines=htmlfile.readlines()
+            try:
+                if str(rulenumber) != '0':
+                    myline=lines[rulenumber-1]
+                else:
+                    myline='Rule Zero (read the rules):     https://pastebin.com/Vrq9bHBD'
+            except IndexError or TypeError:
+                if rulenumber == 69:
+                    myline='giggles'
+                elif rulenumber == 34:
+                    myline='If it exists, there is porn of it.'
+                else:
+                    myline= 'That doesnt appear to be a rule number.'
     
-        if myline == 'giggles':
-            bot.action(myline)
-        else:
-            bot.say(myline)
+            if myline == 'giggles':
+                bot.action(myline)
+            else:
+                bot.say(myline)
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
