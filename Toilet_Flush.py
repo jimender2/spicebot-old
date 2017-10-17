b2289e2d1f312d3a82a950159586a2c5e4f9a56a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:40:40 -0400

    Update Toilet_Flush.py

--- Toilet_Flush.py
+++ Toilet_Flush.py
@@ -3,5 +3,13 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('flush')
 def flush(bot, trigger):
-    #bot.action('jiggles toilet tank lever.')
-    bot.say('*splsssssssssssssshhhhhhh gurgle gurgle gurgle*')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.action('jiggles toilet tank lever.')
+        bot.say('*splsssssssssssssshhhhhhh gurgle gurgle gurgle*')
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
