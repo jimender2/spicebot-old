0181e2f139ea3d1eca4649e850b94eb7a133f537
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:24:05 -0400

    Update Humpday_Wednesday.py

--- Humpday_Wednesday.py
+++ Humpday_Wednesday.py
@@ -5,9 +5,17 @@ import sys
 @sopel.module.rate(120)
 @sopel.module.commands('humpday')
 def humpdaybot(bot,trigger):
-    whatistoday = str(datetime.datetime.today().weekday())        
-    wednesdaynumber = '2'
-    if whatistoday == wednesdaynumber:
-        bot.say("Today is Wednesday, AKA HUMPDAY!!!!")
-    else:
-        bot.say("Today is not humpday.")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        whatistoday = str(datetime.datetime.today().weekday())        
+        wednesdaynumber = '2'
+        if whatistoday == wednesdaynumber:
+            bot.say("Today is Wednesday, AKA HUMPDAY!!!!")
+        else:
+            bot.say("Today is not humpday.")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
