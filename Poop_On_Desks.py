9a27705754a2b27665c134f2af8245da74a54764
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 11:36:39 -0400

    Update Poop_On_Desks.py

--- Poop_On_Desks.py
+++ Poop_On_Desks.py
@@ -18,7 +18,10 @@ def poop(bot, trigger):
                 bot.say(trigger.nick + ' poops on ' + myline + ', maintaining eye contact the entire time!')
             else:
                 bot.say(trigger.nick + ' poops on ' + myline + '\'s desk, maintaining eye contact the entire time!')
-
+    else:
+        instigator = trigger.nick
+        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        
 ## Check Status of Opt In
 def get_disenable(bot, nick):
     disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
