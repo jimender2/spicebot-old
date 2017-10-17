265afde90600775d0cf166d450190550d2211a2d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:25:13 -0400

    Update Is_It_A_Holiday.py

--- Is_It_A_Holiday.py
+++ Is_It_A_Holiday.py
@@ -5,11 +5,14 @@ import json
 @sopel.module.rate(120)
 @sopel.module.commands('isitaholiday')
 def isitaholiday(bot,trigger):
-    holiday = getholiday()
-    if holiday:
-        bot.say('Today is a holiday.')
-    else:
-        bot.say('Today is not a holiday.')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        holiday = getholiday()
+        if holiday:
+            bot.say('Today is a holiday.')
+        else:
+            bot.say('Today is not a holiday.')
 
 def getholiday():
     url = 'http://isitaholiday.herokuapp.com/api/v2/holidays/today/'
@@ -18,3 +21,8 @@ def getholiday():
     jsonholiday = json.loads(result)
     holiday = jsonholiday['status']
     return holiday
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
