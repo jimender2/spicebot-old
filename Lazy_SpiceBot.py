0fdcba2040372038879d184baad57fc1152c2a4d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:27:40 -0400

    Update Lazy_SpiceBot.py

--- Lazy_SpiceBot.py
+++ Lazy_SpiceBot.py
@@ -3,13 +3,23 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('lazy','lazyfuckingspicebot','fuckinglazyspicebot','lazyspicebot')
 def lazy(bot, trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         bot.say('I do not tell you how to do your job, ' + trigger.nick + '!!')
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 ## Check Status of Opt In
 def get_disenable(bot, nick):
