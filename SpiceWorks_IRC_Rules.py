d917f1bf7ac51f0e75d705c70820481626411044
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:33:46 -0400

    Update SpiceWorks_IRC_Rules.py

--- SpiceWorks_IRC_Rules.py
+++ SpiceWorks_IRC_Rules.py
@@ -7,7 +7,9 @@ rulesurl = 'https://pastebin.com/raw/Vrq9bHBD'
 @sopel.module.rate(120)
 @sopel.module.commands('rules','rule')
 def rules(bot, trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         if not trigger.group(2):
@@ -40,7 +42,15 @@ def rules(bot, trigger):
                 bot.say(myline)
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 ## Check Status of Opt In
 def get_disenable(bot, nick):
