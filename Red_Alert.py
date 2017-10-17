081428422af91a31896d45beb47e9705599e7728
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:30:52 -0400

    Update Red_Alert.py

--- Red_Alert.py
+++ Red_Alert.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('redalert')
 def redalert(bot, trigger):
-    bot.say('Shields Up, Captain ' + trigger.nick + '!!')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.say('Shields Up, Captain ' + trigger.nick + '!!')
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
