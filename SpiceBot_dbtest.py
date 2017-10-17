79283b39e1ec3a5bc438edd40d016414c890c627
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:33:09 -0400

    Update SpiceBot_dbtest.py

--- SpiceBot_dbtest.py
+++ SpiceBot_dbtest.py
@@ -6,7 +6,9 @@ from random import randint
 @sopel.module.require_admin
 @sopel.module.commands('dbtest')
 def get_tables(bot, trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         con = bot.db.connect()
@@ -15,7 +17,15 @@ def get_tables(bot, trigger):
         bot.say(str(cursor.fetchall()))
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 ## Check Status of Opt In
 def get_disenable(bot, nick):
