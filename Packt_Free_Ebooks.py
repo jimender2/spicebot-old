200621c60b63d359607e7886148bef6678992a84
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:29:41 -0400

    Update Packt_Free_Ebooks.py

--- Packt_Free_Ebooks.py
+++ Packt_Free_Ebooks.py
@@ -23,7 +23,9 @@ def getpackt(bot):
 @sopel.module.rate(120)
 @sopel.module.commands('packt')
 def packt(bot, trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         packttimediff = getpackttimediff()
@@ -42,7 +44,15 @@ def packt(bot, trigger):
             return
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 def getPacktTitle():
         url = 'https://www.packtpub.com/packt/offers/free-learning'
