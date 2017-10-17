28413a3fa68d42868d9b1431341fe3a0c02089ee
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:17:54 -0400

    Update Diabeetus.py

--- Diabeetus.py
+++ Diabeetus.py
@@ -4,6 +4,14 @@ import random
 @sopel.module.rate(120)
 @sopel.module.commands('diabeetus')
 def diabeetus(bot,trigger):
-    messages = ["Good morning. I'm Wilford Brimley and I'd like to talk to you about Diabeetus.","If you have type 2 Diabeetus, you can get your testing supplies free..."]
-    answer = random.randint(0,len(messages) - 1)
-    bot.say(messages[answer]);
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        messages = ["Good morning. I'm Wilford Brimley and I'd like to talk to you about Diabeetus.","If you have type 2 Diabeetus, you can get your testing supplies free..."]
+        answer = random.randint(0,len(messages) - 1)
+        bot.say(messages[answer]);
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
