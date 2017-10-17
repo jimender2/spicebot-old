8e8cb637e7082471290115cd3350c00a247b157d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:23:51 -0400

    Update Homer_Simpson.py

--- Homer_Simpson.py
+++ Homer_Simpson.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('homer')
 def homer(bot,trigger):
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
 	bot.say("D'ooooh!")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
