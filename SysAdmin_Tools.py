73cf287d249ec664e3a5c6d6c599b0b2aa302749
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:38:35 -0400

    Update SysAdmin_Tools.py

--- SysAdmin_Tools.py
+++ SysAdmin_Tools.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('sysadmintools')
 def sysadmintools(bot, trigger):
-    bot.say('https://sysadmin.it-landscape.info/     https://sysadmin.libhunt.com/     https://github.com/n1trux/awesome-sysadmin')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.say('https://sysadmin.it-landscape.info/     https://sysadmin.libhunt.com/     https://github.com/n1trux/awesome-sysadmin')
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
