29b01e1915d39344f7b5aaf8990633bce43f32b4
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:37:45 -0400

    Update Humpday_Wednesday.py

--- Humpday_Wednesday.py
+++ Humpday_Wednesday.py
@@ -5,7 +5,9 @@ import sys
 @sopel.module.rate(120)
 @sopel.module.commands('humpday')
 def humpdaybot(bot,trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         whatistoday = str(datetime.datetime.today().weekday())        
@@ -16,7 +18,15 @@ def humpdaybot(bot,trigger):
             bot.say("Today is not humpday.")
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 ## Check Status of Opt In
 def get_disenable(bot, nick):
