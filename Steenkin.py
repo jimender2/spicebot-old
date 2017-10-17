42ce786f42d973d1cbbfadc30c4f434449eacb81
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:38:22 -0400

    Update Steenkin.py

--- Steenkin.py
+++ Steenkin.py
@@ -3,5 +3,13 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('steenkin','dontneed')
 def dontneed(bot,trigger):
-    if trigger.group(2):
-        bot.say(trigger.group(2) + "? weee dun neeeed no steenkin " + trigger.group(2) + "!!")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        if trigger.group(2):
+            bot.say(trigger.group(2) + "? weee dun neeeed no steenkin " + trigger.group(2) + "!!")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
