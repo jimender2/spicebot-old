efa431380bae240b9af3ed30c47884cd61ecc5f6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:36:32 -0400

    Update Diabeetus.py

--- Diabeetus.py
+++ Diabeetus.py
@@ -4,7 +4,9 @@ import random
 @sopel.module.rate(120)
 @sopel.module.commands('diabeetus')
 def diabeetus(bot,trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         messages = ["Good morning. I'm Wilford Brimley and I'd like to talk to you about Diabeetus.","If you have type 2 Diabeetus, you can get your testing supplies free..."]
@@ -12,7 +14,15 @@ def diabeetus(bot,trigger):
         bot.say(messages[answer]);
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 ## Check Status of Opt In
 def get_disenable(bot, nick):
