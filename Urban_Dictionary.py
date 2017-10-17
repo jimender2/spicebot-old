51aba1eaad0183c759269eae7f8650e0ce5c43fd
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:35:10 -0400

    Update Urban_Dictionary.py

--- Urban_Dictionary.py
+++ Urban_Dictionary.py
@@ -15,7 +15,9 @@ def format_defn(defn, num, max):
     return lines
 
 def urban(bot, input):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         baseurl = 'http://api.urbandictionary.com/v0/'
@@ -69,7 +71,15 @@ def urban(bot, input):
         urban(Sopel(), re.match('()(.*)', sys.argv[1]))
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 ## Check Status of Opt In
 def get_disenable(bot, nick):
