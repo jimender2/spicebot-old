d51bc6322422dccedf18dafbcfd9240753cc5037
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:30:33 -0400

    Update Promiscuous.py

--- Promiscuous.py
+++ Promiscuous.py
@@ -5,5 +5,13 @@ from random import randint
 @sopel.module.rate(120)
 @sopel.module.commands('sexbot','cockbot','fuckbot')
 def promiscuous(bot,trigger):
-    rando = randint(2, 666)
-    bot.say("Please insert " + str(rando) + " bitcoins, for that kind of service.")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        rando = randint(2, 666)
+        bot.say("Please insert " + str(rando) + " bitcoins, for that kind of service.")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
