52470cca5fe023f8a5dbeee3c429c3fb83c2226f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:26:55 -0400

    Update Meraki_Free.py

--- Meraki_Free.py
+++ Meraki_Free.py
@@ -3,19 +3,27 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('meraki','freemeraki')
 def meraki(bot, trigger):
-    if trigger.group(2):
-        if trigger.group(2) == 'mx':
-            bot.say('MX     https://meraki.cisco.com/tc/freemx')
-        elif trigger.group(2) == 'switch':
-            bot.say('Switch     https://meraki.cisco.com/tc/freeswitch')
-        elif trigger.group(2) == 'ap':
-            bot.say('AP     https://meraki.cisco.com/tc/freeap')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        if trigger.group(2):
+            if trigger.group(2) == 'mx':
+                bot.say('MX     https://meraki.cisco.com/tc/freemx')
+            elif trigger.group(2) == 'switch':
+                bot.say('Switch     https://meraki.cisco.com/tc/freeswitch')
+            elif trigger.group(2) == 'ap':
+                bot.say('AP     https://meraki.cisco.com/tc/freeap')
+            else:
+                normalrun='true'
         else:
             normalrun='true'
-    else:
-        normalrun='true'
-    try:
-        if normalrun:
-            bot.say('Please specify which product. Choices are MX , AP , or switch .')
-    except UnboundLocalError:
-        return
+        try:
+            if normalrun:
+                bot.say('Please specify which product. Choices are MX , AP , or switch .')
+        except UnboundLocalError:
+            return
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
