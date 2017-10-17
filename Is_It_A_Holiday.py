339936b3a6134072830c8e7fef90db8e70b33928
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:27:07 -0400

    Update Is_It_A_Holiday.py

--- Is_It_A_Holiday.py
+++ Is_It_A_Holiday.py
@@ -5,7 +5,9 @@ import json
 @sopel.module.rate(120)
 @sopel.module.commands('isitaholiday')
 def isitaholiday(bot,trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         holiday = getholiday()
@@ -15,7 +17,15 @@ def isitaholiday(bot,trigger):
             bot.say('Today is not a holiday.')
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 def getholiday():
     url = 'http://isitaholiday.herokuapp.com/api/v2/holidays/today/'
