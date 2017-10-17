4058b73619115c245d566ffa2663670f9c2bc6a2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:25:29 -0400

    Update Is_It_Friday_Yet.py

--- Is_It_Friday_Yet.py
+++ Is_It_Friday_Yet.py
@@ -5,15 +5,23 @@ import sys
 @sopel.module.rate(120)
 @sopel.module.commands('isitfriday','isitfridayyet')
 def fridaybot(bot,trigger):
-    whatistoday = str(datetime.datetime.today().weekday())        
-    fridaynumber = '4'
-    if whatistoday == fridaynumber:
-        bot.say("TGIF! It's finally here!!!")
-    elif whatistoday == '5' or whatistoday == '6':
-        bot.say("It's the Weekend. It should be better than Friday!")
-    else:
-        daysmath = int(fridaynumber) - int(whatistoday)
-        if daysmath == int('1'):
-            bot.say("Unfortunately Friday is " + str(daysmath) + " day away. I'm sure we'll make it there!")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        whatistoday = str(datetime.datetime.today().weekday())        
+        fridaynumber = '4'
+        if whatistoday == fridaynumber:
+            bot.say("TGIF! It's finally here!!!")
+        elif whatistoday == '5' or whatistoday == '6':
+            bot.say("It's the Weekend. It should be better than Friday!")
         else:
-            bot.say("Unfortunately Friday is " + str(daysmath) + " days away. I'm sure we'll make it there!")
+            daysmath = int(fridaynumber) - int(whatistoday)
+            if daysmath == int('1'):
+                bot.say("Unfortunately Friday is " + str(daysmath) + " day away. I'm sure we'll make it there!")
+            else:
+                bot.say("Unfortunately Friday is " + str(daysmath) + " days away. I'm sure we'll make it there!")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
