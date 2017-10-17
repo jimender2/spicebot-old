5cbaa5e3e3474777d2bd5300fcb424ba05f2af02
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:33:27 -0400

    Update SpiceBot_Module_Count.py

--- SpiceBot_Module_Count.py
+++ SpiceBot_Module_Count.py
@@ -9,5 +9,13 @@ dirpath = os.path.dirname(__file__)
 @sopel.module.rate(120)
 @sopel.module.commands('modulecount')
 def modulecount(bot,trigger):
-    modulecount = str(len(fnmatch.filter(os.listdir(dirpath), '*.py')))
-    bot.say('There are currently ' + modulecount +' custom modules installed.')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        modulecount = str(len(fnmatch.filter(os.listdir(dirpath), '*.py')))
+        bot.say('There are currently ' + modulecount +' custom modules installed.')
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
