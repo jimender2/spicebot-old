bd217552a71e825d88d855cea347daf1588ec3b4
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:34:22 -0400

    Update Thanks_Spicebot.py

--- Thanks_Spicebot.py
+++ Thanks_Spicebot.py
@@ -4,11 +4,12 @@ import json
 from BeautifulSoup import BeautifulSoup
 from random import randint
 
-
 @sopel.module.rate(120)
 @sopel.module.commands('thanks','thanksspicebot')
 def thanks(bot,trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         gif = yourewelcome()
@@ -18,7 +19,15 @@ def thanks(bot,trigger):
             bot.say('You\'re welcome!')
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
 
 def yourewelcome():
     api = 'Wi33J3WxSDxWsrxLREcQqmO3iJ0dk52N'
