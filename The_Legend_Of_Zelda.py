3847ec7823f0b4e25e503015fb586ace8c1171d0
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:39:58 -0400

    Update The_Legend_Of_Zelda.py

--- The_Legend_Of_Zelda.py
+++ The_Legend_Of_Zelda.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('sword','link','zelda')
 def canYouSeeMe(bot,trigger):
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
 	bot.say("it's dangerous to go alone take this 0==\======>")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
