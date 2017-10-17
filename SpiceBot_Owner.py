25b3a0d82cb8133117615c069d98af1dda591f3c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:33:43 -0400

    Update SpiceBot_Owner.py

--- SpiceBot_Owner.py
+++ SpiceBot_Owner.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('owner','botowner')
 def botowner(bot,trigger):
-    bot.say(bot.config.core.owner)
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.say(bot.config.core.owner)
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
