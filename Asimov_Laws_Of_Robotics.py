c307e85d60082c863f1db8f1a441443667bea3c2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:35:35 -0400

    Update Asimov_Laws_Of_Robotics.py

--- Asimov_Laws_Of_Robotics.py
+++ Asimov_Laws_Of_Robotics.py
@@ -3,7 +3,9 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('asimov')
 def asimov(bot, trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         bot.action('may not injure a human being or, through inaction, allow a human being to come to harm.')
@@ -12,7 +14,15 @@ def asimov(bot, trigger):
         bot.action('must comply with all chatroom rules.')
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 ## Check Status of Opt In
 def get_disenable(bot, nick):
