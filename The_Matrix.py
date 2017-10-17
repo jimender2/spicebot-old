f6c194fd64b152c6615061fb8a0e20f7584bc01e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:40:18 -0400

    Update The_Matrix.py

--- The_Matrix.py
+++ The_Matrix.py
@@ -3,17 +3,25 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('matrix') 
 def matrix(bot, trigger):
-    if trigger.group(2):
-        if trigger.group(2) == 'redpill':
-            bot.say('You take the red pill, you stay in Wonderland, and I show you how deep the rabbit hole goes.')
-        elif trigger.group(2) == 'bluepill':
-            bot.say('You take the blue pill, the story ends. You wake up in your bed and believe whatever you want to believe.')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        if trigger.group(2):
+            if trigger.group(2) == 'redpill':
+                bot.say('You take the red pill, you stay in Wonderland, and I show you how deep the rabbit hole goes.')
+            elif trigger.group(2) == 'bluepill':
+                bot.say('You take the blue pill, the story ends. You wake up in your bed and believe whatever you want to believe.')
+            else:
+                normalrun='true'
         else:
             normalrun='true'
-    else:
-        normalrun='true'
-    try:
-        if normalrun:
-            bot.say('You have two choices. redpill Or bluepill')        
-    except UnboundLocalError:
-        return
+        try:
+            if normalrun:
+                bot.say('You have two choices. redpill Or bluepill')        
+        except UnboundLocalError:
+            return
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
