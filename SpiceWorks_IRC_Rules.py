a07c853fb5f04897d065acd86dadd7bc60f107d9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 11:39:46 -0400

    Update SpiceWorks_IRC_Rules.py

--- SpiceWorks_IRC_Rules.py
+++ SpiceWorks_IRC_Rules.py
@@ -38,7 +38,10 @@ def rules(bot, trigger):
                 bot.action(myline)
             else:
                 bot.say(myline)
-
+    else:
+        instigator = trigger.nick
+        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        
 ## Check Status of Opt In
 def get_disenable(bot, nick):
     disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
