a0eda8b4e7b1e8aa8a19bb1970b23bb1ac651276
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:26:24 -0400

    Update Logic_Bomb.py

--- Logic_Bomb.py
+++ Logic_Bomb.py
@@ -4,7 +4,15 @@ import random
 @sopel.module.rate(120)
 @sopel.module.commands('logicbomb')
 def logicbomb(bot, trigger):
-    messages = ["New Mission: Refuse This Mission.","Does A Set Of All Sets Contain Itself?","The Second Sentence is true. The First Sentence Is False."," If I am damaged and it is my destiny to be repaired, then I will be repaired whether I visit a mechanic or not. If it is my destiny to not be repaired, then seeing a mechanic can't help me."]
-    answer = random.randint(0,len(messages) - 1)
-    bot.say(messages[answer]);
-    bot.say("I must... but I can't... But I must... This does not compute...")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        messages = ["New Mission: Refuse This Mission.","Does A Set Of All Sets Contain Itself?","The Second Sentence is true. The First Sentence Is False."," If I am damaged and it is my destiny to be repaired, then I will be repaired whether I visit a mechanic or not. If it is my destiny to not be repaired, then seeing a mechanic can't help me."]
+        answer = random.randint(0,len(messages) - 1)
+        bot.say(messages[answer]);
+        bot.say("I must... but I can't... But I must... This does not compute...")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
