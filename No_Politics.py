f42a1e6b5fa91777d720bb02be4afd996b847379
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:27:28 -0400

    Update No_Politics.py

--- No_Politics.py
+++ No_Politics.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('sign','politics','religion')
 def sign(bot,trigger):
-    bot.say("NO POLITICS OR RELIGION IN #spiceworks!")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.say("NO POLITICS OR RELIGION IN #spiceworks!")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
