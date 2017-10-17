e28df62dd6420843f39751b90d3ccd5c23cb6be5
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:27:20 -0400

    Update My_Name.py

--- My_Name.py
+++ My_Name.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('spicebot','spicebotdev')
 def name(bot,trigger):
-    bot.say("That's my name. Don't wear it out!")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.say("That's my name. Don't wear it out!")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
