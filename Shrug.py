672b10a210ecd1dbba6c672bd22d7f7e68c2a574
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:31:44 -0400

    Update Shrug.py

--- Shrug.py
+++ Shrug.py
@@ -4,4 +4,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('shrug','unicode','unicodeshrug')
 def shrug(bot,trigger):
-    bot.say(" ¯\_(ツ)_/¯ ")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.say(" ¯\_(ツ)_/¯ ")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
