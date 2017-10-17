d6d413810b807ccb82b2e46aac7d1873c2fbdc42
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:31:54 -0400

    Update Slap_With_Fish.py

--- Slap_With_Fish.py
+++ Slap_With_Fish.py
@@ -3,6 +3,14 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('fish')
 def slapwithfish(bot,trigger):
-    if trigger.group(2):
-        if not trigger.group(2).strip() == bot.nick:
-            bot.say(trigger.nick + " slaps " + trigger.group(2).strip() + " with a fish.")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        if trigger.group(2):
+            if not trigger.group(2).strip() == bot.nick:
+                bot.say(trigger.nick + " slaps " + trigger.group(2).strip() + " with a fish.")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
