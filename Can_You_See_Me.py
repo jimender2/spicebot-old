d092b73336427acc16ee129f9a6c2df98d5fb425
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:13:00 -0400

    Update Can_You_See_Me.py

--- Can_You_See_Me.py
+++ Can_You_See_Me.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('canyouseeme')
 def canYouSeeMe(bot,trigger):
-    bot.say(trigger.nick + " , I can see you.")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.say(trigger.nick + " , I can see you.")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
