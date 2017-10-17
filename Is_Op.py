0574c4d2214920e18bf7e94b3e0eb679e9bda4f3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 11:33:49 -0400

    Update Is_Op.py

--- Is_Op.py
+++ Is_Op.py
@@ -21,6 +21,7 @@ def isop(bot,trigger):
     else:
         instigator = trigger.nick
         bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        
 ## Check Status of Opt In
 def get_disenable(bot, nick):
     disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
