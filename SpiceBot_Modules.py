3b1c3cb132e2a0ec094ec4033a399688bf48b73a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:33:35 -0400

    Update SpiceBot_Modules.py

--- SpiceBot_Modules.py
+++ SpiceBot_Modules.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('spicebotmodules')
 def spicemodules(bot, trigger):
-    bot.say('Spiceworks IRC Modules     https://github.com/deathbybandaid/sopel-modules')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.say('Spiceworks IRC Modules     https://github.com/deathbybandaid/sopel-modules')
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
