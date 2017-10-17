596c949780ba7bdd47bfc1b515561dfa1b850dd2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:24:47 -0400

    Update Im_Sorry_Dave_But_I_Cannot_Help_You.py

--- Im_Sorry_Dave_But_I_Cannot_Help_You.py
+++ Im_Sorry_Dave_But_I_Cannot_Help_You.py
@@ -3,7 +3,15 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('dave','daveb')
 def sorry(bot, trigger):
-    usernickname = trigger.nick.lower()
-    if "dave" in usernickname:
-        bot.say("Is that really you, Dave?")
-    bot.say('Im sorry, ' + trigger.nick + ', but I cannot help you.')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        usernickname = trigger.nick.lower()
+        if "dave" in usernickname:
+            bot.say("Is that really you, Dave?")
+        bot.say('Im sorry, ' + trigger.nick + ', but I cannot help you.')
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
