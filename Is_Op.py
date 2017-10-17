8f83562f82b0f54b67cdd06adc7a623ac2958f3a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:25:50 -0400

    Update Is_Op.py

--- Is_Op.py
+++ Is_Op.py
@@ -4,14 +4,22 @@ from sopel.tools.target import User, Channel
 
 @sopel.module.commands('isop')
 def isop(bot,trigger):
-    if not trigger.group(2):
-        nick = trigger.nick.lower()
-    else:
-        nick = trigger.group(2).lower()
-    try:    
-        if bot.privileges[trigger.sender][nick] == OP:
-            bot.say(nick + ' is an op.')
-        else: 
-            bot.say(nick + ' is not an op.')
-    except KeyError:
-        bot.say(nick + ' is not here right now!')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        if not trigger.group(2):
+            nick = trigger.nick.lower()
+        else:
+            nick = trigger.group(2).lower()
+        try:    
+            if bot.privileges[trigger.sender][nick] == OP:
+                bot.say(nick + ' is an op.')
+            else: 
+                bot.say(nick + ' is not an op.')
+        except KeyError:
+            bot.say(nick + ' is not here right now!')
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
