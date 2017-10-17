17657f4b639d7ad4e612630d77c955a0989074ea
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:31:03 -0400

    Update Promiscuous.py

--- Promiscuous.py
+++ Promiscuous.py
@@ -5,14 +5,24 @@ from random import randint
 @sopel.module.rate(120)
 @sopel.module.commands('sexbot','cockbot','fuckbot')
 def promiscuous(bot,trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         rando = randint(2, 666)
         bot.say("Please insert " + str(rando) + " bitcoins, for that kind of service.")
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 ## Check Status of Opt In
 def get_disenable(bot, nick):
