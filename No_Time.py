6a0b16f507707b16395a12f552e2f6b6353494e6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:27:36 -0400

    Update No_Time.py

--- No_Time.py
+++ No_Time.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('notime')
 def notime(bot,trigger):
-    bot.say("Well I woke up to get me a cold pop and then I thought somebody was barbequing. I said oh lord Jesus it's a fire. Then I ran out, I didn't grab no shoes or nothin' Jesus, I ran for my life. And then the smoke got me, I got bronchitis ain't nobody got time for that.")
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.say("Well I woke up to get me a cold pop and then I thought somebody was barbequing. I said oh lord Jesus it's a fire. Then I ran out, I didn't grab no shoes or nothin' Jesus, I ran for my life. And then the smoke got me, I got bronchitis ain't nobody got time for that.")
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
