401fe91db46abfea8ed638676bc1340112d27eb4
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:29:11 -0400

    Update Pints.py

--- Pints.py
+++ Pints.py
@@ -3,12 +3,20 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('pints','pint')
 def pints(bot, trigger):
-    if not trigger.group(2):
-        winner = "Everybody"
-    else:
-        winner = trigger.group(2).strip()
-        if trigger.group(2) == 'all':
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        if not trigger.group(2):
             winner = "Everybody"
-        elif trigger.group(2) == trigger.nick:
-            winner = "him/her-self"
-    bot.say(trigger.nick + ' buys a pint for ' + winner)
+        else:
+            winner = trigger.group(2).strip()
+            if trigger.group(2) == 'all':
+                winner = "Everybody"
+            elif trigger.group(2) == trigger.nick:
+                winner = "him/her-self"
+        bot.say(trigger.nick + ' buys a pint for ' + winner)
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
