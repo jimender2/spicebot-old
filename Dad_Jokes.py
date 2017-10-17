bdf53b63ab7b6fa6ab7202766a4081d6a98b3285
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:36:07 -0400

    Update Dad_Jokes.py

--- Dad_Jokes.py
+++ Dad_Jokes.py
@@ -4,7 +4,9 @@ import requests
 @sopel.module.rate(120)
 @sopel.module.commands('dad','dadjoke')
 def sayDadJoke(bot,trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         joke = getDadJoke()
@@ -14,7 +16,15 @@ def sayDadJoke(bot,trigger):
             bot.say('My humor module is broken.')
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 def getDadJoke():
     url = 'https://icanhazdadjoke.com'    
