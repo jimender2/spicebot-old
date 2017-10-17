4b65dd6ea71ac21dd73c5f78c0bdb1c44339b2a3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:33:37 -0400

    Update SpiceWorks_IRC_Quotes.py

--- SpiceWorks_IRC_Quotes.py
+++ SpiceWorks_IRC_Quotes.py
@@ -8,7 +8,9 @@ from xml.sax.saxutils import unescape as unescape
 @sopel.module.rate(120)
 @sopel.module.commands('spicyquote')
 def spicyQuote(bot,trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         query = str(trigger.group(2))
@@ -25,7 +27,15 @@ def spicyQuote(bot,trigger):
             bot.say('Please provide a quote number and try again!')
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
 	
 def getQuote(query):
     unescape_xml_entities = lambda s: unescape(s, {"&apos;": "'", "&quot;": '"', "&nbsp;":" "})
