030d743cb8d416a46f0d84a553d980d9837e713c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:35:16 -0400

    Update Wanted.py

--- Wanted.py
+++ Wanted.py
@@ -5,7 +5,9 @@ from random import randint
 @sopel.module.rate(120)
 @sopel.module.commands('wanted')
 def wanted(bot,trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         rando = randint(2, 50)
@@ -15,7 +17,15 @@ def wanted(bot,trigger):
             bot.say(trigger.group(2).strip() + ' was never wanted as a child, but now is wanted in ' + str(rando) + ' states!')
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 ## Check Status of Opt In
 def get_disenable(bot, nick):
