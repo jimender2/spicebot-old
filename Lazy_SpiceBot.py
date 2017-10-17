5e240128c7a11450b71d58f838ee3a6ab783919b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:25:58 -0400

    Update Lazy_SpiceBot.py

--- Lazy_SpiceBot.py
+++ Lazy_SpiceBot.py
@@ -3,4 +3,12 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('lazy','lazyfuckingspicebot','fuckinglazyspicebot','lazyspicebot')
 def lazy(bot, trigger):
-    bot.say('I do not tell you how to do your job, ' + trigger.nick + '!!')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.say('I do not tell you how to do your job, ' + trigger.nick + '!!')
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
