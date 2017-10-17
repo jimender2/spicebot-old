7358100c1a19016b367c80cf861e5fdd8aec8d28
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 09:31:29 -0400

    Update Rimshot.py

--- Rimshot.py
+++ Rimshot.py
@@ -3,5 +3,13 @@ import sopel.module
 @sopel.module.rate(120)
 @sopel.module.commands('rimshot')
 def rimshot(bot, trigger):
-    #bot.action('uses drumsticks to hit both the head and the rim of the drum, then the cymbal.')
-    bot.say('*Ba Dum Tss!!!*')
+    target = trigger.nick
+    targetdisenable = get_disenable(bot, target)
+    if targetdisenable:
+        bot.action('uses drumsticks to hit both the head and the rim of the drum, then the cymbal.')
+        bot.say('*Ba Dum Tss!!!*')
+
+## Check Status of Opt In
+def get_disenable(bot, nick):
+    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
+    return disenable
