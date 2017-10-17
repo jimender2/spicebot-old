e19f9a3ea62fd7c3a37feea1abe91df6d81b91ac
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-17 15:27:14 -0400

    Update Is_It_Friday_Yet.py

--- Is_It_Friday_Yet.py
+++ Is_It_Friday_Yet.py
@@ -5,7 +5,9 @@ import sys
 @sopel.module.rate(120)
 @sopel.module.commands('isitfriday','isitfridayyet')
 def fridaybot(bot,trigger):
+    instigator = trigger.nick
     target = trigger.nick
+    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         whatistoday = str(datetime.datetime.today().weekday())        
@@ -22,7 +24,15 @@ def fridaybot(bot,trigger):
                 bot.say("Unfortunately Friday is " + str(daysmath) + " days away. I'm sure we'll make it there!")
     else:
         instigator = trigger.nick
-        bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
+        if not warned:
+            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        else:
+            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+
+def update_usertotal(bot, nick):
+    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
+    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
         
 ## Check Status of Opt In
 def get_disenable(bot, nick):
