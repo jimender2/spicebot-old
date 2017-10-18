b1377cc57c2fb3b8aa1f333019cce4469e78921c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-18 12:01:36 -0400

    Update rss-reader.py

--- examples/rss-reader.py
+++ examples/rss-reader.py
@@ -3,53 +3,65 @@ import requests
 from xml.dom import minidom
 from fake_useragent import UserAgent
 
+## If Following Template
+feedname = "iT News"
 url = 'https://www.itnews.com.au/RSS/rss.ashx'
+alt_url = 'https://www.itnews.com.au'
+## End Of Template
+
+## Based On Template
+messagestring = str("[" + feedname + "] ")
+trimmedname = feedname.replace(" ","").lower()
+maincommand = str(trimmedname)
+resetcommand = str(maincommand + 'reset')
+lastbuilddatabase = str(maincommand + '_lastbuildcurrent')
+
+## user agent and header
 ua = UserAgent()
 header = {'User-Agent': str(ua.chrome)}
 
+## Automatic Run
 @sopel.module.interval(60)
-def getitnews(bot):
+def autocheck(bot):
     for channel in bot.channels:
         page = requests.get(url, headers=header)
         if page.status_code == 200:
             title, link = checkfornew(bot, page)
-            bot.msg(channel, "[iT News] " + title + ': ' + link)
+	    if title and link:
+                bot.msg(channel, messagestring + title + ': ' + link)
 
 @sopel.module.rate(120)
-@sopel.module.commands('itnews')
+@sopel.module.commands(maincommand)
 def manualCheck(bot,trigger):
     instigator = trigger.nick
     target = trigger.nick
     update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
-    	page = requests.get(url, headers=header)
-	try:
-	    title, link = checkfornew(bot, page)
-	except TypeError:
-	    title = "iT News"
-	    link = "https://www.itnews.com.au"
-        bot.say("[iT News] " + title + ': ' + link)  
+	runprocess(bot)
     else:
-        instigator = trigger.nick
         warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
         if not warned:
             bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
         else:
             bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
 
-def checkfornew(bot, page):
-    xml = page.text
-    xml = xml.encode('ascii', 'ignore').decode('ascii')
-    xmldoc = minidom.parseString(xml)
-    newnews = checkLastBuildDate(bot, xmldoc)
-    if newnews == True:
-	titles = xmldoc.getElementsByTagName('title')
-        title = titles[2].childNodes[0].nodeValue
-        links = xmldoc.getElementsByTagName('link')
-        link = links[2].childNodes[0].nodeValue.split("?")[0]
-        return title, link
-	
+@sopel.module.require_admin
+@sopel.module.commands(resetcommand)
+def reset(bot,trigger):
+    for channel in bot.channels:
+    	bot.say('Resetting LastBuildTime...')
+    	bot.db.set_nick_value(channel, lastbuilddatabase, '')
+
+def runprocess(bot):
+    page = requests.get(url, headers=header)
+    try:
+        title, link = checkfornew(bot, page)
+    except TypeError:
+        title = feedname
+        link = alt_url
+    bot.say(messagestring + title + ': ' + link)  
+		   
 def checkLastBuildDate(bot, xmldoc):
     lastBuildXML = xmldoc.getElementsByTagName('pubDate')
     lastBuildXML = lastBuildXML[0].childNodes[0].nodeValue
@@ -57,20 +69,34 @@ def checkLastBuildDate(bot, xmldoc):
     lastbuildcurrent = get_lastbuildcurrent(bot, lastBuildXML)
     if lastbuildcurrent:
 	if lastBuildXML.strip() == lastbuildcurrent.strip():
-	    newnews = False
+	    newcontent = False
         else:
-            newnews = True
+            newcontent = True
     else:
-        newnews = True
+        newcontent = True
     set_lastbuildcurrent(bot, lastBuildXML)
-    return newnews
+    return newcontent
 
-@sopel.module.require_admin
-@sopel.module.commands('itnewsreset')
-def reset(bot,trigger):
+def checkfornew(bot, page):
+    xml = page.text
+    xml = xml.encode('ascii', 'ignore').decode('ascii')
+    xmldoc = minidom.parseString(xml)
+    newcontent = checkLastBuildDate(bot, xmldoc)
+    if newcontent == True:
+	titles = xmldoc.getElementsByTagName('title')
+        title = titles[1].childNodes[0].nodeValue
+        links = xmldoc.getElementsByTagName('link')
+        link = links[1].childNodes[0].nodeValue.split("?")[0]
+        return title, link
+
+def get_lastbuildcurrent(bot, lastBuildXML):
     for channel in bot.channels:
-    	bot.say('Resetting LastBuildTime...')
-    	bot.db.set_nick_value(channel, 'itnews_lastbuildcurrent', '')
+        lastbuildcurrent = bot.db.get_nick_value(channel, lastbuilddatabase) or 0
+    return lastbuildcurrent
+
+def set_lastbuildcurrent(bot, lastbuildcurrent):
+    for channel in bot.channels:
+        bot.db.set_nick_value(channel, lastbuilddatabase, lastbuildcurrent)
 
 ## Check Status of Opt In
 def get_disenable(bot, nick):
@@ -80,12 +106,3 @@ def get_disenable(bot, nick):
 def update_usertotal(bot, nick):
     usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
     bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
-
-def get_lastbuildcurrent(bot, lastBuildXML):
-    for channel in bot.channels:
-        lastbuildcurrent = bot.db.get_nick_value(channel, 'itnews_lastbuildcurrent') or 0
-    return lastbuildcurrent
-
-def set_lastbuildcurrent(bot, lastbuildcurrent):
-    for channel in bot.channels:
-        bot.db.set_nick_value(channel, 'itnews_lastbuildcurrent', lastbuildcurrent)
