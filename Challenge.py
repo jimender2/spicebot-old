caa93bf8e9b0ba3ff764caff529ccbd18cd4727f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-18 16:34:54 -0400

    Update Challenge.py

--- Challenge.py
+++ Challenge.py
@@ -523,7 +523,8 @@ def weaponslockercmd(bot, trigger):
             for weapon in weaponslist:
                 if weapon.endswith("\n"):
                     bot.say(str(weapon))
-                    weapon = weapon.replace("\n", '')
+                    #weapon = weapon.replace("\n", '')
+                    weapon = weapon.split("\n", 0)[1]
                 weaponslistnew.append(weapon)
             for channel in bot.channels:
                 bot.db.set_nick_value(channel, 'weapons_locker', '')
