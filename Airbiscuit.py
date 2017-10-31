c47698a1bbb19f34bb89bc395fc22af151d637a1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-31 19:47:38 -0400

    Update Airbiscuit.py

--- Airbiscuit.py
+++ Airbiscuit.py
@@ -3,7 +3,7 @@ import sys
 import os
 moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
-import SpicebotShared
+from SpicebotShared import *
 
 @sopel.module.rate(120)
 @sopel.module.commands('airbiscuit','float','floats')
