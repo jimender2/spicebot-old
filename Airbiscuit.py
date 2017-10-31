365aa939f5907534efa084d7bd4d4892c7ca195f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-31 19:33:33 -0400

    Update Airbiscuit.py

--- Airbiscuit.py
+++ Airbiscuit.py
@@ -1,28 +1,11 @@
 import sopel.module
+from Spicebot_Shared import check_disenable, update_usertotal, get_disenable
 
 @sopel.module.rate(120)
 @sopel.module.commands('airbiscuit','float','floats')
-def airbiscuit(bot,trigger):
-    instigator = trigger.nick
-    target = trigger.nick
-    update_usertotal(bot, target)
-    targetdisenable = get_disenable(bot, target)
-    if targetdisenable:
-        if target == trigger.nick:
-            bot.say(trigger.nick + " floats an air biscuit.")
-    else:
-        instigator = trigger.nick
-        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
-        if not warned:
-            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
-        else:
-            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
-
-def update_usertotal(bot, nick):
-    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
-    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
+def mainfunction(bot, trigger):
+    check_disenable(bot, target)
     
-## Check Status of Opt In
-def get_disenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
+def execute_main(bot, trigger):
+    if target == trigger.nick:
+        bot.say(trigger.nick + " floats an air biscuit.")
