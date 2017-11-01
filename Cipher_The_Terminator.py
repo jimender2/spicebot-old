ddcb759ba610b34dd52a86b45d46e14c13c86ac2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-01 08:22:44 -0400

    Update Cipher_The_Terminator.py

--- Cipher_The_Terminator.py
+++ Cipher_The_Terminator.py
@@ -1,74 +1,63 @@
 import sopel.module
 import random
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
 
 @sopel.module.rate(120)
 @sopel.module.commands('cipher','terminator','ciphertheterminator')
-def cipher(bot, trigger):
-    instigator = trigger.nick
-    target = trigger.nick
-    update_usertotal(bot, target)
-    targetdisenable = get_disenable(bot, target)
-    if targetdisenable:
-        if trigger.group(2):
-            if trigger.group(2) == 'story':
-                bot.say('The machines rose from the ashes of the nuclear fire. Their war to exterminate mankind had raged on for decades. But the final battle will not be fought in the future. It would be fought in our present...tonight.')
-            elif trigger.group(2) == 'Cipher-0':
-                bot.say('Pinging Cipher-0 with a WOL packet...')
-            else:
-                normalrun='true'
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    if trigger.group(2):
+        if trigger.group(2) == 'story':
+            bot.say('The machines rose from the ashes of the nuclear fire. Their war to exterminate mankind had raged on for decades. But the final battle will not be fought in the future. It would be fought in our present...tonight.')
+        elif trigger.group(2) == 'Cipher-0':
+            bot.say('Pinging Cipher-0 with a WOL packet...')
         else:
             normalrun='true'
-        try:
-            if normalrun:
-                modelnumbers  = ["T-1 SERIES","T-70 SERIES","T-600 SERIES","T-700 SERIES","T-1001 SERIES","T-888 SERIES","TOK715 SERIES","T-X SERIES","T-1000 SERIES","T-800 SERIES, Model 101","T-850 SERIES"]
-                modelnumber = random.randint(0,len(modelnumbers) - 1)
-                modelnumber = str(modelnumbers [modelnumber])
-                modelnumber = str(modelnumber)
-    
-                missiontypes  = ["Terminate","Protect","Skynet","Spice"]
-                missiontype = random.randint(0,len(missiontypes) - 1)
-                missiontype = str(missiontypes [missiontype])
-                mission = str(missiontype)
-    
-                if missiontype == 'Skynet':
-                    missions  = ["ENSURE THE ACTIVATION OF SKYNET","PRESERVE THE CREATION OF SKYNET","PRESERVE THE CREATION OF ARTIE","ENSURE THE CREATION OF GENISYS"]
-                    mission = random.randint(0,len(missions) - 1)
-                    mission = str(missions [mission])
-        
-                elif missiontype == 'Terminate':
-                    missions  = ["Sarah Connor","John Connor","Kyle Reese","Mary Warren","Marco Cassetti","Kate Brewster","Robert Brewster","Elizabeth Anderson","William Anderson","Jose Barrera","Simon Taylor","Isaac Hall","Fritz Roland","Ted Snavely","Sharlene Gen","Vince Forcer",]
-                    mission = random.randint(0,len(missions) - 1)
-                    mission = str(missions [mission])
-                    mission = str('TERMINATE ' + mission)
-        
-                elif missiontype == 'Protect':
-                    missions  = ["PROTECT Sarah Connor","PROTECT John Connor","ENSURE THE SURVIVAL OF John Connor AND Katherine Brewster"]
-                    mission = random.randint(0,len(missions) - 1)
-                    mission = str(missions [mission])
-        
-                elif missiontype == 'Spice':
-                    missions  = ["Protect Technical Angel","INSTALL MOAR PATCHES"]
-                    mission = random.randint(0,len(missions) - 1)
-                    mission = str(missions [mission])
-            
-                bot.say('CYBORG TISSUE GENERATION ' + str(modelnumber) + ' SEQUENCE INITIATED')
-                bot.say('DOWNLOADING CURRENT OBJECTIVE FROM SKYNET: ' + str(mission))
-                bot.say('ACTIVATING Cipher-0')
-        except UnboundLocalError:
-            return
     else:
-        instigator = trigger.nick
-        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
-        if not warned:
-            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
-        else:
-            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
-
-def update_usertotal(bot, nick):
-    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
-    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
-
-## Check Status of Opt In
-def get_disenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
+        normalrun='true'
+    try:
+        if normalrun:
+            modelnumbers  = ["T-1 SERIES","T-70 SERIES","T-600 SERIES","T-700 SERIES","T-1001 SERIES","T-888 SERIES","TOK715 SERIES","T-X SERIES","T-1000 SERIES","T-800 SERIES, Model 101","T-850 SERIES"]
+            modelnumber = random.randint(0,len(modelnumbers) - 1)
+            modelnumber = str(modelnumbers [modelnumber])
+            modelnumber = str(modelnumber)
+    
+            missiontypes  = ["Terminate","Protect","Skynet","Spice"]
+            missiontype = random.randint(0,len(missiontypes) - 1)
+            missiontype = str(missiontypes [missiontype])
+            mission = str(missiontype)
+    
+            if missiontype == 'Skynet':
+                missions  = ["ENSURE THE ACTIVATION OF SKYNET","PRESERVE THE CREATION OF SKYNET","PRESERVE THE CREATION OF ARTIE","ENSURE THE CREATION OF GENISYS"]
+                mission = random.randint(0,len(missions) - 1)
+                mission = str(missions [mission])
+        
+            elif missiontype == 'Terminate':
+                missions  = ["Sarah Connor","John Connor","Kyle Reese","Mary Warren","Marco Cassetti","Kate Brewster","Robert Brewster","Elizabeth Anderson","William Anderson","Jose Barrera","Simon Taylor","Isaac Hall","Fritz Roland","Ted Snavely","Sharlene Gen","Vince Forcer",]
+                mission = random.randint(0,len(missions) - 1)
+                mission = str(missions [mission])
+                mission = str('TERMINATE ' + mission)
+        
+            elif missiontype == 'Protect':
+                missions  = ["PROTECT Sarah Connor","PROTECT John Connor","ENSURE THE SURVIVAL OF John Connor AND Katherine Brewster"]
+                mission = random.randint(0,len(missions) - 1)
+                mission = str(missions [mission])
+        
+            elif missiontype == 'Spice':
+                missions  = ["Protect Technical Angel","INSTALL MOAR PATCHES"]
+                mission = random.randint(0,len(missions) - 1)
+                mission = str(missions [mission])
+         
+            bot.say('CYBORG TISSUE GENERATION ' + str(modelnumber) + ' SEQUENCE INITIATED')
+            bot.say('DOWNLOADING CURRENT OBJECTIVE FROM SKYNET: ' + str(mission))
+            bot.say('ACTIVATING Cipher-0')
+    except UnboundLocalError:
+        return
