28458b7ebedb84ee88451b3a7a3a578872f20475
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-01 09:07:41 -0400

    Update Oprah.py

--- Oprah.py
+++ Oprah.py
@@ -1,33 +1,22 @@
 import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
 
 @sopel.module.rate(120)
 @sopel.module.commands('oprah')
-def trust(bot,trigger):
-    instigator = trigger.nick
-    target = trigger.nick
-    update_usertotal(bot, target)
-    targetdisenable = get_disenable(bot, target)
-    if targetdisenable:
-        if trigger.group(2):
-            item = trigger.group(2).strip()
-            if item.startswith('a') or item.startswith('e') or item.startswith('i') or item.startswith('o') or item.startswith('u'):
-                item = str('an ' + item)
-            else:
-                item = str('a ' + item)
-            bot.say("You get " + item + "! And You get " + item + "! Everyone gets "+ item + "!")
-    else:
-        instigator = trigger.nick
-        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
-        if not warned:
-            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    if trigger.group(2):
+        item = trigger.group(2).strip()
+        if item.startswith('a') or item.startswith('e') or item.startswith('i') or item.startswith('o') or item.startswith('u'):
+            item = str('an ' + item)
         else:
-            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
-
-def update_usertotal(bot, nick):
-    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
-    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
-        
-## Check Status of Opt In
-def get_disenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
+            item = str('a ' + item)
+        bot.say("You get " + item + "! And You get " + item + "! Everyone gets "+ item + "!")
