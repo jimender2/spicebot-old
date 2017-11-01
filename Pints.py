afb1f107561ef5f3a320693c93ba3d348f89bb9f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-01 09:10:50 -0400

    Update Pints.py

--- Pints.py
+++ Pints.py
@@ -1,35 +1,24 @@
 import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
 
 @sopel.module.rate(120)
 @sopel.module.commands('pints','pint')
-def pints(bot, trigger):
-    instigator = trigger.nick
-    target = trigger.nick
-    update_usertotal(bot, target)
-    targetdisenable = get_disenable(bot, target)
-    if targetdisenable:
-        if not trigger.group(2):
-            winner = "Everybody"
-        else:
-            winner = trigger.group(2).strip()
-            if trigger.group(2) == 'all':
-                winner = "Everybody"
-            elif trigger.group(2) == trigger.nick:
-                winner = "him/her-self"
-        bot.say(trigger.nick + ' buys a pint for ' + winner)
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    if not trigger.group(2):
+        winner = "Everybody"
     else:
-        instigator = trigger.nick
-        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
-        if not warned:
-            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
-        else:
-            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
-
-def update_usertotal(bot, nick):
-    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
-    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
-        
-## Check Status of Opt In
-def get_disenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
+        winner = trigger.group(2).strip()
+        if trigger.group(2) == 'all':
+            winner = "Everybody"
+        elif trigger.group(2) == trigger.nick:
+            winner = "him/her-self"
+    bot.say(trigger.nick + ' buys a pint for ' + winner)
