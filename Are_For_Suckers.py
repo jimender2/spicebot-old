0189eb5f5739c7617da1a1869435e2daf45b8eb9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-10-31 20:25:36 -0400

    Update Are_For_Suckers.py

--- Are_For_Suckers.py
+++ Are_For_Suckers.py
@@ -1,35 +1,24 @@
 import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared.py import *
 
 @sopel.module.rate(120)
 @sopel.module.commands('sucker','suckers')
-def rules(bot, trigger):
-    instigator = trigger.nick
-    target = trigger.nick
-    update_usertotal(bot, target)
-    targetdisenable = get_disenable(bot, target)
-    if targetdisenable:
-        if not trigger.group(2):
-            bot.say("Who/what are for suckers??")
-        else:
-            myline = trigger.group(2).strip()
-            if not myline.lower() == bot.nick:
-                if myline.endswith('s'):
-                    bot.say(myline + ' are for suckers!!')
-                else:
-                    bot.say(myline + ' is for suckers!!')
+def mainfunction(bot, trigger):
+    enablestatus = check_disenable(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    if not trigger.group(2):
+        bot.say("Who/what are for suckers??")
     else:
-        instigator = trigger.nick
-        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
-        if not warned:
-            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
-        else:
-            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
-
-def update_usertotal(bot, nick):
-    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
-    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
-        
-## Check Status of Opt In
-def get_disenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
+        myline = trigger.group(2).strip()
+        if not myline.lower() == bot.nick:
+            if myline.endswith('s'):
+                bot.say(myline + ' are for suckers!!')
+            else:
+                bot.say(myline + ' is for suckers!!')
