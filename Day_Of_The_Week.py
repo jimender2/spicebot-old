091e631e2a0caa904e6bcc8abebf398141dc6b5b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-01 08:32:52 -0400

    Update Day_Of_The_Week.py

--- Day_Of_The_Week.py
+++ Day_Of_The_Week.py
@@ -1,81 +1,68 @@
 import sopel.module
 import datetime
 import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
 
 @sopel.module.rate(120)
 @sopel.module.commands('monday','mondays','fuckmonday','fuckmondays','tuesday','tuesdays','fucktuesday','fucktuesdays','wednesday','wednesdays','fuckwednesday','fuckwednesdays','thursday','thursdays','fuckthursday','fuckthursdays','friday','fridays','fuckfriday','fuckfridays','saterday','saterdays','fucksaturday','fucksaturdays','sunday','sundays','fucksunday','fucksundays')
-def daybot(bot,trigger):
-    instigator = trigger.nick
-    target = trigger.nick
-    update_usertotal(bot, target)
-    targetdisenable = get_disenable(bot, target)
-    if targetdisenable:
-        whichtrig = trigger.group(1)
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
     
-        if whichtrig.endswith('s'):
-            whichtrig = whichtrig[:-1]
+def execute_main(bot, trigger):
+    whichtrig = trigger.group(1)
     
-        if whichtrig.endswith('monday'): 
-            whichtrigday = 'Monday'
-        elif whichtrig.endswith('tuesday'): 
-            whichtrigday = 'Tuesday'
-        elif whichtrig.endswith('wednesday'): 
-            whichtrigday = 'Wednesday'
-        elif whichtrig.endswith('thursday'): 
-            whichtrigday = 'Thursday'
-        elif whichtrig.endswith('friday'): 
-            whichtrigday = 'Friday'
-        elif whichtrig.endswith('saturday'): 
-            whichtrigday = 'Saturday'
-        elif whichtrig.endswith('sunday'): 
-            whichtrigday = 'Sunday'    
-        else:
-            whichtrigday = 'null'
-     
-        if whichtrig.startswith('fuck'):
-            whichtrigmood = 'salty'
-        else:
-            whichtrigmood = 'null'
+    if whichtrig.endswith('s'):
+        whichtrig = whichtrig[:-1]
     
-        whatistoday = str(datetime.datetime.today().weekday())
-        if whatistoday == '0':
-            today = "Monday"
-        elif whatistoday == '1':
-            today = "Tuesday"
-        elif whatistoday == '2':
-            today = "Wednesday"
-        elif whatistoday == '3':
-            today = "Thursday"
-        elif whatistoday == '4':
-            today = "Friday"
-        elif whatistoday == '5':
-            today = "Saturday"
-        elif whatistoday == '6':
-            today = "Sunday"
-    
-        if whichtrigmood == 'salty':
-            if whichtrigday == today:
-                bot.say(today + "s " + "really do suck!")
-            else:
-                bot.say(whichtrigday + "s " + "sometimes do suck!")
-        else:
-            if today == whichtrigday:
-                bot.say("Today is " + today + ", what about it?")
-            else:
-                bot.say(whichtrigday + ", what about it?")
+    if whichtrig.endswith('monday'): 
+        whichtrigday = 'Monday'
+    elif whichtrig.endswith('tuesday'): 
+        whichtrigday = 'Tuesday'
+    elif whichtrig.endswith('wednesday'): 
+        whichtrigday = 'Wednesday'
+    elif whichtrig.endswith('thursday'): 
+        whichtrigday = 'Thursday'
+    elif whichtrig.endswith('friday'): 
+        whichtrigday = 'Friday'
+    elif whichtrig.endswith('saturday'): 
+        whichtrigday = 'Saturday'
+    elif whichtrig.endswith('sunday'): 
+        whichtrigday = 'Sunday'    
     else:
-        instigator = trigger.nick
-        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
-        if not warned:
-            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        whichtrigday = 'null'
+     
+    if whichtrig.startswith('fuck'):
+        whichtrigmood = 'salty'
+    else:
+        whichtrigmood = 'null'
+         whatistoday = str(datetime.datetime.today().weekday())
+    if whatistoday == '0':
+        today = "Monday"
+    elif whatistoday == '1':
+        today = "Tuesday"
+    elif whatistoday == '2':
+        today = "Wednesday"
+    elif whatistoday == '3':
+        today = "Thursday"
+    elif whatistoday == '4':
+        today = "Friday"
+    elif whatistoday == '5':
+        today = "Saturday"
+    elif whatistoday == '6':
+        today = "Sunday"
+    
+    if whichtrigmood == 'salty':
+        if whichtrigday == today:
+            bot.say(today + "s " + "really do suck!")
         else:
-            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
-
-def update_usertotal(bot, nick):
-    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
-    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
-        
-## Check Status of Opt In
-def get_disenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
+            bot.say(whichtrigday + "s " + "sometimes do suck!")
+    else:
+        if today == whichtrigday:
+            bot.say("Today is " + today + ", what about it?")
+        else:
+            bot.say(whichtrigday + ", what about it?")
