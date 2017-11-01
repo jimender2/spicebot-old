dce607fa7847289430ecc614be0e1c2b53fa106e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-01 10:09:35 -0400

    Update The_Matrix.py

--- The_Matrix.py
+++ The_Matrix.py
@@ -1,40 +1,29 @@
 import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
 
 @sopel.module.rate(120)
-@sopel.module.commands('matrix') 
-def matrix(bot, trigger):
-    instigator = trigger.nick
-    target = trigger.nick
-    update_usertotal(bot, target)
-    targetdisenable = get_disenable(bot, target)
-    if targetdisenable:
-        if trigger.group(2):
-            if trigger.group(2) == 'redpill':
-                bot.say('You take the red pill, you stay in Wonderland, and I show you how deep the rabbit hole goes.')
-            elif trigger.group(2) == 'bluepill':
-                bot.say('You take the blue pill, the story ends. You wake up in your bed and believe whatever you want to believe.')
-            else:
-                normalrun='true'
+@sopel.module.commands('matrix')
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    if trigger.group(2):
+        if trigger.group(2) == 'redpill':
+            bot.say('You take the red pill, you stay in Wonderland, and I show you how deep the rabbit hole goes.')
+        elif trigger.group(2) == 'bluepill':
+            bot.say('You take the blue pill, the story ends. You wake up in your bed and believe whatever you want to believe.')
         else:
             normalrun='true'
-        try:
-            if normalrun:
-                bot.say('You have two choices. redpill Or bluepill')        
-        except UnboundLocalError:
-            return
     else:
-        instigator = trigger.nick
-        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
-        if not warned:
-            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
-        else:
-            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
-
-def update_usertotal(bot, nick):
-    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
-    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
-        
-## Check Status of Opt In
-def get_disenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
+        normalrun='true'
+    try:
+        if normalrun:
+            bot.say('You have two choices. redpill Or bluepill')        
+    except UnboundLocalError:
+        return
