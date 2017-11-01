c0935067cae8ce03534cd870da40739588bf7401
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-01 08:15:06 -0400

    Update Blame.py

--- Blame.py
+++ Blame.py
@@ -1,47 +1,29 @@
 import sopel
 from sopel import module, tools
 import random
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
 
 @sopel.module.rate(120)
 @sopel.module.commands('blame')
-@module.require_chanmsg
-def webmd(bot, trigger):
-    instigator = trigger.nick
-    target = trigger.nick
-    channel = trigger.sender
-    update_usertotal(bot, target)
-    targetdisenable = get_spicebotdisenable(bot, target)
-    if targetdisenable:
-        blametargetarray = []
-        for u in bot.channels[channel].users:
-            target = u
-            disenable = get_spicebotdisenable(bot, target)
-            if disenable:
-                if target != instigator:
-                    blametargetarray.append(target)
-        try:
-            whotoblame =random.choice(blametargetarray)
-        except IndexError:
-            whotoblame = str(instigator + "'s mom")
-        bot.say("It's " + whotoblame + "'s fault.")
-    else:
-        instigator = trigger.nick
-        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
-        if not warned:
-            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
-        else:
-            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator) 
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
     
-    
-def update_usertotal(bot, nick):
-    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
-    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
-
-## Check Status of Opt In
-def get_spicebotdisenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
-        
-            
-            
-                
+def execute_main(bot, trigger):
+    blametargetarray = []
+    for u in bot.channels[channel].users:
+        target = u
+        disenable = get_spicebotdisenable(bot, target)
+        if disenable:
+            if target != instigator:
+                blametargetarray.append(target)
+    try:
+        whotoblame =random.choice(blametargetarray)
+    except IndexError:
+        whotoblame = str(instigator + "'s mom")
+    bot.say("It's " + whotoblame + "'s fault.")
