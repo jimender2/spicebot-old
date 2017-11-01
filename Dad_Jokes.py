5fcb3bba6f184fb8f0395416021e44a4beca6fe6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-01 08:29:49 -0400

    Update Dad_Jokes.py

--- Dad_Jokes.py
+++ Dad_Jokes.py
@@ -1,38 +1,27 @@
 import sopel.module
 import requests
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
 
 @sopel.module.rate(120)
 @sopel.module.commands('dad','dadjoke')
-def sayDadJoke(bot,trigger):
-    instigator = trigger.nick
-    target = trigger.nick
-    update_usertotal(bot, target)
-    targetdisenable = get_disenable(bot, target)
-    if targetdisenable:
-        joke = getDadJoke()
-        if joke:
-            bot.say(joke)
-        else:
-            bot.say('My humor module is broken.')
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    joke = getDadJoke()
+    if joke:
+        bot.say(joke)
     else:
-        instigator = trigger.nick
-        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
-        if not warned:
-            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
-        else:
-            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+        bot.say('My humor module is broken.')
 
-def update_usertotal(bot, nick):
-    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
-    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
-        
 def getDadJoke():
     url = 'https://icanhazdadjoke.com'    
     page = requests.get(url,headers = {'Accept':'text/plain'})
     joke = page.content
     return joke
-
-## Check Status of Opt In
-def get_disenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
