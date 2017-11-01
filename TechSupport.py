12a7019bad99e7af47e65af7383b3e801f7aa9ee
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-01 13:07:58 -0400

    Update TechSupport.py

--- TechSupport.py
+++ TechSupport.py
@@ -14,6 +14,6 @@ def mainfunction(bot, trigger):
         execute_main(bot, trigger)
     
 def execute_main(bot, trigger):
-    essages = ["YOU MUST CONSTRUCT ADDITIONAL PYLONS!","Have you tried flinging feces at it?","Have you tried chewing the cable?","Did you try turning it off and on again?","Did you try licking the mouse? Double-lick?","Did your try replacing all the ones with zeros?"]
+    messages = ["YOU MUST CONSTRUCT ADDITIONAL PYLONS!","Have you tried flinging feces at it?","Have you tried chewing the cable?","Did you try turning it off and on again?","Did you try licking the mouse? Double-lick?","Did your try replacing all the ones with zeros?"]
     answer = random.randint(0,len(messages) - 1)
     bot.say(messages[answer]);
