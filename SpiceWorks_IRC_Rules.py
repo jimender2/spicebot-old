e65fe8852106dd4466af2de583ac4fe2339574c2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-01 10:01:54 -0400

    Update SpiceWorks_IRC_Rules.py

--- SpiceWorks_IRC_Rules.py
+++ SpiceWorks_IRC_Rules.py
@@ -1,58 +1,47 @@
 import sopel.module
 import urllib
 from word2number import w2n
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
 
 rulesurl = 'https://pastebin.com/raw/Vrq9bHBD'
 
 @sopel.module.rate(120)
 @sopel.module.commands('rules','rule')
-def rules(bot, trigger):
-    instigator = trigger.nick
-    target = trigger.nick
-    update_usertotal(bot, target)
-    targetdisenable = get_disenable(bot, target)
-    if targetdisenable:
-        if not trigger.group(2):
-            myline='Chat Rules:     https://pastebin.com/Vrq9bHBD'
-        else:
-            rulenumber = trigger.group(2)
-            if not rulenumber[0].isdigit():
-                rulenumber = w2n.word_to_num(str(rulenumber))
-            else:
-                rulenumber = int(rulenumber)
-        
-            htmlfile=urllib.urlopen(rulesurl)
-            lines=htmlfile.readlines()
-            try:
-                if str(rulenumber) != '0':
-                    myline=lines[rulenumber-1]
-                else:
-                    myline='Rule Zero (read the rules):     https://pastebin.com/Vrq9bHBD'
-            except IndexError or TypeError:
-                if rulenumber == 69:
-                    myline='giggles'
-                elif rulenumber == 34:
-                    myline='If it exists, there is porn of it.'
-                else:
-                    myline= 'That doesnt appear to be a rule number.'
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
     
-            if myline == 'giggles':
-                bot.action(myline)
-            else:
-                bot.say(myline)
+def execute_main(bot, trigger):
+    if not trigger.group(2):
+        myline='Chat Rules:     https://pastebin.com/Vrq9bHBD'
     else:
-        instigator = trigger.nick
-        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
-        if not warned:
-            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        rulenumber = trigger.group(2)
+        if not rulenumber[0].isdigit():
+            rulenumber = w2n.word_to_num(str(rulenumber))
         else:
-            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
+            rulenumber = int(rulenumber)
+    
+        htmlfile=urllib.urlopen(rulesurl)
+        lines=htmlfile.readlines()
+        try:
+            if str(rulenumber) != '0':
+                myline=lines[rulenumber-1]
+            else:
+                myline='Rule Zero (read the rules):     https://pastebin.com/Vrq9bHBD'
+        except IndexError or TypeError:
+            if rulenumber == 69:
+                myline='giggles'
+            elif rulenumber == 34:
+                myline='If it exists, there is porn of it.'
+            else:
+                myline= 'That doesnt appear to be a rule number.'
 
-def update_usertotal(bot, nick):
-    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
-    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
-        
-## Check Status of Opt In
-def get_disenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
+        if myline == 'giggles':
+            bot.action(myline)
+        else:
+            bot.say(myline)
