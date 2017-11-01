b389e53817ed66156ca416eacae19a35c4468f92
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-01 08:56:55 -0400

    Update Is_It_Friday_Yet.py

--- Is_It_Friday_Yet.py
+++ Is_It_Friday_Yet.py
@@ -1,40 +1,28 @@
 import sopel.module
 import datetime
 import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
 
 @sopel.module.rate(120)
 @sopel.module.commands('isitfriday','isitfridayyet')
-def fridaybot(bot,trigger):
-    instigator = trigger.nick
-    target = trigger.nick
-    update_usertotal(bot, target)
-    targetdisenable = get_disenable(bot, target)
-    if targetdisenable:
-        whatistoday = str(datetime.datetime.today().weekday())        
-        fridaynumber = '4'
-        if whatistoday == fridaynumber:
-            bot.say("TGIF! It's finally here!!!")
-        elif whatistoday == '5' or whatistoday == '6':
-            bot.say("It's the Weekend. It should be better than Friday!")
-        else:
-            daysmath = int(fridaynumber) - int(whatistoday)
-            if daysmath == int('1'):
-                bot.say("Unfortunately Friday is " + str(daysmath) + " day away. I'm sure we'll make it there!")
-            else:
-                bot.say("Unfortunately Friday is " + str(daysmath) + " days away. I'm sure we'll make it there!")
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    whatistoday = str(datetime.datetime.today().weekday())        
+    fridaynumber = '4'
+    if whatistoday == fridaynumber:
+        bot.say("TGIF! It's finally here!!!")
+    elif whatistoday == '5' or whatistoday == '6':
+        bot.say("It's the Weekend. It should be better than Friday!")
     else:
-        instigator = trigger.nick
-        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
-        if not warned:
-            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
+        daysmath = int(fridaynumber) - int(whatistoday)
+        if daysmath == int('1'):
+            bot.say("Unfortunately Friday is " + str(daysmath) + " day away. I'm sure we'll make it there!")
         else:
-            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
-
-def update_usertotal(bot, nick):
-    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
-    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
-        
-## Check Status of Opt In
-def get_disenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
+            bot.say("Unfortunately Friday is " + str(daysmath) + " days away. I'm sure we'll make it there!")
