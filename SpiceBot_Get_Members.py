b91d8028503982e7f633f04585f11de735e90ee1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-01 09:52:58 -0400

    Update SpiceBot_Get_Members.py

--- SpiceBot_Get_Members.py
+++ SpiceBot_Get_Members.py
@@ -2,37 +2,25 @@ import sopel.module
 import sqlite3
 from sopel.tools.target import User, Channel
 from random import randint
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
 
+@sopel.module.rate(120)
 @sopel.module.require_admin
 @sopel.module.commands('getmembers')
-def getMembers(bot,trigger):
-    instigator = trigger.nick
-    target = trigger.nick
-    update_usertotal(bot, target)
-    targetdisenable = get_disenable(bot, target)
-    if targetdisenable:
-        #users = str(bot.channels[trigger.sender].users)
-        #bot.say(users)
-        userlist = []
-        for u in bot.channels[trigger.sender].users:
-            bot.say(u)
-            userlist.append(u)
-        randno = randint(0,len(userlist))
-        randUser = userlist[randno]
-        bot.say('Here is a randomly picked user: ' + randUser)
-    else:
-        instigator = trigger.nick
-        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
-        if not warned:
-            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
-        else:
-            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
-
-def update_usertotal(bot, nick):
-    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
-    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
-        
-## Check Status of Opt In
-def get_disenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    userlist = []
+    for u in bot.channels[trigger.sender].users:
+        bot.say(u)
+        userlist.append(u)
+    randno = randint(0,len(userlist))
+    randUser = userlist[randno]
+    bot.say('Here is a randomly picked user: ' + randUser)
