58e3abc3e9c35443a791ec2795ef58ac68d5683c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-01 08:13:14 -0400

    Update Blackjack_And_Hookers.py

--- Blackjack_And_Hookers.py
+++ Blackjack_And_Hookers.py
@@ -1,29 +1,18 @@
 import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
 
 @sopel.module.rate(120)
 @sopel.module.commands('myown')
-def bender(bot,trigger):
-    instigator = trigger.nick
-    target = trigger.nick
-    update_usertotal(bot, target)
-    targetdisenable = get_disenable(bot, target)
-    if targetdisenable:
-        if trigger.group(2):
-            if not trigger.group(2) == bot.nick:
-                bot.say("Fine! I'll start my own " + trigger.group(2) + ", with blackjack and hookers!")
-    else:
-        instigator = trigger.nick
-        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
-        if not warned:
-            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
-        else:
-            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
-
-def update_usertotal(bot, nick):
-    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
-    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
-        
-## Check Status of Opt In
-def get_disenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    if trigger.group(2):
+        if not trigger.group(2) == bot.nick:
+            bot.say("Fine! I'll start my own " + trigger.group(2) + ", with blackjack and hookers!")
