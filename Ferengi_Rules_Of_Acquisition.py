5179e06c8564326a8a0d8c66eb510b451b07a2d1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-01 08:40:39 -0400

    Update Ferengi_Rules_Of_Acquisition.py

--- Ferengi_Rules_Of_Acquisition.py
+++ Ferengi_Rules_Of_Acquisition.py
@@ -1,42 +1,36 @@
 import sopel.module
 import random
 import urllib
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
 
 fra='https://raw.githubusercontent.com/deathbybandaid/sopel-modules/dev/otherfiles/ferengi_rules.txt'
 
 @sopel.module.rate(120)
 @sopel.module.commands('ferengi')
-def ferengi(bot, trigger):
-    instigator = trigger.nick
-    target = trigger.nick
-    update_usertotal(bot, target)
-    targetdisenable = get_disenable(bot, target)
-    if targetdisenable:
-        if not trigger.group(2):
-            myline = randomfra()
-        else:
-            rulenumber = int(trigger.group(2))
-            htmlfile=urllib.urlopen(fra)
-            lines=htmlfile.readlines()
-            try:
-                myline = str(lines[rulenumber-1])
-            except IndexError:
-                myline = 'That doesnt appear to be a rule number.'
-            if not myline or myline == '\n':
-                myline = 'There is no cannonized rule tied to this number.'
-        bot.say(myline)
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    if not trigger.group(2):
+        myline = randomfra()
     else:
-        instigator = trigger.nick
-        warned = bot.db.get_nick_value(target, 'spicebothour_warn') or 0
-        if not warned:
-            bot.notice(target + ", you have to run .spiceboton to allow her to listen to you.", instigator)
-        else:
-            bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
-
-def update_usertotal(bot, nick):
-    usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
-    bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
-        
+        rulenumber = int(trigger.group(2))
+        htmlfile=urllib.urlopen(fra)
+        lines=htmlfile.readlines()
+        try:
+            myline = str(lines[rulenumber-1])
+        except IndexError:
+            myline = 'That doesnt appear to be a rule number.'
+        if not myline or myline == '\n':
+            myline = 'There is no cannonized rule tied to this number.'
+    bot.say(myline)
+       
 # random rule
 def randomfra():
     htmlfile=urllib.urlopen(fra)
@@ -45,8 +39,3 @@ def randomfra():
     if not myline or myline == '\n':
         myline = randomfra()
     return myline
-
-## Check Status of Opt In
-def get_disenable(bot, nick):
-    disenable = bot.db.get_nick_value(nick, 'spicebot_disenable') or 0
-    return disenable
