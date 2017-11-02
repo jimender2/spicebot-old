a9346ad9c35474c8c4794e41b56c36dc7e6b5357
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-02 19:11:50 -0400

    Update WebMD.py

--- WebMD.py
+++ WebMD.py
@@ -8,7 +8,6 @@ moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
 
-@sopel.module.rate(120)
 @sopel.module.commands('webmd','webmdadd','webmddel')
 def mainfunction(bot, trigger):
     enablestatus = spicebot_prerun(bot, trigger)
