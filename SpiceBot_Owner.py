e5ce3fee10cb9b243c8208cbbc4226001a442767
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-02 19:38:49 -0400

    Update SpiceBot_Owner.py

--- SpiceBot_Owner.py
+++ SpiceBot_Owner.py
@@ -5,7 +5,6 @@ moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
 
-@sopel.module.rate(120)
 @sopel.module.commands('owner','botowner')
 def mainfunction(bot, trigger):
     enablestatus = spicebot_prerun(bot, trigger)
