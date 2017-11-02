5c107f3f4fef7204b9fa144e4f4b4b05376a3b9e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-02 16:04:27 -0400

    Update SpicebotShared.py

--- SpicebotShared.py
+++ SpicebotShared.py
@@ -3,18 +3,22 @@ import sopel.module
 import time
 
 JOINTIMEOUT = 60
+LASTTIMEOUT = 60
 
 ## Main Check
 def spicebot_prerun(bot,trigger):
     instigator = trigger.nick
     target = trigger.nick
-    update_usertotal(bot, target)
     targetdisenable = get_disenable(bot, target)
     if targetdisenable:
         jointime = get_jointime(bot, target)
+        lastjointime = get_lasttime(bot, target)
         if jointime < JOINTIMEOUT:
             enablestatus = 1
             bot.notice(target + ", you need to wait a minute after joining the channel to use Spicebot.", instigator)
+        elif lasttime < LASTTIMEOUT:
+            enablestatus = 1
+            bot.notice(target + ", you need to wait a minute after you use Spicebot.", instigator)
         else:
             enablestatus = 0
     else:
@@ -25,12 +29,15 @@ def spicebot_prerun(bot,trigger):
         else:
             bot.notice(target + ", it looks like your access to spicebot has been disabled for a while. Check out ##SpiceBotTest.", instigator)
         enablestatus = 1
+    update_usernick(bot, target)
     return enablestatus
 
-## Update Number
-def update_usertotal(bot, nick):
+
+def update_usernick(bot, nick):
     usertotal = bot.db.get_nick_value(nick, 'spicebot_usertotal') or 0
     bot.db.set_nick_value(nick, 'spicebot_usertotal', usertotal + 1)
+    now = time.time()
+    bot.db.set_nick_value(nick, 'spicebotlast_time', now)
     
 ## Check Status of Opt In
 def get_disenable(bot, nick):
@@ -39,5 +46,5 @@ def get_disenable(bot, nick):
 
 def get_jointime(bot, nick):
     now = time.time()
-    last = bot.db.get_nick_value(nick, 'spicebotjoin_time') or 0
+    last = bot.db.get_nick_value(nick, 'spicebotlast_time') or 0
     return abs(now - last)
