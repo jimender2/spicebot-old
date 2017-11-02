27a86ce77e30d17edb4abd72e7100f8cfc2c3261
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-02 19:33:42 -0400

    Update Points.py

--- Points.py
+++ Points.py
@@ -8,7 +8,6 @@ moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
 
-@sopel.module.rate(120)
 @sopel.module.commands('points','takepoints','pants','takepants','minuspants','minuspoints','checkpoints','checkpants')
 def mainfunction(bot, trigger):
     enablestatus = spicebot_prerun(bot, trigger)
