824b45ca013e7fb5a84f7e609bb87ad26447ba8a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-02 10:56:03 -0400

    Update Yo-Dawg.py

--- Yo-Dawg.py
+++ Yo-Dawg.py
@@ -14,5 +14,5 @@ def mainfunction(bot, trigger):
     
 def execute_main(bot, trigger):
     if trigger.group(2):
-        statement = str("Yo Dawg! I heard you liked " + trigger.group(2) + ", So I put a " + trigger.group(2) + " in/on your" + trigger.group(2) + "!!!")
+        statement = str("Yo Dawg! I heard you liked " + trigger.group(2) + ", So I put a " + trigger.group(2) + " in/on your " + trigger.group(2) + "!!!")
         bot.say(statement)
