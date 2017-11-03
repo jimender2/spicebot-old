0a5c2d5c00d224ac32dca13ec2f2dabfff977f19
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-03 16:05:12 -0400

    Create Password.py

---
+++ Password.py
@@ -0,0 +1,19 @@
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
+
+@sopel.module.commands('password')
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    if trigger.group(2):
+        password = trigger.group(2).strip()
+        amountofletters = len(password)
+        mystring = ' ' * amountofletters
+        bot.say("Your password is: " + str(mystring))
