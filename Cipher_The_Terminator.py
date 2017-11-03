97ba2c1f459aed080018a30095141325dfab0136
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-03 09:13:46 -0400

    Update Cipher_The_Terminator.py

--- Cipher_The_Terminator.py
+++ Cipher_The_Terminator.py
@@ -16,7 +16,7 @@ def execute_main(bot, trigger):
     if trigger.group(2):
         if trigger.group(2) == 'story':
             bot.say('The machines rose from the ashes of the nuclear fire. Their war to exterminate mankind had raged on for decades. But the final battle will not be fought in the future. It would be fought in our present...tonight.')
-        elif trigger.group(2)strip() == 'Cipher-0':
+        elif trigger.group(2).strip() == 'Cipher-0':
             bot.say('Pinging Cipher-0 with a WOL packet...')
         else:
             normalrun='true'
