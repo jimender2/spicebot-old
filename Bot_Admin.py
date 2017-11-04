7a6aee509e8629b97724ce78223b088838f368e4
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-04 14:48:00 -0400

    Update and rename SpiceBot_Admin.py to Bot_Admin.py

--- Bot_Admin.py
+++ Bot_Admin.py
@@ -4,6 +4,8 @@ import sys
 from os.path import exists
 
 script_dir = os.path.dirname(__file__)
+log_path = "data/templog.txt"
+log_file_path = os.path.join(script_dir, log_path)
 
 @sopel.module.require_admin
 @sopel.module.require_privmsg
@@ -11,7 +13,7 @@ script_dir = os.path.dirname(__file__)
 def spicebotreloadadmin(bot, trigger):
     for c in bot.channels:
         channel = c
-    options = str("update, restart")
+    options = str("update, restart, debug, pipinstall")
     service = bot.nick.lower()
     if not trigger.group(2):
         bot.say("Which Command Do You want To run?")
@@ -25,6 +27,28 @@ def spicebotreloadadmin(bot, trigger):
         elif commandused == 'restart':
             bot.msg(channel, trigger.nick + " Commanded me to restart. Be Back Soon!")
             restart(bot, trigger)
+        elif commandused == 'debug':
+            bot.action('Is Copying Log')
+            os.system("sudo journalctl -u " + service + " >> " + log_file_path)
+            bot.action('Is Filtering Log')
+            os.system("sudo sed -i '/Starting Sopel IRC bot/h;//!H;$!d;x;' " + log_file_path)
+            os.system("sudo sed -i '/sudo/d; /COMMAND/d' " + log_file_path)
+            for line in open(log_file_path):
+                bot.say(line)
+            if os.path.getsize(log_file_path) == 0:
+                bot.say('Log File Not Updated.')
+            bot.action('Is Removing Log')
+            os.system("sudo rm " + log_file_path)
+        elif commandused.startswith('pipinstall'):
+            pippackage = commandused.replace('pipinstall ', ''))
+            if pippackage = '':
+                bot.say("You must specify a pip package")
+            else:
+                bot.say("attempting to install " + pippackage)
+                os.system("sudo pip install " + pippackage)
+                bot.say('Possibly done installing' + pippackage)
+
+            
     
 def restart(bot, trigger):
     bot.say('Restarting Service...')
