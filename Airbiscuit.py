41300e59e0b207af821a75ac12d7c94e46c6556d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-04 13:13:10 -0400

    Update Airbiscuit.py

--- Airbiscuit.py
+++ Airbiscuit.py
@@ -5,11 +5,12 @@ moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
 
-@sopel.module.commands('airbiscuit','float','floats')
+@sopel.module.commands('airbiscuit','float')
 def mainfunction(bot, trigger):
     enablestatus = spicebot_prerun(bot, trigger)
     if not enablestatus:
         execute_main(bot, trigger)
     
 def execute_main(bot, trigger):
-    bot.say(trigger.nick + " floats an air biscuit.")
+    if not trigger.group(2):
+        bot.say(trigger.nick + " floats an air biscuit.")
