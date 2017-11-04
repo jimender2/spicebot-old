6ec0e59e26965c8f7a5707e56565f0b2fd4c4e30
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-04 15:02:33 -0400

    Update Spicebot_Admin.py

--- Spicebot_Admin.py
+++ Spicebot_Admin.py
@@ -24,10 +24,10 @@ def spicebotreloadadmin(bot, trigger):
         if commandused == 'update':
             bot.msg(channel, trigger.nick + " commanded me to update from Github and restart. Be Back Soon!")
             update(bot, trigger)
-            restart(bot, trigger)
+            restart(bot, trigger, service)
         elif commandused == 'restart':
             bot.msg(channel, trigger.nick + " Commanded me to restart. Be Back Soon!")
-            restart(bot, trigger)
+            restart(bot, trigger, service)
         elif commandused == 'debug':
             bot.action('Is Copying Log')
             os.system("sudo journalctl -u " + service + " >> " + log_file_path)
@@ -50,9 +50,8 @@ def spicebotreloadadmin(bot, trigger):
             #    os.system("sudo pip install " + pippackage)
             #    bot.say('Possibly done installing' + pippackage)
 
-            
-    
-def restart(bot, trigger):
+
+def restart(bot, trigger, service):
     bot.say('Restarting Service...')
     os.system("sudo service " + str(service) + " restart")
     bot.say('If you see this, the service is hanging. Run Command Again.')
