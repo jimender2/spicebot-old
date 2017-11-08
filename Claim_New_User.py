7302402f3ef74814be2a6a0a12154dc5059f8749
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-08 13:05:37 -0500

    Update Claim_New_User.py

--- Claim_New_User.py
+++ Claim_New_User.py
@@ -24,7 +24,7 @@ def execute_main(bot, trigger):
         bot.say("You can't claim yourself!")
     elif target.lower() not in bot.privileges[channel.lower()] or target == "new user":
             bot.say("I'm not sure who that is.")
-    if not target == bot.nick and not target == instigator:
+    elif not target == bot.nick and not target == instigator:
         if trigger.nick == 'IT_Sean':
             message = str(instigator + ' releases the contents of his bladder on ' + target + '! All should recognize this profound claim of ownership upon ' + claimed +'!')
         else:
