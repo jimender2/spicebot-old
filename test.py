33fa3b002a335362ecf1a8b74c6826c2265ac216
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-09 08:23:20 -0500

    Update test.py

--- test.py
+++ test.py
@@ -9,5 +9,13 @@ from SpicebotShared import *
 
 @sopel.module.commands('test')
 def mainfunction(bot, trigger):
-    topic = trigger.args[1]
-    bot.say(str(topic))
+    args = trigger.args[1]
+    one = trigger.group(1)
+    two = trigger.group(2)
+    three = trigger.group(3)
+    four = trigger.group(4)
+    bot.say(str(args))
+    bot.say(str(one))
+    bot.say(str(two))
+    bot.say(str(three))
+    bot.say(str(four))
