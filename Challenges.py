e961d8abd47155cdbda298eef078d1ea303338e3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-10 08:50:49 -0500

    Update Challenges.py

--- Challenges.py
+++ Challenges.py
@@ -179,28 +179,17 @@ def mainfunction(bot, trigger):
             currentleader = ''
             currentleadernumber = 0
             for u in bot.channels[channel].users:
-                bot.say('current leader is ' + str(currentleader) + " with score of " + str(currentleadernumber))
                 target = u
-                bot.say('scanning ' + target)
                 disenable = get_disenable(bot, target)
                 if disenable:
-                    bot.say('enabled')
                     wins = get_wins(bot, target)
                     if wins:
-                        bot.say('has wins ' + str(wins))
                         losses = get_losses(bot, target)
-                        bot.say('losses ' + str(losses))
                         winlosstotal = abs(wins + losses)
-                        bot.say('total ' + str(winlosstotal))
                         winlossratio = float(wins)/winlosstotal
-                        bot.say('winlossratio is ' + str(winlossratio))
                         if winlossratio > currentleadernumber:
-                            bot.say("greater")
                             currentleader = target
                             currentleadernumber = winlossratio
-                        else:
-                            bot.say("not greater")
-                bot.say("   ")
             leaderboardscript = str("The Current Leader in the room is: " + str(currentleader) + " with a ratio of: " + str(currentleadernumber))
             bot.say(leaderboardscript)
             
