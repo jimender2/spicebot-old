1d9bc8e4af927ef28e1aff42b82a4bea520f158c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-15 16:26:08 -0500

    Update Challenges.py

--- Challenges.py
+++ Challenges.py
@@ -72,7 +72,7 @@ def mainfunction(bot, trigger):
                 disenable = get_database_value(bot, target, 'disenable')
                 opttime = get_database_value(bot, target, 'opttime')
                 opttime = abs(now - opttime)
-                if opttime < OPTTIMEOUT:# and not bot.nick.endswith('dev') and not trigger.admin:
+                if disenable and opttime < OPTTIMEOUT:# and not bot.nick.endswith('dev') and not trigger.admin:
                     bot.notice(target + " can't enable/disable challenges for %d seconds." % (OPTTIMEOUT - opttime), instigator)
                 elif not disenable:
                     if commandused == 'on':
