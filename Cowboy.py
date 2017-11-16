f8118a9f8b10d1db30f3992e92c0737a60482fdc
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-16 18:31:54 -0500

    Create Cowboy.py

---
+++ Cowboy.py
@@ -0,0 +1,15 @@
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
+
+@sopel.module.commands('cowboy')
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    bot.say("yeeeee-HAW!")
