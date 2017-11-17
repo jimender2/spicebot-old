9d9cfc2adee2e3daa3a1127c1df706f5efcf775f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-17 10:51:30 -0500

    Update SpiceWorks_Webby.py

--- SpiceWorks_Webby.py
+++ SpiceWorks_Webby.py
@@ -29,7 +29,7 @@ def execute_main(bot, trigger):
         webbytitle = getwebbytitle()
         webbylink = getwebbylink()
         bot.say(webbytimeuntil + '     Title: ' + webbytitle + '     Link: ' + webbylink)
-        if not webbybonus or str(webbybonus) != '[]' or str(webbybonus) != '' or str(webbybonus) != ' ' or webbybonus != 0:
+        if webbybonus != '[]' or webbybonus != '' or webbybonus != ' ':
             bot.say('BONUS: ' + webbybonus)
 
 @sopel.module.interval(60)
@@ -45,7 +45,7 @@ def webbyauto(bot):
                     webbytitle = getwebbytitle()
                     webbylink = getwebbylink()
                     bot.msg(channel, '[15 Minute Webby Reminder]     Title: ' + str(webbytitle) + '     Link: ' + str(webbylink))
-                    if not webbybonus or str(webbybonus) != '[]' or str(webbybonus) != '' or str(webbybonus) != ' ' or webbybonus != 0:
+                    if webbybonus != '[]' or webbybonus != '' or webbybonus != ' ':
                         bot.msg(channel, str(webbybonus))
 
 def getwebbytime():
@@ -81,7 +81,7 @@ def getwebbybonus():
         for r in (("\\r", ""), ("\\n", ""), ("']",""), ("]",""), ('"',''), (" '","")):
             webbybonus = webbybonus.replace(*r)
     except IndexError:
-        webbybonus = 0
+        webbybonus = ''
     return webbybonus
 
 def getwebbytimeuntil():
