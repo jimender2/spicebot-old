c1d95db47197135977e725dd2b7cb28c794f0e68
josh-cunning <joshbcunningham@gmail.com>
2017-11-17 08:46:45 -0800

    Create dj.py
    Will serve as a DJ to generate a random song title and artist.

---
+++ dj.py
@@ -0,0 +1,28 @@
+import sopel.module
+import requests
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
+
+@sopel.module.commands('dj','jukebox','song')
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    song = getsong()
+    if joke:
+        bot.say(joke)
+    else:
+        bot.say('My humor module is broken.')
+
+def getsong():
+	#url replace with a song API
+    #url = 'https://icanhazdadjoke.com'    
+    #page = requests.get(url,headers = {'Accept':'text/plain'}) 
+    #song = page.content
+	song='Party all the Time by Eddie Murphy '
+    return song
