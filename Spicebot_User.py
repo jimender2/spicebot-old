8badb004c7eeaeff19b4eb591f6e9dd233f67ddc
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-20 11:57:54 -0500

    Update Spicebot_User.py

--- Spicebot_User.py
+++ Spicebot_User.py
@@ -52,7 +52,7 @@ def execute_main(bot, trigger):
     voicearray = []
     adminsarray = []
     for u in bot.channels[channel.lower()].users:
-        if u != bot.nick and u != "PM_":
+        if u != bot.nick:
             nametarget = str(u)
             if nametarget.lower() in bot.config.core.owner.lower():
                 botownerarray.append(nametarget)
