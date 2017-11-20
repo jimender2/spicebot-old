d9fe086b0d04c9f52a87843dcb2702116f1a9e4a
josh-cunning <joshbcunningham@gmail.com>
2017-11-20 13:48:25 -0800

    Update Dj.py

--- Dj.py
+++ Dj.py
@@ -12,31 +12,31 @@ friday='https://raw.githubusercontent.com/deathbybandaid/sopel-modules/master/ot
 
 @sopel.module.commands('dj')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
-    if not enablestatus:
-        execute_main(bot, trigger)
+	enablestatus = spicebot_prerun(bot, trigger)
+	if not enablestatus:
+		execute_main(bot, trigger)
     
 def execute_main(bot, trigger):
-    if not trigger.group(2):
-       playlist=party
-    else:
-	query = trigger.group(2).replace(' ','20%')
-	query = str(query)
-	playlist = getplaylist(query)
+	if not trigger.group(2):
+		playlist=party
+	else:
+		query = trigger.group(2).replace(' ','20%')
+		query = str(query)
+		playlist = getplaylist(query)
 		
-    song = getsong(playlist)
-    if song:
-       bot.say(trigger.nick + ' puts a nickel in the jukebox and it start to playing ' + song)
-    else:
-	bot.say('The jukebox starts playing ' + 'Never Gonna Give You Up')
+	song = getsong(playlist)
+	if song:
+		bot.say(trigger.nick + ' puts a nickel in the jukebox and it start to playing ' + song)
+	else:
+		bot.say('The jukebox starts playing ' + 'Never Gonna Give You Up')
 
 def getsong(playlist):
-    htmlfile=urllib.urlopen(playlist)
-    lines=htmlfile.read().splitlines()
-    mysong=random.choice(lines)
-    if not mysong or mysong == '\n':
-	mysong = getsong()
-    return mysong	
+	htmlfile=urllib.urlopen(playlist)
+    	lines=htmlfile.read().splitlines()
+    	mysong=random.choice(lines)
+    	if not mysong or mysong == '\n':
+		mysong = getsong()
+    	return mysong	
 	
 def getplaylist(query):
 	if query == 'party':
@@ -45,4 +45,4 @@ def getplaylist(query):
 		myplaylist = friday
 	else:
 		myplaylist = party
-    return myplaylist	
+	return myplaylist	
