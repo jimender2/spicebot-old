8704437cc061ee0eddbe41365e4b2bd567d81ef2
josh-cunning <joshbcunningham@gmail.com>
2017-11-21 10:49:39 -0800

    Update xkcd.py

--- xkcd.py
+++ xkcd.py
@@ -22,7 +22,7 @@ def execute_main(bot, trigger):
   bot.say('https://xkcd.com/' + mynumber)
    
 def getnumber():
-  thenumber = random.range(0,maxcomics,2)
+  thenumber = random.randint(0,maxcomics)
   if not thenumber or thenumber == '\n':
     thenumber=getnumber()
   return thenumber
