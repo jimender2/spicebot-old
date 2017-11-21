fc17227e4a1a2c14e174584ef6b1859e6d724aad
josh-cunning <joshbcunningham@gmail.com>
2017-11-21 10:25:48 -0800

    Update xkcd.py

--- xkcd.py
+++ xkcd.py
@@ -18,7 +18,7 @@ def execute_main(bot, trigger):
     mynumber =  getnumber()
   else:
     mynumber = int(trigger.group(2))
-  if not mynumber>=1 and <=maxcomics:
+  if not 1 >= mynumber <= maxcomics:
     mynumber=getnumber()
   bot.say('https://xkcd.com/' + mynumber)
    
