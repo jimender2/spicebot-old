0a46359e1b2214331f417855e70045209290221f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-22 13:03:23 -0500

    Create Whatitis.py

---
+++ Whatitis.py
@@ -0,0 +1,16 @@
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
+
+@sopel.module.commands('whatitis')
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    if not trigger.group(2):
+        bot.say("What it do?")
