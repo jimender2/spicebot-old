89c923dfe703b77977184097f488f1f3e71f7b51
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-22 13:57:24 -0500

    Update botname.py

--- botname.py
+++ botname.py
@@ -1,13 +1,12 @@
+import sopel
 import sopel.module
 import sys
 import os
 moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
-from sopel import bot
 
-bot = bot
-botname = bot.nick
+botname = sopel.bot.Sopel
 
 @sopel.module.commands('botname')
 def mainfunction(bot, trigger):
