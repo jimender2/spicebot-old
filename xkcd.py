80af129234e7f4fee8ce076b511774cfcafbbaa1
josh-cunning <joshbcunningham@gmail.com>
2017-11-22 09:49:49 -0800

    Update xkcd.py

--- xkcd.py
+++ xkcd.py
@@ -2,9 +2,11 @@ import sopel.module
 import random
 import sys
 import os
-from SpicebotShared import *
+import html2text
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
 
-maxcomics=1917
+from SpicebotShared import *
 
 @sopel.module.commands('xkcd','comic')
 def mainfunction(bot, trigger):
@@ -13,13 +15,27 @@ def mainfunction(bot, trigger):
     execute_main(bot, trigger)
     
 def execute_main(bot, trigger):
+  maxcomics=getmaxnumber()
+  if not int(maxcomics)<=1:
+    maxcomics = 1918
   if not trigger.group(2):
     mynumber =  getnumber()
   else:
-    mynumber = int(trigger.group(2))
-    if not  mynumber<= int(maxcomics) and mynumber>=1:
-      mynumber=getnumber()
+    mynumber = trigger.group(2)
+  if mynumber = str('today'):
+    mynumber=maxcomics	
+  if not  mynumber<= int(maxcomics) and mynumber>=1:
+    mynumber= getnumber()      
+  else:
+    mynumber = getnumber()
+		
   bot.say('https://xkcd.com/' + str(mynumber))
+   
+def getmaxnumber():
+  url = 'http://xkcd.com/{}/info.0.json'.format(number)
+  data = requests.get(url, verify=verify_ssl).json()
+  data['url'] = 'http://xkcd.com/' + str(data['num'])
+	return data
    
 def getnumber():
   thenumber = random.randint(0,int(maxcomics))
