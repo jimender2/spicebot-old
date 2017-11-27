eb15c0df62d10b642100843d9f7cd21a2caf01eb
Percussive-Maintenance <32780296+Percussive-Maintenance@users.noreply.github.com>
2017-11-27 09:04:32 -0600

    Create Gold_Star.py

---
+++ Gold_Star.py
@@ -0,0 +1,17 @@
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
+
+@sopel.module.commands('goldstar')
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    if trigger.group(2):
+        if not trigger.group(2).strip() == bot.nick:
+            bot.say(trigger.nick + " gives " + trigger.group(2).strip() + " a gold star for participation.")
