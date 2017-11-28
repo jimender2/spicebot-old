7dc4ac352b967866d280683b514fb0ae982dc07d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-28 13:09:49 -0500

    Update Challenges.py

--- Challenges.py
+++ Challenges.py
@@ -85,7 +85,7 @@ def mainfunction(bot, trigger):
             bot.notice(instigator + "This is an admin only function.", instigator)
         elif not targetdisenable and target != instigator and commandused != 'on' and commandused != 'off' and target not in nontargetarray and commandused != 'random' and commandused != 'everyone' and commandused != 'statsadmin' and target != 'random':
             bot.notice(instigator + ", It looks like " + target + " has duels off.", instigator)
-        elif commandused not in inchannelarray and not inchannel.startswith("#"):
+        elif commandused in inchannelarray and not inchannel.startswith("#"):
             bot.notice(instigator + " Duels must be in channel.", instigator)
         elif target == bot.nick and not trigger.admin:
             bot.notice(instigator + " I cannot do that.", instigator)
