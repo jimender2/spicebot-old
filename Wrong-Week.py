86ac4d90d8997783fec48d4a12526e53413e7dc2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-11-28 11:12:48 -0500

    Create Wrong-Week.py

---
+++ Wrong-Week.py
@@ -0,0 +1,16 @@
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
+
+@sopel.module.commands('wrongweek')
+def mainfunction(bot, trigger):
+    enablestatus = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    if not trigger.group(2):
+        bot.say("Looks like " + trigger.nick + " picked the wrong week to stop sniffing glue?")
