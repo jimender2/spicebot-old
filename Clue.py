03a5c59631ee9bce1b7268f9c1da7bb0ac6c2f78
Percussive-Maintenance <32780296+Percussive-Maintenance@users.noreply.github.com>
2017-11-29 14:00:02 -0600

    Update Clue.py

--- Clue.py
+++ Clue.py
@@ -32,6 +32,6 @@ def execute_main(bot, trigger):
         bot.say(players[0] + " killed " + players[1] + " in the " + rooms[0] + " with the " + weapons[0] + ".")
     if trigger.group(2) and trigger.group(2) == players[0]:
             bot.say('You guessed the killer correctly!')
-            bot.say('.points ' + trigger.nick)
+            pointstask(bot, channel, 'Clue', trigger.nick, ' gives ', ' to ', 'up', 'points', trigger.sender)
     #if trigger.nick == players[0]:
     #        bot.say('YOU ARE THE KILLER!')
