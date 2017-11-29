18e7eae0962de6b8b3bc649507ddc6130e1ee376
Percussive-Maintenance <32780296+Percussive-Maintenance@users.noreply.github.com>
2017-11-29 14:33:23 -0600

    Update Clue.py

--- Clue.py
+++ Clue.py
@@ -30,7 +30,10 @@ def execute_main(bot, trigger):
         bot.say(players[1] + " evaded " + players[0] + " by using the secret passage. So " + players[0] + " killed " + players[2] + " with the " + weapons[0] + " instead.")    
     else:
         bot.say(players[0] + " killed " + players[1] + " in the " + rooms[0] + " with the " + weapons[0] + ".")
+    import Points
     if trigger.group(2) and trigger.group(2) == players[0]:
-            import Points
-            bot.say('You guessed the killer correctly!')
-            Points.pointstask(bot, channel, 'SpiceBot', trigger.nick, ' gives ', ' to', 'up', 'points', trigger.sender)
+        bot.say('You guessed the killer correctly!')
+        Points.pointstask(bot, channel, 'SpiceBot', trigger.nick, ' gives ', ' to', 'up', 'points', trigger.sender)
+    if players[0] == trigger.nick:
+        bot.say('You were the killer.')
+        Points.pointstask(bot, channel, 'SpiceBot', trigger.nick, ' takes ', ' from', 'down', 'points', trigger.sender)
