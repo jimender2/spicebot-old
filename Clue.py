8290f525130f4b7d9eaf6d56b8af1f940b50a16e
Percussive-Maintenance <32780296+Percussive-Maintenance@users.noreply.github.com>
2017-11-29 13:37:38 -0600

    Update Clue.py

--- Clue.py
+++ Clue.py
@@ -31,7 +31,7 @@ def execute_main(bot, trigger):
     else:
         bot.say(players[0] + " killed " + players[1] + " in the " + rooms[0] + " with the " + weapons[0] + ".")
         
-    if trigger.group(2).strip() == players[0]:
+    if trigger.group(2) == players[0]:
             bot.say('You guessed the killer correctly!')
     if trigger.nick == players[0]:
             bot.say('YOU ARE THE KILLER!')
