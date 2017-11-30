b0a564bd54edc9bcb83ac16d79a18d2cf568bf21
josh-cunning <joshbcunningham@gmail.com>
2017-11-29 16:05:47 -0800

    Update xkcd.py

--- xkcd.py
+++ xkcd.py
@@ -53,7 +53,7 @@ def execute_main(bot, trigger):
 				mynumber = getnumber(maxcomics)							
 	  		else:
 				bot.say('Let me find a' + data)
-				bot.say('http://www.google.com/search?q=' + data + " site:xkcd.com' + '&btnI')
+				bot.say('http://www.google.com/search?q=' + data + ' site:xkcd.com' + '&btnI')
 				mynumber = 1
 	if not mynumber<= int(maxcomics) and mynumber>=1:
 		bot.say('Please enter a number between 1 and ' +str(maxcomics))
