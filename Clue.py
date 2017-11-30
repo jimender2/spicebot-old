3f98c1702beaeca1a6836bfc9084470cb4b587eb
Percussive-Maintenance <32780296+Percussive-Maintenance@users.noreply.github.com>
2017-11-30 15:39:06 -0600

    Update Clue.py

--- Clue.py
+++ Clue.py
@@ -28,13 +28,18 @@ def execute_main(bot, trigger):
     random.shuffle(weapons)
     random.shuffle(players)
     if rooms[0] == 'secret passage':
-        bot.say(players[1] + " evaded " + players[0] + " by using the secret passage. So " + players[0] + " killed " + players[2] + " with the " + weapons[0] + " instead.")    
+        bot.say(players[2] + " evaded " + players[0] + " by using the secret passage. So " + players[0] + " killed " + players[1] + " with the " + weapons[0] + " instead.")    
     else:
         bot.say(players[0] + " killed " + players[1] + " in the " + rooms[0] + " with the " + weapons[0] + ".")
     import Points
-    if trigger.group(2) and trigger.group(2) == players[0]:
-        bot.say('You guessed the killer correctly!')
-        Points.pointstask(bot, channel, 'SpiceBot', trigger.nick, ' gives ', ' to', 'up', 'points', trigger.sender)
+    if trigger.group(3):
+        if trigger.group(4):
+            if trigger.group(4) == 'killer' and trigger.group(3) == players[0]:
+                bot.say('You guessed the killer correctly!')
+                Points.pointstask(bot, channel, 'SpiceBot', trigger.nick, ' gives ', ' to', 'up', 'points', trigger.sender)
+            elif trigger.group(4) == 'killed' and trigger.group(3) == players[1]:
+                bot.say('You guessed the person murdered!')
+                Points.pointstask(bot, channel, 'SpiceBot', trigger.nick, ' gives ', ' to', 'up', 'points', trigger.sender)
     if players[0] == trigger.nick:
         bot.say('You were the killer.')
         Points.pointstask(bot, channel, 'SpiceBot', trigger.nick, ' takes ', ' from', 'down', 'points', trigger.sender)
