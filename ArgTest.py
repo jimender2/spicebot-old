877a3178799f9678bc42c5a4af094a050d6d9937
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 18:06:26 -0500

    Update ArgTest.py

--- ArgTest.py
+++ ArgTest.py
@@ -20,6 +20,11 @@ def mainfunction(bot, trigger):
     six = trigger.group(6)
     sevenandup = str(trigger.group(2).split(six, 1)[1]).strip()
     wordfive = sevenandup(' ', 1)[0]
+    wordsix = sevenandup(wordfive, )[0]
+    wordseven = sevenandup(wordsix, 1)[0]
+    wordeight = sevenandup(wordseven, 1)[0]
+    wordnine = sevenandup(wordeight, 1)[0]
+    wordten = sevenandup(wordnine, 1)[0]
     bot.say("Full command = " + str(args) + " = trigger.group(args)")
     bot.say("Full command = " + str(zero) + " = trigger.group(0)")
     bot.say("command without period = " + str(one) + " = trigger.group(1)")
@@ -30,3 +35,8 @@ def mainfunction(bot, trigger):
     bot.say("word4 = " + str(six) + " = trigger.group(6)")
     bot.say("word5andup = " + str(sevenandup) + " = str(trigger.group(2).split(six, 1)[1]).strip()")
     bot.say("word5 = " + str(wordfive) + " = sevenandup(' ', 1)[0]")
+    bot.say("word6 = " + str(wordsix) + " = sevenandup(wordfive, )[0]")
+    bot.say("word7 = " + str(wordseven) + " = sevenandup(wordsix, 1)[0]")
+    bot.say("word8 = " + str(wordeight) + " = sevenandup(wordseven, 1)[0]")
+    bot.say("word9 = " + str(wordnine) + " = sevenandup(wordeight, 1)[0]")
+    bot.say("word10 = " + str(wordten) + " = sevenandup(wordnine, 1)[0]")
