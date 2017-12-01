a49167eb04f15b617e564460ea5b9e27d6f282cb
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 18:59:17 -0500

    Update ArgTest.py

--- ArgTest.py
+++ ArgTest.py
@@ -11,52 +11,20 @@ from SpicebotShared import *
 @sopel.module.commands('argtest')
 def mainfunction(bot, trigger):
     fullcommandused = trigger.group(2)
-    arraynumber = 0
-    if trigger.group(2):
-        triggerargsarray = []
-        for word in fullcommandused.split():
-            bot.say(word)
-        #    triggerargsarray.append(word)
-        
-        #for x in triggerargsarray:
-        #    test = triggerargsarray[arraynumber]
-        #    bot.say(str(arraynumber) + " " + str(test))
-        #    arraynumber = arraynumber + 1
-            
-    else:
-        bot.say("No args given")
+    argone = fullcommandused.triggerargsnumber(0)
     
-    #args = trigger.args[1]
-    #zero = trigger.group(0)
-    #one = trigger.group(1)
-    #two = trigger.group(2)
-    #three = trigger.group(3)
-    #four = trigger.group(4)
-    #five = trigger.group(5)
-    #six = trigger.group(6)
-    #sevenandup = str(trigger.group(2).split(six, 1)[1]).strip()
-    #wordfive = splitme(sevenandup, 0)
-    #wordfive = sevenandup(' ', 1)[0]
-    #wordsix = sevenandup(wordfive, 1)[0]
-    #wordseven = sevenandup(wordsix, 1)[0]
-    #wordeight = sevenandup(wordseven, 1)[0]
-    #wordnine = sevenandup(wordeight, 1)[0]
-    #wordten = sevenandup(wordnine, 1)[0]
-    #bot.say("Full command = " + str(args) + " = trigger.group(args)")
-    #bot.say("Full command = " + str(zero) + " = trigger.group(0)")
-    #bot.say("command without period = " + str(one) + " = trigger.group(1)")
-    #bot.say("words1-? = " + str(two) + " = trigger.group(2)")
-    #bot.say("word1 = " + str(three) + " = trigger.group(3)")
-    #bot.say("word2 = " + str(four) + " = trigger.group(4)")
-    #bot.say("word3 = " + str(five) + " = trigger.group(5)")
-    #bot.say("word4 = " + str(six) + " = trigger.group(6)")
-    #bot.say("word5andup = " + str(sevenandup) + " = str(trigger.group(2).split(six, 1)[1]).strip()")
-    #bot.say("word5 = " + str(wordfive))
-    #bot.say("word6 = " + str(wordsix))
-    #bot.say("word7 = " + str(wordseven))
-    #bot.say("word8 = " + str(wordeight))
-    #bot.say("word9 = " + str(wordnine))
-    #bot.say("word10 = " + str(wordten))
+def triggerargsnumber(number):
+    triggerargsarray = []
+    for word in fullcommandused.split():
+        triggerargsarray.append(word)
+    entriestotal = len(triggerargsarray)
+    #if number <= entriestotal:
+    #    triggerarg = triggerargsarray[arraynumber] or 0
+    #else:
+    #    triggerarg = 0
+    triggerarg = triggerargsarray[arraynumber] or ''
+    return triggerarg
 
-#def splitme(text, n):
-#    return text.split()[n]
+
+
+
