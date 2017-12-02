f1295d8482f752a5a44010f091c2e88d8052b4aa
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:48:21 -0500

    Update Points.py

--- Points.py
+++ Points.py
@@ -10,11 +10,11 @@ from SpicebotShared import *
 
 @sopel.module.commands('points','takepoints','pants','takepants','minuspants','minuspoints','checkpoints','checkpants')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     for c in bot.channels:
         channel = c
     commandused = trigger.group(1)
