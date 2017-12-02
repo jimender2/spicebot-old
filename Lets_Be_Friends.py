caa567908485b1cf9283db13861509e183e9b85a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:46:04 -0500

    Update Lets_Be_Friends.py

--- Lets_Be_Friends.py
+++ Lets_Be_Friends.py
@@ -7,9 +7,9 @@ from SpicebotShared import *
 
 @sopel.module.commands('fuckyouspicebot','fuckspicebot','banspicebot','kickspicebot','hatespicebot','fuckoffspicebot')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     bot.say(trigger.nick + ", I'm sorry I offended you. Lets try to be friends.")
