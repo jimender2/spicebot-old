14b65921a40f50f87ca5d9f34b79d12c150efad1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:43:11 -0500

    Update Echo.py

--- Echo.py
+++ Echo.py
@@ -5,14 +5,13 @@ moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
 
-@sopel.module.rate(120)
 @sopel.module.commands('echo','echo','echo')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     if trigger.group(2):
         bot.say(trigger.group(2))
     else:
