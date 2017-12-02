dee066b6a88f67c363a8ca521513bfbc69a0918e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:37:35 -0500

    Update Blackjack_And_Hookers.py

--- Blackjack_And_Hookers.py
+++ Blackjack_And_Hookers.py
@@ -5,13 +5,12 @@ moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
 
-@sopel.module.commands('myown')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     if trigger.group(2):
         if not trigger.group(2) == bot.nick:
             bot.say("Fine! I'll start my own " + trigger.group(2) + ", with blackjack and hookers!")
