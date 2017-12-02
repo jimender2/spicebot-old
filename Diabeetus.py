0be96661b2f411be429ab13de2f9582add14d0c8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:42:18 -0500

    Update Diabeetus.py

--- Diabeetus.py
+++ Diabeetus.py
@@ -8,11 +8,11 @@ from SpicebotShared import *
 
 @sopel.module.commands('diabeetus')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     messages = ["Good morning. I'm Wilford Brimley and I'd like to talk to you about Diabeetus.","If you have type 2 Diabeetus, you can get your testing supplies free..."]
     answer = random.randint(0,len(messages) - 1)
     bot.say(messages[answer]);
