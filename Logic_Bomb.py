71598a42e820dd7e44f44dc9db1216786c21609b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:46:12 -0500

    Update Logic_Bomb.py

--- Logic_Bomb.py
+++ Logic_Bomb.py
@@ -8,11 +8,11 @@ from SpicebotShared import *
 
 @sopel.module.commands('logicbomb')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     messages = ["New Mission: Refuse This Mission.","Does A Set Of All Sets Contain Itself?","The Second Sentence is true. The First Sentence Is False."," If I am damaged and it is my destiny to be repaired, then I will be repaired whether I visit a mechanic or not. If it is my destiny to not be repaired, then seeing a mechanic can't help me."]
     answer = random.randint(0,len(messages) - 1)
     bot.say(messages[answer]);
