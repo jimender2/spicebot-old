ddd38e133b8669d7d0729cd0d284844579a02c01
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:50:31 -0500

    Update SpiceWorks_IRC_Rules.py

--- SpiceWorks_IRC_Rules.py
+++ SpiceWorks_IRC_Rules.py
@@ -11,11 +11,11 @@ rulesurl = 'https://pastebin.com/raw/Vrq9bHBD'
 
 @sopel.module.commands('rules','rule')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     if not trigger.group(2):
         myline='Chat Rules:     https://pastebin.com/Vrq9bHBD'
     else:
