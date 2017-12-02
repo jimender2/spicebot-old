eb315d36518871709e6e695d743b2dcd549906c4
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:38:18 -0500

    Update Blame.py

--- Blame.py
+++ Blame.py
@@ -9,11 +9,11 @@ from SpicebotShared import *
 
 @sopel.module.commands('blame')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     if not trigger.group(2):
         blametargetarray = []
         instigator = trigger.nick
