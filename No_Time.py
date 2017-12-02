399ddcc3177b85cfeb29554949b92fe3516be5e9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:47:18 -0500

    Update No_Time.py

--- No_Time.py
+++ No_Time.py
@@ -7,9 +7,9 @@ from SpicebotShared import *
 
 @sopel.module.commands('notime')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     bot.say("Well I woke up to get me a cold pop and then I thought somebody was barbequing. I said oh lord Jesus it's a fire. Then I ran out, I didn't grab no shoes or nothin' Jesus, I ran for my life. And then the smoke got me, I got bronchitis ain't nobody got time for that.")
