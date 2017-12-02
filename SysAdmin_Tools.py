eee104ba569078c8194346d42ab1e9024455d651
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:56:46 -0500

    Update SysAdmin_Tools.py

--- SysAdmin_Tools.py
+++ SysAdmin_Tools.py
@@ -7,9 +7,9 @@ from SpicebotShared import *
 
 @sopel.module.commands('sysadmintools')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     bot.say('https://sysadmin.it-landscape.info/     https://sysadmin.libhunt.com/     https://github.com/n1trux/awesome-sysadmin')
