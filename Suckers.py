b51bfe8069577170e23056307c25ad71b585ad2e
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:56:39 -0500

    Update Suckers.py

--- Suckers.py
+++ Suckers.py
@@ -7,11 +7,11 @@ from SpicebotShared import *
 
 @sopel.module.commands('sucker','suckers')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     if not trigger.group(2):
         bot.say("Who/what are for suckers??")
     else:
