9898bbdaa52fe58bcd37241514ec5a3b3e992b2b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 19:46:10 -0500

    Update ArgTest.py

--- ArgTest.py
+++ ArgTest.py
@@ -1,4 +1,11 @@
+# -*- coding: utf-8 -*/
 from sopel import module
+from sopel.tools import events
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
 
 @sopel.module.require_admin
 @sopel.module.commands('argtest')
