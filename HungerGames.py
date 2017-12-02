aebdccd6ff50ccf726c489c4586ded9236423514
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:44:37 -0500

    Update HungerGames.py

--- HungerGames.py
+++ HungerGames.py
@@ -9,11 +9,11 @@ from SpicebotShared import *
 
 @sopel.module.commands('hungergames')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     randomtargetarray = []
     for c in bot.channels:
         channel = c
