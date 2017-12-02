b3d644a7f340e39c32e9a4df2ebd99d2d5979cc6
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:38:54 -0500

    Update ChuckNorris.py

--- ChuckNorris.py
+++ ChuckNorris.py
@@ -10,11 +10,11 @@ from SpicebotShared import *
 
 @sopel.module.commands('chucknorris','chuck')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     joke = getJoke()
     if joke:
         if not trigger.group(2):
