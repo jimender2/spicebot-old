f192d5ff37b4b10902f9f64a1ad3585d07e52288
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:57:01 -0500

    Update TechSupport.py

--- TechSupport.py
+++ TechSupport.py
@@ -8,11 +8,11 @@ from SpicebotShared import *
 
 @sopel.module.commands('techsupport','itsupport')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     messages = ["YOU MUST CONSTRUCT ADDITIONAL PYLONS!","Have you tried flinging feces at it?","Have you tried chewing the cable?","Did you try turning it off and on again?","Did you try licking the mouse? Double-lick?","Did your try replacing all the ones with zeros?"]
     answer = random.randint(0,len(messages) - 1)
     bot.say(messages[answer]);
