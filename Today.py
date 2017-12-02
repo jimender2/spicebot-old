177886e175928506348fe0eb98e2c096b1cc4d62
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-01 20:58:04 -0500

    Update Today.py

--- Today.py
+++ Today.py
@@ -8,11 +8,11 @@ from SpicebotShared import *
 
 @sopel.module.commands('today')
 def mainfunction(bot, trigger):
-    enablestatus = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
-        execute_main(bot, trigger)
+        execute_main(bot, trigger, triggerargsarray)
     
-def execute_main(bot, trigger):
+def execute_main(bot, trigger, triggerargsarray):
     whatistoday, whatdayofweek = whatdayofweeknow()
     daystilfriday = howlonguntilfriday(whatistoday)
     if whatdayofweek == 'Monday':
