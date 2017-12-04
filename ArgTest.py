d05d3c4590e2bbc7310c0c7600d0a15657712f01
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-04 17:12:03 -0500

    Update ArgTest.py

--- ArgTest.py
+++ ArgTest.py
@@ -13,43 +13,15 @@ def mainfunction(bot, trigger):
         execute_main(bot, trigger, triggerargsarray)
     
 def execute_main(bot, trigger, triggerargsarray):
-    newvalue = get_trigger_arg(triggerargsarray, '5+')
-    newvalueb = get_trigger_arg(triggerargsarray, '5-')
-    newvaluec = get_trigger_arg(triggerargsarray, '5<')
-    newvalued = get_trigger_arg(triggerargsarray, '5>')
-    newvaluee = get_trigger_arg(triggerargsarray, 'last')
-    newvaluef = get_trigger_arg(triggerargsarray, '5^7')
-    newvalueg = get_trigger_arg(triggerargsarray, '5!')
     totalarray = len(triggerargsarray)
     totalarray = totalarray + 1
+    simulatedvaluearray = ["'5+'","'5-'","'5<'","'5>'","'last'","'5^7'","'5!'"]
     for i in range(0,totalarray):
         arg = get_trigger_arg(triggerargsarray, i)
         bot.say("arg" + str(i) + " = " + str(arg))
-    if newvalue != '':
-        bot.say("simulating arg5+ " + str(newvalue))
-    else:
-        bot.say("can't simulate 5+ as it returns empty values.")
-    if newvalueb != '':
-        bot.say("simulating arg5- " + str(newvalueb))
-    else:
-        bot.say("can't simulate 5- as it returns empty values.")
-    if newvaluec != '':
-        bot.say("simulating arg5< " + str(newvaluec))
-    else:
-        bot.say("can't simulate 5< as it returns empty values.")
-    if newvalued != '':
-        bot.say("simulating arg5> " + str(newvalued))
-    else:
-        bot.say("can't simulate 5> as it returns empty values.")
-    if newvaluee != '':
-        bot.say("simulating arg last " + str(newvaluee))
-    else:
-        bot.say("can't simulate arg last as it returns empty values.")
-    if newvaluef != '':
-        bot.say("simulating arg 5^7 " + str(newvaluef))
-    else:
-        bot.say("can't simulate arg 5^7 as it returns empty values.")
-    if newvalueg != '':
-        bot.say("simulating arg 5! " + str(newvalueg))
-    else:
-        bot.say("can't simulate arg 5! as it returns empty values.")
+    for x in simulatedvaluearray:
+        value = get_trigger_arg(triggerargsarray, x)
+        if value != '':
+            bot.say("simulating arg-" + str(x) + " " + str(value))
+        else:
+            bot.say("can't simulate arg-" + str(x) + " as it returns empty values.")
