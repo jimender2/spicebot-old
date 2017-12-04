2a02423b5f115b058f6e134b9f300a3a15a9956a
Percussive-Maintenance <32780296+Percussive-Maintenance@users.noreply.github.com>
2017-12-04 13:47:02 -0600

    Update Spicebucks.py

--- Spicebucks.py
+++ Spicebucks.py
@@ -32,7 +32,7 @@ def execute_main(bot, trigger, triggerargsarray):
         elif commandused.startswith('paydayreset'): ##to be removed
             paydayreset(bot,trigger.nick)
         elif commandused.startswith('bank'):
-            bot.say(checkbank(bot, trigger.nick))
+            bot.say(bank(bot, trigger.nick))
         elif commandused.startswith('transfer'):
             bot.say('transfer money to another user')
             
