1e042f03001ed70237a24cf8e9616dd86bb623ee
josh-cunning <joshbcunningham@gmail.com>
2017-12-05 13:52:47 -0800

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -25,9 +25,9 @@ def execute_main(bot, trigger, args):
   else:
     if not int(arg[0])>=1:      
       bot.say('Please enter the amount you wish to bet first')
-    elif not int(arg[1])<=maxwheel AND int(arg[1])>=1:
+    elif not int(arg[1])<=maxwheel and int(arg[1])>=1:
       bot.say("Please pick a number between 1 and " + maxwheel)
-    elif not arg[2] == 'red' OR arg[2] == 'black':
+    elif not arg[2] == 'red' or arg[2] == 'black':
       bot.say("Please select either red or black")	
     else:
       mybet=int(arg[0])
