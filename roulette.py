5f7fc906e7964dab7ecaccc0c8fab5671e29d013
josh-cunning <joshbcunningham@gmail.com>
2017-12-04 16:38:52 -0800

    Create roulette.py

---
+++ roulette.py
@@ -0,0 +1,9 @@
+import sopel.module
+from sopel import module, tools
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
+
+#A roulette game to be used with Spicebucks
