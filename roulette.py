11b5b8c07af2c4078ee01c4146035d715c99ec97
josh-cunning <joshbcunningham@gmail.com>
2017-12-06 12:31:05 -0800

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -33,30 +33,30 @@ def execute_main(bot, trigger, arg):
       elif not (arg[2] == 'red' or arg[2] == 'black'):
         bot.say('Please select either red or black')	
       else:
+        mywinnings=0
+        mybet=int(arg[0])
+        mynumber=int(arg[1])
+        mycolor=arg[2]         
         if Spicebucks.spicebucks(bot, trigger.nick, 'minus', mybet) == 'true':
-          mywinnings=0
-          mybet=int(arg[0])
-          mynumber=int(arg[1])
-          mycolor=arg[2]         
           bot.say(trigger.nick + ' puts ' + str(mybet) + ' on the table spins the wheel')
           winningnumber,pickedcolor = spinwheel()  
-          if pickedcolor == 0:
-            color = 'black'
-          else:
-            color = 'red' 
-          bot.say('The wheel stops on ' + str(winningnumber) + ' ' + color)
-          if mynumber == winningnumber:
-            mywinnings=mywinnings+(mybet*4)
-          elif mycolor == color:
-            mywinnings=mywinnings+mybet
-          if mywinnings >=1:
-            bot.say(trigger.nick + ' has won ' + mywinnings)
-            Spicebucks.spicebucks(bot, trigger.nick, 'plus', mywinnings)
-            Spicebucks.spicebucks(bot, trigger.nick, 'plus', mybet)
-          else:
-            bot.say(trigger.nick + ' has lost ' + mybet)
+            if pickedcolor == 0:
+              color = 'black'
+            else:
+              color = 'red' 
+            bot.say('The wheel stops on ' + str(winningnumber) + ' ' + color)
+            if mynumber == winningnumber:
+              mywinnings=mywinnings+(mybet*4)
+            elif mycolor == color:
+              mywinnings=mywinnings+mybet
+            if mywinnings >=1:
+              bot.say(trigger.nick + ' has won ' + mywinnings)
+              Spicebucks.spicebucks(bot, trigger.nick, 'plus', mywinnings)
+              Spicebucks.spicebucks(bot, trigger.nick, 'plus', mybet)
+            else:
+              bot.say(trigger.nick + ' has lost ' + mybet)
         else:
-          bot.say('You do not have enough Spicebucks for your bet')
+            bot.say('You do not have enough Spicebucks for your bet')
       
 def spinwheel():
   random.seed()
