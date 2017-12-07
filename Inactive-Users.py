d0574cb726761fad9bbb3bc68e4ed756e1e49e7b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-07 14:10:55 -0500

    Create Inactive-Users.py

---
+++ Inactive-Users.py
@@ -0,0 +1,16 @@
+import sopel
+from sopel import module, tools
+import random
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
+
+@sopel.module.commands('chanreport')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray)
+    
+def execute_main(bot, trigger, triggerargsarray):
