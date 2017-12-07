c9319eab52d4589922637ce6969a5e47611b0f67
josh-cunning <joshbcunningham@gmail.com>
2017-12-07 14:14:36 -0800

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -25,6 +25,7 @@ def execute_main(bot, trigger, arg):
 	elif not arg[0].isdigit():
 		bot.say('Please enter your bet followed by number and/or the color you wish to bet on')	
 	else:
+		mywinnings=0
     		if len(arg) == 3:
 			if not int(arg[0])>=1:      
 				bot.say('Please enter the amount you wish to bet first')
@@ -36,7 +37,28 @@ def execute_main(bot, trigger, arg):
 				mybet=int(arg[0])
 				mynumber=int(arg[1])
 				mycolor=arg[2]
-	    			payouts(mybet,mycolor,mynumber)
+				#check winnings and make pay out
+	    			if mybet<=0:
+					bot.say('Please enter your bet followed by number and the color you wish to bet on')
+				else:
+					if Spicebucks.spicebucks(bot, trigger.nick, 'minus', mybet) == 'true':
+					bot.say(trigger.nick + ' puts ' + str(mybet) + ' on the table spins the wheel')
+	    				winningnumber,pickedcolor = spinwheel()  
+	    				if pickedcolor == 0:
+						color = 'black'
+	    				else:
+						color = 'red' 
+				bot.say('The wheel stops on ' + str(winningnumber) + ' ' + color)
+	  			if mynumber == winningnumber:
+		  			mywinnings=mywinnings+(mybet*4)
+	  			elif mycolor == color:
+		 			 mywinnings=mywinnings+mybet
+	  			if mywinnings >=1:
+		  			bot.say(trigger.nick + ' has won ' + str(mywinnings))
+		  			Spicebucks.spicebucks(bot, trigger.nick, 'plus', mywinnings)
+		  			Spicebucks.spicebucks(bot, trigger.nick, 'plus', mybet)
+				else:
+		  			bot.say(trigger.nick + ' has lost ' + str(mybet))        
 		elif len(arg)==2:
   			if int(arg[0])>=1:
 				if(str(arg[1]) == 'red' or str(arg[1]) == 'black'):
@@ -47,40 +69,32 @@ def execute_main(bot, trigger, arg):
 				bot.say('You have choosen to bet on black')
 				mycolor='black'
 				mynumber=0
-			payouts(mybet,mycolor,mynumber)	
+			if mybet<=0:
+				bot.say('Please enter your bet followed by number and the color you wish to bet on')
+			else:
+				if Spicebucks.spicebucks(bot, trigger.nick, 'minus', mybet) == 'true':
+				bot.say(trigger.nick + ' puts ' + str(mybet) + ' on the table spins the wheel')
+				winningnumber,pickedcolor = spinwheel()  
+				if pickedcolor == 0:
+					color = 'black'
+				else:
+					color = 'red' 
+				bot.say('The wheel stops on ' + str(winningnumber) + ' ' + color)
+				if mynumber == winningnumber:
+					mywinnings=mywinnings+(mybet*4)
+				elif mycolor == color:
+					mywinnings=mywinnings+mybet
+				if mywinnings >=1:
+					bot.say(trigger.nick + ' has won ' + str(mywinnings))
+					Spicebucks.spicebucks(bot, trigger.nick, 'plus', mywinnings)
+					Spicebucks.spicebucks(bot, trigger.nick, 'plus', mybet)
+				else:
+					bot.say(trigger.nick + ' has lost ' + str(mybet))  
 		
 		else:
-			bot.say('Please enter your bet followed by number and or the color you wish to bet on')	
-	
-      
+			bot.say('Please enter your bet followed by number and or the color you wish to bet on') 
         
          
-      
-def payouts(mybet,mycolor,mynumber):
-	mywinnings=0
-	if mybet<=0:
-		bot.say('Please enter your bet followed by number and the color you wish to bet on')
-	else:
-		if Spicebucks.spicebucks(bot, trigger.nick, 'minus', mybet) == 'true':
-			bot.say(trigger.nick + ' puts ' + str(mybet) + ' on the table spins the wheel')
-	    		winningnumber,pickedcolor = spinwheel()  
-	    		if pickedcolor == 0:
-				color = 'black'
-	    		else:
-				color = 'red' 
-    			bot.say('The wheel stops on ' + str(winningnumber) + ' ' + color)
-	  		if mynumber == winningnumber:
-		  		mywinnings=mywinnings+(mybet*4)
-	  		elif mycolor == color:
-		 		 mywinnings=mywinnings+mybet
-	  		if mywinnings >=1:
-		  		bot.say(trigger.nick + ' has won ' + str(mywinnings))
-		  		Spicebucks.spicebucks(bot, trigger.nick, 'plus', mywinnings)
-		  		Spicebucks.spicebucks(bot, trigger.nick, 'plus', mybet)
-	  		else:
-		  		bot.say(trigger.nick + ' has lost ' + str(mybet))        
-
-	  
 def spinwheel():
   	random.seed()
 	thenumber = random.randint(0,maxwheel)
