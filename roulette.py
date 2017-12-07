d8ae3fcc2aed3cd36c58297778f485f5fc5b7850
josh-cunning <joshbcunningham@gmail.com>
2017-12-07 14:50:18 -0800

    Update roulette.py

--- roulette.py
+++ roulette.py
@@ -19,82 +19,81 @@ def mainfunction(bot, trigger):
 		execute_main(bot, trigger, triggerargsarray)
         
 def execute_main(bot, trigger, arg):
-  #get triggerwords from player to allow number,color and even/odd choices
-	if str(arg[0])=='payout':
-		bot.say('Picking the correct number gives 4 times your bet. Picking the correct color gives double your bet')
-	elif not arg[0].isdigit():
-		bot.say('Please enter your bet followed by number and/or the color you wish to bet on')	
-	else:
-		mywinnings=0
-    		if len(arg) == 3:
-			if not int(arg[0])>=1:      
-				bot.say('Please enter the amount you wish to bet first')
-			elif not (int(arg[1])<=maxwheel and int(arg[1])>=1):
-				bot.say('Please pick a number between 1 and ' + str(maxwheel))
-	      		elif not (arg[2] == 'red' or arg[2] == 'black'):
-				bot.say('Please select either red or black')	
-	      		else:        
-				mybet=int(arg[0])
-				mynumber=int(arg[1])
-				mycolor=arg[2]
-				#check winnings and make pay out
-	    			if mybet<=0:
+  	if not (len(arg)>3 and len(arg)<1):	
+		if str(arg[0])=='payout':
+			bot.say('Picking the correct number gives 4 times your bet. Picking the correct color gives double your bet')
+		elif not arg[0].isdigit():
+			bot.say('Please enter your bet followed by number and/or the color you wish to bet on')	
+		else:
+			mywinnings=0
+    			if len(arg) == 3:
+				if not int(arg[0])>=1:      
+					bot.say('Please enter the amount you wish to bet first')
+				elif not (int(arg[1])<=maxwheel and int(arg[1])>=1):
+					bot.say('Please pick a number between 1 and ' + str(maxwheel))
+	      			elif not (arg[2] == 'red' or arg[2] == 'black'):
+					bot.say('Please select either red or black')	
+	      			else:        
+					mybet=int(arg[0])
+					mynumber=int(arg[1])
+					mycolor=arg[2]					
+	    				if mybet<=0:
+						bot.say('Please enter your bet followed by number and the color you wish to bet on')
+					else:
+						if Spicebucks.spicebucks(bot, trigger.nick, 'minus', mybet) == 'true':
+							bot.say(trigger.nick + ' puts ' + str(mybet) + ' on the table spins the wheel')
+	    						winningnumber,pickedcolor = spinwheel()  
+	    						if pickedcolor == 0:
+								color = 'black'
+	    						else:
+								color = 'red' 
+							bot.say('The wheel stops on ' + str(winningnumber) + ' ' + color)
+	  						if mynumber == winningnumber:
+								mywinnings=mywinnings+(mybet*4)
+	  						elif mycolor == color:
+								mywinnings=mywinnings+mybet
+							if mywinnings >=1:
+		  						bot.say(trigger.nick + ' has won ' + str(mywinnings))
+		  						Spicebucks.spicebucks(bot, trigger.nick, 'plus', mywinnings)
+		  						Spicebucks.spicebucks(bot, trigger.nick, 'plus', mybet)
+							else:
+		  						bot.say(trigger.nick + ' has lost ' + str(mybet))        
+			elif len(arg)==2:  			
+				mybet = int(arg[0])
+				if(str(arg[1]) == 'red' or str(arg[1]) == 'black'):
+					mycolor = arg[1]
+					mynumber=0
+		  		elif (int(arg[1])<=maxwheel and int(arg[1])>=1):
+					mynumber=int(arg[1])
+					mycolor=' '
+		 		else:
+					bot.say('You have choosen to bet on black')
+					mycolor='black'
+					mynumber=0
+				if mybet<=0:
 					bot.say('Please enter your bet followed by number and the color you wish to bet on')
 				else:
 					if Spicebucks.spicebucks(bot, trigger.nick, 'minus', mybet) == 'true':
 						bot.say(trigger.nick + ' puts ' + str(mybet) + ' on the table spins the wheel')
-	    					winningnumber,pickedcolor = spinwheel()  
-	    					if pickedcolor == 0:
+						winningnumber,pickedcolor = spinwheel()  
+						if pickedcolor == 0:
 							color = 'black'
-	    					else:
+						else:
 							color = 'red' 
 						bot.say('The wheel stops on ' + str(winningnumber) + ' ' + color)
-	  					if mynumber == winningnumber:
+						if mynumber == winningnumber:
 							mywinnings=mywinnings+(mybet*4)
-	  					elif mycolor == color:
+						elif mycolor == color:
 							mywinnings=mywinnings+mybet
 						if mywinnings >=1:
-		  					bot.say(trigger.nick + ' has won ' + str(mywinnings))
-		  					Spicebucks.spicebucks(bot, trigger.nick, 'plus', mywinnings)
-		  					Spicebucks.spicebucks(bot, trigger.nick, 'plus', mybet)
+							bot.say(trigger.nick + ' has won ' + str(mywinnings))
+							Spicebucks.spicebucks(bot, trigger.nick, 'plus', mywinnings)
+							Spicebucks.spicebucks(bot, trigger.nick, 'plus', mybet)
 						else:
-		  					bot.say(trigger.nick + ' has lost ' + str(mybet))        
-		elif len(arg)==2:  			
-			mybet = int(arg[0])
-			if(str(arg[1]) == 'red' or str(arg[1]) == 'black'):
-				mycolor = arg[1]
-				mynumber=0
-		  	elif (int(arg[1])<=maxwheel and int(arg[1])>=1):
-				mynumber=int(arg[1])
-				mycolor=' '
-		 	else:
-				bot.say('You have choosen to bet on black')
-				mycolor='black'
-				mynumber=0
-			if mybet<=0:
-				bot.say('Please enter your bet followed by number and the color you wish to bet on')
-			else:
-				if Spicebucks.spicebucks(bot, trigger.nick, 'minus', mybet) == 'true':
-					bot.say(trigger.nick + ' puts ' + str(mybet) + ' on the table spins the wheel')
-					winningnumber,pickedcolor = spinwheel()  
-					if pickedcolor == 0:
-						color = 'black'
-					else:
-						color = 'red' 
-					bot.say('The wheel stops on ' + str(winningnumber) + ' ' + color)
-					if mynumber == winningnumber:
-						mywinnings=mywinnings+(mybet*4)
-					elif mycolor == color:
-						mywinnings=mywinnings+mybet
-					if mywinnings >=1:
-						bot.say(trigger.nick + ' has won ' + str(mywinnings))
-						Spicebucks.spicebucks(bot, trigger.nick, 'plus', mywinnings)
-						Spicebucks.spicebucks(bot, trigger.nick, 'plus', mybet)
-					else:
-						bot.say(trigger.nick + ' has lost ' + str(mybet))  
+							bot.say(trigger.nick + ' has lost ' + str(mybet))  
 		
-		else:
-			bot.say('Please enter your bet followed by number and or the color you wish to bet on') 
+	else:
+		bot.say('Please enter your bet followed by number and or the color you wish to bet on') 
         
          
 def spinwheel():
