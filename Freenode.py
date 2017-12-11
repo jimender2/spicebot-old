bb292cfb58db8d4050d8b92449b690fc7c823e21
Percussive-Maintenance <32780296+Percussive-Maintenance@users.noreply.github.com>
2017-12-11 09:55:18 -0600

    Create Freenode.py

---
+++ Freenode.py
@@ -0,0 +1,18 @@
+import sopel.module
+import sys
+import os
+import random
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
+
+@sopel.module.commands('msg', 'nick', 'attach', 'server', 'join', 'whois', 'me', 'ban')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray)
+    
+def execute_main(bot, trigger, triggerargsarray):
+    bot.say(trigger)
+    bot.say(triggerargsarray
+    
