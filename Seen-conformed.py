de9caaf3bc43e995d75cc634acb7b6a52e7feeed
Dyson Parkes <dyson@berserkir.net>
2017-12-11 16:51:47 +1300

    Update Seen-conformed.py

--- Seen-conformed.py
+++ Seen-conformed.py
@@ -44,7 +44,7 @@ def execute_main(bot, trigger, triggerargsarray):
 
 #        tz = get_timezone(bot.db, bot.config, None, trigger.nick,
 #                          trigger.sender)
-        tz = "NZDT"
+        tz = "NZT"
         saw = datetime.datetime.utcfromtimestamp(timestamp)
         timestamp = format_time(bot.db, bot.config, tz, trigger.nick,
                                 trigger.sender, saw)
