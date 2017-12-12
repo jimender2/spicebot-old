df07ce2bf0256ac4468b3c2430c4093e54a3c1e9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-12 09:13:23 -0500

    Update Challenges.py

--- Challenges.py
+++ Challenges.py
@@ -1021,7 +1021,10 @@ def get_streaktext(bot, winner, loser, winner_loss_streak, loser_win_streak):
     streak = ' (Streak: %d)' % win_streak if win_streak > 1 else ''
     broken_streak = ', recovering from a streak of %d losses' % winner_loss_streak if winner_loss_streak > 1 else ''
     broken_streak += ', ending %s\'s streak of %d wins' % (loser, loser_win_streak) if loser_win_streak > 1 else ''
-    streaktext = str("%s %s %s" % (winner, broken_streak, streak))
+    if broken_streak or streak:
+        streaktext = str("%s %s %s" % (winner, broken_streak, streak))
+    else:
+        streaktext = ''
     return streaktext
     
 ###############
