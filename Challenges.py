a589b089569df5fe90ecf5eec5e9870af6ac9575
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-12 15:40:03 -0500

    Update Challenges.py

--- Challenges.py
+++ Challenges.py
@@ -21,6 +21,7 @@ TIMEOUT = 180
 TIMEOUTC = 40
 ALLCHAN = 'entirechannel'
 OPTTIMEOUT = 1800
+CLASSTIMEOUT = 86400
 lootitemsarray = ['healthpotion','manapotion','poisonpotion','timepotion','mysterypotion']
 backpackarray = ['coins','healthpotion','manapotion','poisonpotion','timepotion','mysterypotion']
 transactiontypesarray = ['buy','sell','trade','use']
@@ -298,7 +299,7 @@ def execute_main(bot, trigger, triggerargsarray):
                     bot.say("You don't appear to have a class set. Options are : " + classes +". Run .duel class set    to set your class.")
                 elif not subcommand:
                     bot.say("Your class is currently set to " + str(yourclass))
-                elif yourclasstime <= 86400:# and not bot.nick.endswith('dev'):
+                elif yourclasstime < CLASSTIMEOUT:# and not bot.nick.endswith('dev'):
                     bot.say("You may not change your class more than once per day.")
                 elif subcommand == 'set':
                     if yourclass:
