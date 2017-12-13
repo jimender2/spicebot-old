57414c9cb2cff7c98551fdcf9f90bbd3e7acf550
Percussive-Maintenance <32780296+Percussive-Maintenance@users.noreply.github.com>
2017-12-13 14:16:42 -0600

    Update HungerGames.py

--- HungerGames.py
+++ HungerGames.py
@@ -6,7 +6,6 @@ import os
 moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
-from random import *
 
 @sopel.module.commands('hungergames')
 def mainfunction(bot, trigger):
