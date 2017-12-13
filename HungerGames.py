1b25e01e275ced9136f14b10489261586476d2f8
Percussive-Maintenance <32780296+Percussive-Maintenance@users.noreply.github.com>
2017-12-13 16:09:51 -0600

    Update HungerGames.py

--- HungerGames.py
+++ HungerGames.py
@@ -50,29 +50,25 @@ def execute_main(bot, trigger, triggerargsarray):
                     tributerow = [tribute, 100, weapons[0]]
                     tributes.append(tributerow)
                 totaltributes = len(tributes)
-                deathorder = ''
                 while totaltributes > 1:
                     random.shuffle(tributes)
-                    damageone = randint(10, 30)
-                    damagetwo = randint(10, 30)
-                    bot.say(tributes[0][0] + " hits " + tributes[1][0] + " with a " + tributes[0][2] + "(-" + str(damageone) + "). " + tributes[1][0] + " hits " + tributes[0][0] + " with a " + tributes[1][2] + "(-" + str(damagetwo) + "). ")
+                    damageone = randint(50, 80)
+                    damagetwo = randint(50, 80)
+                    bot.notify(tributes[0][0] + " hits " + tributes[1][0] + " with a " + tributes[0][2] + "(-" + str(damageone) + "). " + tributes[1][0] + " hits " + tributes[0][0] + " with a " + tributes[1][2] + "(-" + str(damagetwo) + "). ")
                     tributes[0][1] = tributes[0][1] - damageone
                     tributes[1][1] = tributes[1][1] - damageone
                     if tributes[0][1] <= 0:
-                        bot.say(tributes[1][0] + " killed " + tributes[0][0])
+                        bot.notify(tributes[1][0] + " killed " + tributes[0][0])
                     if tributes[1][1] <= 0:
                         if len(tributes) > 1:
-                            bot.say(tributes[0][0] + " killed " + tributes[1][0])
+                            bot.notify(tributes[0][0] + " killed " + tributes[1][0])
                     if tributes[1][1] <= 0: #remove second tribute first is killed to not mess up order if first is killed
-                        deathorder = str(totaltributes) + ": " + tributes[1][0] + " " + deathorder
                         tributes.pop(1)
                     if tributes[0][1] <= 0:
                         if len(tributes) > 1:
-                            deathorder = str(totaltributes) + ": " + tributes[0][0] + " " + deathorder
                             tributes.pop(0)
                     totaltributes = len(tributes)
                 bot.say("The victor is " + tributes[0][0])
-                deathorder = "1: " + tributes[0][0] + " " + deathorder
-                bot.say(deathorder)
+
                         
                         
