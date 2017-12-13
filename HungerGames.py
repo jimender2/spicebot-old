4becb8438225df41c2191e0809ed19c5c16acd05
Percussive-Maintenance <32780296+Percussive-Maintenance@users.noreply.github.com>
2017-12-13 13:54:36 -0600

    Update HungerGames.py

--- HungerGames.py
+++ HungerGames.py
@@ -7,6 +7,9 @@ moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
 
+weapon = ['dagger', 'sword', 'knife', 'bow and arrow']
+selfdeath = ['starvation', 'drowning', 'hypothermia']
+
 @sopel.module.commands('hungergames')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
@@ -41,3 +44,29 @@ def execute_main(bot, trigger, triggerargsarray):
                 randomtargetarray.pop(2)
                 random.shuffle(randomtargetarray)
                 bot.say(volunteer + " volunteered as tribute for " + safetribute + ". The first to die was " + str(randomtargetarray[1]) + ". The victor is " + str(randomtargetarray[0]))
+                
+                tributes = []
+                for tribute in randomtargetarray:
+                    weapon = random.shuffle(weapon)
+                    tributerow = [tribute, 100, weapon]
+                    tributes.append(tributerow)
+                totaltributes = len(tributes)
+                while totaltributes > 1:
+                    tributes = random.shuffle(tributes)
+                    damageone = randint(1, 100)
+                    damagetwo = randint(1, 100)
+                    bot.say(tributes[0][0] + " hits " + tributes[1][0] + " with a " + tributes[0][3] + "(-" + damageone + "). " + tributes[1][0] + " hits " + tributes[0][0] + " with a " + tributes[1][3] + "(-" + damagetwo + "). ")
+                    tributes[0][2] = tributes[0][2] - damageone
+                    tributes[1][2] = tributes[1][2] - damageone
+                    if tributes[0][2] <= 0:
+                        bot.say(tributes[1][0] + " killed " + tributes[0][0])
+                    if tributes[1][2] <= 0:
+                        bot.say(tributes[0][0] + " killed " + tributes[1][0])
+                    if tributes[1][2] <= 0: #remove second tribute first is killed to not mess up order if first is killed
+                        tributes.pop(1)
+                    if tributes[0][2] <= 0:
+                        tributes.pop(0)
+                    totaltributes = len(tributes)
+                bot.say("The victor is " + tributes[0][0])
+                        
+                        
