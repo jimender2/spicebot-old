0aa4636b097e979cf56915437900450fb969d24f
josh-cunning <joshbcunningham@gmail.com>
2017-12-14 14:12:08 -0800

    Update BlackJack.py

--- BlackJack.py
+++ BlackJack.py
@@ -18,7 +18,7 @@ def execute_main(bot, trigger, arg):
   deck = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]*4
   bot.say(str(len(deck)))
   myhand,mydeck = deal(deck)
-  dealerhand,dealerdeck = deal(deck)
+  #dealerhand,dealerdeck = deal(deck)
   bot.say('You hand is ' + str(myhand[0]) + ' and ' + str(myhand[1]))  
   bot.say(str(len(mydeck)))
 
