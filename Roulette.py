9729d47e37060f8c0a2b5f9b73196fe92a4b8f73
josh-cunning <joshbcunningham@gmail.com>
2017-12-14 10:53:30 -0800

    Update Roulette.py

--- Roulette.py
+++ Roulette.py
@@ -28,6 +28,9 @@ def execute_main(bot, trigger, arg):
 		if str(arg[0])=='payout':
 			bot.say('Picking the correct number gives 4 times your bet. Picking the correct color gives double your bet')
 			inputcheck=0
+		elif str(arg[0])=='cashout':
+			 Spicebucks.spicebucks(bot, trigger.nick, 'plus', 1000)
+			 bot.say('You have received 1000')
 		#elif str(arg[0])=='betitall':			
 			#spicebucks = bot.db.get_nick_value(trigger.nick, 'spicebucks_bank') or 0
 			#bot.say(trigger.nick + ' is going all in and letting ' + str(spicebucks) + ' ride.')
