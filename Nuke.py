685158d63f4c1ef524f3bc5ad6fd78363c5a7c20
Dyson Parkes <dyson@berserkir.net>
2017-12-14 23:04:58 +1300

    Update Nuke.py
    Add killit

--- Nuke.py
+++ Nuke.py
@@ -5,11 +5,14 @@ moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
 
-@sopel.module.commands('nuke')
+@sopel.module.commands('nuke','killit')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
     
 def execute_main(bot, trigger, triggerargsarray):
-    bot.say("Nuke it from orbit... it's the only way to be sure?")
+    if commandused == 'nuke':
+        bot.say("Nuke it from orbit... it's the only way to be sure?")
+    elif commandused == 'killit':
+        bot.say("Kill it with fire. Now.")
