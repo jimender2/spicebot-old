67de4fc8247c9a7123b4505992eb7590bc734e15
Dyson Parkes <dyson@berserkir.net>
2017-12-14 23:08:47 +1300

    Update Nuke.py
    Added terminate

--- Nuke.py
+++ Nuke.py
@@ -5,7 +5,7 @@ moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
 
-@sopel.module.commands('nuke','killit')
+@sopel.module.commands('nuke','killit','terminate')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
@@ -17,3 +17,5 @@ def execute_main(bot, trigger, triggerargsarray):
         bot.say("Nuke it from orbit... it's the only way to be sure?")
     elif commandused == 'killit':
         bot.say("Kill it with fire. Now.")
+    elif commandused == 'terminate':
+        bot.say("Terminate it with extreme prejudice.")
