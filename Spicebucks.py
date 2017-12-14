c278b21c53d66510ecb42cfb0422f21fa342bdae
Dyson Parkes <dyson@berserkir.net>
2017-12-14 23:52:59 +1300

    Update Spicebucks.py

--- Spicebucks.py
+++ Spicebucks.py
@@ -118,14 +118,17 @@ def transfer(bot, channel, instigator, target, amount):
         success = 0
     
     if success == 1:
-        bot.say(str(spicebucktotal))
-        if amount >= spicebucktotal:
-            validamount = 0
-            bot.say("You do not have enough spicebucks to make this transfer.")
-        else:
-            validamount = 1
+        #bot.say(str(spicebucktotal) + str(amount))
+        validamount = 1
+        bot.say(str(amount))
+        #if amount >= spicebucktotal:
+            #validamount = 0
+            #bot.say("You do not have enough spicebucks to make this transfer.")
+        #else:
+            #validamount = 1
     
-    if success == 1 and validamount = 1:
+    #if success == 1 and validamount = 1:
+    if validamount = 1:
         if amount <= 0:
             bot.say(instigator + " gave no spicefucks about " + target + "'s comment.")
         else:
