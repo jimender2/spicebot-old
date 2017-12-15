a93d3ed2a798ac417315490249b2e71a85199545
Dyson Parkes <dyson@berserkir.net>
2017-12-15 16:26:08 +1300

    Update usage.py
    Add user total check

--- usage.py
+++ usage.py
@@ -14,6 +14,7 @@ def mainfunction(bot, trigger):
 def execute_main(bot, trigger, triggerargsarray):
     ## Initial ARGS of importance
     usagefor = ''
+    querytype = 'specific'
     triggerargsarray = create_args_array(trigger.group(2))
     commandused = get_trigger_arg(triggerargsarray, 0)
     moduletocheck = get_trigger_arg(triggerargsarray, 1)
@@ -28,14 +29,25 @@ def execute_main(bot, trigger, triggerargsarray):
             usagefor = checktarget
     elif not checktarget:
         usagefor = str(trigger.nick)
+    if moduletocheck == 'total':
+        usagefor = str(trigger.nick)
+    if moduletocheck.lower() in bot.privileges[channel.lower()]:
+        querytype = 'user'    
     count = get_botdatabase_value(bot, usagefor, moduletocheck+"usage")
     
-    if count == 0:
-        message = str('It appears that ' + usagefor + ' has not run ' + moduletocheck + ' at all.')
-    elif count == 1:
-        message = str(usagefor + ' has run ' + moduletocheck + ' a total of ' + str(count) + ' time.')
+    if querytype == 'user':
+        if count == 0:
+            message = str('It appears that ' + usagefor + ' has not run any commands at all.')
+        elif count == 1:
+            message = str(usagefor + ' has run a total of ' + str(count) + ' commands.')
+        else:
+            message = str(usagefor + ' has run a total of ' + str(count) + ' commands.')
     else:
-        message = str(usagefor + ' has run ' + moduletocheck + ' a total of ' + str(count) + ' times.')
-    ##says the variables
-    #bot.action("checks usage of " + moduletocheck + " for " + usagefor + " " + str(count))
+        if count == 0:
+            message = str('It appears that ' + usagefor + ' has not run ' + moduletocheck + ' at all.')
+        elif count == 1:
+            message = str(usagefor + ' has run ' + moduletocheck + ' a total of ' + str(count) + ' time.')
+        else:
+            message = str(usagefor + ' has run ' + moduletocheck + ' a total of ' + str(count) + ' times.')
+    
     bot.say(message)
