e193ad0583b45cf6271413aca0b29dd3a6e3f235
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-14 22:54:49 -0500

    Update FeatureRequest.py

--- FeatureRequest.py
+++ FeatureRequest.py
@@ -35,6 +35,7 @@ def execute_main(bot, trigger, triggerargsarray):
 
 def make_github_issue(bot, title):
     '''Create an issue on github.com using the given parameters.'''
+    bot.say(title)
     body=title
     labels=None
     # Our url to create issues via POST
