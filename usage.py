ecb074e8d1a59167979a3591bd40c9d07b2f4a55
Dyson Parkes <dyson@berserkir.net>
2017-12-15 14:13:55 +1300

    Create usage.py

---
+++ usage.py
@@ -0,0 +1,20 @@
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+from SpicebotShared import *
+
+@sopel.module.commands('usage')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray)
+    
+def execute_main(bot, trigger, triggerargsarray):
+  commandused = trigger.group(1)
+  module = trigger.group(2)
+  target = trigger.group(3)
+  #if not target:
+    #target = bot.channel
+bot.say(commandused + module + target)
