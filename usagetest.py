a575c6ef377b6c0eba7d7f8643e46de0c40b87b2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-14 23:18:56 -0500

    Update usagetest.py

--- usagetest.py
+++ usagetest.py
@@ -13,56 +13,36 @@ def mainfunction(bot, trigger):
     
 def execute_main(bot, trigger, triggerargsarray):
     instigator = trigger.nick
+    for c in bot.channels:
+        channel = c
     checktarget = get_trigger_arg(triggerargsarray, 2)
     if not checktarget:
         bot.say('No target or command')
-    elif moduletocheck.lower() in bot.privileges[channel.lower()]:
-    
-    
-    ## Initial ARGS of importance
-    usagefor = ''
-    querytype = 'specific'
-    
-
-    commandused = get_trigger_arg(triggerargsarray, 0)
-    moduletocheck = get_trigger_arg(triggerargsarray, 1)
-    
-    for c in bot.channels:
-        channel = c
-
-    if not moduletocheck:
-        querytype = 'user'
-        moduletocheck = 'total'
-        usagefor = str(trigger.nick)
-    if moduletocheck.lower() in bot.privileges[channel.lower()]:
-        querytype = 'user'
-        usagefor = str(moduletocheck)
-        bot.say(str(moduletocheck))
-    elif moduletocheck:
-        if checktarget:
-            if checktarget == 'channel':
-                usagefor = trigger.sender
-            else:
-                usagefor = checktarget
-        elif not checktarget:
-            usagefor = str(trigger.nick)
-        
-    ## get count
-    count = get_botdatabase_value(bot, usagefor, moduletocheck+"usage")
-    
-    if querytype == 'user':
+        return
+    elif checktarget.lower() not in bot.privileges[channel.lower()] and checktarget != 'channel':
+        target = instigator
+        checkcmd = checktarget
+    elif checktarget == 'channel':
+        target = channel
+        checkcmd = get_trigger_arg(triggerargsarray, 3)
+    else:
+        target = checktarget
+        checkcmd = get_trigger_arg(triggerargsarray, 3)
+    if not checkcmd:
+        bot.say('what command')
+        return
+    else:
+        count = get_botdatabase_value(bot, usagefor, checkcmd+"usage")
         if count == 0:
             message = str('It appears that ' + usagefor + ' has not run any commands at all.')
         elif count == 1:
             message = str(usagefor + ' has run a total of ' + str(count) + ' commands.')
         else:
             message = str(usagefor + ' has run a total of ' + str(count) + ' commands.')
-    else:
         if count == 0:
             message = str('It appears that ' + usagefor + ' has not run ' + moduletocheck + ' at all.')
         elif count == 1:
             message = str(usagefor + ' has run ' + moduletocheck + ' a total of ' + str(count) + ' time.')
         else:
             message = str(usagefor + ' has run ' + moduletocheck + ' a total of ' + str(count) + ' times.')
-    
-    bot.say(message)
+        bot.say(message)
