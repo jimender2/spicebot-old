7c477aba16dc5c4fab265892ae01519761a6ccbf
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-14 22:36:02 -0500

    Update FeatureRequest.py

--- FeatureRequest.py
+++ FeatureRequest.py
@@ -3,9 +3,19 @@ import sys
 import os
 import json
 import requests
+import ConfigParser
 moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
+
+# Authentication for user filing issue (must have read/write access to
+# repository to add issue to)
+USERNAME = 'SpiceBot'
+#PASSWORD = ''
+
+# The repository to add this issue to
+REPO_OWNER = 'deathbybandaid'
+REPO_NAME = 'sopel-modules'
 
 @sopel.module.commands('featurerequest'')
 def mainfunction(bot, trigger):
@@ -17,4 +27,27 @@ def execute_main(bot, trigger, triggerargsarray):
     if not trigger.group(2):
         bot.say("What feature do you want?")
     else:
-    
+        title = get_trigger_arg(triggerargsarray, 0)
+        make_github_issue(bot, title, body=None, assignee=None, milestone=None, labels=None)
+
+def make_github_issue(bot, title, body=None, assignee=None, milestone=None, labels=None):
+    config = ConfigParser.ConfigParser()
+    config.read("/etc/spicecred.txt")
+    PASSWORD = config.get("configuration","password")
+    url = 'https://api.github.com/repos/%s/%s/issues' % (REPO_OWNER, REPO_NAME)
+    session = requests.session(auth=(USERNAME, PASSWORD))
+    # Create our issue
+    issue = {'title': title,
+             'body': body,
+             'assignee': assignee,
+             'milestone': milestone,
+             'labels': labels}
+    # Add the issue to our repository
+    r = session.post(url, json.dumps(issue))
+    if r.status_code == 201:
+        bot.say('Successfully created Issue "%s"' % title)
+    else:
+        bot.say('Could not create Issue "%s"' % title)
+        bot.say(str('Response:', r.content))
+
+
