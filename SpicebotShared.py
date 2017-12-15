69c1f1fc93c4104a7c162d890fee7c43b555fa02
Dyson Parkes <dyson@berserkir.net>
2017-12-15 13:51:46 +1300

    Update SpicebotShared.py

--- SpicebotShared.py
+++ SpicebotShared.py
@@ -86,9 +86,9 @@ def spicebot_prerun(bot,trigger):
         set_botdatabase_value(bot, instigator, 'lastusagetime', now)
     
     ## Add usage counter for individual/channel module counts
-    adjust_botdatabase_value(bot, channel, str(commandused + "usage"), 1)
+    adjust_botdatabase_value(bot, botchannel, str(commandused + "usage"), 1)
     adjust_botdatabase_value(bot, trigger.nick, str(commandused + "usage"), 1)
-    bot.say(bot + channel + str(commandused + "usage"))
+    bot.say(bot + botchannel + str(commandused + "usage"))
     bot.say(bot + trigger.nick + str(commandused + "usage"))
     
     ## message, if any
