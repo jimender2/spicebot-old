ee4c3210ded69691c3f9ec04f0654ec8bc8e0355
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-14 22:52:30 -0500

    Update FeatureRequest.py

--- FeatureRequest.py
+++ FeatureRequest.py
@@ -30,22 +30,21 @@ def execute_main(bot, trigger, triggerargsarray):
     if not trigger.group(2):
         bot.say("What feature do you want?")
     else:
-        title = get_trigger_arg(triggerargsarray, 0)
-        make_github_issue(bot, title, body=None, assignee=None, milestone=None, labels=None)
+        title = str(get_trigger_arg(triggerargsarray, 0))
+        make_github_issue(bot, title)
 
-def make_github_issue(bot, title, body=None, assignee=None, milestone=None, labels=None):
+def make_github_issue(bot, title):
     '''Create an issue on github.com using the given parameters.'''
+    body=title
+    labels=None
     # Our url to create issues via POST
     url = 'https://api.github.com/repos/%s/%s/issues' % (REPO_OWNER, REPO_NAME)
     # Create an authenticated session to create the issue
-    #session = requests.session(auth=(USERNAME, PASSWORD))
     session = requests.Session()
     session.auth = (USERNAME, PASSWORD)
     # Create our issue
     issue = {'title': title,
              'body': body,
-             'assignee': assignee,
-             'milestone': milestone,
              'labels': labels}
     # Add the issue to our repository
     r = session.post(url, json.dumps(issue))
@@ -53,6 +52,4 @@ def make_github_issue(bot, title, body=None, assignee=None, milestone=None, labe
         bot.say('Successfully created Issue "%s"' % title)
     else:
         bot.say('Could not create Issue "%s"' % title)
-        bot.say(str('Response:' + r.content))
-
-
+        bot.say('Response:' + r.content))
