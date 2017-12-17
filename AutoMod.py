983fdb93b8f5ded2646f099f71d461c213eb3c9d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-17 17:14:19 -0500

    Update AutoMod.py

--- AutoMod.py
+++ AutoMod.py
@@ -17,14 +17,14 @@ from SpicebotShared import *
 def automod(bot, trigger):
     if not trigger.is_privmsg:
         
-        floodyell = 0
-        antifloodwarning = str(instigator + ", please do not flood the channel.")
-        
+        ## vars
         channel = trigger.sender
         instigator = trigger.nick
         currentmessage = trigger.group(2)
 
         ## Flooding is 5 lines in a row by the same person or 3 identical lines
+        floodyell = 0
+        antifloodwarning = str(instigator + ", please do not flood the channel.")
         lastnicksubmit = get_botdatabase_value(bot, channel, 'automod_antifloodnick') or bot.nick
         if lastnicksubmit != instigator:
             set_botdatabase_value(bot, channel, 'automod_antifloodnick', instigator)
@@ -45,7 +45,7 @@ def automod(bot, trigger):
             lastnicksubmit = get_botdatabase_value(bot, channel, 'automod_antifloodnick') or bot.nick
             adjust_botdatabase_value(bot, channel, 'automod_antifloodcount', 1)
             getcurrentcount = get_botdatabase_value(bot, channel, 'automod_antifloodcount') or 1
-            if int(getcurrentcount) >= 5:
+            if int(getcurrentcount) > 6:
                 lastnicksubmitwarned = get_botdatabase_value(bot, channel, 'automod_antifloodnickwarned') or bot.nick
                 if lastnicksubmitwarned != instigator:
                     set_botdatabase_value(bot, channel, 'automod_antifloodnickwarned', instigator)
