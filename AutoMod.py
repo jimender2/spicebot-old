8ee3fd0b125d2a99c51f56d9c5f1ed217218a13f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-17 17:13:00 -0500

    Update and rename Anti-Flood-Mod.py to AutoMod.py

--- AutoMod.py
+++ AutoMod.py
@@ -14,23 +14,43 @@ from SpicebotShared import *
 @thread(False)
 @rule('(.*)')
 @priority('low')
-def automodspam(bot, trigger):
-    channel = trigger.sender
-    instigator = trigger.nick
+def automod(bot, trigger):
     if not trigger.is_privmsg:
+        
+        floodyell = 0
+        antifloodwarning = str(instigator + ", please do not flood the channel.")
+        
+        channel = trigger.sender
+        instigator = trigger.nick
+        currentmessage = trigger.group(2)
+
+        ## Flooding is 5 lines in a row by the same person or 3 identical lines
         lastnicksubmit = get_botdatabase_value(bot, channel, 'automod_antifloodnick') or bot.nick
         if lastnicksubmit != instigator:
             set_botdatabase_value(bot, channel, 'automod_antifloodnick', instigator)
             set_botdatabase_value(bot, channel, 'automod_antifloodcount', 1)
             set_botdatabase_value(bot, channel, 'automod_antifloodnickwarned', None)
+            set_botdatabase_value(bot, channel, 'automod_antifloodmessage', currentmessage)
+            set_botdatabase_value(bot, channel, 'automod_antifloodmessagecount', 1)
         else:
+            lastmessage = get_botdatabase_value(bot, channel, 'automod_antifloodmessage') or ''
+            if currentmessage != lastmessage:
+                set_botdatabase_value(bot, channel, 'automod_antifloodmessage', currentmessage)
+                set_botdatabase_value(bot, channel, 'automod_antifloodmessagecount', 1)
+            else:
+                adjust_botdatabase_value(bot, channel, 'automod_antifloodmessagecount', 1)
+                getcurrentmessagecount = get_botdatabase_value(bot, channel, 'automod_antifloodmessagecount') or 1
+                if int(getcurrentmessagecount) >= 3:
+                    floodyell = 1
+            lastnicksubmit = get_botdatabase_value(bot, channel, 'automod_antifloodnick') or bot.nick
             adjust_botdatabase_value(bot, channel, 'automod_antifloodcount', 1)
             getcurrentcount = get_botdatabase_value(bot, channel, 'automod_antifloodcount') or 1
             if int(getcurrentcount) >= 5:
-                antifloodwarning = str(instigator + ", Please do not flood the channel.")
                 lastnicksubmitwarned = get_botdatabase_value(bot, channel, 'automod_antifloodnickwarned') or bot.nick
                 if lastnicksubmitwarned != instigator:
                     set_botdatabase_value(bot, channel, 'automod_antifloodnickwarned', instigator)
-                    bot.msg(channel,antifloodwarning)
+                    floodyell = 1 
+        if floodyell:
+            bot.msg(channel,antifloodwarning)
         
 
