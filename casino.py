66c0e91988788073bbec613548dc145ef67afca8
josh-cunning <joshbcunningham@gmail.com>
2017-12-18 08:23:49 -0800

    Update casino.py

--- casino.py
+++ casino.py
@@ -8,7 +8,7 @@ moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
 
-@sopel.module.commands('gamble, casino')
+@sopel.module.commands('gamble', 'casino')
 def mainfunction(bot, trigger):
   enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
   if not enablestatus:
