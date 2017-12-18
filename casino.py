69e0bc4c7ccdc3a7c84ed984cd70fd76b3e088d7
josh-cunning <joshbcunningham@gmail.com>
2017-12-18 08:15:27 -0800

    Update casino.py

--- casino.py
+++ casino.py
@@ -16,40 +16,41 @@ def mainfunction(bot, trigger):
         
 def execute_main(bot, trigger, arg):
   #using computer terms instead of fruit
-  if Spicebucks.spicebucks(bot, trigger.nick, 'minus', 1) == 'true':
-    #spicebanktotal = bot.db.get_nick_value('SpiceBank', 'spicebucks_bank') or 0
-    #Spicebucks.spicebucks(bot, 'SpiceBank', 'plus', 1 + spicebanktotal)
-    #Spicebucks.spicebucks(bot, 'spicebucksslots', 'plus', 1)
-    mywinnings = 0
-    bot.say(trigger.nick + ' inserts 1 spicebuck and pulls the handle on the slot machine')  
-    wheel = ['Modem', 'BSOD'] 
-    wheel1 = spin(wheel)
-    wheel2 = spin(wheel)
-    wheel3 = spin(wheel)
-    reel = [wheel1, wheel2, wheel3]
-    bot.say('The slot machine displays ' + str(reel))
-    for i in reel:
-      if i=='BSOD':
-         mywinnings = mywinnings + 5
-    seen = set()
-    seen_add = seen.add
-    # adds all elements it doesn't know yet to seen and all other to seen_twice
-    seen_twice = set( x for x in reel if x in seen or seen_add(x) )
-    bot.say(str(seen_twice))
-    # turn the set into a list (as requested)
-    if(wheel1 == wheel2 and wheel2 == wheel3):
-      bot.say(trigger.nick + ' got 3 ' + str(wheel1))
-      if wheel1 == 'BSOD':     
-        #spicebanktotal = bot.db.get_nick_value('spicebucksslots', 'spicebucks_bank')
-        bot.say('You hit the Jackpot!!! ' + trigger.nick + ' gets ' + str(mywinnings) + '  spicebucks')
-        Spicebucks.spicebucks(bot, trigger.nick, 'plus', 1)
+  if arg[0] == 'slots':
+    if Spicebucks.spicebucks(bot, trigger.nick, 'minus', 1) == 'true':
+      #spicebanktotal = bot.db.get_nick_value('SpiceBank', 'spicebucks_bank') or 0
+      #Spicebucks.spicebucks(bot, 'SpiceBank', 'plus', 1 + spicebanktotal)
+      #Spicebucks.spicebucks(bot, 'spicebucksslots', 'plus', 1)
+      mywinnings = 0
+      bot.say(trigger.nick + ' inserts 1 spicebuck and pulls the handle on the slot machine')  
+      wheel = ['Modem', 'BSOD'] 
+      wheel1 = spin(wheel)
+      wheel2 = spin(wheel)
+      wheel3 = spin(wheel)
+      reel = [wheel1, wheel2, wheel3]
+      bot.say('The slot machine displays ' + str(reel))
+      for i in reel:
+        if i=='BSOD':
+           mywinnings = mywinnings + 5
+      seen = set()
+      seen_add = seen.add
+      # adds all elements it doesn't know yet to seen and all other to seen_twice
+      seen_twice = set( x for x in reel if x in seen or seen_add(x) )
+      bot.say(str(seen_twice))
+      # turn the set into a list (as requested)
+      if(wheel1 == wheel2 and wheel2 == wheel3):
+        bot.say(trigger.nick + ' got 3 ' + str(wheel1))
+        if wheel1 == 'BSOD':     
+          #spicebanktotal = bot.db.get_nick_value('spicebucksslots', 'spicebucks_bank')
+          bot.say('You hit the Jackpot!!! ' + trigger.nick + ' gets ' + str(mywinnings) + '  spicebucks')
+          Spicebucks.spicebucks(bot, trigger.nick, 'plus', 1)
+        else:
+          bot.say('You get 25 spicebucks')
+      elif(wheel1 == wheel2 or wheel2==wheel3 or wheel3==wheel1):
+        bot.say(trigger.nick + ' got 2 correct and ' + str(mywinnings) + ' spicebucks')
+        Spicebucks.spicebucks(bot, trigger.nick, 'plus', 5)
       else:
-        bot.say('You get 25 spicebucks')
-    elif(wheel1 == wheel2 or wheel2==wheel3 or wheel3==wheel1):
-      bot.say(trigger.nick + ' got 2 correct and ' + str(mywinnings) + ' spicebucks')
-      Spicebucks.spicebucks(bot, trigger.nick, 'plus', 5)
-    else:
-      bot.say(trigger.nick + ' gets nothing')                   
+        bot.say(trigger.nick + ' gets nothing')                   
   
 
 def spin(wheel):
