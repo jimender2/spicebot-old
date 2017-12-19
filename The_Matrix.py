bf056b26f7a125c8a4e7138c63cd73cc86790f3b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-19 14:15:07 -0500

    Update The_Matrix.py

--- The_Matrix.py
+++ The_Matrix.py
@@ -12,17 +12,11 @@ def mainfunction(bot, trigger):
         execute_main(bot, trigger, triggerargsarray)
     
 def execute_main(bot, trigger, triggerargsarray):
-    if trigger.group(2):
-        if trigger.group(2) == 'redpill':
-            bot.say('You take the red pill, you stay in Wonderland, and I show you how deep the rabbit hole goes.')
-        elif trigger.group(2) == 'bluepill':
-            bot.say('You take the blue pill, the story ends. You wake up in your bed and believe whatever you want to believe.')
-        else:
-            normalrun='true'
-    else:
-        normalrun='true'
-    try:
-        if normalrun:
-            bot.say('You have two choices. redpill Or bluepill')        
-    except UnboundLocalError:
-        return
+    pill = get_trigger_arg(triggerargsarray, 1)
+    if not pill:
+        bot.say('You have two choices. redpill Or bluepill')      
+    elif pill == 'redpill':
+        bot.say('You take the red pill, you stay in Wonderland, and I show you how deep the rabbit hole goes.')
+    elif pill == 'bluepill':
+        bot.say('You take the blue pill, the story ends. You wake up in your bed and believe whatever you want to believe.')
+    
