6e2640f7d80e28ddb8d1cfc3ec515abe249ffe79
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-19 12:09:34 -0500

    Update Blame.py

--- Blame.py
+++ Blame.py
@@ -16,6 +16,8 @@ def mainfunction(bot, trigger):
 def execute_main(bot, trigger, triggerargsarray):
     instigator = trigger.nick
     whotoblame = get_trigger_arg(triggerargsarray, 1)
+    for c in bot.channels:
+        channel = c
     if not whotoblame:
         blametargetarray = []
         for channel in bot.channels:
