8fd2a1c50f7a98cc171daa63ce7b9571fdcdf0e9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-19 12:36:42 -0500

    Update Magic_8Ball.py

--- Magic_8Ball.py
+++ Magic_8Ball.py
@@ -7,6 +7,8 @@ moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
 
+eightarray  = ["Only on Tuesdays","42","Not so sure","Negative", "Could be", "Unclear, ask again", "Yes", "No", "Possible, but not probable","Most likely","Absolutely not","I see good things happening","Never","Outlook is good","It is certain"," It is decidedly so","Without a doubt","Yes definitely","You may rely on it","As I see it yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","God says no","Very doubtful","Outlook not so good"]
+
 @sopel.module.commands('8ball')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
@@ -15,6 +17,5 @@ def mainfunction(bot, trigger):
     
 def execute_main(bot, trigger, triggerargsarray):
     bot.say("Let me dig deep into the waters of life, and find your answer")
-    responses  = ["Only on Tuesdays","42","Not so sure","Negative", "Could be", "Unclear, ask again", "Yes", "No", "Possible, but not probable","Most likely","Absolutely not","I see good things happening","Never","Outlook is good","It is certain"," It is decidedly so","Without a doubt","Yes definitely","You may rely on it","As I see it yes","Most likely","Outlook good","Yes","Signs point to yes","Reply hazy try again","Ask again later","Better not tell you now","Cannot predict now","Concentrate and ask again","Don't count on it","My reply is no","God says no","Very doubtful","Outlook not so good"]
-    reply = random.randint(0,len(responses) - 1)
-    bot.say(responses [reply]);
+    reply = get_trigger_arg(eightarray, 'random')
+    bot.say(reply)
