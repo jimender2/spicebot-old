1ebddb59eb2db309edc7cde7de0455626cdfbed5
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-19 12:12:22 -0500

    Update Airbiscuit.py

--- Airbiscuit.py
+++ Airbiscuit.py
@@ -13,6 +13,8 @@ def mainfunction(bot, trigger):
     
 def execute_main(bot, trigger, triggerargsarray):
     target = get_trigger_arg(triggerargsarray, 1)
+    for c in bot.channels:
+        channel = c
     if not target:
         bot.say(trigger.nick + " floats an air biscuit.")
     elif target.lower() not in bot.privileges[channel.lower()]:
