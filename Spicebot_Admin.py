0270dab2f32897f48959306907e7fd2e20554df9
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-19 07:55:25 -0500

    Update Spicebot_Admin.py

--- Spicebot_Admin.py
+++ Spicebot_Admin.py
@@ -4,24 +4,18 @@ import sys
 import re
 from os.path import exists
 import git 
-
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
 from SpicebotShared import *
-
-script_dir = os.path.dirname(__file__)
-log_path = "data/templog.txt"
-log_pathc = "data/*.txt"
-log_file_path = os.path.join(script_dir, log_path)
 
 @sopel.module.require_admin
 @sopel.module.require_privmsg
 @sopel.module.commands('spicebotadmin')
 def spicebotadmin(bot, trigger):
-    triggerargsarray = create_args_array(trigger.group(4))
-    instigator = trigger.nick
-    target = get_trigger_arg(triggerargsarray, '3') or instigator
+    triggerargsarray = create_args_array(trigger.group(2))
     for c in bot.channels:
         channel = c
-    options = str("update, restart, debugreset, debug, pipinstall, countreset, timeoutreset")
+    options = str("update, restart, debugreset, debug, pipinstall, resetcount")
     service = bot.nick.lower()
     if not trigger.group(2):
         bot.say("Which Command Do You want To run?")
@@ -36,26 +30,16 @@ def spicebotadmin(bot, trigger):
             message = get_trigger_arg(triggerargsarray, '2+')
             if message:
                 bot.msg(channel,message)
-        if commandused == 'countreset':
-            bot.msg(channel, trigger.nick + ' wants me to reset the count for ' + target)
-        if commandused == 'timeoutreset':
-            lastusetime = get_botdatabase_value(bot, target, 'lastusagetime') ## get last usetime
-            #target = trigger.group(3)
-            bot.msg(channel, instigator + ', the last use time for ' + target + ' is ' + str(lastusetime))
-            ## Update user's last use timestamp
+        if commandused == 'resetcount':
+            bot.msg(channel, trigger.nick + 'wants me to do things here')
+            bot.action('will think about it')
         elif commandused == 'update':
             bot.msg(channel, trigger.nick + " commanded me to update from Github and restart. Be Back Soon!")
             update(bot, trigger)
-            #cleandir(bot, trigger)
-            #debuglogreset(bot, trigger)
             restart(bot, trigger, service)
         elif commandused == 'restart':
             bot.msg(channel, trigger.nick + " Commanded me to restart. Be Back Soon!")
-            #cleandir(bot, trigger)
-            #debuglogreset(bot, trigger)
             restart(bot, trigger, service)
-        #elif commandused == 'debugreset':
-            #debuglogreset(bot, trigger)
         elif commandused == 'debug':
             debugloglinenumberarray = []
             bot.action('Is Copying Log')
@@ -95,83 +79,7 @@ def restart(bot, trigger, service):
 
 def update(bot, trigger):
     bot.say('Pulling From Github...')
-    g = git.cmd.Git(script_dir)
+    g = git.cmd.Git(moduledir)
     g.pull()
-    
-def cleandir(bot, trigger):
-    bot.say('Cleaning Directory...')
-    os.system("sudo rm " + script_dir + "/*.pyc")
-    
-def debuglogreset(bot, trigger):
-    service = bot.nick.lower()
-    bot.action('Is Copying Log')
-    os.system("sudo journalctl -u " + service + " >> " + log_file_path)
-    bot.action('Is Removing Log')
-    os.system("sudo rm " + log_file_path)
 
-##########
-## Args ##
-##########
 
-def create_args_array(fullstring):
-    triggerargsarray = []
-    if fullstring:
-        for word in fullstring.split():
-            triggerargsarray.append(word)
-    return triggerargsarray
-
-def get_trigger_arg(triggerargsarray, number):
-    totalarray = len(triggerargsarray)
-    totalarray = totalarray + 1
-    triggerarg = ''
-    if "^" in str(number) or number == 0 or str(number).endswith("+") or str(number).endswith("-") or str(number).endswith("<") or str(number).endswith(">"):
-        if str(number).endswith("+"):
-            rangea = re.sub(r"\+", '', str(number))
-            rangea = int(rangea)
-            rangeb = totalarray
-        elif str(number).endswith("-"):
-            rangea = 1
-            rangeb = re.sub(r"-", '', str(number))
-            rangeb = int(rangeb) + 1
-        elif str(number).endswith(">"):
-            rangea = re.sub(r">", '', str(number))
-            rangea = int(rangea) + 1
-            rangeb = totalarray
-        elif str(number).endswith("<"):
-            rangea = 1
-            rangeb = re.sub(r"<", '', str(number))
-            rangeb = int(rangeb)
-        elif "^" in str(number):
-            rangea = number.split("^", 1)[0]
-            rangeb = number.split("^", 1)[1]
-            rangea = int(rangea)
-            rangeb = int(rangeb) + 1
-        elif number == 0:
-            rangea = 1
-            rangeb = totalarray
-        if rangea <= totalarray:
-            for i in range(rangea,rangeb):
-                arg = get_trigger_arg(triggerargsarray, i)
-                if triggerarg != '':
-                    triggerarg = str(triggerarg + " " + arg)
-                else:
-                    triggerarg = str(arg)
-    elif number == 'last':
-        totalarray = totalarray -2
-        triggerarg = str(triggerargsarray[totalarray])
-    elif str(number).endswith("!"):
-        number = re.sub(r"!", '', str(number))
-        for i in range(1,totalarray):
-            if int(i) != int(number):
-                arg = get_trigger_arg(triggerargsarray, i)
-                if triggerarg != '':
-                    triggerarg = str(triggerarg + " " + arg)
-                else:
-                    triggerarg = str(arg)
-    else:
-        number = int(number) - 1
-        try:
-            triggerarg = triggerargsarray[number]
-        except IndexError:
-            triggerarg = ''
-    return triggerarg
