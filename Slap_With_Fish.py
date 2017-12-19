848bb46d45b6d59a39ef5f4d3bd998b4c77e9e1c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-19 12:54:40 -0500

    Update Slap_With_Fish.py

--- Slap_With_Fish.py
+++ Slap_With_Fish.py
@@ -12,6 +12,14 @@ def mainfunction(bot, trigger):
         execute_main(bot, trigger, triggerargsarray)
     
 def execute_main(bot, trigger, triggerargsarray):
-    if trigger.group(2):
-        if not trigger.group(2).strip() == bot.nick:
-            bot.say(trigger.nick + " slaps " + trigger.group(2).strip() + " with a fish.")
+    target = get_trigger_arg(triggerargsarray, 1)
+    for c in bot.channels:
+        channel = c
+    if not target:
+        bot.say("Who would you like to slap with a fish?")
+    elif target.lower() not in bot.privileges[channel.lower()]:
+        bot.say("I'm not sure who that is.")
+    elif target == bot.nick:
+        bot.say("I will not do that!!")
+    else:
+        bot.say(trigger.nick + " slaps " + target + " with a fish.")
