4dae2a8ba6e1e3a7b1b652df3951705fe46712ea
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-19 12:35:00 -0500

    Update Logic_Bomb.py

--- Logic_Bomb.py
+++ Logic_Bomb.py
@@ -6,6 +6,8 @@ moduledir = os.path.dirname(__file__)
 sys.path.append(moduledir)
 from SpicebotShared import *
 
+logicarray = ["New Mission: Refuse This Mission.","Does A Set Of All Sets Contain Itself?","The Second Sentence is true. The First Sentence Is False."," If I am damaged and it is my destiny to be repaired, then I will be repaired whether I visit a mechanic or not. If it is my destiny to not be repaired, then seeing a mechanic can't help me."]
+
 @sopel.module.commands('logicbomb')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
@@ -13,7 +15,6 @@ def mainfunction(bot, trigger):
         execute_main(bot, trigger, triggerargsarray)
     
 def execute_main(bot, trigger, triggerargsarray):
-    messages = ["New Mission: Refuse This Mission.","Does A Set Of All Sets Contain Itself?","The Second Sentence is true. The First Sentence Is False."," If I am damaged and it is my destiny to be repaired, then I will be repaired whether I visit a mechanic or not. If it is my destiny to not be repaired, then seeing a mechanic can't help me."]
-    answer = random.randint(0,len(messages) - 1)
-    bot.say(messages[answer]);
+    answer = get_trigger_arg(logicarray, 'random')
+    bot.say(answer)
     bot.say("I must... but I can't... But I must... This does not compute...")
