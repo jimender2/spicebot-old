c937a6187f10b0875f91527387034a5a614228a1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-19 08:51:51 -0500

    Update Spicebotnew.py

--- Spicebotnew.py
+++ Spicebotnew.py
@@ -22,34 +22,57 @@ TOOMANYTIMES = 15
 LASTTIMEOUT = 60
 LASTTIMEOUTHOUR = 3600
 
+GITWIKIURL = "https://github.com/deathbybandaid/sopel-modules/wiki"
+
+validsubcommandarray = ['options','docs','help','warn,'channel','modulecount','isowner']
+
 @sopel.module.commands('spicebot','spicebotadmin')
 def main_command(bot, trigger):
+    now = time.time()
     maincommandused = trigger.group(1)
-    subcommand = get_trigger_arg(triggerargsarray, '3')
+    subcommand = get_trigger_arg(triggerargsarray, 2)
     instigator = trigger.nick
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
-    botownerarray, operatorarray, voicearray, adminsarray = special_users(bot)
+    botownerarray, operatorarray, voicearray, adminsarray, allusersinroomarray, channel = special_users(bot)
     inchannel = trigger.sender
-    if maincommandused.endswith('admin') and instigator not in adminsarray:
-        bot.notice(instigator + "This is an admin only function.", instigator)
-    elif maincommandused.endswith('admin') and inchannel.startswith("#"):
-        bot.notice(instigator + "This admin function must be used in Privmsg.", instigator)
-    elif maincommandused.endswith('admin') and instigator in adminsarray:
-        admin_functions(bot, trigger, triggerargsarray)
-    else:
-        allowedcommandsarray = ['on','off']
-        if not enablestatus or subcommand in allowedcommandsarray:
-            user_functions(bot, trigger, triggerargsarray)
-        
-def admin_functions(bot, trigger, triggerargsarray):
-    bot.say('admin')
-        
-def user_functions(bot, trigger, triggerargsarray):
-    now = time.time()
-    bot.say('user')
+    commandlist = get_trigger_arg(validsubcommandarray, "list")
+    if not subcommand:
+        bot.say("That's my name. Don't wear it out!")
+    elif subcommand not in validsubcommandarray or subcommand == 'options':
+        bot.say("Invalid command. Options are: " + commandlist +".")
     
+    ## Docs
+    elif subcommand == 'help' or subcommand == 'docs':
+        bot.notice(instigator + ", Online Docs: " + GITWIKIURL, instigator)
         
-
+    ## Warn against Bot abuse
+    elif subcommand == 'warn':
+        target = get_trigger_arg(triggerargsarray, 3) or ''
+        bot.msg(channel, target + "This is just a warning. Overuse of the bot, can get you kicked or banned by an operator. If you want to purely play with the bot, go to ##SpiceBotTest, or send Spicebot a PrivateMessage.")
         
+    ## Channel
+    elif subcommand == 'channel':
+        bot.say("You can find me in " + channel)
+    
+    ## Modules
+    elif subcommand == 'modulecount':
+        modulecount = str(len(fnmatch.filter(os.listdir(moduledir), '*.py')))
+        bot.say('There are currently ' + modulecount +' custom modules installed.')
+        
+    ## Bot Owner
+    elif subcommand == 'isowner':
+        target = get_trigger_arg(triggerargsarray, 3) or instigator
+        if target not in allusersinroomarray:
+            bot.notice(instigator + ", It looks like " + target + " is either not here, or not a valid person.", instigator)
+            
+        
+        
+    ##
+    
+    ##
+    
+    ##
+    
+    ##
         
     
