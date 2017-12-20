721728e8f416b3406ae8315cb89e6de20db23683
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-20 14:23:18 -0500

    Update Freenode.py

--- modules/tools/Freenode.py
+++ modules/tools/Freenode.py
@@ -7,20 +7,17 @@ import os
 import random
 from sopel.module import OP
 
-responseperson = ['Dumbledore', 'Gandalf', 'Frodo', 'Captain James T. Kirk', 'Rainbow Bright', 'New Kids on the Block']
-
 @sopel.module.commands('msg', 'nick', 'attach', 'server', 'join', 'whois', 'me', 'ban')
 def execute_main(bot, trigger):
     for c in bot.channels:
         channel = c
-    random.shuffle(responseperson)
     operatorarray = []
     for u in bot.channels[channel.lower()].users:
         if bot.privileges[channel.lower()][u] == OP:
             operatorarray.append(u)
     if trigger.startswith('.ban') and trigger.nick.lower() not in operatorarray:
-        bot.say('"You have no power here." - ' + responseperson[0])
+        bot.say("You have no power here.")
     else:
         trigger = trigger.replace('.', '/', 1)
-        bot.say('"I believe you wanted to say ' + trigger + '" - ' + responseperson[0])
+        bot.say('"I believe you wanted to say ' + trigger)
                 
