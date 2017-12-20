0dd6c9560da09552e68aa6f56657a0094180e4cb
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-20 08:27:12 -0500

    Update HungerGames.py

--- HungerGames.py
+++ HungerGames.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
 import sopel
 from sopel import module, tools
 import random
