4232d8cf9f4e747cd0cd9f78bc70d0dc4225c0af
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-20 10:13:45 -0500

    Update dbnew.py

--- hackypatches/dbnew.py
+++ hackypatches/dbnew.py
@@ -1,1 +1,31 @@
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+##################################################################################################
+## This is an attempt to bridge the gap between Sopel and Willie in regards to database support ##
+## sqlite seems to not be capable of handling what SpiceBot needs it to                         ##
+##################################################################################################
+
+import os
+import json
+import os.path
+import sys
+from sopel.tools import Identifier
+
+if sys.version_info.major >= 3:
+    unicode = str
+    basestring = str
+    
+# def _deserialize(value)   goes here
+    
+    
+    
+class SopelDB(object):
+    
+
+
+
+
+
+
 
