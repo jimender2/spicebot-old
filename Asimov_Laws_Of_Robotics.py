07dfe1eff7d7bf53a7912322b91fde335dec5593
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-20 08:26:14 -0500

    Update Asimov_Laws_Of_Robotics.py

--- Asimov_Laws_Of_Robotics.py
+++ Asimov_Laws_Of_Robotics.py
@@ -15,8 +15,7 @@ def mainfunction(bot, trigger):
         execute_main(bot, trigger, triggerargsarray)
     
 def execute_main(bot, trigger, triggerargsarray):
-    if not trigger.group(2):
-        bot.action('may not injure a human being or, through inaction, allow a human being to come to harm.')
-        bot.action('must obey orders given it by human beings except where such orders would conflict with the First Law.')
-        bot.action('must protect its own existence as long as such protection does not conflict with the First or Second Law.')
-        bot.action('must comply with all chatroom rules.')
+    bot.action('may not injure a human being or, through inaction, allow a human being to come to harm.')
+    bot.action('must obey orders given it by human beings except where such orders would conflict with the First Law.')
+    bot.action('must protect its own existence as long as such protection does not conflict with the First or Second Law.')
+    bot.action('must comply with all chatroom rules.')
