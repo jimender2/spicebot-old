095af2b2bb71e9f85fce3181af451f596df5a11f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-20 14:21:19 -0500

    Update Freenode.py

--- modules/tools/Freenode.py
+++ modules/tools/Freenode.py
@@ -6,18 +6,10 @@ import sys
 import os
 import random
 from sopel.module import OP
-moduledir = os.path.dirname(__file__)
-sys.path.append(moduledir)
-from SpicebotShared import *
 
 responseperson = ['Dumbledore', 'Gandalf', 'Frodo', 'Captain James T. Kirk', 'Rainbow Bright', 'New Kids on the Block']
 
 @sopel.module.commands('msg', 'nick', 'attach', 'server', 'join', 'whois', 'me', 'ban')
-def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
-    if not enablestatus:
-        execute_main(bot, trigger)
-    
 def execute_main(bot, trigger):
     for c in bot.channels:
         channel = c
