4bdce862877e24431b77aca4546549108d346da1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-21 15:26:23 -0500

    Update WebMD.py

--- modules/development/WebMD.py
+++ modules/development/WebMD.py
@@ -7,8 +7,8 @@ from sopel import module, tools
 import random
 import sys
 import os
-moduledir = os.path.dirname(__file__)
-sys.path.append(moduledir)
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
 from SpicebotShared import *
 
 @sopel.module.commands('webmd','webmdadd','webmddel')
