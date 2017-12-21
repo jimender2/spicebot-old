964b68657d7db7dced056762c54eb37d33d2bdba
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-21 12:44:13 -0500

    Update Quote.py

--- modules/development/Quote.py
+++ modules/development/Quote.py
@@ -6,8 +6,8 @@ import requests
 import json
 import sys
 import os
-moduledir = os.path.dirname(__file__)
-sys.path.append(moduledir)
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
 from SpicebotShared import *
 
 @sopel.module.commands('quote')
