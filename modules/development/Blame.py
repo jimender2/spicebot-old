f3aa0418109cb784cea3a238a4e45b17d18a7d74
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-21 15:23:22 -0500

    Update Blame.py

--- modules/development/Blame.py
+++ modules/development/Blame.py
@@ -5,9 +5,8 @@ import sopel
 from sopel import module, tools
 import random
 import sys
-import os
-moduledir = os.path.dirname(__file__)
-sys.path.append(moduledir)
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
 from SpicebotShared import *
 
 @sopel.module.commands('blame')
