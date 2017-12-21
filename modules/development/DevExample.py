8c4c9713696933d39c4e6ecbf6c06989680af12a
Percussive-Maintenance <32780296+Percussive-Maintenance@users.noreply.github.com>
2017-12-21 16:48:06 -0600

    Create DevExample.py

---
+++ modules/development/DevExample.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+import sopel.module
+import sys
+import os
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from SpicebotShared import *
+
+@sopel.module.commands('devexample')
+def mainfunction(bot, trigger):
+    execute_main(bot, trigger)
+    
+def execute_main(bot, trigger):
+    bot.notice("This is to help the dev team understand values.", trigger.nick)
+    bot.notice("Trigger.nick: " + trigger.nick, trigger.nick)
+    bot.notice("Trigger.args: " + trigger.args, trigger.nick)
+    
