7b43e52bc45daeeab6087e53e867db3d1f2a2aa5
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-21 16:04:55 -0500

    Update test.py

--- modules/development/test.py
+++ modules/development/test.py
@@ -14,13 +14,6 @@ def mainfunction(bot, trigger):
     execute_main(bot, trigger, triggerargsarray)
     
 def execute_main(bot, trigger, triggerargsarray):
-    for u in bot.channels[channel.lower() for channel in bot.channels].users:
-        bot.say(str(u))
-    
-    #for u in [channel.lower() for channel in bot.channels]:
-    #    bot.say(str(u))
-    
-    
-    #for channel in bot.channels:
-    #    for u in bot.channels[channel.lower()].users:
-    #        bot.say(str(u))
+    for channel in bot.channels:
+        for u in bot.channels[channel.lower()].users:
+            bot.say(str(u))
