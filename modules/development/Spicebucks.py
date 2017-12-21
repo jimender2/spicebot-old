5d7705c937137bd81f784502808cbba4f4d525c2
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-21 15:25:52 -0500

    Update Spicebucks.py

--- modules/development/Spicebucks.py
+++ modules/development/Spicebucks.py
@@ -6,8 +6,8 @@ import datetime
 from sopel import module, tools
 import sys
 import os
-moduledir = os.path.dirname(__file__)
-sys.path.append(moduledir)
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
 from SpicebotShared import *
 
 @sopel.module.commands('spicebucks')
