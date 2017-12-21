aade3c09b4a53a5dce67a49bde28e7554719250f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-21 12:42:46 -0500

    Update Logic_Bomb.py

--- modules/development/Logic_Bomb.py
+++ modules/development/Logic_Bomb.py
@@ -5,8 +5,8 @@ import sopel.module
 import random
 import sys
 import os
-moduledir = os.path.dirname(__file__)
-sys.path.append(moduledir)
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
 from SpicebotShared import *
 
 logicarray = ["New Mission: Refuse This Mission.","Does A Set Of All Sets Contain Itself?","The Second Sentence is true. The First Sentence Is False."," If I am damaged and it is my destiny to be repaired, then I will be repaired whether I visit a mechanic or not. If it is my destiny to not be repaired, then seeing a mechanic can't help me."]
