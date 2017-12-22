38585cba0a9f8a2417fb26f8fa960eb87539a011
Dyson Parkes <dyson@berserkir.net>
2017-12-23 10:21:23 +1300

    Create testimport.py

---
+++ modules/development/testimport.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from SpicebotShared import *
+import nuke
+
+@sopel.module.commands('importtest')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray)
+  
+def execute_main(bot, trigger, triggerargsarray):
+    ## Initial ARGS
+    triggerargsarray = create_args_array(trigger.group(2)) ## triggerarg 0 = commandused
+    subcommandused = get_trigger_arg(triggerargsarray, 1)
+     
+    if subcommandused == 'nuke':
+        nukeit(bot, trigger, triggerargsarray)
+    else:
+      bot.say('Module error')
