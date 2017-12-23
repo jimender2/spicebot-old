d8febf480f0071c87996163e61422cb5b8bdec06
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-23 11:48:23 -0500

    Update SpicebotShared.py

--- modules/SpicebotShared.py
+++ modules/SpicebotShared.py
@@ -243,6 +243,13 @@ def get_timesince(bot, nick, databasekey):
     last = get_botdatabase_value(bot, nick, databasekey) or 0
     return abs(now - int(last))
 
+def get_timeuntil(bot, nowtime, futuretime):
+    a = arrow.get(now)
+    b = arrow.get(futuretime)
+    timecompare = (b.humanize(a, granularity='auto'))
+    return timecompare
+
+
 ###########
 ## Tools ##
 ###########
