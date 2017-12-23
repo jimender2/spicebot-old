3bb28ec66b780facc7148bc4f23ad60e1bedb6af
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-23 16:42:11 -0500

    Update Search.py

--- modules/development/Search.py
+++ modules/development/Search.py
@@ -20,7 +20,7 @@ def searchfor(data):
 
 @commands('google')
 def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
     
