d5b2ede085e03eee09ac65dc76dcbae2fc8406df
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-23 16:43:31 -0500

    Update Casino.py

--- modules/development/Casino.py
+++ modules/development/Casino.py
@@ -15,7 +15,7 @@ from SpicebotShared import *
 
 @sopel.module.commands('gamble', 'casino')
 def mainfunction(bot, trigger):
-	enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
+	enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
   	if not enablestatus:
     		execute_main(bot, trigger, triggerargsarray)
         
