cfadfd84f017ef97b7e4c0beeea8cf8d68f47211
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-23 16:32:27 -0500

    Update Asimov_Laws_Of_Robotics.py

--- modules/Main/Asimov_Laws_Of_Robotics.py
+++ modules/Main/Asimov_Laws_Of_Robotics.py
@@ -12,7 +12,7 @@ from SpicebotShared import *
 
 @sopel.module.commands('asimov')
 def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
     
