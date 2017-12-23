a306deef48f8f3a70eb3af70b754589f842275d1
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-23 16:37:12 -0500

    Update Shrug.py

--- modules/Main/Shrug.py
+++ modules/Main/Shrug.py
@@ -10,7 +10,7 @@ from SpicebotShared import *
 
 @sopel.module.commands('shrug','unicode','unicodeshrug')
 def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
     
