70e9f0d22939775fef46a54730d9e04b18a435b3
josh-cunning <joshbcunningham@gmail.com>
2017-12-23 08:34:12 -0800

    Update Search.py

--- modules/development/Search.py
+++ modules/development/Search.py
@@ -11,19 +11,35 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from SpicebotShared import *
 
-@sopel.module.commands('google','googleit','search')
+@sopel.module.commands('google','googleit','search', 'video', 'youtube')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
     
 def execute_main(bot, trigger, triggerargsarray):
-    searchterm = get_trigger_arg(triggerargsarray, 0)
+    triggerargsarray = create_args_array(trigger.group(2)) ## triggerarg 0 = commandused
+    commandused = trigger.group(1) 
+    searchterm = get_trigger_arg(triggerargsarray, 1)
     if not searchterm:
         bot.say('Please enter a term to search for')
     else:
-        data = searchterm.strip().lower()
-        data=data.replace(' ', '%20').replace('site:', 'site%3A')
-        var = requests.get(r'http://www.google.com/search?q=' + data + '&btnI')
-        bot.say(str(var.url))
+        if (commandused=='video', or commandused=='youtube'):
+            data = searchterm.strip().lower()
+            data.append('site%3Ayoutube.com')
+             query=searchfor(data)
+        else:
+            data = searchterm.strip().lower()
+            data=data .replace('site:', 'site%3A')
+            query=searchfor(data)
+    if not query:
+         bot.say('I cannot find anything about that')
+    else:
+         bot.say(query)
+            
+def searchfor(data):
+    data=data.replace(' ', '%20')
+    var = requests.get(r'http://www.google.com/search?q=' + data + '&btnI')
+    query=str(var.url)
+    return query
     
