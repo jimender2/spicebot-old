29af1ae5c12df39fbb56b954f59a2cc5fdc1843b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-23 13:01:24 -0500

    Update User-Commands.py

--- modules/Bot/User-Commands.py
+++ modules/Bot/User-Commands.py
@@ -2,19 +2,8 @@
 # coding=utf-8
 from __future__ import unicode_literals, absolute_import, print_function, division
 import sopel.module
-from sopel import module, tools
-from sopel.module import ADMIN
-from sopel.module import VOICE
-from sopel.module import event, rule
-from sopel.module import OP
-from sopel.tools.target import User, Channel
-import time
 import os
 import sys
-import fnmatch
-import re
-import git 
-from os.path import exists
 moduledir = os.path.dirname(__file__)
 shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
