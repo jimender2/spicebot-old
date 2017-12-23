1c1b19493cf79258cd89840d9b57a3211bb29bbb
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-23 16:34:33 -0500

    Update Giphy.py

--- modules/Main/Giphy.py
+++ modules/Main/Giphy.py
@@ -14,7 +14,7 @@ from SpicebotShared import *
 
 @sopel.module.commands('gif','giphy')
 def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
     
