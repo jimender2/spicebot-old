b129aaa34ea318944087edfb807f16495819f7bf
josh-cunning <joshbcunningham@gmail.com>
2017-12-23 06:39:01 -0800

    Update SpiceWorks_IRC_Rules.py

--- modules/IT/SpiceWorks_IRC_Rules.py
+++ modules/IT/SpiceWorks_IRC_Rules.py
@@ -12,6 +12,7 @@ rulesurl = 'https://pastebin.com/raw/Vrq9bHBD'
 @sopel.module.commands('rules','rule')
 def execute_main(bot, trigger):
     rulenumber = trigger.group(2)
+    bot.say(str(rulenumber)
     if not rulenumber:
         myline='Chat Rules:     https://pastebin.com/Vrq9bHBD'
     else:
@@ -26,20 +27,20 @@ def execute_main(bot, trigger):
                     myline= 'That doesnt appear to be a rule number.'
             else:
                 rulenumber = int(rulenumber)    
-                htmlfile=urllib.urlopen(rulesurl)
-                lines=htmlfile.readlines()
-                try:
-                    if str(rulenumber) != '0':
-                        myline=lines[rulenumber-1]
-                    else:
-                        myline='Rule Zero (read the rules):     https://pastebin.com/Vrq9bHBD'
-                except IndexError or TypeError:
-                    if rulenumber == 69:
-                        myline='giggles'
-                    elif rulenumber == 34:
-                        myline='If it exists, there is porn of it.'
-                    else:
-                        myline= 'That doesnt appear to be a rule number.'
+            htmlfile=urllib.urlopen(rulesurl)
+            lines=htmlfile.readlines()
+            try:
+                if str(rulenumber) != '0':
+                    myline=lines[rulenumber-1]
+                else:
+                    myline='Rule Zero (read the rules):     https://pastebin.com/Vrq9bHBD'
+            except IndexError or TypeError:
+                if rulenumber == 69:
+                    myline='giggles'
+                elif rulenumber == 34:
+                    myline='If it exists, there is porn of it.'
+                else:
+                    myline= 'That doesnt appear to be a rule number.'
         if myline == 'giggles':
             bot.action(myline)
         else:
