644f637ea3731d6ad77a84179eddc5492918ab32
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-23 14:04:53 -0500

    Update Nuke.py

--- modules/Main/Nuke.py
+++ modules/Main/Nuke.py
@@ -8,7 +8,7 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from SpicebotShared import *
 
-@sopel.module.commands('nuke','killit','terminate')
+@sopel.module.commands('nuke','killit','terminate','testkill')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
     if not enablestatus:
