869d6a004e465a5f0b43c1d87dcbe9c2ee99996b
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-23 11:30:15 -0500

    Create duels.py

---
+++ modules/development/duels.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+from sopel.module import OP
+from sopel.module import ADMIN
+from sopel.module import VOICE
+import sopel
+from sopel import module, tools
+import random
+from random import randint
+import time
+import datetime
+import re
+import sys
+import os
+from os.path import exists
