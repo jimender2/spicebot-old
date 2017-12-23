582f13f031b8363f2c73b4c9298d151c1eaafb42
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-23 16:35:15 -0500

    Update Lets_Be_Friends.py

--- modules/Main/Lets_Be_Friends.py
+++ modules/Main/Lets_Be_Friends.py
@@ -10,7 +10,7 @@ from SpicebotShared import *
 
 @sopel.module.commands('fuckyouspicebot','fuckspicebot','banspicebot','kickspicebot','hatespicebot','fuckoffspicebot')
 def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
     
