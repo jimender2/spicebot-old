f21603b470c75b4fc845fcc4df2cf7ae5048127f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-23 12:47:50 -0500

    Create Warn.py

---
+++ modules/Bot/Warn.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from SpicebotShared import *
+
+@sopel.module.commands('warn')
+def mainfunction(bot, trigger):
+  
+  target = get_trigger_arg(triggerargsarray, 2) or ''
