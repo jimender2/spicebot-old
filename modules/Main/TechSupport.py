e8c94bcca3a13c1be55109e7b44813eb43812279
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-12-23 16:37:42 -0500

    Update TechSupport.py

--- modules/Main/TechSupport.py
+++ modules/Main/TechSupport.py
@@ -13,7 +13,7 @@ techmessages = ["YOU MUST CONSTRUCT ADDITIONAL PYLONS!","Have you tried flinging
 
 @sopel.module.commands('techsupport','itsupport')
 def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
     
