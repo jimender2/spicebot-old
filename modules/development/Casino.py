47d721a3451dec4b1301cd62d62f48e3edd7f9ad
josh-cunning <joshbcunningham@gmail.com>
2018-01-02 11:55:10 -0800

    Update Casino.py

--- modules/development/Casino.py
+++ modules/development/Casino.py
@@ -18,7 +18,7 @@ maxbet = 100
 
 @sopel.module.commands('gamble', 'casino')
 def mainfunction(bot, trigger):
-	enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, 'casino')
+	enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, 'gamble')
   	if not enablestatus:
     		execute_main(bot, trigger, triggerargsarray)
         
