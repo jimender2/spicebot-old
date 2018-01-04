8d5c6a8e1a1948949f46dbb30928a55b01b3ccfc
josh-cunning <joshbcunningham@gmail.com>
2018-01-03 16:00:51 -0800

    Update Casino.py

--- modules/development/Casino.py
+++ modules/development/Casino.py
@@ -43,7 +43,7 @@ def execute_main(bot, trigger, arg):
 	elif mygame == 'colors':
 		currentcolors =bot.db.get_nick_value('ColorCount','colors') or 0
 		bot.say(currentcolors)
-		bot.db.set_nick_value('ColorCount','colors', 0)
+		bot.db.set_nick_value('ColorCount','colors', ' ')
 		
     	else:
         	bot.say('Please choose a game')
@@ -174,8 +174,8 @@ def roulette(bot,trigger,arg):
             		winningnumber = spin(wheel)
             		color = spin(colors)
 			if bot.nick.endswith('dev'): 					
-				currentcolors =bot.db.get_nick_value('ColorCount','colors') or 0
-				currentcolors = color+currentcolors
+				currentcolors =bot.db.get_nick_value('ColorCount','colors') or ' '
+				currentcolors = color+str(currentcolors)
 				bot.db.set_nick_value('ColorCount','colors', currentcolors)
 		 	bot.say('The wheel stops on ' + str(winningnumber) + ' ' + color)
             		mywinnings=0
