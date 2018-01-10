b606eac37537f6d7e66bf33f9befee6508a4312b
Dyson Parkes <dyson@berserkir.net>
2018-01-11 07:48:31 +1300

    Update Suckers.py

--- modules/Main/Suckers.py
+++ modules/Main/Suckers.py
@@ -10,7 +10,7 @@ from SpicebotShared import *
 
 @sopel.module.commands('sucker','suckers')
 def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, 'sucker')
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
     
