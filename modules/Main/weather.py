f3d27c425fe2e0499644dc153f272dabb3972f58
josh-cunning <joshbcunningham@gmail.com>
2018-01-10 10:45:58 -0800

    Update weather.py

--- modules/Main/weather.py
+++ modules/Main/weather.py
@@ -133,6 +133,14 @@ def execute_main(bot, trigger, triggerargsarray):
     location = trigger.group(2)
     if location == 'setlocation':
         update_location(bot, triggersarray[2])
+    elif location == 'getlocation':
+        woeid = bot.db.get_nick_value(trigger.nick, 'woeid') or 0
+        if woeid == 0:
+            bot.say("You must first set a location using .weather setloction <place>")
+        else:
+            bot.say(trigger.nick + "'s location is set to " +str(woeid))
+            
+        
     else:
         woeid = ''
         if not location:
