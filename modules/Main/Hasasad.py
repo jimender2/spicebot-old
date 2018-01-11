24ab09e6f675b5989106d570bc9824e423c7c4a5
Dyson Parkes <dyson@berserkir.net>
2018-01-12 11:54:15 +1300

    Update and rename modules/development/Hasasad.py to modules/Main/Hasasad.py

--- modules/Main/Hasasad.py
+++ modules/Main/Hasasad.py
@@ -12,7 +12,7 @@ from SpicebotShared import *
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, 'hasasad')
     if not enablestatus:
-        execute_main(bot, trigger, triggerargsarray)
+        execute_main(bot, trigger)
     
-def execute_main(bot, trigger, triggerargsarray):
+def execute_main(bot, trigger):
     bot.action("sits in the corner and cries.")
