471a69ae5ceac544b51b4784d1fae52f774926bc
Dyson Parkes <dyson@berserkir.net>
2018-01-24 23:27:41 +1300

    Update Fuckery.py

--- modules/development/Fuckery.py
+++ modules/development/Fuckery.py
@@ -18,7 +18,7 @@ def main_command(bot, trigger):
     instigator = trigger.nick
     botownerarray, operatorarray, voicearray, adminsarray, allusersinroomarray = special_users(bot)
     botusersarray = get_botdatabase_value(bot, bot.nick, 'botusers') or []
-    duelusersarray = get_botdatabase_value(bot, bot.nick, 'duelusers') or []
+    duelusersarray = get_database_value(bot, bot.nick, 'duelusers') or []
     inchannel = trigger.sender
     
     if not subcommand:
@@ -34,11 +34,38 @@ def main_command(bot, trigger):
             if instigator not in botusersarray:
                 adjust_database_array(bot, bot.nick, instigator, 'botusers', 'add')
             if instigator not in duelusersarray:
-                adjust_database_array(bot, bot.nick, instigator, 'duelusers', 'add')
+                adjust_db_array(bot, bot.nick, instigator, 'duelusers', 'add')
             bot.say(instigator + " has now opted in to every optional module. Let the fuckery commence!")
         else:
             if instigator in botusersarray:
                 adjust_database_array(bot, bot.nick, instigator, 'botusers', 'del')
             if instigator in duelusersarray:
-                adjust_database_array(bot, bot.nick, instigator, 'duelusers', 'del')
+                adjust_db_array(bot, bot.nick, instigator, 'duelusers', 'del')
             bot.say(instigator + " has opted out of using the bot. Quitter.")
+
+## duel database functions
+def get_database_value(bot, nick, databasekey):
+    databasecolumn = str('duels_' + databasekey)
+    database_value = bot.db.get_nick_value(nick, databasecolumn) or 0
+return database_value
+
+def adjust_db_array(bot, nick, entry, databasekey, adjustmentdirection):
+    adjustarray = get_database_value(bot, nick, databasekey) or []
+    adjustarraynew = []
+    for x in adjustarray:
+        adjustarraynew.append(x)
+    set_database_value(bot, nick, databasekey, None)
+    adjustarray = []
+    if adjustmentdirection == 'add':
+        if entry not in adjustarraynew:
+            adjustarraynew.append(entry)
+    elif adjustmentdirection == 'del':
+        if entry in adjustarraynew:
+            adjustarraynew.remove(entry)
+    for x in adjustarraynew:
+        if x not in adjustarray:
+            adjustarray.append(x)
+    if adjustarray == []:
+        set_database_value(bot, nick, databasekey, None)
+    else:
+set_database_value(bot, nick, databasekey, adjustarray)
