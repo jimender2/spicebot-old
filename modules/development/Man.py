9f5c6547546424684e0a6fa8ab0cb84f855ffe76
Dyson Parkes <dyson@berserkir.net>
2018-01-25 16:49:34 +1300

    Create Man.py

---
+++ modules/development/Man.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from SpicebotShared import *
+
+GITWIKIURL = "https://github.com/deathbybandaid/SpiceBot/wiki"
+custompagearray=['modules',
+            'casino','gamble',
+-Duels
+-Github
+-Points
+-Search
+-Spicebucks
+-Weather]
+
+@sopel.module.commands('man')
+def mainfunction(bot, trigger):
+  bot.say("Online Docs: " + GITWIKIURL)
