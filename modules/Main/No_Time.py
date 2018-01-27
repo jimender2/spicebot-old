733c82205fcb4a21755b05a7d8e50369e4fb52cc
Dyson Parkes <dyson@berserkir.net>
2018-01-28 08:16:55 +1300

    Update No_Time.py

--- modules/Main/No_Time.py
+++ modules/Main/No_Time.py
@@ -12,4 +12,5 @@ def mainfunction(bot, trigger):
         execute_main(bot, trigger, triggerargsarray)
     
 def execute_main(bot, trigger, triggerargsarray):
-    bot.say("Well I woke up to get me a cold pop and then I thought somebody was barbequing. I said oh lord Jesus it's a fire. Then I ran out, I didn't grab no shoes or nothin' Jesus, I ran for my life. And then the smoke got me, I got bronchitis ain't nobody got time for that.")
+    notimefor = get_trigger_arg(triggerargsarray, '2+') or "that"
+    bot.say("Well I woke up to get me a cold pop and then I thought somebody was barbequing. I said oh lord Jesus it's a fire. Then I ran out, I didn't grab no shoes or nothin' Jesus, I ran for my life. And then the smoke got me, I got bronchitis ain't nobody got time for " + str(notimefor) + ".")
