76ac06c2b087e16f7b7a17cf1ed86f27b528fec7
Dyson Parkes <dyson@berserkir.net>
2018-01-28 08:21:58 +1300

    Update No_Time.py
    Add ability to specify "time for x"

--- modules/Main/No_Time.py
+++ modules/Main/No_Time.py
@@ -13,5 +13,4 @@ def mainfunction(bot, trigger):
     
 def execute_main(bot, trigger, triggerargsarray):
     notimefor = get_trigger_arg(triggerargsarray, 0) or "that"
-    bot.say(str(notimefor))
     bot.say("Well I woke up to get me a cold pop and then I thought somebody was barbequing. I said oh lord Jesus it's a fire. Then I ran out, I didn't grab no shoes or nothin' Jesus, I ran for my life. And then the smoke got me, I got bronchitis ain't nobody got time for " + str(notimefor) + ".")
