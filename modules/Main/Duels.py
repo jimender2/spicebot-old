cafd2cca9f973866e031c160a711a486b0bbc6c0
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-01-31 13:12:57 -0500

    Update Duels.py

--- modules/Main/Duels.py
+++ modules/Main/Duels.py
@@ -260,6 +260,7 @@ def execute_main(bot, trigger, triggerargsarray):
         except NameError:
             bot.notice("This looks like an invalid command or an invalid person.", instigator)
             return
+        bot.say(str(commandeval))
         tierpepperrequired = get_tierpepper(bot, commandeval)
         currenttier = get_database_value(bot, duelrecorduser, 'levelingtier') or 0
         tiermath = commandeval - currenttier
@@ -363,6 +364,7 @@ def execute_main(bot, trigger, triggerargsarray):
             else:
                 dispmsg = str("The current tier is " + str(currenttier)+ ". ")
                 tiereval = eval("tierunlock"+command)
+                bot.say(str(commandeval))
                 tierpepperrequired = get_tierpepper(bot, commandeval)
                 tiermath = tiereval - currenttier
                 if tiereval <= currenttier:
