c2d8ab5459d13d90e67fb35f8cf5374a08372a83
josh-cunning <joshbcunningham@gmail.com>
2018-02-03 08:33:54 -0800

    Update PI.py

--- modules/development/PI.py
+++ modules/development/PI.py
@@ -37,6 +37,7 @@ def execute_main(bot, trigger, triggerargsarray):
     
 def chudnovskyBig(n): #http://en.wikipedia.org/wiki/Chudnovsky_algorithm
     pi = Decimal(0) 
+    k=1
     pi += (Decimal(-1)**k)*(Decimal(factorial(6*k))/((factorial(k)**3)*(factorial(3*k)))* (13591409+545140134*k)/(640320**(3*k)))    
     pi = pi * Decimal(10005).sqrt()/4270934400
     pi = pi**(-1)
