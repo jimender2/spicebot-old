a4c3c12ce3aa9fe4376b08199697db3575e5a762
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-02-14 13:58:43 -0500

    Update SpicebotShared.py

--- modules/SpicebotShared.py
+++ modules/SpicebotShared.py
@@ -131,95 +131,250 @@ def increment_counter(bot, trigger, commandused):
     adjust_botdatabase_value(bot, instigator, str(commandused + "moduleusage"), 1) ## User usage of specific module
     adjust_botdatabase_value(bot, instigator, "spicebottotalusage", 1) ## User usage of bot overall
 
-##########
-## ARGS ##
-##########
+####################################
+## Array/List/String Manipulation ##
+####################################
 
-def create_args_array(fullstring):
-    triggerargsarray = []
-    if fullstring:
-        for word in fullstring.split():
-            triggerargsarray.append(word)
-    return triggerargsarray
-
-def get_trigger_arg(triggerargsarray, number):
-    if number == 'create':
-        triggerargsarraynew = []
-        if triggerargsarray:
-            for word in triggerargsarray.split():
-                triggerargsarraynew.append(word)
-        return triggerargsarraynew
-    totalarray = len(triggerargsarray)
-    totalarray = totalarray + 1
-    triggerarg = ''
-    if "^" in str(number) or number == 0 or str(number).endswith("+") or str(number).endswith("-") or str(number).endswith("<") or str(number).endswith(">"):
-        if str(number).endswith("+"):
-            rangea = re.sub(r"\+", '', str(number))
-            rangea = int(rangea)
-            rangeb = totalarray
-        elif str(number).endswith("-"):
-            rangea = 1
-            rangeb = re.sub(r"-", '', str(number))
-            rangeb = int(rangeb) + 1
-        elif str(number).endswith(">"):
-            rangea = re.sub(r">", '', str(number))
-            rangea = int(rangea) + 1
-            rangeb = totalarray
-        elif str(number).endswith("<"):
-            rangea = 1
-            rangeb = re.sub(r"<", '', str(number))
-            rangeb = int(rangeb)
-        elif "^" in str(number):
-            rangea = number.split("^", 1)[0]
-            rangeb = number.split("^", 1)[1]
-            rangea = int(rangea)
-            rangeb = int(rangeb) + 1
-        elif number == 0:
-            rangea = 1
-            rangeb = totalarray
-        if rangea <= totalarray:
-            for i in range(rangea,rangeb):
-                arg = get_trigger_arg(triggerargsarray, i)
-                if triggerarg != '':
-                    triggerarg = str(triggerarg + " " + arg)
-                else:
-                    triggerarg = str(arg)
-    elif number == 'last':
-        if totalarray > 1:
-            totalarray = totalarray -2
-            triggerarg = str(triggerargsarray[totalarray])
-    elif str(number).endswith("!"):
-        number = re.sub(r"!", '', str(number))
-        for i in range(1,totalarray):
-            if int(i) != int(number):
-                arg = get_trigger_arg(triggerargsarray, i)
-                if triggerarg != '':
-                    triggerarg = str(triggerarg + " " + arg)
-                else:
-                    triggerarg = str(arg)
-    elif number == 'random':
-        if totalarray > 1:
-            try:
-                shuffledarray = random.shuffle(triggerargsarray)
-                randomselected = random.randint(0,len(shuffledarray) - 1)
-                triggerarg = str(shuffledarray [randomselected])
-            except TypeError:
-                triggerarg = get_trigger_arg(triggerargsarray, 1)
-        else:
-            triggerarg = get_trigger_arg(triggerargsarray, 1)
-    elif number == 'list':
-        for x in triggerargsarray:
-            if triggerarg != '':
-                triggerarg  = str(triggerarg  + ", " + x)
+## Hub
+def get_trigger_arg(bot, inputs, outputtask):
+    ## Create
+    if outputtask == 'create':
+        return create_array(bot, inputs)
+    ## reverse
+    if outputtask == 'reverse':
+        return reverse_array(bot, inputs)
+    ## Comma Seperated List
+    if outputtask == 'list':
+        return list_array(bot, inputs)
+    if outputtask == 'random':
+        return random_array(bot, inputs)
+    ## Last element
+    if outputtask == 'last':
+        return last_array(bot, inputs)
+    ## Complete String
+    if outputtask == 0 or outputtask == 'complete' or outputtask == 'string':
+        return string_array(bot, inputs)
+    ## Number
+    if str(outputtask).isdigit():
+        return number_array(bot, inputs, outputtask)
+    ## Exlude from array
+    if str(outputtask).endswith("!"):
+        return excludefrom_array(bot, inputs, outputtask)
+    ## Inclusive range starting at
+    if str(outputtask).endswith("+"):
+        return incrange_plus_array(bot, inputs, outputtask)
+    ## Inclusive range ending at
+    if str(outputtask).endswith("-"):
+        return incrange_minus_array(bot, inputs, outputtask)
+    ## Exclusive range starting at
+    if str(outputtask).endswith(">"):
+        return excrange_plus_array(bot, inputs, outputtask)
+    ## Exclusive range ending at
+    if str(outputtask).endswith("<"):
+        return excrange_minus_array(bot, inputs, outputtask)
+    ## Range Between Numbers
+    if "^" in str(outputtask):
+        return rangebetween_array(bot, inputs, outputtask)
+    string = ''
+    return string
+    
+## Convert String to array
+def create_array(bot, inputs):
+    if isinstance(inputs, list):
+        string = ''
+        for x in inputs:
+            if string != '':
+                string = str(string + " " + str(x))
             else:
-                triggerarg  = str(x)
-    else:
-        number = int(number) - 1
-        try:
-            triggerarg = triggerargsarray[number]
-        except IndexError:
-            triggerarg = ''
-    return triggerarg
+                string = str(x)
+        inputs = string
+    outputs = []
+    if inputs:
+        for word in inputs.split():
+            outputs.append(word)
+    return outputs
+
+## Convert Array to String
+def string_array(bot, inputs):
+    if not isinstance(inputs, list):
+        inputs = create_array(bot, inputs)
+    string = ''
+    for x in inputs:
+        if string != '':
+            string = str(string + " " + str(x))
+        else:
+            string = str(x)
+    return string
+
+## output reverse order
+def reverse_array(bot, inputs):
+    if not isinstance(inputs, list):
+        inputs = create_array(bot, inputs)
+    if len(inputs) == 1:
+        return inputs
+    outputs = []
+    if inputs == []:
+        return outputs
+    for d in inputs:
+        outputs.append(d)
+    outputs.reverse()
+    return outputs
+
+## Comma Seperated List
+def list_array(bot, inputs):
+    if not isinstance(inputs, list):
+        inputs = create_array(bot, inputs)
+    string = ''
+    if inputs == []:
+        return string
+    for x in inputs:
+        if string != '':
+            string  = str(string  + ", " + x)
+        else:
+            string  = str(x)
+    return string
+
+## Random element
+def random_array(bot, inputs):
+    if not isinstance(inputs, list):
+        inputs = create_array(bot, inputs)
+    string = ''
+    if inputs == []:
+        return string
+    temparray = []
+    for d in inputs:
+        temparray.append(d)
+    shuffledarray = random.shuffle(temparray)
+    randomselected = random.randint(0,len(temparray) - 1)
+    string = str(temparray [randomselected])
+    return string
+
+## Last element
+def last_array(bot, inputs):
+    if not isinstance(inputs, list):
+        inputs = create_array(bot, inputs)
+    string = ''
+    if inputs == []:
+        return string
+    string = inputs[len(inputs)-1]
+    return string
+
+def number_array(bot, inputs, number):
+    if not isinstance(inputs, list):
+        inputs = create_array(bot, inputs)
+    string = ''
+    if str(number).isdigit():
+        numberadjust = int(number) -1
+        if numberadjust< len(inputs) and numberadjust >= 0:
+            number = int(number) - 1
+            string = inputs[number]
+    return string
+
+def range_array(bot, inputs, rangea, rangeb):
+    if not isinstance(inputs, list):
+        inputs = create_array(bot, inputs)
+    string = ''
+    if not str(rangea).isdigit() or not str(rangeb).isdigit():
+        return string
+    if int(rangeb) == int(rangea):
+        return number_array(bot, inputs, rangeb)
+    if int(rangeb) < int(rangea):
+        tempa, tempb = rangeb, rangea
+        rangea, rangeb = tempa, tempb
+    if int(rangea) < 1:
+        rangea = 1
+    if int(rangeb) > len(inputs):
+        rangeb = len(inputs)
+    for i in range(int(rangea),int(rangeb) + 1):
+        arg = number_array(bot, inputs, i)
+        if string != '':
+            string = str(string + " " + arg)
+        else:
+            string = str(arg)
+    return string
+
+def excludefrom_array(bot, inputs, number):
+    if not isinstance(inputs, list):
+        inputs = create_array(bot, inputs)
+    string = ''
+    if str(number).endswith("!"):
+        number = re.sub(r"!", '', str(number))
+    if str(number).isdigit():
+        for i in range(1,len(inputs)):
+            if int(i) != int(number):
+                arg = number_array(bot, inputs, i)
+                if string != '':
+                    string = str(string + " " + arg)
+                else:
+                    string = str(arg)
+    return string
+
+def rangebetween_array(bot, inputs, number):
+    if not isinstance(inputs, list):
+        inputs = create_array(bot, inputs)
+    string = ''
+    rangea = 'error'
+    rangeb = 'handling'
+    if "^" in str(number):
+        rangea = number.split("^", 1)[0]
+        rangeb = number.split("^", 1)[1]
+    if not str(rangea).isdigit() or not str(rangeb).isdigit():
+        return string
+    return range_array(bot, inputs, rangea, rangeb)
+
+def incrange_plus_array(bot, inputs, number):
+    if not isinstance(inputs, list):
+        inputs = create_array(bot, inputs)
+    string = ''
+    rangea = 'error'
+    rangeb = 'handling'
+    if str(number).endswith("+"):
+        rangea = re.sub(r"\+", '', str(number))
+        rangeb = len(inputs)
+    if not str(rangea).isdigit() or not str(rangeb).isdigit():
+        return string
+    return range_array(bot, inputs, rangea, rangeb)
+
+def incrange_minus_array(bot, inputs, number):
+    if not isinstance(inputs, list):
+        inputs = create_array(bot, inputs)
+    string = ''
+    rangea = 'error'
+    rangeb = 'handling'
+    if str(number).endswith("-"):
+        rangea = 1
+        rangeb = re.sub(r"-", '', str(number))
+    if not str(rangea).isdigit() or not str(rangeb).isdigit():
+        return string
+    return range_array(bot, inputs, rangea, rangeb)
+
+def excrange_plus_array(bot, inputs, number):
+    if not isinstance(inputs, list):
+        inputs = create_array(bot, inputs)
+    string = ''
+    rangea = 'error'
+    rangeb = 'handling'
+    if str(number).endswith(">"):
+        rangea = re.sub(r">", '', str(number))
+        rangea = int(rangea) + 1
+        rangeb = len(inputs)
+    if not str(rangea).isdigit() or not str(rangeb).isdigit():
+        return string
+    return range_array(bot, inputs, rangea, rangeb)
+
+def excrange_minus_array(bot, inputs, number):
+    if not isinstance(inputs, list):
+        inputs = create_array(bot, inputs)
+    string = ''
+    rangea = 'error'
+    rangeb = 'handling'
+    if str(number).endswith("<"):
+        rangea = 1
+        rangeb = re.sub(r"<", '', str(number))
+        rangeb = int(rangeb) - 1
+    if not str(rangea).isdigit() or not str(rangeb).isdigit():
+        return string
+    return range_array(bot, inputs, rangea, rangeb)
 
 ##############
 ## Database ##
