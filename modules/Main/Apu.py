318b07658813658ec39e81ad54f549a38f500a7a
Dyson Parkes <dyson@berserkir.net>
2018-02-15 22:39:12 +1300

    Update and rename modules/development/Apu.py to modules/Main/Apu.py

--- modules/Main/Apu.py
+++ modules/Main/Apu.py
@@ -16,7 +16,7 @@ def mainfunction(bot, trigger):
         execute_main(bot, trigger, triggerargsarray)
     
 def execute_main(bot, trigger, triggerargsarray):
-    decide = random.randint(1,3)
+    decide = random.randint(1,10)
     if decide == 1:
         bot.action("needs the Kwik-E-Mart")
         time.sleep(2)
