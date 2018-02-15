552b05f4c1cd94ee7210afd45519181b1f3fa6a3
Dyson Parkes <dyson@berserkir.net>
2018-02-15 21:38:23 +1300

    Update Claim.py

--- modules/Main/Claim.py
+++ modules/Main/Claim.py
@@ -8,6 +8,7 @@ import os
 import datetime
 shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
+
 from SpicebotShared import *
 import Spicebucks
 
@@ -18,23 +19,44 @@ def mainfunction(bot, trigger):
         execute_main(bot, trigger, triggerargsarray)
     
 def execute_main(bot, trigger, triggerargsarray):
+    # Names/nicks for code
     instigator = trigger.nick
     owner = bot.config.core.owner
+    mastername = bot.db.get_nick_value(instigator,'claimed') or ''
     target = get_trigger_arg(triggerargsarray, 1)
     admintarget = get_trigger_arg(triggerargsarray, 2)
+    
+    # Names of channel
     inchannel = trigger.sender
     channel = trigger.sender
-    todaydate = datetime.date.today()  
+    
+    # Dates for usage
+    todaydate = datetime.date.today()
     storedate = str(todaydate)
+    
+    # Good to claim?
     okaytoclaim = 1
+    
+    # Days before reclaim available
     maxtime = 7
-    mastername = bot.db.get_nick_value(instigator,'claimed') or ''
+    
+    # Spicebuck reward values
+    firstclaim = 5
+    renewclaim = 2
+    stolenclaim = 10
+    masterclaim = 5 #take, not give
+    
+    # Make sure claims happen in channel, not privmsg
     if not inchannel.startswith("#"):
         okaytoclaim = 0
         bot.say("Claims must be done in channel")
-    elif not target:
+        
+    # Handle if no target is specified
+    if not target:
         okaytoclaim = 0
         bot.say("Who do you want to claim?")
+        
+    # Check if somebody is claimed, return if/when
     elif target == 'check':
         okaytoclaim = 0
         if not admintarget:
@@ -51,16 +73,8 @@ def execute_main(bot, trigger, triggerargsarray):
                 bot.say("You were claimed by " + str(claimedby) + " on " + str(claimdate) +", " + str(instigator) + ".")
             else:
                 bot.say(str(admintarget) + " was claimed by " + str(claimedby) + " on " + str(claimdate) +", " + instigator + ".")
-    elif target == instigator:
-        okaytoclaim = 0
-        bot.say("You can't claim yourself!")
-    elif target == bot.nick:
-        okaytoclaim = 0
-        bot.say("I have already been claimed by " + owner +"!")
-    elif target == mastername:
-        okaytoclaim = 0
-        bot.action("facepalms")
-        bot.say("You can't claim " + target + ", "+ instigator + ". They already have a claim on you.")
+    
+    # Bot admins can reset claims
     elif target == 'reset':
         okaytoclaim = 0
         if trigger.admin:
@@ -74,23 +88,42 @@ def execute_main(bot, trigger, triggerargsarray):
                 bot.say("Claim info for " + admintarget + " reset on " + str(todaydate))
         else:
             bot.say("This function is only available for bot admins.")
-    elif target.lower() not in bot.privileges[channel.lower()] and target != 'reset':
-        bot.say("I'm not sure who that is.")
-    elif instigator == 'IT_Sean':
+
+    # Can't claim yourself
+    elif target == instigator:
         okaytoclaim = 0
-        claimedby = bot.db.get_nick_value(target,'claimed') or ''
-        if claimedby == '':
-            bot.say(instigator + ' releases the contents of his bladder on ' + target + '! All should recognize this profound claim of ownership upon ' + claimed +'!')
-            bot.db.set_nick_value(target,'claimed',instigator)
-            bot.db.set_nick_value(target,'claimdate',storedate)
-        else: 
-            bot.say(target + " has already been claimed by " + str(claimedby) + ", so back off!")
+        bot.say("You can't claim yourself!")
+        
+    # Can't claim the bot
+    elif target == bot.nick:
+        okaytoclaim = 0
+        bot.say("I have already been claimed by " + owner +"!")
+    
+    # Can't claim your claimant
+    elif target == mastername:
+        okaytoclaim = 0
+        bot.action("facepalms")
+        bot.say("You can't claim " + target + ", "+ instigator + ". They already have a claim on you.")
+        #take spicebucks
+
+    # If the target is not online OR a subcommand, handle it
+    elif target.lower() not in bot.privileges[channel.lower()] and target != 'reset': 
+        bot.say("I'm not sure who that is.") 
+    
+    # Input checks out. Verify dates and go ahead.
     elif okaytoclaim:
         claimedby = bot.db.get_nick_value(target,'claimed') or ''
+        # First time claimed
         if claimedby == '':
-            bot.say(instigator + ' urinates on ' + target + '! Claimed!')
+            if instigator == 'Reginald':
+                message = instigator + " releases the contents of his bladder on " + target + "! All should recognize this profound claim of ownership upon " + claimed +"!"
+            else:
+                message = instigator + " urinates on " + target + "! Claimed!"
+            bot.say(message)
             bot.db.set_nick_value(target,'claimed',instigator)
             bot.db.set_nick_value(target,'claimdate',storedate)
+            #spicebucks give instigator,firstclaim
+        # Renewed claim
         elif claimedby == instigator:
             claimdate = bot.db.get_nick_value(target, 'claimdate') or '1999-12-31'
             datea = arrow.get(todaydate)
@@ -98,24 +131,32 @@ def execute_main(bot, trigger, triggerargsarray):
             timepassed = datea - dateb
             dayspassed = timepassed.days
             if timepassed.days > int(maxtime):
-                bot.say(instigator + " urinates on " + target + " again! The claim has been renewed!")
+                if instigator == 'Reginald':
+                    message = instigator + " releases the contents of his bladder on " + target + " again! All should recognize this almighty renewal of ownership over " + target + "!"
+                else:
+                    message = instigator + " urinates on " + target + " again! The claim has been renewed!"
+                bot.say(message)
                 bot.db.set_nick_value(target,'claimed',instigator)
                 bot.db.set_nick_value(target,'claimdate',storedate)
+                #spicebucks give instigator,renewclaim
             else:
                 bot.say(instigator + ", you already claimed " + target +".")
         else:
+            # Stolen claim
             claimdate = bot.db.get_nick_value(target, 'claimdate') or '1999-12-31'
             datea = arrow.get(todaydate)
             dateb = arrow.get(claimdate)
             timepassed = datea - dateb
             dayspassed = timepassed.days
             if timepassed.days > int(maxtime):
-                if claimedby == '':
-                    bot.say(instigator + " urinates on " + target + "! Claimed!")
+                if instigator == 'Reginald':
+                    message = instigator + " empties their bladder all over " + target + "! The claim of " + str(claimedby) + " has been overpowered by " + instigator + "!")
                 else:
-                    bot.say(instigator + " urinates on " + target + "! The claim has been stolen from " + claimedby + "!")
+                    message = instigator + " urinates on " + target + "! The claim has been stolen from " + claimedby + "!"
+                bot.say(message)
                 bot.db.set_nick_value(target,'claimed',instigator)
                 bot.db.set_nick_value(target,'claimdate',storedate)
+                #spicebucks give instigator,renewclaim
             else:
                 bot.say(target + " has already been claimed by " + str(claimedby) + ", so back off!")
     else:
