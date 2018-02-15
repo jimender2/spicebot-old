b62e4725f84ee82ab56a16fa06da240aa376808f
Dyson Parkes <dyson@berserkir.net>
2018-02-15 22:44:37 +1300

    Update Struggle.py

--- modules/development/Struggle.py
+++ modules/development/Struggle.py
@@ -8,7 +8,7 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from SpicebotShared import *
 
-@sopel.module.commands('zoidberg')
+@sopel.module.commands('struggle')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
