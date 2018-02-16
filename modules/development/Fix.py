c08f7836c39a1066c59fe6dcafb534bff5bb19bb
Dyson Parkes <dyson@berserkir.net>
2018-02-17 11:21:10 +1300

    Update Fix.py

--- modules/development/Fix.py
+++ modules/development/Fix.py
@@ -9,8 +9,7 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from SpicebotShared import *
 
-commandarray = ["add","remove","count"]
-#list?
+commandarray = ["add","remove","count","list"]
 
 @sopel.module.commands('fix')
 def mainfunction(bot, trigger):
@@ -20,25 +19,32 @@ def mainfunction(bot, trigger):
     
 def execute_main(bot, trigger, triggerargsarray):
     instigator = trigger.nick
+    inchannel = trigger.sender
     command = get_trigger_arg(triggerargsarray, 1)
     fixstring = get_trigger_arg(triggerargsarray, '2+')
     existingfixarray = get_botdatabase_value(bot, bot.nick, 'fixes') or []
+    bot.say(str(inchannel))
     if command in commandarray:
         if command == "add":
             if fixstring not in existingfixarray:
                 adjust_botdatabase_array(bot, bot.nick, fixstring, 'fixes', 'add')
-                message = fixstring + " added to database."
+                message = "Fix added to database."
             else:
-                message = "That's already in the array"
+                message = "That fix is already in the database."
         elif command == "remove":
             if fixstring not in existingfixarray:
-                message = fixstring + " not found in database."
+                message = "That fix was not found in the database."
             else:
                 adjust_botdatabase_array(bot, bot.nick, fixstring, 'fixes', 'del')
-                message = fixstring + " removed from database."
+                message = "Fix removed from database."
         elif command == "count":
             fixcount = len(existingfixarray)
             message = "There are currently " + fixcount + " in the list."
+        elif command == "list":
+            if inchannel:
+                message = "List can only be run in privmsg to avoid flooding."
+            else:
+                message = "This command will tell you what is in the database eventually."
     else:
         message = random_array(bot, existingfixarray) or ''
         if message == '':
