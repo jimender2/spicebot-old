db987f8ffe7c1defe7c307f88749e836538b320e
Dyson Parkes <dyson@berserkir.net>
2018-02-17 11:01:02 +1300

    Update Fix.py

--- modules/development/Fix.py
+++ modules/development/Fix.py
@@ -9,7 +9,8 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from SpicebotShared import *
 
-commandarray = ["count","add","remove","list"]
+commandarray = ["add","remove"]
+#count, list?
 
 @sopel.module.commands('fix')
 def mainfunction(bot, trigger):
@@ -19,10 +20,23 @@ def mainfunction(bot, trigger):
     
 def execute_main(bot, trigger, triggerargsarray):
     instigator = trigger.nick
-    commandorquery = get_trigger_arg(triggerargsarray, 1)
-    if commandorquery in commandarray:
-        bot.say("Command detected")
-        # if commandorquery == "add":
-        #adjust_botdatabase_array()
+    command = get_trigger_arg(triggerargsarray, 1)
+    fixstring = get_trigger_arg(triggerargsarray, '2+')
+    existingfixarray = get_botdatabase_value(bot, bot.nick, 'fixes')
+    if command in commandarray:
+        #bot.say("Command detected")
+        if command == "add":
+            if fixstring in existingfixarray:
+                message = "That's already in the array"
+            else:
+                adjust_botdatabase_array(bot, bot.nick, fixstring, 'fixes', 'add')
+                message = fixstring + " added to database."
+        if subcommand == "remove":
+            if fixstring in existingfixarray:
+                adjust_botdatabase_array(bot, bot.nick, fixstring, 'fixes', 'del')
+                message = fixstring + " removed from database."
+            else:
+                message = fixstring + " not found in database."
     else:
-        bot.say("line")
+        message = "No string found"
+    bot.say(message)
