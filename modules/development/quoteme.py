b788edf5322908dd9b4689bbeb74ef81fa7efd97
Dyson Parkes <dyson@berserkir.net>
2018-02-17 19:35:52 +1300

    Create quoteme.py

---
+++ modules/development/quoteme.py
@@ -0,0 +1,23 @@
+!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import os
+import sys
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from SpicebotShared import *
+from Sayings.py import *
+commandarray = ["add","remove","count","last"]
+#add reset and sort list
+
+@sopel.module.commands('quoteme')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, 'quoteme')
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray)
+    
+def execute_main(bot, trigger, triggerargsarray):
+    saying = getsaying(bot, trigger, triggerargsarray, 'confucius')
+    bot.say(saying)
