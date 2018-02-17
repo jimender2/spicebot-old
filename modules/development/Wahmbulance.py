dce96356f9bc29656bea6f32b169d0d6102e76ca
josh-cunning <joshbcunningham@gmail.com>
2018-02-17 14:55:26 -0800

    Update Wahmbulance.py

--- modules/development/Wahmbulance.py
+++ modules/development/Wahmbulance.py
@@ -8,7 +8,7 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from SpicebotShared import *
 
-@sopel.module.commands('wanted')
+@sopel.module.commands('wah')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
