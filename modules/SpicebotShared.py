f2db1459555067171bb56ed693a613d6a3fa930e
josh-cunning <joshbcunningham@gmail.com>
2018-02-17 14:38:50 -0800

    Update SpicebotShared.py

--- modules/SpicebotShared.py
+++ modules/SpicebotShared.py
@@ -376,6 +376,42 @@ def excrange_minus_array(bot, inputs, number):
         return string
     return range_array(bot, inputs, rangea, rangeb)
 
+####################################
+##########Check for target##########
+###If target  validtarget =1       #
+###if bot is target validtarget=2  #
+###if target is instigator         #
+###validtarget =2                  #
+####################################
+def targetcheck(bot, target,instigator):
+    validtarget = 0
+    validtargetmsg = ''
+    botusersarray=[]
+    botuseron=[]
+    for channel in bot.channels:
+        botusersarray = get_botdatabase_value(bot, bot.nick, 'botusers')    
+    for u in bot.users:
+        if u in botusersarray:
+            botuseron.append(u)   
+    if not target:
+        validtargetmsg = str(instigator + ", you must specify a target.")
+        validtarget = 0
+    else:
+        if target.lower() == (bot.nick).lower():
+            validtargetmsg = str(instigator + ", can't targetbot.")
+            validtarget=2  
+        elif target == instigator:       
+            validtargetmsg = str(instigator + ", is the target")
+            validtarget=3         
+            
+        elif not target.lower() in [u.lower() for u in botuseron]:
+            validtargetmsg = str(instigator + " " + target +  "isn't a valid target")            
+        else:
+            validtarget = 1
+    
+    return validtarget, validtargetmsg
+
+
 #############
 ## Old arg ##
 #############
