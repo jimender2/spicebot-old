f3d9a5482d284458ef0999ad2686a0e1299fce70
Dyson Parkes <dyson@berserkir.net>
2018-02-17 19:35:19 +1300

    Update Sayings.py

--- modules/development/Sayings.py
+++ modules/development/Sayings.py
@@ -14,7 +14,7 @@ commandarray = ["add","remove","count","last"]
 
 @sopel.module.commands('testquote')
 def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, 'yes')
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, 'testquote')
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
     
