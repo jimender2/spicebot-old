074e5d7ab7052827c8aaf4ef03bca497deb33e4a
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-02-20 10:34:15 -0500

    Update Duels.py

--- modules/Main/Duels.py
+++ modules/Main/Duels.py
@@ -427,12 +427,12 @@ def commandortargetsplit(bot, trigger, triggerargsarray, instigator, botvisibleu
         bot.say("test2")
         ## Mis-spellings ## TODO add alternative commands
         ## Check Commands
-        while commandortarget == comorig:
-            for com in validcommands:
-                similarlevel = similar(commandortarget.lower(),com)
-                if similarlevel >= .75:
-                    commandortarget = com
-            ## Check players, but only if we didn't alreayd match a command
+        for com in validcommands:
+            similarlevel = similar(commandortarget.lower(),com)
+            if similarlevel >= .75:
+                commandortarget = com
+        ## Check players, but only if we didn't alreayd match a command
+        if commandortarget != comorig:
             for player in botvisibleusers:
                 similarlevel = similar(commandortarget.lower(),player)
                 if similarlevel >= .75:
