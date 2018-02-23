cdedce624f9378016d8ba78957b34f90eaf8a070
josh-cunning <joshbcunningham@gmail.com>
2018-02-22 18:26:44 -0800

    Create clocktest.py

---
+++ modules/testexamples/clocktest.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+from sopel import module, tools
+import sys
+import os
+import random
+moduledir = os.path.dirname(__file__)
+sys.path.append(moduledir)
+import Spicebucks
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from SpicebotShared import *
+
+@sopel.module.commands('testtarget')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray)
+    
+def execute_main(bot, trigger, triggerargsarray):
