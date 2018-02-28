ef824c577d759016696bfb89359fbd2b866a23d5
josh-cunning <joshbcunningham@gmail.com>
2018-02-28 13:40:41 -0800

    Update lmgtfy.py

--- modules/development/lmgtfy.py
+++ modules/development/lmgtfy.py
@@ -14,7 +14,7 @@ from SpicebotShared import *
 
 @sopel.module.commands('lmgtfy', 'lmgify', 'gify', 'gtfy')
 def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, 'lmgtfy)
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, 'lmgtfy')
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
 
