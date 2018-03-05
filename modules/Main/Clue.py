a4bcacc9aa190513531fa1ecfa4ce810fad2daf5
josh-cunning <joshbcunningham@gmail.com>
2018-03-05 15:13:39 -0800

    Update Clue.py

--- modules/Main/Clue.py
+++ modules/Main/Clue.py
@@ -28,7 +28,7 @@ def mainfunction(bot, trigger):
 def execute_main(bot, trigger, triggerargsarray):
     channel = trigger.sender
     instigator = trigger.nick
-    pointsworth = random.uniform(.05,.3)   
+    pointsworth = random.uniform(.3,.8)   
     if not channel.startswith("#"):
         bot.notice(instigator + " Clue must be in a channel.", instigator)
         return
