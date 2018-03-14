8bd44f89c3459189d78f8de3788a4773f8bbcb20
Dyson Parkes <dyson@berserkir.net>
2018-03-15 09:32:13 +1300

    Update SysAdmin_Tools.py
    Add SysInternals

--- modules/IT/SysAdmin_Tools.py
+++ modules/IT/SysAdmin_Tools.py
@@ -8,7 +8,7 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from SpicebotShared import *
 
-systoolsarray = ["https://sysadmin.it-landscape.info/","https://sysadmin.libhunt.com/","https://github.com/n1trux/awesome-sysadmin"]
+systoolsarray = ["https://sysadmin.it-landscape.info/","https://sysadmin.libhunt.com/","https://github.com/n1trux/awesome-sysadmin","https://docs.microsoft.com/en-us/sysinternals/"]
 
 @sopel.module.commands('sysadmintools')
 def execute_main(bot, trigger):
