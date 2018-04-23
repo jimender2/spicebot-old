9419724b3271f5ac285f58cc4fc7fa5d0dbd77c4
zsutton92 <zsutton92@gmail.com>
2018-04-23 08:07:38 -0500

    Update Trivia.py

--- modules/Games/Trivia.py
+++ modules/Games/Trivia.py
@@ -47,10 +47,10 @@ def getQuestionFromDb(bot):
     question = get_database_value(bot,'triviauser','triviaq')
     arrAnswers = get_database_value(bot,'triviauser','triviachoices')
     bot.say("Still waiting for someone to answer this one: " + question)
- '   if len(str(arrAnswers) > 2):
+    #if len(str(arrAnswers) > 2):
     bot.say("Choices:" + arrAnswers[0] + " " + arrAnswers[1] + " " + arrAnswers[2] + " " + arrAnswers[3])
-   ' else:
-        'bot.say("Choices:" + arrAnswers[0] + " " + arrAnswers[1])
+    #else:
+    #bot.say("Choices:" + arrAnswers[0] + " " + arrAnswers[1])
     
 def getQuestion():
     url = 'https://opentdb.com/api.php?amount=1'
