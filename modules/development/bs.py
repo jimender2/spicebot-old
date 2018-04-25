96a133f396b983374f36d0c832e823828044d464
zsutton92 <zsutton92@gmail.com>
2018-04-25 11:15:50 -0500

    Update bs.py

--- modules/development/bs.py
+++ modules/development/bs.py
@@ -12,7 +12,7 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from SpicebotShared import *
 
-@sopel.module.commands('bs','bullshit')
+@sopel.module.commands('bs')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
