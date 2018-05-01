d971a76428e2dceae696810c7879bff0e97aa020
Dyson Parkes <dyson@berserkir.net>
2018-05-01 14:20:20 +1200

    Add 'Cipher' shortening

--- modules/Main/Disclaimer.py
+++ modules/Main/Disclaimer.py
@@ -8,7 +8,7 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from SpicebotShared import *
 
-specifics = ['brightlights','doctor','EULA','IT','Cipher-0','IT_Sean']
+specifics = ['brightlights','doctor','EULA','IT','Cipher-0','Cipher','IT_Sean']
 
 @sopel.module.commands('disclaimer')
 def mainfunction(bot, trigger):
@@ -28,7 +28,7 @@ def execute_main(bot, trigger, triggerargsarray):
             bot.say("Spicebot may occasionally (read 'frequently') use colorful language to carry out its tasks. By remaining in this channel and continuing to use the bot you acknowledge that you are not, in fact, too weak to handle this fact.")
         if subdisclaimer == 'IT':
             bot.say("While most members of this channel have some level of technical knowledge, your decision to trust the recommendations of " + instigator + " are entirely your own risk.")
-        if subdisclaimer == 'Cipher-0':
+        if subdisclaimer == 'Cipher-0' or 'Cipher':
             bot.say("Frivolously pestering Cipher-0 comes with a high risk of termination, " + instigator)
         if subdisclaimer == 'IT_Sean':
             bot.say("Should you ever encounter gases released by IT_Sean, please be sure to inform your nearest biosafety agency of the incident.")
