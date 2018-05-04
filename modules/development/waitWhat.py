826e2d7bbad1d7490d104036069451b871168bd0
BuildTools <jmeredit@ashland.edu>
2018-05-03 22:03:09 -0400

    Fix to ww?

--- modules/development/waitWhat.py
+++ modules/development/waitWhat.py
@@ -16,4 +16,4 @@ def mainfunction(bot, trigger):
     
 def execute_main(bot, trigger, triggerargsarray):
     what = get_trigger_arg(bot, triggerargsarray, 1)
-	bot.say("Wait...What? there's a " + what + " now?")
+	bot.say("Wait...What? there\'s a " + what + " now?")
