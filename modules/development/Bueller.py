430d6602971002f5bfba3168529ec30eac8e391e
BuildTools <jmeredit@ashland.edu>
2018-05-04 07:26:50 -0400

    forgot to change trigger

--- modules/development/Bueller.py
+++ modules/development/Bueller.py
@@ -10,7 +10,7 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from SpicebotShared import *
 
-@sopel.module.commands('bart')
+@sopel.module.commands('bueller')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
