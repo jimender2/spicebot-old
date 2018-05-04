9513479ea91e95eabf39d63bfb1dd1c243a2df8b
BuildTools <jmeredit@ashland.edu>
2018-05-03 22:42:52 -0400

    fix

--- modules/development/waitWhat.py
+++ modules/development/waitWhat.py
@@ -15,5 +15,5 @@ def mainfunction(bot, trigger):
         execute_main(bot, trigger, triggerargsarray)
     
 def execute_main(bot, trigger, triggerargsarray):
-	what =  get_trigger_arg(triggerargsarray, 0)
+	what =  get_trigger_arg(bot, triggerargsarray, 0)
 	bot.say("Wait...What? there\'s a " + what + " now?")
