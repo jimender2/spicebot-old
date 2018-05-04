c0e6e6f5347fa5c958719d40346576beea886b1a
BuildTools <jmeredit@ashland.edu>
2018-05-03 22:19:05 -0400

    Cleaned up code

--- modules/development/waitWhat.py
+++ modules/development/waitWhat.py
@@ -8,7 +8,7 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from SpicebotShared import *
 
-@sopel.module.commands('ww','waitwhat')
+@sopel.module.commands('ww')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
