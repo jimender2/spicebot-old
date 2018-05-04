dab1631897210eac3f59548c2afb3ba8ac39cf8f
BuildTools <jmeredit@ashland.edu>
2018-05-04 07:25:56 -0400

    Bueller

---
+++ modules/development/Bueller.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import urllib
+from xml.dom.minidom import parseString
+import sys
+import os
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from SpicebotShared import *
+
+@sopel.module.commands('bart')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray)
+    
+def execute_main(bot, trigger, triggerargsarray):
+    loop = 1
+	while loop >= 5
+		bot.say("Bueller")
+		loop = loop + 1
