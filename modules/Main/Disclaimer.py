5f32c54d251f651a1843fffc8712465ad25c46a0
Dyson Parkes <dyson@berserkir.net>
2018-05-11 10:34:12 +1200

    Refactor strings to prepare for targeting

--- modules/Main/Disclaimer.py
+++ modules/Main/Disclaimer.py
@@ -18,19 +18,20 @@ def mainfunction(bot, trigger):
 
 def execute_main(bot, trigger, triggerargsarray):
     instigator = trigger.nick
+    person = instigator
     subdisclaimer = get_trigger_arg(bot,triggerargsarray,1)
     if subdisclaimer in specifics:
         if subdisclaimer == 'brightlights':
             bot.say("Individuals sensitive to bright lights or with epilepsy may find the quick bright text the bot speaks with to be troublesome. Any epileptic reaction is not the fault of the bot, the channel, or its denizens.")
         elif subdisclaimer == 'doctor':
-            bot.say(instigator + " is not your doctor. The views/opinions/information expressed by " + instigator + " is not intended or implied to be a substitute for professional medical advice, diagnosis or treatment.")
+            bot.say(person + " is not your doctor. The views/opinions/information expressed by " + person + " is not intended or implied to be a substitute for professional medical advice, diagnosis or treatment.")
         elif subdisclaimer == 'EULA':
             bot.say("Spicebot may occasionally (read 'frequently') use colorful language to carry out its tasks. By remaining in this channel and continuing to use the bot you acknowledge that you are not, in fact, too weak to handle this.")
         elif subdisclaimer == 'IT':
-            bot.say("While most members of this channel have some level of technical knowledge, your decision to trust the recommendations of " + instigator + " are entirely your own risk.")
+            bot.say("While most members of this channel have some level of technical knowledge, your decision to trust the recommendations of " + person + " are entirely your own risk.")
         elif subdisclaimer == 'Cipher-0' or 'Cipher':
-            bot.say("Frivolously pestering Cipher comes with a high risk of termination, " + instigator)
+            bot.say("Frivolously pestering Cipher comes with a high risk of termination, " + person)
         elif subdisclaimer == 'IT_Sean':
             bot.say("Should you ever encounter gases released by Sean, please be sure to inform your nearest biosafety agency of the incident.")
     else:
-        bot.say(instigator + " is not your doctor. The views/opinions/information expressed by " + instigator + " is not intended or implied to be a substitute for professional medical advice, diagnosis or treatment.")
+        bot.say(person + " is not your doctor. The views/opinions/information expressed by " + person + " is not intended or implied to be a substitute for professional medical advice, diagnosis or treatment.")
