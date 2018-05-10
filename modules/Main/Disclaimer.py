67a07672d490de285d228418b00533326c0fe1b5
Dyson Parkes <dyson@berserkir.net>
2018-05-11 10:31:19 +1200

    Adjust specific functions (double triggering)

--- modules/Main/Disclaimer.py
+++ modules/Main/Disclaimer.py
@@ -22,15 +22,15 @@ def execute_main(bot, trigger, triggerargsarray):
     if subdisclaimer in specifics:
         if subdisclaimer == 'brightlights':
             bot.say("Individuals sensitive to bright lights or with epilepsy may find the quick bright text the bot speaks with to be troublesome. Any epileptic reaction is not the fault of the bot, the channel, or its denizens.")
-        if subdisclaimer == 'doctor':
+        elif subdisclaimer == 'doctor':
             bot.say(instigator + " is not your doctor. The views/opinions/information expressed by " + instigator + " is not intended or implied to be a substitute for professional medical advice, diagnosis or treatment.")
-        if subdisclaimer == 'EULA':
+        elif subdisclaimer == 'EULA':
             bot.say("Spicebot may occasionally (read 'frequently') use colorful language to carry out its tasks. By remaining in this channel and continuing to use the bot you acknowledge that you are not, in fact, too weak to handle this.")
-        if subdisclaimer == 'IT':
+        elif subdisclaimer == 'IT':
             bot.say("While most members of this channel have some level of technical knowledge, your decision to trust the recommendations of " + instigator + " are entirely your own risk.")
-        if subdisclaimer == 'Cipher-0' or 'Cipher':
-            bot.say("Frivolously pestering Cipher-0 comes with a high risk of termination, " + instigator)
-        if subdisclaimer == 'IT_Sean':
-            bot.say("Should you ever encounter gases released by IT_Sean, please be sure to inform your nearest biosafety agency of the incident.")
+        elif subdisclaimer == 'Cipher-0' or 'Cipher':
+            bot.say("Frivolously pestering Cipher comes with a high risk of termination, " + instigator)
+        elif subdisclaimer == 'IT_Sean':
+            bot.say("Should you ever encounter gases released by Sean, please be sure to inform your nearest biosafety agency of the incident.")
     else:
         bot.say(instigator + " is not your doctor. The views/opinions/information expressed by " + instigator + " is not intended or implied to be a substitute for professional medical advice, diagnosis or treatment.")
