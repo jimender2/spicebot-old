447328d81f23a7fdef87a212e22ce3e3fdd642d2
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2018-05-11 14:53:10 -0400

    Update murder.py

--- modules/development/murder.py
+++ modules/development/murder.py
@@ -2,15 +2,13 @@
 # coding=utf-8
 from __future__ import unicode_literals, absolute_import, print_function, division
 import sopel.module
-import urllib
-from xml.dom.minidom import parseString
 import sys
 import os
 shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from SpicebotShared import *
 
-replies = ["Died by axe"]
+weapontypes = ["Axe","example"]
 
 @sopel.module.commands('murder')
 def mainfunction(bot, trigger):
@@ -19,5 +17,29 @@ def mainfunction(bot, trigger):
         execute_main(bot, trigger, triggerargsarray)
     
 def execute_main(bot, trigger, triggerargsarray):
-    answer = get_trigger_arg(bot, replies, 'random')
-    bot.say(answer)
+    target = get_trigger_arg(bot, triggerargsarray, 1)
+    reason = get_trigger_arg(bot, triggerargsarray, '2+')
+    message = "Whoops, something went wrong."
+    weapontype = get_trigger_arg(bot,weapontypes,'random')
+    msg = "a " + weapontype
+    
+    # No target specified
+    if not target:
+        bot.say("Who/what would you like to murder?")
+    
+    # Can slap the bot
+    if target == bot.nick:
+        message = trigger.nick + " slaps " + target + " with " + msg + " for " + reason + "."
+        bot.say(message)
+        bot.say("That hurts")
+        
+    # Target is fine
+    else:
+        if not reason:
+            message = trigger.nick + " slaps " + target + " with " + msg + "."
+        else:
+            if reason.startswith('for ') or reason.startswith('because ') or reason.startswith('cause '):
+                message = trigger.nick + " slaps " + target + " with " + msg + " " + reason + "."
+            else:
+                message = trigger.nick + " slaps " + target + " with " + msg + " for " + reason + "."
+        bot.say(message)
