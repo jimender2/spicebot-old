274a893da9fcf3e402b3469aa78d3ea9bcf748ad
jimender2 <jmeredit@ashland.edu>
2018-05-15 22:05:19 -0400

    made my own if else

--- modules/development/murder.py
+++ modules/development/murder.py
@@ -33,11 +33,12 @@ def execute_main(bot, trigger, triggerargsarray):
         bot.say(message)
         bot.say("You can not kill a nonliving entity")
 	
-	elif (target == trigger.nick):
-		message = trigger.nick + " cannot murder themselves because if they did it would be suicide."
-		bot.say(message)
+	else:
+		if (target == trigger.nick):
+			message = trigger.nick + " cannot murder themselves because if they did it would be suicide."
+			bot.say(message)
 		
 	# Target is fine
-	else:
-		message = trigger.nick + " murders " + target + " with " + msg + "."
-		bot.say(message)
+		else:
+			message = trigger.nick + " murders " + target + " with " + msg + "."
+			bot.say(message)
