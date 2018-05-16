93e188e1f0536d455fdbf7173eaf3dec93e3ba8d
Dyson Parkes <dyson@berserkir.net>
2018-05-17 10:01:15 +1200

    Amend 'policy'

--- modules/development/policy.py
+++ modules/development/policy.py
@@ -8,7 +8,7 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from SpicebotShared import *
 
-@sopel.module.commands('ride')
+@sopel.module.commands('policy')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
@@ -16,6 +16,9 @@ def mainfunction(bot, trigger):
 
 def execute_main(bot, trigger, triggerargsarray):
     instigator = trigger.nick
-    target = get_trigger_arg(bot, triggerargsarray, 1)
-    message = target + " says pics or it never happened."
+    policytype = get_trigger_arg(bot, triggerargsarray, 1) or "proof"
+    if policytype == "evidence":
+        message = "No evidence, no crime."
+    else:
+        message = "Pictures or it never happened."
     bot.say(message)
