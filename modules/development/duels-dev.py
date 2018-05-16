c5a815e8ea6e51a5832cea66a5b1c2a844a7bc06
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-05-16 12:34:51 -0400

    Update duels-dev.py

--- modules/development/duels-dev.py
+++ modules/development/duels-dev.py
@@ -238,18 +238,18 @@ stats_view_functions = ['winlossratio','timeout_timeout'] ## stats that use thei
 ########################
 
 ## work with /me ACTION (does not work with manual weapon)
-@module.rule('^(?:challenges|(?:fi(?:ght|te)|duel)s(?:\s+with)?)\s+([a-zA-Z0-9\[\]\\`_\^\{\|\}-]{1,32}).*')
-@module.intent('ACTION')
-@module.require_chanmsg
-def duel_action(bot, trigger):
-    triggerargsarray = get_trigger_arg(bot, trigger.group(1), 'create') # enable if not using with spicebot
-    execute_main(bot, trigger, triggerargsarray, 'actionduel') # enable if not using with spicebot
+#@module.rule('^(?:challenges|(?:fi(?:ght|te)|duel)s(?:\s+with)?)\s+([a-zA-Z0-9\[\]\\`_\^\{\|\}-]{1,32}).*')
+#@module.intent('ACTION')
+#@module.require_chanmsg
+#def duel_action(bot, trigger):
+#    triggerargsarray = get_trigger_arg(bot, trigger.group(1), 'create') # enable if not using with spicebot
+#    execute_main(bot, trigger, triggerargsarray, 'actionduel') # enable if not using with spicebot
     #enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, 'duel') ## not needed if using without spicebot
     #if not enablestatus: ## not needed if using without spicebot
     #    execute_main(bot, trigger, triggerargsarray, 'actionduel') ## not needed if using without spicebot
 
 ## Base command
-@sopel.module.commands('duel','challenge')
+@sopel.module.commands('duels','challenges')
 def mainfunction(bot, trigger):
     triggerargsarray = get_trigger_arg(bot, trigger.group(2), 'create') # enable if not using with spicebot
     execute_main(bot, trigger, triggerargsarray, 'normalcom') # enable if not using with spicebot
@@ -1275,6 +1275,12 @@ def subcommand_roulette(bot, instigator, triggerargsarray, botvisibleusers, curr
         roulettechamber = randint(1, 6)
         set_database_value(bot, duelrecorduser, 'roulettechamber', roulettechamber)
 
+    ## subcommands
+    roulettesubcom = get_trigger_arg(bot, triggerargsarray, 2)
+    if roulettesubcom == 'last':
+        onscreentext(bot, inchannel, "The Last person to spin was " + roulettelastplayer)
+        return
+
     ## Display Text
     instigatorcurse = get_database_value(bot, instigator, 'curse') or 0
     if instigatorcurse:
@@ -2148,7 +2154,7 @@ def subcommand_deathblow(bot, instigator, triggerargsarray, botvisibleusers, cur
         onscreentext(bot, inchannel, instigator + " strikes a deathblow upon " + deathblowtarget + ".")
         deathblowkilltext = whokilledwhom(bot, instigator, deathblowtarget) or ''
         onscreentext(bot, inchannel, deathblowkilltext)
-    
+
 ## Loot ## TODO
 def subcommand_loot(bot, instigator, triggerargsarray, botvisibleusers, currentuserlistarray, dueloptedinarray, commandortarget, now, trigger, currenttier, inchannel, currentduelplayersarray, canduelarray, fullcommandused, tiercommandeval, tierpepperrequired, tiermath, devenabledchannels, validcommands):
     instigatorclass = get_database_value(bot, instigator, 'class_setting')
