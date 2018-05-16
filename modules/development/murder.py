1092950fcec90286a8cc7a9399fad0ab81919438
Dyson Parkes <dyson@berserkir.net>
2018-05-16 14:14:24 +1200

    Fix 'if' statements

--- modules/development/murder.py
+++ modules/development/murder.py
@@ -15,29 +15,30 @@ def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
-    
+
 def execute_main(bot, trigger, triggerargsarray):
+    instigator = trigger.nick
     target = get_trigger_arg(bot, triggerargsarray, 1)
     reason = get_trigger_arg(bot, triggerargsarray, '2+')
     message = "Whoops, something went wrong."
     weapontype = get_trigger_arg(bot,weapontypes,'random')
     msg = "a " + weapontype
-    
+
     # No target specified
     if not target:
         bot.say("Who/what would you like to murder?")
-    
+
     # How to kill spicebot
-    if (target == bot.nick):
-        message = trigger.nick + " attempts to murder " + target + " with " + msg + " for " + reason + "."
+    if target == bot.nick:
+        message = instigator + " attempts to murder " + target + " with " + msg + " for " + reason + "."
         bot.say(message)
         bot.say("You can not kill a nonliving entity")
-	
-	elif (target == trigger.nick):
-		message = trigger.nick + " cannot murder themselves because if they did it would be suicide."
+
+	elif target == instigator:
+		message = instigator + " cannot murder themselves because if they did it would be suicide."
 		bot.say(message)
-		
+
 	# Target is fine
 	else:
-		message = trigger.nick + " murders " + target + " with " + msg + "."
+		message = instigator + " murders " + target + " with " + msg + "."
 		bot.say(message)
