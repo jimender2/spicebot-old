c43684cc70c085bd9a8cb11dbfa87b36982be92f
Dyson Parkes <dyson@berserkir.net>
2018-05-16 14:09:31 +1200

    Add parent disclaimer

--- modules/Main/Disclaimer.py
+++ modules/Main/Disclaimer.py
@@ -29,7 +29,8 @@ def execute_main(bot, trigger, triggerargsarray):
             bot.say("Spicebot may occasionally (read 'frequently') use colorful language to carry out its tasks. By remaining in this channel and continuing to use the bot you acknowledge that you are not, in fact, too weak to handle this.")
         elif subdisclaimer == 'IT':
             bot.say("While most members of this channel have some level of technical knowledge, your decision to trust the recommendations of " + person + " are entirely your own risk.")
-        elif subdisclaimer == 'parent'
+        elif subdisclaimer == 'parent':
+            bot.say(person + "is not your parent. Don't expect them to deal with your shit.")
         elif subdisclaimer == 'Cipher-0' or 'Cipher':
             bot.say("Frivolously pestering Cipher comes with a high risk of termination, " + person)
         elif subdisclaimer == 'IT_Sean':
