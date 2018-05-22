1cfc0c2f5cd2e8bf018afa17e0880b58d6fb899b
Dyson Parkes <dyson@berserkir.net>
2018-05-22 16:49:47 +1200

    Adjust string for no args

--- modules/development/Drugs.py
+++ modules/development/Drugs.py
@@ -24,8 +24,8 @@ def execute_main(bot, trigger, triggerargsarray):
 
     # Nothing special
     if not locationorperson:
+        drugdisplay = "to " + druglocation
         displaymsg = person + " contemplates selling everything and moving " + drugdisplay + " to sell drugs on a beach."
-
     # Someone specified
     if locationorperson.lower() in [u.lower() for u in bot.users]:
         person = locationorperson
