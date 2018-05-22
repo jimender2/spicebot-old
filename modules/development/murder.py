78875e544c1e2bdc4e9f07bb3c961ab14359afd5
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2018-05-21 21:57:37 -0400

    Update murder.py

--- modules/development/murder.py
+++ modules/development/murder.py
@@ -11,6 +11,7 @@ from SpicebotShared import *
 
 weapontypes = ["Axe","Sword","Revolver"]
 adjectives = ["big", "small", "tiny", "huge", "bloody", "shiny"]
+commandarray = ["add","remove","count","last"]
 
 @sopel.module.commands('murder','moida')
 def mainfunction(bot, trigger):
@@ -21,77 +22,6 @@ def mainfunction(bot, trigger):
 def execute_main(bot, trigger, triggerargsarray):
     instigator = trigger.nick
 
-
-#start of test code
-    allchanweaponsarray = []
-    for u in bot.users:
-        weaponslist = get_database_value(bot, u, 'weaponslocker_complete') #or ['fist']
-        for x in weaponslist:
-        allchanweaponsarray.append(x)
-    weapontype = get_trigger_arg(bot, allchanweaponsarray, 'random')
-#end of test code
-
-
-    target = get_trigger_arg(bot, triggerargsarray, 1)
-    reason = get_trigger_arg(bot, triggerargsarray, '2+')
-    message = "Whoops, something went wrong."
-#    weapontype = get_trigger_arg(bot,weapontypes,'random')
-    adjective = get_trigger_arg(bot,adjectives,'random')
-    msg = "a " + adjective + " " + weapontype
-
-    # No target specified
-    if not target:
-        bot.say("Who/what would you like to murder?")
-
-    # Cannot kill spicebot
-    elif target == bot.nick:
-        bot.say("You cannot kill a nonliving entity")
-
-    # Cannot kill self
-    elif target == instigator:
-		message = "Killing yourself would be suicide, " + instigator + ", not murder. Idiot."
-		bot.say(message)
-
-    # Target is fine
-    else:
-        if not reason:
-            message = instigator + " murders " + target + " with " + msg + "."
-        else:
-            message = instigator + " murders " + target + " with " + msg + " for " + reason + "."
-        bot.say(message)
-
-#
-#
-#
-#
-#TEST CODE
-def get_database_value(bot, nick, databasekey):
-    databasecolumn = str('duels_' + databasekey)
-    database_value = bot.db.get_nick_value(nick, databasecolumn) or 0
-    return database_value
-
-
-
-#!/usr/bin/env python
-# coding=utf-8
-from __future__ import unicode_literals, absolute_import, print_function, division
-import sopel.module
-import os
-import sys
-moduledir = os.path.dirname(__file__)
-shareddir = os.path.dirname(os.path.dirname(__file__))
-sys.path.append(shareddir)
-from SpicebotShared import *
-
-commandarray = ["add","remove","count","last"]
-
-@sopel.module.commands('fix','user')
-def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, 'fix')
-    if not enablestatus:
-        execute_main(bot, trigger, triggerargsarray)
-    
-def execute_main(bot, trigger, triggerargsarray):
     instigator = trigger.nick
     inchannel = trigger.sender
     databasekey = 'fixes'
@@ -120,5 +50,34 @@ def execute_main(bot, trigger, triggerargsarray):
     else:
         message = get_trigger_arg(bot, existingarray, "random") or ''
         if message == '':
-            message = "No response found. Have any been added?"
-    bot.say(message)
+        	message = "No response found. Have any been added?"
+	target = get_trigger_arg(bot, triggerargsarray, 1)
+	reason = get_trigger_arg(bot, triggerargsarray, '2+')
+	adjective = get_trigger_arg(bot,adjectives,'random')
+	msg = "a " + adjective + " " + weapontype
+	
+	# No target specified
+	if not target:
+	bot.say("Who/what would you like to murder?")
+
+	# Cannot kill spicebot
+	elif target == bot.nick:
+	bot.say("You cannot kill a nonliving entity")
+	
+	# Cannot kill self
+	elif target == instigator:
+		message = "Killing yourself would be suicide, " + instigator + ", not murder. Idiot."
+		bot.say(message)
+
+	# Target is fine
+	else:
+		if not reason:
+			message = instigator + " murders " + target + " with " + msg + "."
+		else:
+			message = instigator + " murders " + target + " with " + msg + " for " + reason + "."
+        bot.say(message)
+	
+def get_database_value(bot, nick, databasekey):
+	databasecolumn = str('duels_' + databasekey)
+	database_value = bot.db.get_nick_value(nick, databasecolumn) or 0
+	return database_value
