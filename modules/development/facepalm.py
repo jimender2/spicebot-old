cfd9adf1ea2b4475ff2517bd42c10ab35713781e
jimender2 <jmeredit@ashland.edu>
2018-05-24 23:12:21 -0400

    facepalm update

--- modules/development/facepalm.py
+++ modules/development/facepalm.py
@@ -30,7 +30,7 @@ def execute_main(bot, trigger, triggerargsarray):
     elif target == "major":
         bot.say("There is not enough facepalm in the world for this")           
     else:
-            
+        bot.say("You are really facepalmming")
 def getGif(query):
     api = 'Wi33J3WxSDxWsrxLREcQqmO3iJ0dk52N'
     limit = 50
