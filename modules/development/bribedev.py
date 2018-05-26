3b6a599cf0e39347628a290b8ad7461a6a75a2a3
jimender2 <jmeredit@ashland.edu>
2018-05-25 23:16:06 -0400

    update

--- modules/development/bribedev.py
+++ modules/development/bribedev.py
@@ -28,7 +28,7 @@ def execute_main(bot, trigger, triggerargsarray):
     if command in commandarray:
         if command == "accept":
             amount = get_database_value(bot, instigator, databasekey)
-            adjust_botdatabase_array(bot, instigator, money, databasekey, 'remove')
+            adjust_botdatabase_array(bot, instigator, amount, databasekey, 'remove')
             bot.say("debug " + str(amount))
             message = "Accepted the bribe."
             bot.say(message)
