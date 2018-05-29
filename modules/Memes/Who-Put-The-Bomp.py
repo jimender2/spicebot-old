adc131821f0ef16975d00cf13bedd5b36d37575c
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-05-29 17:48:31 -0400

    Create Who-Put-The-Bomp.py

---
+++ modules/Memes/Who-Put-The-Bomp.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from SpicebotShared import *
+
+@nickname_commands('who put the')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray)
+    
+def execute_main(bot, trigger, triggerargsarray):
+    funlist = ["Who put the bomp in the bomp bah bomp bah bomp","Who put the ram in the rama lama ding dong","Who put the bop in the bop shoo bop shoo bop","Who put the dip in the dip da dip da dip"]
+    answer = get_trigger_arg(bot, funlist, 'random')
+    bot.say(answer)
