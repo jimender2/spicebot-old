0c4c52e42519d2872bedc3ab09f04a12432032ce
Dyson Parkes <dyson@berserkir.net>
2018-05-30 10:42:50 +1200

    Set huffy response.

--- modules/development/DTell.py
+++ modules/development/DTell.py
@@ -116,7 +116,7 @@ def execute_main(bot, trigger):
     if tellee == bot.nick:
         return bot.reply("I'm here now, you can tell me whatever you want!")
     if tellee.lower() in [u.lower() for u in bot.users]:
-        bot.say("I'm not sure who that is.")
+        return bot.reply("Tell %s that yourself you lazy fuck, they're online now." % tellee)
 
     if not tellee in (Identifier(teller), bot.nick, 'me'):
         tz = get_timezone(bot.db, bot.config, None, tellee)
