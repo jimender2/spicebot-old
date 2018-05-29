d7f05b2171b36ac69a24e25cf999010ef551ed2d
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-05-29 18:05:29 -0400

    Update Who-Put-The-Bomp.py

--- modules/development/Who-Put-The-Bomp.py
+++ modules/development/Who-Put-The-Bomp.py
@@ -9,8 +9,7 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from SpicebotShared import *
 
-@nickname_commands('^(who|\S|put|\S|the|*')
-#@sopel.module.commands('who')
+@nickname_commands('who')
 @commands('who')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
