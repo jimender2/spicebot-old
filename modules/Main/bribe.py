c8d69a341ee9825bee86baee59eb7eb74d6c95c5
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2018-06-01 22:09:07 -0400

    Update bribe.py

--- modules/Main/bribe.py
+++ modules/Main/bribe.py
@@ -12,6 +12,8 @@ sys.path.append(shareddir)
 
 from SpicebotShared import *
 
+commandarray = ["accept","delete","money"]
+
 @sopel.module.commands('bribe')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
@@ -19,7 +21,57 @@ def mainfunction(bot, trigger):
         execute_main(bot, trigger, triggerargsarray)
 
 def execute_main(bot, trigger, triggerargsarray):
+    databasekey = "bribe"
     instigator = trigger.nick
+    command = get_trigger_arg(bot, triggerargsarray, 1)
     target = get_trigger_arg(bot, triggerargsarray, 1)
-    money = random.randint(1,100001)
-    bot.say(instigator + " bribes " + target + " with $" + str(money) + " in nonsequental, unmarked bills.")
+    if command in commandarray:
+        if command == "accept":
+            amo = get_botdatabase_value(bot, instigator, 'bets') or '0'
+            amount = int(amo)
+	    reset_botdatabase_value(bot,instigator, 'bets')
+            spicebucks(bot, instigator, "plus", amount)
+            if amount == 0:
+                bot.say("There are no bribes for you to accept")
+            else:
+		bot.say(instigator + " accepted the bribe of $" + amount + ".")
+        elif command == "decline":
+            bot.say(instigator + " declines a bribe worth $" + amount +".")
+    
+    else:
+         if target.lower() in [u.lower() for u in bot.users]:
+            balance = bank(bot, instigator)
+            money = random.randint(0, balance)
+            bot.say(instigator + " bribes " + target + " with $" + str(money) + " in nonsequental, unmarked bills.")
+            inputstring = str(money)
+            set_botdatabase_value(bot,target, 'bets', inputstring)
+            spicebucks(bot, instigator, 'minus', money)
+         else:
+            bot.say("I'm sorry, I do not know who " + target + " is.")
+
+def bank(bot, nick):
+    balance = get_botdatabase_value(bot,nick,'spicebucks_bank') or 0
+    return balance
+
+def spicebucks(bot, target, plusminus, amount):
+    #command for getting and adding money to account
+    success = 'false'
+    if type(amount) == int:
+        inbank = bank(bot,target)
+    if plusminus == 'plus':
+       adjust_botdatabase_value(bot,target, 'spicebucks_bank', amount)
+       success = 'true'
+    elif plusminus == 'minus':
+        if inbank - amount < 0:
+            success = 'false'
+        else:
+            adjust_botdatabase_value(bot,target, 'spicebucks_bank', -amount)
+            success = 'true'
+    else:
+        success = 'false'
+    return success
+
+def get_database_value(bot, nick, databasekey):
+	databasecolumn = databasekey
+	database_value = bot.db.get_nick_value(nick, databasecolumn) or 0
+	return database_value
