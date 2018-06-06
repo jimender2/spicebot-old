5db883fafd79fdf14594b04b7dd8daf6eda530fd
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-06 11:51:01 -0400

    Update Duels.py

--- modules/Games/Duels.py
+++ modules/Games/Duels.py
@@ -584,8 +584,9 @@ def subcommands(bot, trigger, triggerargsarray, instigator, command_full , comma
             elif not channel_current.startswith("#") and len(duels_dev_channels) > 0:
                 allowpass = 1
             else:
-                onscreentext(bot, channel_current, "Duel " + command_main + " will be unlocked when somebody reaches " + str(tierpepperrequired) + ". " + str(tiermath) + " tier(s) remaining!")
-                return
+                if channel_current not in duels_dev_channels:
+                    onscreentext(bot, channel_current, "Duel " + command_main + " will be unlocked when somebody reaches " + str(tierpepperrequired) + ". " + str(tiermath) + " tier(s) remaining!")
+                    return
 
     ## Temporary during rewrite
     if botvisibleusers == []:
