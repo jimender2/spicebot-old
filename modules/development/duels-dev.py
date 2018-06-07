2797270c8d58873c095817a2757a118ada3b8584
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-07 11:11:07 -0400

    Update duels-dev.py

--- modules/development/duels-dev.py
+++ modules/development/duels-dev.py
@@ -1916,7 +1916,7 @@ def subcommand_mayhem(bot, instigator, triggerargsarray, botvisibleusers, curren
             playerafought = get_database_value(bot, playera, 'mayhemorganizer') or []
             playerbfought = get_database_value(bot, playerb, 'mayhemorganizer') or []
             if playera not in playerbfought and playerb not in playerafought:
-                duel_combat(bot, instigator, playera, playerb, triggerargsarray, now, channel_current, 'assault', duels_dev_channels, duels_enabled_channels)
+                duel_combat(bot, instigator, playera, [playerb], triggerargsarray, now, channel_current, 'assault', duels_dev_channels, duels_enabled_channels)
                 adjust_database_array(bot, playera, playerb, 'mayhemorganizer', 'add')
                 adjust_database_array(bot, playerb, playera, 'mayhemorganizer', 'add')
                 
