3d6c19a29c587cf18f94255d5221c49bf7278210
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-06-18 22:56:10 -0400

    Added piglatin

--- modules/development/PigLatin.py
+++ modules/development/PigLatin.py
@@ -11,9 +11,9 @@ from SpicebotShared import *
 
 #author jimender2
 
-@sopel.module.commands('eyeroll')
+@sopel.module.commands('piglatin','pl')
 def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, 'eyeroll')
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, 'piglatin')
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
 
@@ -21,7 +21,15 @@ def execute_main(bot, trigger, triggerargsarray):
     instigator = trigger.nick
     inchannel = trigger.sender
     target = get_trigger_arg(bot, triggerargsarray, 1)
-    reason = get_trigger_arg(bot, triggerargsarray, '2+')
+    message = get_trigger_arg(bot, triggerargsarray, '2+')
+    if not message:
+        message = instigator + " rolls their eyes at " + target
+    bot.say(message)
+
+    databasekey = 'murder'
+    command = get_trigger_arg(bot, triggerargsarray, 1)
+    inputstring = get_trigger_arg(bot, triggerargsarray, '2+')
+    existingarray = get_botdatabase_value(bot, bot.nick, databasekey) or []
     
 	# No target specified
 	if not target:
@@ -30,8 +38,8 @@ def execute_main(bot, trigger, triggerargsarray):
 	# Target is fine
 	else:
 		if not reason:
-			message = instigator + " rolls their eyes at " + target + "."
+			message = instigator + " rolls " + gender + " at " + target + "."
         		bot.say(message)
 		else:
-			message = instigator + " rolls their eyes at " + target + " because " + reason + "."
+			message = instigator + " rolls " + gender + " at " + target + " because " + reason + "."
         		bot.say(message)
