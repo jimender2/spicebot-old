458897cc20c3496366feb4f40f71b7f5d80abbea
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2018-06-19 15:01:57 -0400

    Update dbbtest.py

--- modules/testexamples/dbbtest.py
+++ modules/testexamples/dbbtest.py
@@ -26,18 +26,3 @@ from sopel.module import commands, rule, example, priority
 @sopel.module.commands('dbbtest')
 def execute_main(bot, trigger):
     bot.say("This is deathbybandaid's test module")
-    
-    
-def setup(self):
-    fn = self.nick + '-' + self.config.core.host + '.dbbatest.db'
-    self.tell_filename = os.path.join(self.config.core.homedir, fn)
-    if not os.path.exists(self.tell_filename):
-        try:
-            f = open(self.tell_filename, 'w')
-        except OSError:
-            pass
-        else:
-            f.write('')
-            f.close()
-#    self.memory['tell_lock'] = threading.Lock()
-#    self.memory['reminders'] = loadReminders(self.tell_filename, self.memory['tell_lock'])
