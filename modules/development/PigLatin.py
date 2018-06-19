7d037075baa932495d38eaa26c16b60ef123dd76
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-06-18 23:24:54 -0400

    piglatin work

--- modules/development/PigLatin.py
+++ modules/development/PigLatin.py
@@ -19,27 +19,8 @@ def mainfunction(bot, trigger):
 
 def execute_main(bot, trigger, triggerargsarray):
     instigator = trigger.nick
-    inchannel = trigger.sender
-    target = get_trigger_arg(bot, triggerargsarray, 1)
-    message = get_trigger_arg(bot, triggerargsarray, '2+')
-    if not message:
-        message = instigator + " rolls their eyes at " + target
-    bot.say(message)
-
-    databasekey = 'murder'
-    command = get_trigger_arg(bot, triggerargsarray, 1)
-    inputstring = get_trigger_arg(bot, triggerargsarray, '2+')
-    existingarray = get_botdatabase_value(bot, bot.nick, databasekey) or []
+    message = get_trigger_arg(bot, triggerargsarray, '1+')
     
-	# No target specified
-	if not target:
-		bot.say("Who/what would you like to murder?")
-
-	# Target is fine
-	else:
-		if not reason:
-			message = instigator + " rolls " + gender + " at " + target + "."
-        		bot.say(message)
-		else:
-			message = instigator + " rolls " + gender + " at " + target + " because " + reason + "."
-        		bot.say(message)
+    numberOfWords = len(message)
+    
+    bot.say(message)
