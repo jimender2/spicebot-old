e9ba9ad912fc162a4f572d0a1de491c6638bdae4
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-06-18 22:02:23 -0400

    eyeroll

--- modules/development/EyeRoll.py
+++ modules/development/EyeRoll.py
@@ -29,38 +29,22 @@ def execute_main(bot, trigger, triggerargsarray):
     inputstring = get_trigger_arg(bot, triggerargsarray, '2+')
     existingarray = get_botdatabase_value(bot, bot.nick, databasekey) or []
     if command in commandarray:
-        if command == "add":
+        if command == "gender":
+            gender = get_trigger_arg(bot, triggerargsarray, 3)
+            if gender == male
+                adjust_botdatabase_array
             if inputstring not in existingarray:
                 adjust_botdatabase_array(bot, bot.nick, inputstring, databasekey, 'add')
                 message = "Added to database."
 		bot.say(message)
             else:
-                message = "That response is already in the database."
+                message = "You are already a " + gender
 		bot.say(message)
-        elif command == "remove":
-            if inputstring not in existingarray:
-                message = "That response was not found in the database."
-		bot.say(message)
-            else:
-                adjust_botdatabase_array(bot, bot.nick, inputstring, databasekey, 'del')
-                message = "Removed from database."
-		bot.say(message)
-        elif command == "count":
-		messagecount = len(existingarray)
-		message = "There are currently " + str(messagecount) + " responses for that in the database."
-		bot.say(message)
-
+        
         elif command == "last":
 		message = get_trigger_arg(bot, existingarray, "last")
 		bot.say(message)
-    else:
-        weapontype = get_trigger_arg(bot, existingarray, "random") or ''
-        if weapontype == '':
-        	message = "No response found. Have any been added?"
-	target = get_trigger_arg(bot, triggerargsarray, 1)
-	reason = get_trigger_arg(bot, triggerargsarray, '2+')
-	msg = "a " + weapontype
-
+    
 	# No target specified
 	if not target:
 		bot.say("Who/what would you like to murder?")
@@ -68,10 +52,10 @@ def execute_main(bot, trigger, triggerargsarray):
 	# Target is fine
 	else:
 		if not reason:
-			message = instigator + " murders " + target + " with " + msg + "."
+			message = instigator + " rolls " + gender + " at " + target + "."
         		bot.say(message)
 		else:
-			message = instigator + " murders " + target + " with " + msg + " for " + reason + "."
+			message = instigator + " rolls " + gender + " at " + target + " because " + reason + "."
         		bot.say(message)
 
 def get_database_value(bot, nick, databasekey):
