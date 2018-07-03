b95da5c429033de25f71f8324b8f35eca5e64fc8
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-07-02 23:25:13 -0400

    update

--- modules/development/moreYouKnow.py
+++ modules/development/moreYouKnow.py
@@ -31,7 +31,7 @@ def magicFingers():
     api = 'Wi33J3WxSDxWsrxLREcQqmO3iJ0dk52N'
     url = 'http://api.giphy.com/v1/gifs/search?q=themoreyouknow&api_key=' + api + '&limit=100'
     data = json.loads(urllib2.urlopen(url).read())
-    randno = randint(0,50)
+    randno = randint(1,50)
     id = data['data'][randno]['id']
     gif = 'https://media2.giphy.com/media/'+id+'/giphy.gif'
     return gif
