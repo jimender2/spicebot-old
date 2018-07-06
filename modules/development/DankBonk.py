e674c82d7c286311f6070568943c417b29c2b731
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-07-05 21:30:16 -0400

    dankbonk

---
+++ modules/development/DankBonk.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import urllib
+from xml.dom.minidom import parseString
+import sys
+import os
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from SpicebotShared import *
+
+#author jimender2
+
+@sopel.module.commands('dankbonk')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, dankbonk)
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray)
+    
+def execute_main(bot, trigger, triggerargsarray):
+    target = get_trigger_arg(bot,triggerargsarray,1)
+    instigator = trigger.nick
+    reason = get_trigger_arg(bot,triggerargsarray, '2+')
+    
+    #no target
+    if not target:
+        message = "Who would you like to wack?"
+    #target is spicebot
+    elif target == bot.nick:
+		message = "Spicebot teaches %s a lesson by sitting on %s" %instigator %instigator
+    #target is the instigator
+	elif target == instigator:
+        message = "Sorry, I cannot let you harm yourself %s" %instigator
+    #target is fine
+    else:
+        #no reason
+        if not reason:
+            message = "%s whacks %s with a 42 pound sack of primo cheeba." %target %instigator
+        #reason
+        else:
+            message = "%s whacks %s with a 42 pound sack of primo cheeba because %s." %target %instigator %reason
+
+    bot.say(message)
