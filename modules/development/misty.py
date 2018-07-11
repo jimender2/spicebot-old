87408d22d29a988cb6d2afcfad23def1b7c6f228
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2018-07-11 15:44:46 -0400

    misty

---
+++ modules/development/misty.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import urllib2
+import json
+from BeautifulSoup import BeautifulSoup
+from random import randint
+import sys
+import os
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from SpicebotShared import *
+
+#author jimender2
+
+@sopel.module.commands('misty')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray)
+    
+def execute_main(bot, trigger, triggerargsarray):
+
+    instigator = trigger.nick
+    inputstring = get_trigger_arg(bot, triggerargsarray, '1+')
+    bot.say("%s thinks it's starting to get a little misty up in %s")
