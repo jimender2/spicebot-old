d522235d43f375a731c3673896d06fde2a0212ee
deathbybandaid <sam@deathbybandaid.net>
2018-07-12 14:17:05 -0400

    sdfgdsf

--- modules/SpiceBot/Bot/Warn.py
+++ modules/SpiceBot/Bot/Warn.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 import sopel.module
 import sys
 import os
-shareddir = os.path.dirname(os.path.dirname(__file__))
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
 from BotShared import *
 
