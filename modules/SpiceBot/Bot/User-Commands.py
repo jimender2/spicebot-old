be02fd5e2a5c62b92301e52b101e9308e537bae7
deathbybandaid <sam@deathbybandaid.net>
2018-07-12 23:11:52 -0400

    More formatting

--- modules/SpiceBot/Bot/User-Commands.py
+++ modules/SpiceBot/Bot/User-Commands.py
@@ -9,6 +9,7 @@ shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
 from BotShared import *
 
+
 @sopel.module.commands('spicebot')
 def main_command(bot, trigger):
     triggerargsarray = get_trigger_arg(bot, trigger.group(2), 'create')
