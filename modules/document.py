45e62f20f6741bb33103fb8f855731f83148d2d9
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-07-12 21:38:52 -0400

    advertisements

---
+++ modules/document.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding=utf-8
+import os
+
+def save():
+    #write buffer to the file
+    f.flush()
+    os.fsync(f.fileno())
+   
+##############
+##############
+######ONE#####
+##############
+##############   
+def one():
+    fileToRead = raw_input("What is the file name?")
+    read = open(fileToRead, "r")
+    line = read.readline()
+    
+
+
+##############
+##############
+#####MAIN#####
+##############
+##############
+version = raw_input("What version do you want to run? (all or one?)")
+
+if version == "one":
+    one()
+elif version == "all":
+    all()
+
