f0832306e3812034122276c41a9939bc6be322e6
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-07-13 23:06:37 -0400

    lint

--- modules/SpiceBot/Main/Dj.py
+++ modules/SpiceBot/Main/Dj.py
@@ -8,8 +8,9 @@ shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
 from BotShared import *
 
-party='https://raw.githubusercontent.com/deathbybandaid/SpiceBot/master/Text-Files/jukebox_party.txt'
-friday='https://raw.githubusercontent.com/deathbybandaid/SpiceBot/master/Text-Files/jukebox_friday.txt'
+party = 'https://raw.githubusercontent.com/deathbybandaid/SpiceBot/master/Text-Files/jukebox_party.txt'
+friday = 'https://raw.githubusercontent.com/deathbybandaid/SpiceBot/master/Text-Files/jukebox_friday.txt'
+
 
 @sopel.module.commands('dj')
 def mainfunction(bot, trigger):
@@ -17,34 +18,37 @@ def mainfunction(bot, trigger):
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
 
+
 def execute_main(bot, trigger, triggerargsarray):
-	if not trigger.group(2):
-		playlist=party
-	else:
-		query = trigger.group(2).replace(' ','20%')
-		query = str(query)
-		playlist = getplaylist(query)
-		if playlist == party:
-			bot.say('You have selected the Party Playlist')
-	song = getsong(playlist)
-	if song:
-		bot.say(trigger.nick + ' puts a nickel in the jukebox and it starts to play ' + song)
-	else:
-		bot.say('The jukebox starts playing ' + 'Never Gonna Give You Up')
+    if not trigger.group(2):
+        playlist = party
+    else:
+        query = trigger.group(2).replace(' ','20%')
+        query = str(query)
+        playlist = getplaylist(query)
+        if playlist == party:
+            bot.say('You have selected the Party Playlist')
+    song = getsong(playlist)
+    if song:
+        bot.say(trigger.nick + ' puts a nickel in the jukebox and it starts to play ' + song)
+    else:
+        bot.say('The jukebox starts playing ' + 'Never Gonna Give You Up')
+
 
 def getsong(playlist):
-	htmlfile=urllib.urlopen(playlist)
-    	lines=htmlfile.read().splitlines()
-    	mysong=random.choice(lines)
-    	if not mysong or mysong == '\n':
-		mysong = getsong()
-    	return mysong
+    htmlfile = urllib.urlopen(playlist)
+    lines = htmlfile.read().splitlines()
+    mysong = random.choice(lines)
+    if not mysong or mysong == '\n':
+        mysong = getsong()
+        return mysong
+
 
 def getplaylist(query):
-	if query == 'party':
-		myplaylist = party
-	elif query =='friday':
-		myplaylist = friday
-	else:
-		myplaylist = party
-	return myplaylist
+    if query == 'party':
+        myplaylist = party
+    elif query == 'friday':
+        myplaylist = friday
+    else:
+        myplaylist = party
+    return myplaylist
--- modules/SpiceBot/Main/Giphy.py
+++ modules/SpiceBot/Main/Giphy.py
@@ -13,11 +13,13 @@ shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
 from BotShared import *
 
+
 @sopel.module.commands('gif','giphy')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray)
+
 
 def execute_main(bot, trigger, triggerargsarray):
     target = get_trigger_arg(bot, triggerargsarray, 0)
@@ -36,6 +38,7 @@ def execute_main(bot, trigger, triggerargsarray):
     else:
         bot.say("Tell me what you're looking for!")
 
+
 def getGif(query):
     api = 'Wi33J3WxSDxWsrxLREcQqmO3iJ0dk52N'
     limit = 50
@@ -49,13 +52,14 @@ def getGif(query):
         gif = ""
     return gif,randno
 
+
 def roulette():
     api = 'Wi33J3WxSDxWsrxLREcQqmO3iJ0dk52N'
-    #randno = randint(0,9)
-    #if randno == 4:
+    # randno = randint(0,9)
+    # if randno == 4:
     url = 'http://api.giphy.com/v1/gifs/random?api_key=' + str(api) + '&rating=nsfw'
-    #else:
-     #   url = 'http://api.giphy.com/v1/gifs/random?api_key=' + str(api) + '&rating=g'
+    # else:
+    #    url = 'http://api.giphy.com/v1/gifs/random?api_key=' + str(api) + '&rating=g'
     data = json.loads(urllib2.urlopen(url).read())
     try:
         id = data['data']['id']
