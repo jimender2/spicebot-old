c5352f8816565db73d4274e5a9b627c02a1dd8bb
deathbybandaid <sam@deathbybandaid.net>
2018-07-15 15:00:31 -0400

    sdfgsdf

--- modules/Bot/Bot_Control.py
+++ modules/Bot/Bot_Control.py
@@ -518,7 +518,6 @@ def bot_command_function_restart(bot,trigger,botcom,triggerargsarray):
         return
 
     for channel in bot.channels:
-        bot.say(str(bot.nick))
         if bot.nick.lower() != 'spicerpg' and bot.nick.lower() != 'spicerpgdev':
             onscreentext(bot, [channel], trigger.nick + " commanded me to restart. Be Back Soon!")
         else:
