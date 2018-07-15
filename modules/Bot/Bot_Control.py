fdc9d50ed8862f89531e3905f7ebe264847ee9d2
deathbybandaid <sam@deathbybandaid.net>
2018-07-15 15:05:14 -0400

    cxzvb

--- modules/Bot/Bot_Control.py
+++ modules/Bot/Bot_Control.py
@@ -517,9 +517,8 @@ def bot_command_function_restart(bot,trigger,botcom,triggerargsarray):
         osd_notice(bot, botcom.instigator, "You are unauthorized to use this function.")
         return
 
-
     for channel in bot.channels:
-        bot.say(bot.nick.lower())
+        bot.say("test " + bot.nick.lower())
         if bot.nick.lower() != 'spicerpg' and bot.nick.lower() != 'spicerpgdev':
             onscreentext(bot, [channel], trigger.nick + " commanded me to restart. Be Back Soon!")
         else:
