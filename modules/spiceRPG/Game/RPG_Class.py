4400ff22e3f3ad222a96dbf3ae35149e1a8efca8
deathbybandaid <sam@deathbybandaid.net>
2018-07-15 18:39:34 -0400

    test

--- modules/spiceRPG/Game/RPG_Class.py
+++ modules/spiceRPG/Game/RPG_Class.py
@@ -5,9 +5,10 @@ Dynamic Classes
 
 def class_create(bot,classname):
     exec_str = str("class rpg_class_" + classname + " : pass")
-    bot.say(str(exec_str))
-    exec(exec_str)
     eval_str = str("class rpg_class_" + classname)
+    bot.say(str(exec_str))
+    bot.say(str(eval_str))
+    exec(exec_str)
     newclass = str("class rpg_class_" + classname)
     newclass = eval(eval_str)
     return newclass
