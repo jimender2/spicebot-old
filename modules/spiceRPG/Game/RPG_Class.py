b70757954bbe441ea8d88ea365325584720aedd0
deathbybandaid <sam@deathbybandaid.net>
2018-07-15 19:20:42 -0400

    test

--- modules/spiceRPG/Game/RPG_Class.py
+++ modules/spiceRPG/Game/RPG_Class.py
@@ -9,16 +9,9 @@ def class_create(classname):
     return newclass
 
 
-def class_create_new(bot, classname):
-    exec("class rpg_class_" + str(classname) + ":" +
-         "pass")
-    newclass = eval('rpg_class_'+classname+"()")
-    return newclass
-
-
 class MyClass():
     def __init__(self):
-        self.default = "I am an instance of " + str(self) + " at address "+hex(id(self))
+        self.default = "I am an instance of MyClass at address "+hex(id(self))
 
     def __repr__(self):
         return self.default
--- modules/spiceRPG/Game/__init__.py
+++ modules/spiceRPG/Game/__init__.py
@@ -72,10 +72,6 @@ def execute_main(bot, trigger, triggerargsarray, rpg):
     instigatortest = MyClass()
     instigatortest.default = "awesometest"
     bot.say("Output of instigatortest : " + str(instigatortest))
-
-    instigatortest = class_create_new(bot, 'test')
-    instigatortest.default = "awesometest"
-    bot.say("Output of instigatortest : " + str(instigatortest))
     bot.say(instigatortest.default)
 
     return
