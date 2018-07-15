328bf373f47c889367bab3c8bf3974a2e2f4d803
deathbybandaid <sam@deathbybandaid.net>
2018-07-15 19:05:58 -0400

    test

--- modules/spiceRPG/Game/__init__.py
+++ modules/spiceRPG/Game/__init__.py
@@ -69,8 +69,7 @@ def execute_main(bot, trigger, triggerargsarray, rpg):
 
     bot.say("Output of instigator : " + str(instigator))
 
-    instigatortest = class_create('instigatortest')
-    instigatortest.__repr__ = "test"
+    instigatortest = MyClass()
     bot.say("Output of instigatortest : " + str(instigatortest))
 
     return
