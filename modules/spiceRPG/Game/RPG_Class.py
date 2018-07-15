c002229fe34df0b1d2a149b895e6a220d9751e9f
deathbybandaid <sam@deathbybandaid.net>
2018-07-15 18:44:27 -0400

    test

--- modules/spiceRPG/Game/RPG_Class.py
+++ modules/spiceRPG/Game/RPG_Class.py
@@ -4,10 +4,14 @@ Dynamic Classes
 
 
 def class_create(bot,classname):
-    exec_str = str("class rpg_class_" + classname + " : pass")
-    eval_str = str("class rpg_class_" + classname + "()")
-    bot.say(str(exec_str))
-    bot.say(str(eval_str))
-    exec("class rpg_class_" + classname + " : pass")
-    newclass = eval("class rpg_class()")
+    try:
+        exec("class rpg_class_" + str(classname) + " : pass")
+    except SyntaxError:
+        bot.say("exec")
+    try:
+        newclass = eval("class rpg_class_" + str(classname) + "()")
+    except SyntaxError:
+        bot.say("eval")
+
+    newclass = "test"
     return newclass
