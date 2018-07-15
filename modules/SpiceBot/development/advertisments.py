833d9c2f2edc89c4d35f332128de63375f66bcb6
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-07-14 23:24:04 -0400

    bug fix

--- modules/SpiceBot/development/advertisments.py
+++ modules/SpiceBot/development/advertisments.py
@@ -20,6 +20,12 @@ from BotShared import *
 
 
 @sopel.module.commands('ads', 'advertisements', 'ad', 'advertisement')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray = spicebot_prerun(bot, trigger,'ads')
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray)
+
+
 def execute_main(bot, trigger):
     instigator = trigger.nick
     inchannel = trigger.sender
