b4cb0c321b1d186f73c9f73b55647ed7304fed4b
deathbybandaid <sam@deathbybandaid.net>
2018-07-15 19:50:35 -0400

    test

--- modules/spiceRPG/Game/RPG_Class.py
+++ modules/spiceRPG/Game/RPG_Class.py
@@ -9,15 +9,15 @@ def class_create(classname):
     return newclass
 
 
-def class_create_new(bot,classname):
-    exec("class rpg_class_" + str(classname) + " : pass")
+def class_create_new(classname):
+    exec("class rpg_class_" + str(classname) + " : " + exec("def __init__(self): self.default = 'This is the dynamic class ' + str(self.__class__.__name__)") + exec("def __repr__(self): return self.default") + exec("pass"))
     newclass = eval('rpg_class_'+classname+"()")
     return newclass
 
 
 class class_instigator():
     def __init__(self):
-        self.default = "This is a Dynamic Class "+str(self.__class__.__name__)
+        self.default = "This is the dynamic class " + str(self.__class__.__name__)
 
     def __repr__(self):
         return self.default
