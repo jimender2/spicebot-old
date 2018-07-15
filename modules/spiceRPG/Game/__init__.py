9c56de349920f7eaac3bb6d5065c123ce511e7e0
deathbybandaid <sam@deathbybandaid.net>
2018-07-15 16:57:44 -0400

    test

--- modules/spiceRPG/Game/__init__.py
+++ modules/spiceRPG/Game/__init__.py
@@ -61,6 +61,7 @@ def execute_main(bot, trigger, triggerargsarray, rpg):
             rpg.multi_com_list.append(command_split)
 
     # instigator
+    bot.say(str(trigger))
     instigator = rpg_instigator()
     bot.say(str(instigator))
 
