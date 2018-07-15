1a6aaaab8f167067d384ab2b752f35158e10fc74
deathbybandaid <sam@deathbybandaid.net>
2018-07-15 19:04:59 -0400

    test

--- modules/spiceRPG/Game/RPG_Class.py
+++ modules/spiceRPG/Game/RPG_Class.py
@@ -7,3 +7,9 @@ def class_create(classname):
     exec("class rpg_class_" + str(classname) + " : pass")
     newclass = eval('rpg_class_'+classname+"()")
     return newclass
+
+
+class MyClass():
+    def __repr__(self):
+        return "I am an instance of MyClass at address "+hex(id(self))
+    pass
