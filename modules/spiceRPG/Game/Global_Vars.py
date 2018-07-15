466a50ddda8a71f6a3c1ba01f3df5d2cfb24a464
deathbybandaid <sam@deathbybandaid.net>
2018-07-15 15:34:42 -0400

    making things magically work

--- modules/spiceRPG/Game/Global_Vars.py
+++ modules/spiceRPG/Game/Global_Vars.py
@@ -2,7 +2,10 @@
 # coding=utf-8
 from __future__ import unicode_literals, absolute_import, print_function, division
 
-from .Global_Vars import *
+from .Database_adjust import *
+from .Array_manipulation import *
+from .Display_Text import *
+from .RPG_Class import *
 
 """
 ConFigurables
