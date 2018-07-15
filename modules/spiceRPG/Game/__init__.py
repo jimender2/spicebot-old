69fde6dcdb8d6b717ea6c31cf7b736234916e564
deathbybandaid <sam@deathbybandaid.net>
2018-07-15 16:46:41 -0400

    test

--- modules/spiceRPG/Game/__init__.py
+++ modules/spiceRPG/Game/__init__.py
@@ -29,9 +29,9 @@ def rpg_trigger_main(bot, trigger):
 
 
 # respond to alternate start for command
-@module.rule('^(?:rpg)*')
-@module.rule('^(?:!rpg)*')
-@module.rule('^(?:,rpg)*')
+@module.rule('^(?:rpg)\s+?.*')
+@module.rule('^(?:!rpg)\s+?.*')
+@module.rule('^(?:,rpg)\s+?.*')
 @sopel.module.thread(True)
 def rpg_trigger_precede(bot, trigger):
     rpg = rpg_class()
