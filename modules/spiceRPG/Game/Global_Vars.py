e7475f693fbf43e92f04f1d99c875f9acd5d4b94
deathbybandaid <sam@deathbybandaid.net>
2018-07-15 15:38:07 -0400

    sadd

--- modules/spiceRPG/Game/Global_Vars.py
+++ modules/spiceRPG/Game/Global_Vars.py
@@ -2,11 +2,6 @@
 # coding=utf-8
 from __future__ import unicode_literals, absolute_import, print_function, division
 
-from .Database_adjust import *
-from .Array_manipulation import *
-from .Display_Text import *
-from .RPG_Class import *
-
 """
 ConFigurables
 """
