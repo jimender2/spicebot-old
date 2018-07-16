44a34ea6447eecc0f29c7bae5362e38c64435dbe
Dyson Parkes <dyson@berserkir.net>
2018-07-17 11:44:47 +1200

    Ditch Unicode cast

--- modules/SpiceBot/testexamples/instigator.py
+++ modules/SpiceBot/testexamples/instigator.py
@@ -92,7 +92,7 @@ def class_create(classname):
         def __repr__(self):
             return repr(self.default)
         def __str__(self):
-            return unicode(self.default).encode('utf-8')
+            return str(self.default)
         def __iter__(self):
             return str(self.default)
         def __unicode__(self):
