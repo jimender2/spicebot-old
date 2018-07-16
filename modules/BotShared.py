171df29bfdeb8f6c1c3d0d9e54bbd9d4426cfd41
deathbybandaid <sam@deathbybandaid.net>
2018-07-16 08:39:47 -0400

    test

--- modules/BotShared.py
+++ modules/BotShared.py
@@ -816,8 +816,8 @@ def class_create(classname):
         def __init__(self):
             self.default = str(self.__class__.__name__)
         def __repr__(self):
-            self.default = str(self.default)
-            return self.default
+            returndefault = str(self.default)
+            return returndefault
         pass
         """
     exec(compile("class class_" + str(classname) + ": " + compiletext,"","exec"))
