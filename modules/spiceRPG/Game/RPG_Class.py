d836470b15f26148b260d0c0c3fa759dc59e59df
deathbybandaid <sam@deathbybandaid.net>
2018-07-16 09:43:24 -0400

    test

--- modules/spiceRPG/Game/RPG_Class.py
+++ modules/spiceRPG/Game/RPG_Class.py
@@ -8,7 +8,11 @@ def class_create(classname):
         def __init__(self):
             self.default = str(self.__class__.__name__)
         def __repr__(self):
-            return self.default
+            return repr(self.default)
+        def __str__(self):
+            return str(self.default)
+        def __iter__(self):
+            return str(self.default)
         pass
         """
     exec(compile("class class_" + str(classname) + ": " + compiletext,"","exec"))
