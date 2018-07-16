da1fd88d663347cf3efdc7e3700a57fb03ddc762
deathbybandaid <sam@deathbybandaid.net>
2018-07-16 08:44:21 -0400

    test

--- modules/Bot/Bot_Control.py
+++ modules/Bot/Bot_Control.py
@@ -40,6 +40,8 @@ def bot_command_hub(bot, trigger):
 
 def bot_command_process(bot,trigger,triggerargsarray):
 
+    bot.say(str(instigator))
+
     # Dyno Classes
     botcom = class_create('bot')
     instigator = class_create('instigator')
