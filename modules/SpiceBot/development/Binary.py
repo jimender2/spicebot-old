e13d83eac1c942e0709aab2da49e8e2f35926a94
deathbybandaid <sam@deathbybandaid.net>
2018-07-16 14:48:57 -0400

    test

--- modules/SpiceBot/development/Binary.py
+++ modules/SpiceBot/development/Binary.py
@@ -3,7 +3,6 @@
 from __future__ import unicode_literals, absolute_import, print_function, division
 import sopel.module
 from random import randint
-import binascii
 import sys
 import os
 moduledir = os.path.dirname(__file__)
@@ -24,27 +23,13 @@ def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
     if not current_translate:
         spititout = randint(0, 1)
     elif str(current_translate).isdigit():
-        spititout = text_to_bits(current_translate)
-        # spititout = ' '.join(format(ord(x), 'b') for x in current_translate)
-        # spititout = spititout.replace("[","")
-        # spititout = spititout.replace("]","")
-        # spititout = spititout.replace("'","")
+        spititout = string2bits(current_translate)
     else:
-        spititout = text_from_bits(current_translate)
+        spititout = bits2string(current_translate)
     onscreentext(bot, ['say'], str(spititout))
 
+def string2bits(s=''):
+    return [bin(ord(x))[2:].zfill(8) for x in s]
 
-def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
-    bits = bin(int(binascii.hexlify(text.encode(encoding, errors)), 16))[2:]
-    return bits.zfill(8 * ((len(bits) + 7) // 8))
-
-
-def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
-    n = int(bits, 2)
-    return int2bytes(n).decode(encoding, errors)
-
-
-def int2bytes(i):
-    hex_string = '%x' % i
-    n = len(hex_string)
-    return binascii.unhexlify(hex_string.zfill(n + (n & 1)))
+def bits2string(b=None):
+    return ''.join([chr(int(x, 2)) for x in b])
