2ffa3919a8dbb437403a7bc3a0662c3f34c6d96b
Dyson Parkes <dyson@berserkir.net>
2018-07-17 15:28:32 +1200

    isinstance(self,self.default)

--- modules/SpiceBot/testexamples/instigator.py
+++ modules/SpiceBot/testexamples/instigator.py
@@ -92,7 +92,7 @@ def class_create(classname):
         def __repr__(self):
             return repr(self.default)
         def __str__(self):
-            if isinstance(self.default):
+            if isinstance(self, self.default):
                 outputstring = str(self.default)
                 return outputstring
             else:
