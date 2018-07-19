608e3b8d43b66190d2b303fab6842a2750157972
deathbybandaid <sam@deathbybandaid.net>
2018-07-19 11:12:25 -0400

    test

--- modules/spiceRPG/Game/__init__.py
+++ modules/spiceRPG/Game/__init__.py
@@ -137,8 +137,8 @@ def rpg_errors_start(bot, rpg):
     for error_type in rpg_error_list:
         current_error_type = eval("rpg_error_" + error_type)
         for i in range(0,len(current_error_type)):
-            bot.say(str(i))
-            # current_error = get_trigger_arg(bot, current_error_type, total_errors)
+            current_error = get_trigger_arg(bot, current_error_type, i - 1)
+            bot.say(str(current_error))
         # for i in len(current_error_type):
             # current_error_value = str("rpg.errors." + x + " = []")
             # exec(current_error_value)
