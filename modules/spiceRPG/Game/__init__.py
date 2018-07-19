cd0307284332c09e4694d51b13ee2d8deb33e88b
deathbybandaid <sam@deathbybandaid.net>
2018-07-19 12:03:38 -0400

    test

--- modules/spiceRPG/Game/__init__.py
+++ modules/spiceRPG/Game/__init__.py
@@ -174,6 +174,9 @@ def rpg_errors_end(bot, rpg):
                 if "$count" in errormessage:
                     totalnumber = len(currenterrorvalue)
                     errormessage = str(errormessage.replace("$count", "(" + str(totalnumber) + ")"))
+                if "$list" in errormessage:
+                    errorlist = get_trigger_arg(bot, currenterrorvalue, 'list')
+                    errormessage = str(errormessage.replace("$list", str(errorlist))
                 if errormessage not in rpg.error_display:
                     rpg.error_display.append(errormessage)
 
