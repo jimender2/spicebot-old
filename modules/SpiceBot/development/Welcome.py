38db57a7ad61f625469faa6fc0d13b251a447d63
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-07-19 22:37:52 -0400

    welcome module

---
+++ modules/SpiceBot/development/Welcome.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+
+@sopel.module.commands('welcome')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'welcome')
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray, botcom, instigator)
+
+
+def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+    target = get_trigger_arg(bot, triggerargsarray, 2)
+    command = get_trigger_arg(bot, triggerargsarray, 1)
+    instigator = trigger.nick
+    channel = channel
+    if command == "first":
+        message = instigator + " welcomes " + target + " to " + channel
+    else:
+        message = instigator + " is glad to see " + target + " again."
+
+    osd(bot, trigger.sender, 'say', message)
