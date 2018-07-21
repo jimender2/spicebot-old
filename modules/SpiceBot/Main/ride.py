edded92c75bb17a3150420ed9313cf2e46198f0a
deathbybandaid <sam@deathbybandaid.net>
2018-07-21 12:44:06 -0400

    fix randint

--- modules/SpiceBot/Main/ride.py
+++ modules/SpiceBot/Main/ride.py
@@ -2,6 +2,7 @@
 # coding=utf-8
 from __future__ import unicode_literals, absolute_import, print_function, division
 import sopel.module
+from random import randint
 import sys
 import os
 moduledir = os.path.dirname(__file__)
