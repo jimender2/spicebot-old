7d74b6dcc6dbf2866daa5266d65ac33bedd8a0fe
deathbybandaid <sam@deathbybandaid.net>
2018-07-21 14:24:44 -0400

    test

--- modules/SpiceBot/development/Pull-My-Finger.py
+++ modules/SpiceBot/development/Pull-My-Finger.py
@@ -18,4 +18,5 @@ def mainfunction(bot, trigger):
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
-    osd(bot, trigger.sender, 'say', "include target, and result is floating an airbiscuit")
+    target = get_trigger_arg(bot, [x for x in triggerargsarray if x in botcom.users_all], 1) or botcom.instigator
+    osd(bot, trigger.sender, 'say', trigger.nick + " floats an air biscuit in the general direction of " + target + ".")
