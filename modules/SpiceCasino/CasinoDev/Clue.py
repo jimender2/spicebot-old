576dfa611fbe710e6e00d651af0328a919773019
deathbybandaid <sam@deathbybandaid.net>
2018-07-21 10:34:40 -0400

    pep

--- modules/SpiceCasino/CasinoDev/Clue.py
+++ modules/SpiceCasino/CasinoDev/Clue.py
@@ -13,9 +13,10 @@ sys.path.append(shareddir)
 from BotShared import *
 from Bucks import *
 
-cluefee=5 #amount charged to use the command
+cluefee = 5  # amount charged to use the command
 rooms = ['Ballroom', 'Billiard Room', 'Cellar', 'Conservatory', 'Dining Room', 'Kitchen', 'Hall', 'Library', 'Lounge', 'Study', 'secret passage', 'Spa', 'Theater', 'Nearby Guest House']
 weapons = ['Candlestick', 'Knife', 'Lead Pipe', 'Revolver', 'Rope', 'Wrench', 'Dumbbell', 'Trophy', 'Poison']
+
 
 @sopel.module.commands('clue')
 def mainfunction(bot, trigger):
@@ -23,22 +24,23 @@ def mainfunction(bot, trigger):
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray, botcom, instigator)
 
+
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
     channel = trigger.sender
     instigator = trigger.nick
-    pointsworth = random.uniform(.03,.08)
+    pointsworth = random.uniform(.03, .08)
     if not channel.startswith("#"):
         osd(bot, instigator, 'notice', "Clue must be in a channel.")
         return
     target = get_trigger_arg(bot, triggerargsarray, 1)
     suspect = get_trigger_arg(bot, triggerargsarray, 2)
-    cluefee=5
+    cluefee = 5
     if not target:
-        cluefee=0
-        pointsworth=0
+        cluefee = 0
+        pointsworth = 0
     players = []
     if Spicebucks.transfer(bot, trigger.nick, 'SpiceBank', cluefee) == 1:
-        bankbalance=Spicebucks.bank(bot,instigator)
+        bankbalance = Spicebucks.bank(bot, instigator)
         pointsworth = (int(pointsworth*bankbalance)) + 5
         pointsvalue = str(pointsworth)
         osd(bot, trigger.sender, 'say', trigger.nick + " paid " + str(cluefee) + " spicebucks and started a game of clue.")
@@ -62,17 +64,17 @@ def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
                 elif suspect == 'killed' and target == players[1]:
                     osd(bot, trigger.sender, 'say', 'You guessed the person murdered!')
                     osd(bot, trigger.sender, 'say', bot.nick + ' gives ' + pointsvalue + 'Spicebucks to ' + instigator)
-                    Spicebucks.spicebucks(bot,instigator,'plus',pointsworth)
+                    Spicebucks.spicebucks(bot, instigator, 'plus', pointsworth)
         elif target and target == players[0]:
             osd(bot, trigger.sender, 'say', 'You guessed the killer correctly!')
             osd(bot, trigger.sender, 'say', bot.nick + ' gives ' + pointsvalue + ' Spicebucks to' + instigator)
-            Spicebucks.spicebucks(bot,instigator,'plus',pointsworth)
+            Spicebucks.spicebucks(bot, instigator, 'plus', pointsworth)
         if players[0] == trigger.nick:
             osd(bot, trigger.sender, 'say', 'You were the killer.')
 
-            if pointsworth>bankbalance:
-                pointsworth=bankbalance
-            Spicebucks.spicebucks(bot,instigator,'minus',pointsworth)
+            if pointsworth > bankbalance:
+                pointsworth = bankbalance
+            Spicebucks.spicebucks(bot, instigator, 'minus', pointsworth)
             osd(bot, trigger.sender, 'say', bot.nick + ' takes ' + pointsvalue + ' Spicebucks from ' + instigator)
     else:
         osd(bot, instigator, 'priv', "You need " + str(cluefee) + " spicebucks to use this command.")
--- modules/SpiceCasino/CasinoDev/Monopoly.py
+++ modules/SpiceCasino/CasinoDev/Monopoly.py
@@ -15,14 +15,14 @@ from Bucks import *
 
 monopolyfee = 5
 
-gooddeck = ["an 'Advance to Go' card","a 'Bank error in your favor' card","a 'Your crypto miner pays off' card"]
-baddeck = ["a 'Pay poor tax' card","a 'Hit with ransomware' card","a 'License audit fails' card"]
-neutraldeck = ["a 'Get out of Jail Free' card","a 'Go directly to Jail, Do not pass Go, Do not collect 200 dollars' card","a 'Go Back 3 Spaces' card"]
+gooddeck = ["an 'Advance to Go' card", "a 'Bank error in your favor' card", "a 'Your crypto miner pays off' card"]
+baddeck = ["a 'Pay poor tax' card", "a 'Hit with ransomware' card", "a 'License audit fails' card"]
+neutraldeck = ["a 'Get out of Jail Free' card", "a 'Go directly to Jail, Do not pass Go, Do not collect 200 dollars' card", "a 'Go Back 3 Spaces' card"]
 
 
-@sopel.module.commands('monopoly','chance')
+@sopel.module.commands('monopoly', 'chance')
 def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot,trigger, 'monopoly')
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'monopoly')
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray, botcom, instigator)
 
@@ -30,25 +30,25 @@ def mainfunction(bot, trigger):
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
     channel = trigger.sender
     instigator = trigger.nick
-    deckchoice = randint(1,3)
-    payment = random.uniform(0.1,0.3)
-    balance = bank(bot,instigator)
+    deckchoice = randint(1, 3)
+    payment = random.uniform(0.1, 0.3)
+    balance = bank(bot, instigator)
     payout = int(payment*balance)
 
     if Spicebucks.transfer(bot, trigger.nick, 'SpiceBank', monopolyfee) == 1:
         if deckchoice == 1:
-          chancecard = get_trigger_arg(bot,gooddeck,'random')
-          msg = chancecard + " and wins " + str(payout) + " Spicebucks"
+            chancecard = get_trigger_arg(bot, gooddeck, 'random')
+            msg = chancecard + " and wins " + str(payout) + " Spicebucks"
         elif deckchoice == 2:
-          chancecard=get_trigger_arg(bot,baddeck,'random')
-          msg = chancecard + " and loses " + str(payout) + " Spicebucks"
-          payout=-payout
+            chancecard = get_trigger_arg(bot, baddeck, 'random')
+            msg = chancecard + " and loses " + str(payout) + " Spicebucks"
+            payout = - payout
         elif deckchoice == 3:
-          msg = get_trigger_arg(bot,neutraldeck,'random')
-          payout = 0
+            msg = get_trigger_arg(bot, neutraldeck, 'random')
+            payout = 0
         osd(bot, trigger.sender, 'say', instigator + " risks " + str(monopolyfee) + " Spicebucks to draw a card from the chance deck! " + instigator + " gets " + msg + ".")
         if (balance + payout) < 0:
-          payout = balance
-        addbucks(bot,instigator, payout)
+            payout = balance
+        addbucks(bot, instigator, payout)
     else:
         osd(bot, instigator, 'priv', "You need " + str(monopolyfee) + " Spicebucks to use this command.")
