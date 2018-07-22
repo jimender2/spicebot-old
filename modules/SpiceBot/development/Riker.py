68bee2ca61303cec7b38036104c985e806a7a162
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-07-21 23:40:14 -0400

    riker

--- modules/SpiceBot/development/Riker.py
+++ modules/SpiceBot/development/Riker.py
@@ -2,15 +2,20 @@
 # coding=utf-8
 from __future__ import unicode_literals, absolute_import, print_function, division
 import sopel.module
+import random
+import urllib
 import sys
 import os
+from word2number import w2n
 moduledir = os.path.dirname(__file__)
 shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
 from BotShared import *
 
+site = 'https://raw.githubusercontent.com/deathbybandaid/SpiceBot/master/Text-Files/riker.txt'
 
-@sopel.module.commands('yourcommandhere')
+
+@sopel.module.commands('riker')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
@@ -18,4 +23,14 @@ def mainfunction(bot, trigger):
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
-    osd(bot, trigger.sender, 'say', "do the thing")
+    data = urllib2.urlopen(site).read()
+    data = data.split(" ")
+    max = get_trigger_arg(bot, triggerargsarray, 1)
+    max = max - 1
+    message = ""
+
+    i = 0
+    while i <= max:
+        meassage = message + data[i]
+
+    osd(bot, trigger.sender, 'say', message)
