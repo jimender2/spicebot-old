03ab8401d4a8090c33951eaecdaacff74f45466e
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-07-21 22:50:23 -0400

    update

--- modules/SpiceBot/development/Riker.py
+++ modules/SpiceBot/development/Riker.py
@@ -2,8 +2,6 @@
 # coding=utf-8
 from __future__ import unicode_literals, absolute_import, print_function, division
 import sopel.module
-from random import random
-from random import randint
 import sys
 import os
 moduledir = os.path.dirname(__file__)
@@ -12,7 +10,7 @@ sys.path.append(shareddir)
 from BotShared import *
 
 
-@sopel.module.commands('sexbot', 'cockbot', 'fuckbot')
+@sopel.module.commands('yourcommandhere')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
@@ -20,17 +18,4 @@ def mainfunction(bot, trigger):
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
-    rando = randint(2, 22)
-    osd(bot, trigger.sender, 'say', "Please insert " + str(rando) + " bitcoins, for that kind of service.")
-    price = get_price()
-    osd(bot, trigger.sender, 'say', "They are $" + str(price) + " for one right now.")
-
-
-def get_price():
-    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
-    data = json.loads(urllib2.urlopen(url).read())
-    try:
-        price = USD['rate']
-    except IndexError:
-        price = "unknown"
-    return price
+    osd(bot, trigger.sender, 'say', "do the thing")
