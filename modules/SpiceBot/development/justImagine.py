39c0f33367ba7c5565d140fbeb315f9e94385dd1
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-07-23 22:34:57 -0400

    just imagine

---
+++ modules/SpiceBot/development/justImagine.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+# author jimender2
+
+
+@sopel.module.commands('imagine', 'justimagine')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'imagine')
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray, botcom, instigator)
+
+
+def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+    instigator = trigger.nick
+    person = get_trigger_arg(bot, triggerargsarray, 1)
+    reason = get_trigger_arg(bot, triggerargsarray, '2+')
+
+    if not person:
+        person = trigger.nick
+    if not reason:
+        reason = "all the time in the world"
+    message = "Just imagine if " + person + " had " + reason
+    osd(bot, trigger.sender, 'say', message)
