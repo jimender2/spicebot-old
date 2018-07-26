d2ff22412518cef54a732d6104139f99d0498e7c
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-07-25 20:02:49 -0400

    moved a bunch of modules to main.

--- modules/SpiceBot/development/Totes.py
+++ modules/SpiceBot/development/Totes.py
@@ -18,15 +18,20 @@ def mainfunction(bot, trigger):
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
-    rand = random.randint(1, 5)
-    if rand == 1:
-        message = "I will have a non-fat double chocolate expresso mocho soy cappachino express latte (aka crap (whatever happened to a plain cup of joe?))"
-    elif rand == 2:
-        message = "I want a participation medel"
-    elif rand == 3:
-        message = "Lifes not fair!"
-    elif rand == 4:
-        message = "My mommy says Im a special snowflake"
-    elif rand == 5:
-        message = ""
+    command = get_trigger_arg(bot, triggerargsarray, 1)
+    if not command:
+        rand = random.randint(1, 4)
+        if rand == 1:
+            message = "I will have a non-fat double chocolate expresso mocho soy cappachino express latte (aka crap (whatever happened to a plain cup of joe?))"
+        elif rand == 2:
+            message = "I want a participation medel"
+        elif rand == 3:
+            message = "Lifes not fair!"
+        elif rand == 4:
+            message = "My mommy says Im a special snowflake"
+        else:
+            message = "I fucked the code up real well"
+    else:
+        instigator = trigger.nick
+        message = instigator " thinks that " command + " is totes obvious."
     osd(bot, trigger.sender, 'say', message)
