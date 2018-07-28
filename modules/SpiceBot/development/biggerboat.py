862f615bfb20206c21eebb5773a38ef8fcf40296
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-07-27 22:22:12 -0400

    bigger boat

---
+++ modules/SpiceBot/development/biggerboat.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+# author jimender2
+
+
+@sopel.module.commands('biggerboat')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'biggerboat')
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray, botcom, instigator)
+
+
+def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+    something = get_trigger_arg(bot, triggerargsarray, '1+')
+    if not something:
+        something = "boat"
+    message = "We're gonna need a bigger " + something
+    osd(bot, trigger.sender, 'say', message)
