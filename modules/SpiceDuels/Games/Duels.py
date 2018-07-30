8d1748b06dfeab7a74e6cd9c3658c800e60fb97d
deathbybandaid <sam@deathbybandaid.net>
2018-07-30 09:29:10 -0400

    test

--- modules/SpiceDuels/Games/Duels.py
+++ modules/SpiceDuels/Games/Duels.py
@@ -7728,7 +7728,100 @@ On Screen Text
 """
 
 
-def osd(bot, target_array, text_type_array, text_array):
+def osd(bot, target_array, text_type, text_array):
+
+    # if text_array is a string, make it an array
+    textarraycomplete = []
+    if not isinstance(text_array, list):
+        textarraycomplete.append(str(text_array))
+    else:
+        for x in text_array:
+            textarraycomplete.append(str(x))
+
+    # if target_array is a string, make it an array
+    texttargetarray = []
+    if not isinstance(target_array, list):
+        if not str(target_array).startswith("#"):
+            target_array = nick_actual(bot, str(target_array))
+        texttargetarray.append(target_array)
+    else:
+        for target in target_array:
+            if not str(target).startswith("#"):
+                target = nick_actual(bot, str(target))
+            texttargetarray.append(target)
+
+    # Make sure we don't cross over IRC limits
+    for target in texttargetarray:
+        temptextarray = []
+        if text_type == 'notice':
+            temptextarray.append(target + ", ")
+        for part in textarraycomplete:
+            temptextarray.append(part)
+
+        # 'say' can equal 'priv'
+        if text_type == 'say' and not str(target).startswith("#"):
+            text_type = 'priv'
+
+        # Make sure no individual string ins longer than it needs to be
+        currentstring = ''
+        texttargetarray = []
+        for textstring in temptextarray:
+            if len(textstring) > osd_limit:
+                chunks = textstring.split()
+                for chunk in chunks:
+                    if currentstring == '':
+                        currentstring = chunk
+                    else:
+                        tempstring = str(currentstring + " " + chunk)
+                        if len(tempstring) <= osd_limit:
+                            currentstring = tempstring
+                        else:
+                            texttargetarray.append(currentstring)
+                            currentstring = chunk
+                if currentstring != '':
+                    texttargetarray.append(currentstring)
+            else:
+                texttargetarray.append(textstring)
+
+        # Split text to display nicely
+        combinedtextarray = []
+        currentstring = ''
+        for textstring in texttargetarray:
+            if currentstring == '':
+                currentstring = textstring
+            elif len(textstring) > osd_limit:
+                if currentstring != '':
+                    combinedtextarray.append(currentstring)
+                    currentstring = ''
+                combinedtextarray.append(textstring)
+            else:
+                tempstring = str(currentstring + "   " + textstring)
+                if len(tempstring) <= osd_limit:
+                    currentstring = tempstring
+                else:
+                    combinedtextarray.append(currentstring)
+                    currentstring = textstring
+        if currentstring != '':
+            combinedtextarray.append(currentstring)
+
+        # display
+        textparts = len(combinedtextarray)
+        textpartsleft = textparts
+        for combinedline in combinedtextarray:
+            if text_type == 'action' and textparts == textpartsleft:
+                bot.action(combinedline, target)
+            elif str(target).startswith("#"):
+                bot.msg(target, combinedline)
+            elif text_type == 'notice' or text_type == 'priv':
+                bot.notice(combinedline, target)
+            elif text_type == 'say':
+                bot.say(combinedline)
+            else:
+                bot.say(combinedline)
+            textpartsleft = textpartsleft - 1
+
+
+def osd_new(bot, target_array, text_type_array, text_array):
 
     # if text_array is a string, make it an array
     textarraycomplete = []
