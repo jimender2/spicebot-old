0b067daffff1a003b555ec5960286dcc0de1c675
deathbybandaid <sam@deathbybandaid.net>
2018-07-31 19:37:17 -0400

    night

---
+++ modules/SpiceBot/development/Night.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+# author yournamehere
+
+
+@sopel.module.commands('night')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, trigger.group(1))
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray, botcom, instigator)
+
+
+def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+    target = get_trigger_arg(bot, triggerargsarray, 0) or instigator.default
+    osd(bot, trigger.sender, 'action', "Tucks " + target + " into bed")
