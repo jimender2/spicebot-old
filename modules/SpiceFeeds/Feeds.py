c321446e56960f70b534f0ed0a860c479087d0b6
deathbybandaid <sam@deathbybandaid.net>
2018-07-31 11:45:07 -0400

    test

--- modules/SpiceFeeds/Feeds.py
+++ modules/SpiceFeeds/Feeds.py
@@ -219,7 +219,7 @@ def feeds_display(bot, botcom, feed, feeds, displayifnotnew):
             dispmsg.append(title)
 
             tomorrow = now + datetime.timedelta(days=1)
-            dailytime = datetime(tomorrow.year, tomorrow.month, tomorrow.day, int(timehour), int(timeminute), 0, 0)
+            dailytime = datetime.datetime(tomorrow.year, tomorrow.month, tomorrow.day, int(timehour), int(timeminute), 0, 0)
             dailytz = pytz.timezone(scrapetimezone)
             dailytime = parser.parse(dailytime)
             dailytime = webbytz.localize(dailytime)
