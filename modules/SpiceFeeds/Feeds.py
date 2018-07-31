4b2fde8112c66fbcdd12ecf1b541336d3df12622
deathbybandaid <sam@deathbybandaid.net>
2018-07-31 09:36:19 -0400

    test

--- modules/SpiceFeeds/Feeds.py
+++ modules/SpiceFeeds/Feeds.py
@@ -78,7 +78,7 @@ def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
         else:
             current_feed_list = [feed_select]
         for feed in current_feed_list:
-            dispmsg = feeds_display(bot, feed, botcom, feeds, 1) or []
+            dispmsg = feeds_display(bot, botcom, feed, feeds, 1) or []
             if dispmsg == []:
                 osd(bot, botcom.channel_current, 'say', feed_select + " appears to have had an unknown error.")
             else:
@@ -90,7 +90,7 @@ def feeds_display(bot, botcom, feed, feeds, displayifnotnew):
 
     dispmsg = []
 
-    feed_url = eval("feeds." + str(feed) + ".url")
+    feed_url = eval("feeds." + feed + ".url")
     page = requests.get(feed_url, headers=header)
     if page.status_code == 200:
 
