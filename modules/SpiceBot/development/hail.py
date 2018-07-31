627c64bb8049789bec43da320d3c502116e6e398
Jonathan Meredith <35303639+jimender2@users.noreply.github.com>
2018-07-30 20:21:51 -0400

    hail

--- modules/SpiceBot/development/hail.py
+++ modules/SpiceBot/development/hail.py
@@ -9,12 +9,12 @@ shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
 from BotShared import *
 
-# author yournamehere
+# author jimender2
 
 
-@sopel.module.commands('yourcommandhere')
+@sopel.module.commands('hail')
 def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, trigger.group(1))
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'hail')
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray, botcom, instigator)
 
