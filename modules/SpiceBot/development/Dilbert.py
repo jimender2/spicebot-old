eaaef6ea1d33ef2effb59d9481447d2a92678b0a
deathbybandaid <sam@deathbybandaid.net>
2018-08-01 17:16:11 -0400

    note

--- modules/SpiceBot/development/Dilbert.py
+++ modules/SpiceBot/development/Dilbert.py
@@ -8,6 +8,8 @@ shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
 from BotShared import *
 
+# module deprecated after feeds.py
+
 
 @sopel.module.commands('dilbert')
 def mainfunction(bot, trigger):
