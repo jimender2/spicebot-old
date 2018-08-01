198d20eb1d08c272a9fa43903bd30512c8ea5f18
deathbybandaid <sam@deathbybandaid.net>
2018-08-01 15:41:19 -0400

    test

--- modules/SpiceFeeds/Feeds.py
+++ modules/SpiceFeeds/Feeds.py
@@ -191,7 +191,7 @@ def feeds_display(bot, feed, feeds, displayifnotnew):
             lastBuildXML = lastBuildXML[0].childNodes[0].nodeValue
             lastBuildXML = parser.parse(str(lastBuildXML))
 
-            if displayifnotnew or lastBuildXML != lastbuildcurrent:
+            if displayifnotnew or lastBuildXML > lastbuildcurrent:
 
                 titleappend = 1
 
@@ -309,7 +309,7 @@ def feeds_display(bot, feed, feeds, displayifnotnew):
             lastbuildcurrent = get_database_value(bot, bot.nick, feed + '_lastbuildcurrent') or datetime.datetime(1999, 1, 1, 1, 1, 1, 1).replace(tzinfo=pytz.UTC)
             lastbuildcurrent = parser.parse(str(lastbuildcurrent))
 
-            if displayifnotnew or lastBuildXML != lastbuildcurrent:
+            if displayifnotnew or scrapedtime > lastbuildcurrent:
 
                 titleappend = 1
 
