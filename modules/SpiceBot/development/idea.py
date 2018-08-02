2424785380dd5c25f63015060f09e1796b359371
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-08-01 23:45:26 -0400

    ideas

---
+++ modules/SpiceBot/development/idea.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+# author jimender2
+
+################################################################################
+################### Still working on this. Touch and die #######################
+################################################################################
+
+array = ["jump off a bridge"]
+
+
+@sopel.module.commands('idea')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'idea')
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray, botcom, instigator)
+
+
+def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+    osd(bot, trigger.sender, 'say', "do the thing")
+    inputstring = get_trigger_arg(bot, triggerargsarray, '1+')
+
+    if not command:
+        database_initialize(bot, bot.nick, testarray, 'idea')
+        existingarray = get_database_value(bot, bot.nick, 'idea') or []
+    else:
+        existingarray = get_database_value(bot, bot.nick, 'idea') or []
+        if inputstring not in existingarray:
+            adjust_database_array(bot, bot.nick, inputstring, 'idea', 'add')
+            message = ""
+            osd(bot, trigger.sender, 'say', message)
