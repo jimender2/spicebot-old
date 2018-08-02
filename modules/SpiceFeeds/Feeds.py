48da8f7f55bdcd83f37387b627dd6dcd2b14b462
deathbybandaid <sam@deathbybandaid.net>
2018-08-02 11:55:32 -0400

    fix

--- modules/SpiceFeeds/Feeds.py
+++ modules/SpiceFeeds/Feeds.py
@@ -214,8 +214,6 @@ def feeds_display(bot, feed, feeds, displayifnotnew):
 
         if feed_type in ['rss', 'youtube', 'github']:
 
-            childnumber = int(eval("feeds." + feed + ".childnumber"))
-
             lastbuildcurrent = get_database_value(bot, bot.nick, feed + '_lastbuildcurrent') or datetime.datetime(1999, 1, 1, 1, 1, 1, 1).replace(tzinfo=pytz.UTC)
             lastbuildcurrent = parser.parse(str(lastbuildcurrent))
 
