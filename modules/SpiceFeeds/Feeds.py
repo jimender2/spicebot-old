303ec3ec57ea64ba450f2b8e713042994787155d
deathbybandaid <sam@deathbybandaid.net>
2018-08-02 12:07:39 -0400

    test

--- modules/SpiceFeeds/Feeds.py
+++ modules/SpiceFeeds/Feeds.py
@@ -449,7 +449,6 @@ def feeds_configs(bot, feeds):
             config.read(feedfile)
             for each_section in config.sections():
                 for (each_key, each_val) in config.items(each_section):
-                    bot.say(str(each_key) + " = " + str(each_val))
                     exec("feeds." + feed + "." + each_key + " = each_val")
 
     return feeds
