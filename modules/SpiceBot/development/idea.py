35f2a6447f4747eee180de0b35311e1cc1b6723a
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-08-04 22:53:17 -0400

    idea

--- modules/SpiceBot/development/idea.py
+++ modules/SpiceBot/development/idea.py
@@ -23,7 +23,6 @@ def mainfunction(bot, trigger):
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
-    osd(bot, trigger.sender, 'say', "do the thing")
     command = get_trigger_arg(bot, triggerargsarray, 1)
     inputstring = get_trigger_arg(bot, triggerargsarray, '2+')
 
