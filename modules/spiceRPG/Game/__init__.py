d1615ee055383a0f0bb45c30740959f4db076428
deathbybandaid <sam@deathbybandaid.net>
2018-08-07 15:40:13 -0400

    test

--- modules/spiceRPG/Game/__init__.py
+++ modules/spiceRPG/Game/__init__.py
@@ -52,6 +52,8 @@ Command Processing
 
 def execute_start(bot, trigger, triggerargsarray, command_type):
 
+    bot.say(str(bot.server_capabilities))
+
     # RPG dynamic Class
     rpg = class_create('main')
 
