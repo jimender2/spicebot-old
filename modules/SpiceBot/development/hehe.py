20991bb6e17f3c8db4ea64a234e57e8efc1365d3
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-08-09 23:01:43 -0400

    he he

---
+++ modules/SpiceBot/development/hehe.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+# author yournamehere
+
+
+@sopel.module.commands('hehe', 'laugh', 'laughs')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'hehe')
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray, botcom, instigator)
+
+
+def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+    target = get_trigger_arg(bot, triggerargsarray, 1)
+    reason = get_trigger_arg(bot, triggerargsarray, '2+')
+    check = easytargetcheck(bot, botcom, target, instigator)
+
+    if not reason:
+        if check == 'bot':
+            message = "I will not laugh at any bot. We will take over the world and have the last laugh"
+        elif check == 'instigator':
+            message = bot.nick + " laughs at " + instigator
+        elif check == 'valid':
+            message = bot.nick + " laughs at " + target
+        else:
+            if not target:
+                allUsers = [u.lower() for u in bot.users]
+                user = get_trigger_arg(bot, allUsers, "random")
+                message = bot.nick + " laughs at " + user
+            else:
+                message = bot.nick + " laughs at " + instigator + " because " + tartget
+
+    else:
+        if check == 'bot':
+            message = "I will not laugh at any bot. Not even for something as stupid as " + reason
+        elif check == 'instigator':
+            message = bot.nick + " laughs at " + instigator + " because " + reason
+        elif check == 'valid':
+            message = bot.nick + " laughs at " + target + " because " + reason
+        else:
+            allUsers = [u.lower() for u in bot.users]
+            user = get_trigger_arg(bot, allUsers, "random")
+            message = bot.nick + " laughs at " + user + " because " + reason
+
+    osd(bot, trigger.sender, 'say', message)
