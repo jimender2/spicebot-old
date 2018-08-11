4c48f3460a4caa693d3780caa216e44878e8aa0c
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-08-10 22:48:46 -0400

    pube

--- modules/SpiceBot/development/pube.py
+++ modules/SpiceBot/development/pube.py
@@ -9,15 +9,34 @@ shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
 from BotShared import *
 
-# author yournamehere
+# author jimender2
 
 
-@sopel.module.commands('yourcommandhere')
+@sopel.module.commands('pube')
 def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, trigger.group(1))
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'pube')
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray, botcom, instigator)
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
-    osd(bot, trigger.sender, 'say', "do the thing")
+    instigator = trigger.nick
+    target = get_trigger_arg(bot, triggerargsarray, 1)
+    targetcheck = easytargetcheck(bot, botcom, target, instigator)
+
+    if targetcheck == "instigator":
+        osd(bot, trigger.sender, 'say', "Really?? You dont know if the curtains match the drapes!!")
+    elif targetcheck == "bot":
+        osd(bot, trigger.sender, 'say', "You know I dont care what you say")
+    elif targetcheck == "false":
+        allUsers = [u.lower() for u in bot.users]
+        users = get_trigger_arg(bot, allUsers, 0) or 'spicebot'
+        osd(bot, trigger.sender, 'say', instigator + " wants to know if " + target + " carpets matches the drapes")
+    elif targetcheck == "offline":
+        osd(bot, trigger.sender, 'say', "You cannot ask someone that is offline")
+    elif targetcheck == "online":
+        osd(bot, trigger.sender, 'say', instigator + " wants to know if " + target + " carpets matches the drapes")
+    else:
+        allUsers = [u.lower() for u in bot.users]
+        users = get_trigger_arg(bot, allUsers, 0) or 'spicebot'
+        osd(bot, trigger.sender, 'say', instigator + " wants to know if " + target + " carpets matches the drapes")
