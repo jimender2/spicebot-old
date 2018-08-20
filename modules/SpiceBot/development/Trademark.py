382ba8024b9a9138bcf6529d8a43ec8e3d101965
deathbybandaid <sam@deathbybandaid.net>
2018-08-20 15:01:53 -0400

    trademark

---
+++ modules/SpiceBot/development/Trademark.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+# author yournamehere
+
+
+@sopel.module.commands('trademark', 'tm')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'trademark')
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray, botcom, instigator)
+
+
+def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+    complete = get_trigger_arg(bot, triggerargsarray, 0)
+    osd(bot, trigger.sender, 'say', complete + "ᵀᴹ")
