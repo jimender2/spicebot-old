65d40680c2f60f9604929bd1d07cedfba4857d31
deathbybandaid <sam@deathbybandaid.net>
2018-08-21 11:55:20 -0400

    should work now

--- modules/SpiceBot/development/Reddit.py
+++ modules/SpiceBot/development/Reddit.py
@@ -134,9 +134,8 @@ def sub_exists(sub):
 
 def user_exists(user):
     exists = True
-    reddit.redditor(user).fullname
-    # try:
-    #    reddit.get_redditor(user).fullname
-    # except praw.errors.NotFound:
-    #    exists = False
+    try:
+        reddit.redditor(user).fullname
+    except praw.errors.NotFound:
+        exists = False
     return exists
