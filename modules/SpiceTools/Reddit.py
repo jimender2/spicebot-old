ac05765c2bffe15cfc2dd498cbb934e92664cfff
deathbybandaid <sam@deathbybandaid.net>
2018-08-21 12:49:35 -0400

    dispmsg

--- modules/SpiceTools/Reddit.py
+++ modules/SpiceTools/Reddit.py
@@ -131,18 +131,11 @@ def reddit_r(bot, triggerargsarray, rclass):
         osd(bot, rclass.channel_current, 'say', "An error has occured.")
         return
     for submission in submissions:
-        bot.say(str(submission.title))
-
-
-"""
-    # assume you have a Subreddit instance bound to variable `subreddit`
-for submission in subreddit.hot(limit=10):
-    print(submission.title)  # Output: the submission's title
-    print(submission.score)  # Output: the submission's score
-    print(submission.id)     # Output: the submission's ID
-    print(submission.url)    # Output: the URL the submission points to
-                             # or the submission's URL if it's a self post
-"""
+        dispmsg = []
+        dispmsg.append("[" + rclass.urltype + "/" + rclass.urlsearch + " " + subcommand + "]")
+        dispmsg.append(submission.score)
+        dispmsg.append(submission.title)
+        dispmsg.append(submission.url)
 
 
 def sub_exists(sub):
