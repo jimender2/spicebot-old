227a05bb18cec88e7fe5a2b78268fd817ff62c7a
deathbybandaid <sam@deathbybandaid.net>
2018-08-23 13:28:15 -0400

    templates

---
+++ modules/SpiceBot/development/Or.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+# author deathbybandaid
+
+
+@sopel.module.commands('or')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, trigger.group(1))
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray, botcom, instigator)
+
+
+def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+    osd(bot, trigger.sender, 'say', " ...or did I?")
