c5fae527e2220d35b3f48a9f7fa815d3922c5f75
deathbybandaid <sam@deathbybandaid.net>
2018-08-23 18:01:44 -0400

    idk

--- modules/BotShared.py
+++ modules/BotShared.py
@@ -644,8 +644,7 @@ def osd(bot, target_array, text_type_array, text_array):
                         currentstring = ''
                     combinedtextarray.append(textstring)
                 else:
-                    tempstring = unicode(currentstring + "   " + textstring).encode('utf8')
-                    # tempstring = str(currentstring + "   " + textstring)
+                    tempstring = str(currentstring + "   " + textstring)
                     if len(tempstring) <= osd_limit:
                         currentstring = tempstring
                     else:
