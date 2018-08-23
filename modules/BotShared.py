d956e28f731b26be9b72ff76e113a81c10c14c27
deathbybandaid <sam@deathbybandaid.net>
2018-08-23 18:39:15 -0400

    test

--- modules/BotShared.py
+++ modules/BotShared.py
@@ -556,11 +556,10 @@ def osd(bot, target_array, text_type_array, text_array):
     # unicode patch
     textarraycomplete = []
     for string in textarraycompletestart:
-        string = string.decode('utf-8')
-        # try:
-            # string = string.decode('utf-8')
-        # except:
-        #    pass
+        try:
+            string = string.decode('utf-8')
+        except UnicodeDecodeError:
+            pass
         string = unicode(string).encode('utf8')
         textarraycomplete.append(string)
 
