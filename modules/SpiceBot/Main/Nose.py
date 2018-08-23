c1a8f813288deeb9590cdd870d30441209b5c73c
deathbybandaid <sam@deathbybandaid.net>
2018-08-23 14:47:59 -0400

    nosey

--- modules/SpiceBot/Main/Nose.py
+++ modules/SpiceBot/Main/Nose.py
@@ -9,10 +9,10 @@ shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
 from BotShared import *
 
-# author yournamehere
+# author deathbybandaid
 
 
-@sopel.module.commands('yourcommandhere')
+@sopel.module.commands('nose')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, trigger.group(1))
     if not enablestatus:
