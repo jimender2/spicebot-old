09f8903333215c12e5256be4b8e042ee56310db0
deathbybandaid <sam@deathbybandaid.net>
2018-08-23 18:23:54 -0400

    test

--- modules/BotShared.py
+++ modules/BotShared.py
@@ -636,7 +636,7 @@ def osd(bot, target_array, text_type_array, text_array):
             combinedtextarray = []
             currentstring = ''
             for textstring in texttargetarray:
-                textstring = textstring.decode('utf8').encode('utf-8')
+                textstring = str(textstring.decode('utf8').encode('utf-8'))
                 if currentstring == '':
                     currentstring = textstring
                 elif len(textstring) > osd_limit:
