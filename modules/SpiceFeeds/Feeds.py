e66e2d0f6e0828190b4253ef2b2ca5c7695069ee
deathbybandaid <sam@deathbybandaid.net>
2018-08-24 14:17:11 -0400

    test

--- modules/SpiceFeeds/Feeds.py
+++ modules/SpiceFeeds/Feeds.py
@@ -452,7 +452,7 @@ def feeds_display(bot, feed, feeds, displayifnotnew):
             for tweet in currenttweats:
                 listarray.append(tweet)
             tweet = listarray[0]
-            osd(bot, channel, 'say', str(tweet))
+            osd(bot, "#spicebottest", 'say', str(tweet))
 
             scrapedtime = parser.parse(str(tweet.created_at)).replace(tzinfo=pytz.UTC)
 
