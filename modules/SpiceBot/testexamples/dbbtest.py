054b235f15e55e2e045555ac2151c2cd8b3d86ca
deathbybandaid <sam@deathbybandaid.net>
2018-08-24 14:50:38 -0400

    test

--- modules/SpiceBot/testexamples/dbbtest.py
+++ modules/SpiceBot/testexamples/dbbtest.py
@@ -28,12 +28,10 @@ from sopel.module import commands, rule, example, priority
 def execute_main(bot, trigger):
     osd(bot, trigger.sender, 'say', "This is deathbybandaid's test module")
 
-    rpg = class_create('main')
-
     # thisdict = dict(apple="green", banana="yellow", cherry="red")
 
     # bot.say(str(thisdict["apple"]))
 
-    dict = get_database_value(bot, nick, databasekey) or dict()
+    dict = get_database_value(bot, bot.nick, 'rpg') or dict()
 
     bot.say(str(dict))
