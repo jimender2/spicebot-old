61c680a5a7198e437621fd42ac1117c1f7618a3a
deathbybandaid <sam@deathbybandaid.net>
2018-08-24 09:46:54 -0400

    test

--- modules/SpiceFeeds/Feeds.py
+++ modules/SpiceFeeds/Feeds.py
@@ -458,14 +458,10 @@ def feeds_display(bot, feed, feeds, displayifnotnew):
             lastbuildcurrent = get_database_value(bot, bot.nick, feed + '_lastbuildcurrent') or datetime.datetime(1999, 1, 1, 1, 1, 1, 1).replace(tzinfo=pytz.UTC)
             lastbuildcurrent = parser.parse(str(lastbuildcurrent))
 
-            bot.say(str(scrapedtime))
-            bot.say(str(lastbuildcurrent))
-
-            lastbuildcurrent = get_database_value(bot, bot.nick, feed + '_lastbuildcurrent')
             if displayifnotnew or scrapedtime > lastbuildcurrent:
                 return
             if not displayifnotnew:
-                set_database_value(bot, bot.nick, feed + '_lastbuildcurrent', str(lastBuildXML))
+                set_database_value(bot, bot.nick, feed + '_lastbuildcurrent', str(scrapedtime))
 
             dispmsg.append("test")
 
