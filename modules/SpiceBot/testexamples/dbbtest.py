bced1fe53d08bb4d44217cbfd3508b8933bc8370
deathbybandaid <sam@deathbybandaid.net>
2018-08-25 13:18:33 -0400

    test

--- modules/SpiceBot/testexamples/dbbtest.py
+++ modules/SpiceBot/testexamples/dbbtest.py
@@ -19,34 +19,36 @@ from sopel.logger import get_logger
 from sopel.module import commands, rule, example, priority
 
 
-# @sopel.module.commands('dbbtest')
-# def mainfunction(bot, trigger):
-#    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, trigger.group(1))
-#    execute_main(bot, trigger, triggerargsarray, botcom, instigator)
-
 @sopel.module.commands('dbbtest')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, trigger.group(1))
+    execute_main(bot, trigger, triggerargsarray, botcom, instigator)
+
+
 def execute_main(bot, trigger):
     osd(bot, trigger.sender, 'say', "This is deathbybandaid's test module")
+
+    command = get_trigger_arg(bot, triggerargsarray, 1)
 
     # RPG dynamic Class
     rpg = class_create('rpg')
     rpg.default = 'rpg'
 
-    # thisdict = dict(apple="green", banana="yellow", cherry="red")
-
-    # bot.say(str(thisdict["apple"]))
-
-    coin = get_rpg_user_dict(bot, rpg, bot.nick, 'coin')
-    bot.say(str(coin))
-
-    adjust_rpg_user_dict(bot, rpg, bot.nick, 'coin', 20)
+    if command == 'get':
+        coin = get_rpg_user_dict(bot, rpg, bot.nick, 'coin')
+        bot.say(str(coin))
+    elif command == 'set':
+        set_rpg_user_dict(bot, rpg, bot.nick, 'coin', 20)
+    elif command == 'reset':
+        reset_rpg_user_dict(bot, rpg, bot.nick, 'coin')
+    elif command == 'adjustup':
+        adjust_rpg_user_dict(bot, rpg, bot.nick, 'coin', 20)
+    elif command == 'adjustdown':
+        adjust_rpg_user_dict(bot, rpg, bot.nick, 'coin', -20)
 
     """
     End of all of the rpg stuff after error handling
     """
-
-    coin = get_rpg_user_dict(bot, rpg, bot.nick, 'coin')
-    bot.say(str(coin))
 
     save_rpg_user_dict(bot, rpg)
 
