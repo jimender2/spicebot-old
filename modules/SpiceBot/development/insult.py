948819951b38b5c06b7ff8415793b0e84d835cb0
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-08-25 22:45:05 -0400

    insult and personal

---
+++ modules/SpiceBot/development/insult.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+# author jimender2
+
+insalton = ["clock speed",
+            "test"]
+
+insalttw = ["clock speed",
+            "test"]
+
+insaltth = ["clock speed",
+            "test"]
+
+
+@sopel.module.commands('insult')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'insult')
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray, botcom, instigator)
+
+
+def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+    osd(bot, trigger.sender, 'say', "do the thing")
