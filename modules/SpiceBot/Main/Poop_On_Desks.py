573ef7fdd5647de517b2d38018a3c1b2a1df6773
deathbybandaid <sam@deathbybandaid.net>
2018-08-28 09:32:06 -0400

    test

--- modules/SpiceBot/Main/Poop_On_Desks.py
+++ modules/SpiceBot/Main/Poop_On_Desks.py
@@ -25,10 +25,11 @@ def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
         failureodds = 2
     # target = get_trigger_arg(bot, triggerargsarray, 1)
     target = get_trigger_arg(bot, [x for x in triggerargsarray if x in botcom.users_all], 1) or 0
-    backfires = [" drops their pants and squats on " + str(target) + "'s desk, but all they manage to do is fart.",
-                 " overestimated their capabilities and poops themselves.",
-                 " gets halfway through pooping before realising that this is their own desk, not " + str(target) + "'s.",
-                 " trips over taking their pants off and shits everywhere BUT the desk."]
+    if target:
+        backfires = [" drops their pants and squats on " + target + "'s desk, but all they manage to do is fart.",
+                     " overestimated their capabilities and poops themselves.",
+                     " gets halfway through pooping before realising that this is their own desk, not " + target + "'s.",
+                     " trips over taking their pants off and shits everywhere BUT the desk."]
 
     if not target:
         versionblank = get_trigger_arg(bot, ['corner', 'boss'], 'random')
