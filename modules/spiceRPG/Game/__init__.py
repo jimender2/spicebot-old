1a43f65574ea83646f46073d93585d9a66ee224f
deathbybandaid <sam@deathbybandaid.net>
2018-08-29 15:29:15 -0400

    test

--- modules/spiceRPG/Game/__init__.py
+++ modules/spiceRPG/Game/__init__.py
@@ -412,10 +412,10 @@ def rpg_map_nick_get(bot, rpg, nick):
                 if nick in latlongnicklist:
                     nickcoord = coordinates
 
-    if not nickmap or not nickcoord:
-        bot.say("shit")
-        nickmap = get_trigger_arg(bot, rpg_map_names, 1)
-        nickcoord = rpg_map_town(bot, rpg, nickmap)
+    # if not nickmap or not nickcoord:
+    #    bot.say("shit")
+    #    nickmap = get_trigger_arg(bot, rpg_map_names, 1)
+    #    nickcoord = rpg_map_town(bot, rpg, nickmap)
     rpg_map_move_nick(bot, rpg, nick, nickmap, str(nickcoord))
 
     return nickmap, nickcoord
