cd454ba984744385b5115575a987e6aef6cd29d3
Dyson Parkes <dyson@berserkir.net>
2018-09-01 10:00:19 +1200

    Change response type

--- modules/SpiceTools/Search.py
+++ modules/SpiceTools/Search.py
@@ -24,12 +24,14 @@ LOGGER = get_logger(__name__)
 
 @commands('google', 'search', 'lookup')
 def mainfunction(bot, trigger):
+    """Check to see if module is enabled."""
     enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'google')
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray, botcom, instigator)
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+    """Carry out relevant search."""
     if len(triggerargsarray) >= 1:
         mysite = get_trigger_arg(bot, triggerargsarray, 1).lower()
         searchterm = get_trigger_arg(bot, triggerargsarray, '1+')
@@ -98,6 +100,7 @@ def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
 
 
 def searchfor(bot, data):
+    """Search Google."""
     lookfor = data.replace(':', '%3A')
     var = requests.get(r'http://www.google.com/search?q=' + lookfor + '&btnI')
     query = str(var.url)
@@ -105,12 +108,13 @@ def searchfor(bot, data):
 
 
 def urbansearch(bot, searchterm):
+    """Search Urban Dictionary."""
     try:
         data = web.get("http://api.urbandictionary.com/v0/define?term={0}".format(web.quote(searchterm)))
         data = json.loads(data)
     except:
         return osd(bot, trigger.sender, 'say', "Error connecting to urban dictionary")
-    if data['result_type'] == 'no_results':
+    if data['Response'] == 'False':
         return "No results found for {0}".format(searchterm)
     result = data['list'][0]
     url = 'http://www.urbandictionary.com/define.php?term={0}'.format(web.quote(searchterm))
