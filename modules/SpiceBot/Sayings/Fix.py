acb8dbcf65496dc19308d93474e7cfbc035b0889
Dyson Parkes <dyson@berserkir.net>
2018-09-05 15:02:42 +1200

    Migrate fix to shared sayings function

--- modules/SpiceBot/Sayings/Fix.py
+++ modules/SpiceBot/Sayings/Fix.py
@@ -9,49 +9,20 @@ shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
 from BotShared import *
 
-commandarray = ["add", "remove", "count", "last"]
-# add reset and sort list
+defaultfixes = ["User complained monitor was hurting their eyes. Cut out users eyes.", "User complained mouse was too small for their hand. Cut off half users hand to fit mouse."]
 
 
 @sopel.module.commands('fix', 'user')
 def mainfunction(bot, trigger):
+    """Check to see if module is enabled"""
     enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'fix')
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray, botcom, instigator)
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
-    instigator = trigger.nick
-    inchannel = trigger.sender
+    """Retrieve a saying from the database."""
     databasekey = 'fixes'
-    command = get_trigger_arg(bot, triggerargsarray, 1)
-    inputstring = get_trigger_arg(bot, triggerargsarray, '2+')
-    existingarray = get_database_value(bot, bot.nick, databasekey) or []
-    if command in commandarray:
-        if command == "add":
-            if inputstring not in existingarray:
-                adjust_database_array(bot, bot.nick, inputstring, databasekey, 'add')
-                message = "Added to database."
-            else:
-                message = "That response is already in the database."
-        elif command == "remove":
-            if inputstring not in existingarray:
-                message = "That response was not found in the database."
-            else:
-                adjust_database_array(bot, bot.nick, inputstring, databasekey, 'del')
-                message = "Removed from database."
-        elif command == "count":
-            messagecount = len(existingarray)
-            message = "There are currently " + str(messagecount) + " responses for that in the database."
-        # elif command == "list":
-        #    if inchannel.startswith("#"):
-        #        message = "List can only be run in privmsg to avoid flooding."
-        #    else:
-        #        message = get_trigger_arg(bot, existingarray, "list")
-        elif command == "last":
-            message = get_trigger_arg(bot, existingarray, "last")
-    else:
-        message = get_trigger_arg(bot, existingarray, "random") or ''
-        if message == '':
-            message = "No response found. Have any been added?"
+    command = get_trigger_arg(bot, triggerargsarray, 1) or 'get'
+    message = sayingsmodule(bot, databasekey, triggerargsarray, command)
     osd(bot, trigger.sender, 'say', message)
