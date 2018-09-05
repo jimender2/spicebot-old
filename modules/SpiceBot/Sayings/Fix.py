26e5664751d663c444b6ab3118896b75746ad1c3
Dyson Parkes <dyson@berserkir.net>
2018-09-05 16:03:40 +1200

    Added default options to fix module

--- modules/SpiceBot/Sayings/Fix.py
+++ modules/SpiceBot/Sayings/Fix.py
@@ -9,12 +9,13 @@ shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
 from BotShared import *
 
+# author = dysonparkes
 defaultfixes = ["User complained monitor was hurting their eyes. Cut out users eyes.", "User complained mouse was too small for their hand. Cut off half users hand to fit mouse."]
 
 
 @sopel.module.commands('fix', 'user')
 def mainfunction(bot, trigger):
-    """Check to see if module is enabled"""
+    """Check to see if module is enabled."""
     enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'fix')
     if not enablestatus:
         execute_main(bot, trigger, triggerargsarray, botcom, instigator)
@@ -24,5 +25,7 @@ def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
     """Retrieve a saying from the database."""
     databasekey = 'fixes'
     command = get_trigger_arg(bot, triggerargsarray, 1) or 'get'
+    if not sayingscheck(bot, databasekey) and command != "add":
+        sayingsmodule(bot, databasekey, defaultoptions, 'initialise')
     message = sayingsmodule(bot, databasekey, triggerargsarray, command)
     osd(bot, trigger.sender, 'say', message)
