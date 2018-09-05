ac3a5cfc1d29dc29ff655c44b6d194cf81cd688e
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-09-04 20:38:38 -0400

    guess

--- modules/SpiceBot/development/Guess.py
+++ modules/SpiceBot/development/Guess.py
@@ -9,7 +9,7 @@ shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
 from BotShared import *
 
-# author yournamehere
+# author jimender2
 
 
 @sopel.module.commands('guess')
@@ -20,4 +20,32 @@ def mainfunction(bot, trigger):
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
-    osd(bot, trigger.sender, 'say', "do the thing")
+    instigator = trigger.nick
+    inchannel = trigger.sender
+    databasekey = 'guess'
+    command = get_trigger_arg(bot, triggerargsarray, 1)
+    inputstring = get_trigger_arg(bot, triggerargsarray, '2+')
+    existingarray = get_database_value(bot, bot.nick, databasekey) or []
+    if command in commandarray:
+        if command == "add":
+            if inputstring not in existingarray:
+                adjust_database_array(bot, bot.nick, inputstring, databasekey, 'add')
+                message = "Added to database."
+            else:
+                message = "That is already in the database."
+        elif command == "remove":
+            if inputstring not in existingarray:
+                message = "That was not found in the database."
+            else:
+                adjust_database_array(bot, bot.nick, inputstring, databasekey, 'del')
+                message = "Removed from database."
+        elif command == "count":
+            messagecount = len(existingarray)
+            message = "I guess there are currently " + str(messagecount) + " responses in the database."
+        elif command == "last":
+            message = get_trigger_arg(bot, existingarray, "last")
+    else:
+        message = get_trigger_arg(bot, existingarray, "random") or ''
+        if message == '':
+            message = "No response found. I guess none have been added."
+    osd(bot, trigger.sender, 'say', message)
