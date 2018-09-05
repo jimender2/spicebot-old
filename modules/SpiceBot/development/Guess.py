f5af9a47e472f92b99da232b72ef283b205d3a16
Dyson Parkes <dyson@berserkir.net>
2018-09-05 15:22:15 +1200

    Remove shared function and test further

--- modules/SpiceBot/development/Guess.py
+++ modules/SpiceBot/development/Guess.py
@@ -22,5 +22,49 @@ def mainfunction(bot, trigger):
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
     databasekey = 'guess'
     command = get_trigger_arg(bot, triggerargsarray, 1) or 'get'
+    if not sayingscheck:
+        for entry in defaultsayings:
+            entry = "load up " + entry
+            sayingsmodule(bot, databasekey, entry, 'add')
     message = sayingsmodule(bot, databasekey, triggerargsarray, command)
     osd(bot, trigger.sender, 'say', message)
+
+
+def sayingsmodule(bot, databasekey, triggerargsarray, thingtodo):
+    """Handle the creation and manipulation of modules that return sayings."""
+    # add, remove, last, count, list, initialise?
+    response = "Something went wrong. Oops."
+    inputstring = get_trigger_arg(bot, triggerargsarray, '2+')
+    existingarray = get_database_value(bot, bot.nick, databasekey) or []
+    if thingtodo == "add":
+        if inputstring not in existingarray:
+            adjust_database_array(bot, bot.nick, inputstring, databasekey, 'add')
+            response = "Added to database."
+        else:
+            response = "That is already in the database."
+    elif thingtodo == "remove":
+        if inputstring not in existingarray:
+            response = "That was not found in the database."
+        else:
+            adjust_database_array(bot, bot.nick, inputstring, databasekey, 'del')
+            response = "Removed from database."
+    elif thingtodo == "count":
+        messagecount = len(existingarray)
+        response = "I'm seeing " + str(messagecount) + " responses in the database."
+    elif thingtodo == "last":
+        response = get_trigger_arg(bot, existingarray, "last")
+    else:
+        response = get_trigger_arg(bot, existingarray, "random") or ''
+        if response == '':
+            response = "I'm afraid I couldn't find an entry for that."
+    return response
+
+
+def sayingscheck(bot, databasekey):
+    """Confirm that there are responses for the given key."""
+    saying_entries = get_database_value(bot, bot.nick, databasekey) or []
+    saying_entrycount = len(saying_entries)
+    if saying_entrycount > 0:
+        return True
+    else:
+        return False
