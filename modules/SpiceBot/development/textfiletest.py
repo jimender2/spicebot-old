997f6b430147be6a08d61964f7491344c2fb34bb
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-09-08 23:32:48 -0400

    direct

--- modules/SpiceBot/development/textfiletest.py
+++ modules/SpiceBot/development/textfiletest.py
@@ -21,7 +21,7 @@ def mainfunction(bot, trigger):
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
 
-    count = txtCount('/home/spicebot/.sopel/SpiceBot/modules/SpiceBot/development/test.txt')
+    count = txtCount("/home/spicebot/.sopel/SpiceBotdev/modules/SpiceBot/development/test.txt")
     osd(bot, trigger.sender, 'say', count)
 
 
