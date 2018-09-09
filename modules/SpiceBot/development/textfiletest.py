035cf85aff75f8b03821095e7f880fab5597ba45
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-09-08 23:09:56 -0400

    test

---
+++ modules/SpiceBot/development/textfiletest.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+# author jimender2
+
+
+@sopel.module.commands('count')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, trigger.group(1))
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray, botcom, instigator)
+
+
+def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+
+    count = txtCount('test.txt')
+    osd(bot, trigger.sender, 'say', count)
+
+
+def txtCount(path):
+    with open(path) as f:
+        line_count = 0
+        for line in f:
+            line_count += 1
+
+    return line_count
