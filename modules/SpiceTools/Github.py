05ff765a50bd8dca44253031c957dc17bc5ea23b
deathbybandaid <sam@deathbybandaid.net>
2018-09-10 14:01:37 -0400

    test

--- modules/SpiceTools/Github.py
+++ modules/SpiceTools/Github.py
@@ -100,7 +100,9 @@ def execute_main(bot, trigger):
     reqrepdict['body'] = instigator + reqrepdict['body'] + ": " + inputtext
 
     if not reqrepdict['assignee']:
-        del reqrepdict['assignee']
+        reqrepdict['assignee'] = get_trigger_arg(bot, [x for x in triggerargsarray if x.startswith("@")], 1) or None
+        if not reqrepdict['assignee']:
+            del reqrepdict['assignee']
 
     bot.say(str(reqrepdict))
     return
