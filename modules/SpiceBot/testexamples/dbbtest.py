a2665b1765e7d06b359ce08bd454a60d43ff2137
deathbybandaid <sam@deathbybandaid.net>
2018-09-11 17:10:28 -0400

    test

--- modules/SpiceBot/testexamples/dbbtest.py
+++ modules/SpiceBot/testexamples/dbbtest.py
@@ -171,6 +171,7 @@ def spicemanip(bot, inputs, outputtask, output_type='default'):
 
 # sort two lists together
 def spicemanip_arrange(bot, indexitem, outputtask, sortbyarray, arrayb):
+    bot.say("here")
     sortbyarray, arrayb = (list(x) for x in zip(*sorted(zip(sortbyarray, arrayb), key=itemgetter(0))))
     bot.say("test " + str([sortbyarray, arrayb]))
     return [sortbyarray, arrayb]
