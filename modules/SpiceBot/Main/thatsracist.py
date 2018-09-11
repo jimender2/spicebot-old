b454fca38315ee759e8892d5243d9659f33a75e7
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-09-10 23:42:47 -0400

    thats racist

--- modules/SpiceBot/Main/thatsracist.py
+++ modules/SpiceBot/Main/thatsracist.py
@@ -20,4 +20,8 @@ def mainfunction(bot, trigger):
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
-    osd(bot, trigger.sender, 'say', "That's Racist!™")
+    target = spicemanip(bot, triggerargsarray, '1')
+    if target == "stupid" or target == "users" or target == "user"
+        osd(bot, trigger.sender, 'say', "That's Racist against stupid users!")
+    else:
+        osd(bot, trigger.sender, 'say', "That's Racist!™")
