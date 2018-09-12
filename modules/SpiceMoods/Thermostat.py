41e0dd8b7788061b9df201a987fc5e479c854546
deathbybandaid <sam@deathbybandaid.net>
2018-09-12 08:20:56 -0400

    test

--- modules/SpiceMoods/Thermostat.py
+++ modules/SpiceMoods/Thermostat.py
@@ -21,10 +21,10 @@ def mainfunction(bot, trigger):
     enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'thermostat')
     if not enablestatus:
         # IF "&&" is in the full input, it is treated as multiple commands, and is split
-        commands_array = spicemanip(bot, triggerargsarray, "split_&&") or []
+        commands_array = spicemanip(bot, triggerargsarray, "split_&&")
+        bot.say(str(commands_array))
         for command_split_partial in commands_array:
-            triggerargsarray_part = spicemanip(bot, command_split_partial, 'create') or []
-            bot.say(str(triggerargsarray_part))
+            triggerargsarray_part = spicemanip(bot, command_split_partial, 'create')
             execute_main(bot, trigger, triggerargsarray_part, botcom, instigator)
 
 
