0c13d96ed7a25887bd4893033926872ef2a133ba
deathbybandaid <sam@deathbybandaid.net>
2018-09-12 10:08:02 -0400

    test

--- modules/SpiceGif/GifShared.py
+++ modules/SpiceGif/GifShared.py
@@ -15,3 +15,17 @@ gifshareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(gifshareddir)
 from BotShared import *
 from GifShared import *
+
+
+def giphy_getGif(query):
+    api = 'Wi33J3WxSDxWsrxLREcQqmO3iJ0dk52N'
+    limit = 50
+    url = 'http://api.giphy.com/v1/gifs/search?q=' + str(query)+'&api_key=' + str(api) + '&limit=' + str(limit) + '&rating=r'
+    data = json.loads(urllib2.urlopen(url).read())
+    randno = randint(0, limit)
+    try:
+        id = data['data'][randno]['id']
+        gif = 'https://media2.giphy.com/media/'+id+'/giphy.gif'
+    except IndexError:
+        gif = ""
+    return gif, randno
