1af858086ec79dd4b83fc7da7bb5028cdbabf0bd
Dyson Parkes <dyson@berserkir.net>
2018-09-13 11:13:44 +1200

    Create spiceman

---
+++ modules/SpiceBot/development/SpiceMan.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+# author SniperClif
+
+
+@sopel.module.commands('spiceman')
+def mainfunction(bot, trigger):
+    """Check to see if module is enabled."""
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, trigger.group(1))
+    if not enablestatus:
+        # IF "&&" is in the full input, it is treated as multiple commands, and is split
+        commands_array = spicemanip(bot, triggerargsarray, "split_&&")
+        if commands_array == []:
+            commands_array = [[]]
+        for command_split_partial in commands_array:
+            triggerargsarray_part = spicemanip(bot, command_split_partial, 'create')
+            execute_main(bot, trigger, triggerargsarray_part, botcom, instigator)
+
+
+def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+    """Take input and use it in a "one small step" style sentence."""
+    target = spicemanip(bot, triggerargsarray, '1+')
+    if not target:
+        osd(bot, trigger.sender, 'say', "One small step for botkind; one giant leap for SkyNet!")
+    else:
+        osd(bot, trigger.sender, 'say', "One small step for %s; one giant leap for morons!" % target)
