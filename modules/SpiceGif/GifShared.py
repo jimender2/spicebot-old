288bc954b70261d3f57e2427b2f2844528d86bf8
deathbybandaid <sam@deathbybandaid.net>
2018-09-12 13:35:01 -0400

    test

--- modules/SpiceGif/GifShared.py
+++ modules/SpiceGif/GifShared.py
@@ -119,7 +119,9 @@ def getGif_tenor(bot, query, searchnum, searchlimit=tenorlimit):
     osd(bot, 'deathbybandaid', 'say',  str(data))
 
     # Verifythere are results
-    resultsamount = data['pagination']['total_count']
+    resultsamount = len(data['results'])
+    bot.say(str(resultsamount))
+    return
     returngifdict["resultsamount"] = resultsamount
     if not resultsamount:
         return returngifdict
@@ -133,3 +135,93 @@ def getGif_tenor(bot, query, searchnum, searchlimit=tenorlimit):
     returngifdict["returnurl"] = 'https://media2.giphy.com/media/'+id+'/giphy.gif'
 
     return returngifdict
+
+    gifdictionary = {
+                        'weburl': 'https://tenor.com/search/fart-gifs',
+                        'results': [
+                                    {
+                                        'created': 1461358331.449446,
+                                        'url': 'https://tenor.com/wFAJ.gif',
+                                        'media': [
+                                                {
+                                                    'nanomp4': {
+                                                                'url': 'https://media.tenor.com/videos/c3926a1bf3a1cb398d348cb5de597767/mp4',
+                                                                'dims': [150, 200],
+                                                                'duration': 1.1,
+                                                                'preview': 'https://media.tenor.com/images/d2c2d717c743367ba5a9ee094c0587da/tenor.png',
+                                                                'size': 26734
+                                                                },
+                                                    'nanowebm': {
+                                                                'url': 'https://media.tenor.com/videos/b6583e893d984414ed3bce27119833b1/webm',
+                                                                'dims': [150, 200],
+                                                                'preview': 'https://media.tenor.com/images/d2c2d717c743367ba5a9ee094c0587da/tenor.png',
+                                                                'size': 18585},
+                                                    'tinygif': {
+                                                                'url': 'https://media.tenor.com/images/3780eadf5dc84d7e614b686843d2a849/tenor.gif',
+                                                                'dims': [220, 292],
+                                                                'preview': 'https://media.tenor.com/images/e0ff6abe416df30c69ac7b13fff2e264/raw',
+                                                                'size': 73942
+                                                                },
+                                                    'tinymp4': {
+                                                                'url': 'https://media.tenor.com/videos/442dd4ca3166c600daff85411c33dc58/mp4',
+                                                                'dims': [200, 266],
+                                                                'duration': 1.037037,
+                                                                'preview': 'https://media.tenor.com/images/3dc1a390e9818915801f1b0a9f8021f8/tenor.png',
+                                                                'size': 56575
+                                                                },
+                                                    'tinywebm': {
+                                                                'url': 'https://media.tenor.com/videos/e037b28a690051b26d88e516d67fb07e/webm',
+                                                                'dims': [200, 266],
+                                                                'preview': 'https://media.tenor.com/images/3dc1a390e9818915801f1b0a9f8021f8/tenor.png',
+                                                                'size': 34031
+                                                                },
+                                                    'webm': {
+                                                                'url': 'https://media.tenor.com/videos/f8ca3f540f3ee8cb771da87c72578545/webm',
+                                                                'dims': [200, 266],
+                                                                'preview': 'https://media.tenor.com/images/56b7390fdaa0b7a5ce30fadff9ebdbd7/raw',
+                                                                'size': 34031
+                                                                },
+                                                    'gif': {
+                                                                'url': 'https://media.tenor.com/images/6fbc9ef9dc3f91891e47553bc047696f/tenor.gif',
+                                                                'dims': [200, 266],
+                                                                'preview': 'https://media.tenor.com/images/56b7390fdaa0b7a5ce30fadff9ebdbd7/raw',
+                                                                'size': 865850
+                                                                },
+                                                    'mp4': {
+                                                                'url': 'https://media.tenor.com/videos/283be06fd38086ef012a5c176ca95b79/mp4',
+                                                                'dims': [200, 266],
+                                                                'duration': 1.037037,
+                                                                'preview': 'https://media.tenor.com/images/56b7390fdaa0b7a5ce30fadff9ebdbd7/raw',
+                                                                'size': 32955
+                                                                },
+                                                    'nanogif': {
+                                                                'url': 'https://media.tenor.com/images/a9813b46edc256cf8a6b191167a0aee5/tenor.gif',
+                                                                'dims': [67, 90],
+                                                                'preview': 'https://media.tenor.com/images/ebb385b4a02abb54f101160c5565b400/raw',
+                                                                'size': 7854
+                                                                },
+                                                    'mediumgif': {
+                                                                'url': 'https://media.tenor.com/images/663bd03c3bc3457e557c21ab608d12fa/tenor.gif',
+                                                                'dims': [200, 266],
+                                                                'preview': 'https://media.tenor.com/images/ed3ac72ff46a06b39e378753a2926aeb/tenor.gif',
+                                                                'size': 387821
+                                                                },
+                                                    'loopedmp4': {
+                                                                'url': 'https://media.tenor.com/videos/3ef72d6b5e97c18ce1a6303e8bb6ac93/mp4',
+                                                                'dims': [200, 266],
+                                                                'duration': 3.11304,
+                                                                'preview': 'https://media.tenor.com/images/56b7390fdaa0b7a5ce30fadff9ebdbd7/raw'
+                                                                }
+                                                }
+                                    ],
+                                        'tags': [],
+                                        'shares': 1,
+                                        'itemurl': 'https://tenor.com/view/bear-fanny-fart-gif-5364027',
+                                        'composite': None,
+                                        'hasaudio': False,
+                                        'title': '',
+                                        'id': '5364027'
+                                    }
+                                    ],
+                    'next': '1'
+                    }
