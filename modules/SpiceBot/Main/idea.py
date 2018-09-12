8a7ae2009eda66a9b4b0f5e808dacf3cf453aad4
deathbybandaid <sam@deathbybandaid.net>
2018-09-12 08:52:55 -0400

    Main

--- modules/SpiceBot/Main/idea.py
+++ modules/SpiceBot/Main/idea.py
@@ -20,7 +20,13 @@ array = ["jump off a bridge"]
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'idea')
     if not enablestatus:
-        execute_main(bot, trigger, triggerargsarray, botcom, instigator)
+        # IF "&&" is in the full input, it is treated as multiple commands, and is split
+        commands_array = spicemanip(bot, triggerargsarray, "split_&&")
+        if commands_array == []:
+            commands_array = [[]]
+        for command_split_partial in commands_array:
+            triggerargsarray_part = spicemanip(bot, command_split_partial, 'create')
+            execute_main(bot, trigger, triggerargsarray_part, botcom, instigator)
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
