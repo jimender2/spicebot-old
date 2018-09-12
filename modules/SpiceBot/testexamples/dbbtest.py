5adc47d06d557bd2577e7bf431a9c7acb97f22d7
deathbybandaid <sam@deathbybandaid.net>
2018-09-12 09:00:56 -0400

    testexamples

--- modules/SpiceBot/testexamples/dbbtest.py
+++ modules/SpiceBot/testexamples/dbbtest.py
@@ -24,7 +24,13 @@ sys.setdefaultencoding('utf-8')
 @sopel.module.commands('dbbtest')
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, trigger.group(1))
-    execute_main(bot, trigger, triggerargsarray, botcom, instigator)
+    # IF "&&" is in the full input, it is treated as multiple commands, and is split
+    commands_array = spicemanip(bot, triggerargsarray, "split_&&")
+    if commands_array == []:
+        commands_array = [[]]
+    for command_split_partial in commands_array:
+        triggerargsarray_part = spicemanip(bot, command_split_partial, 'create')
+        execute_main(bot, trigger, triggerargsarray_part, botcom, instigator)
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
