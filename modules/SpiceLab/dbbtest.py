7956b582d65dda7886e106597696e4816746077a
deathbybandaid <sam@deathbybandaid.net>
2018-09-13 10:31:05 -0400

    test

--- modules/SpiceLab/dbbtest.py
+++ modules/SpiceLab/dbbtest.py
@@ -23,12 +23,13 @@ testvar = 72
 def mainfunction(bot, trigger):
     enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, trigger.group(1))
     # IF "&&" is in the full input, it is treated as multiple commands, and is split
-    commands_array = spicemanip(bot, triggerargsarray, "split_&&")
-    if commands_array == []:
-        commands_array = [[]]
-    for command_split_partial in commands_array:
-        triggerargsarray_part = spicemanip(bot, command_split_partial, 'create')
-        execute_main(bot, trigger, triggerargsarray_part, botcom, instigator)
+    # commands_array = spicemanip(bot, triggerargsarray, "split_&&")
+    # if commands_array == []:
+    #    commands_array = [[]]
+    # for command_split_partial in commands_array:
+    #    triggerargsarray_part = spicemanip(bot, command_split_partial, 'create')
+    #    execute_main(bot, trigger, triggerargsarray_part, botcom, instigator)
+    execute_main(bot, trigger, triggerargsarray_part, botcom, instigator)
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
