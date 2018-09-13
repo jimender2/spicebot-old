8274af358c1663769ed4adf8f79cf37032ae87e8
deathbybandaid <sam@deathbybandaid.net>
2018-09-13 09:11:00 -0400

    begin test of global dict

--- modules/SpiceLab/dbbtest.py
+++ modules/SpiceLab/dbbtest.py
@@ -33,371 +33,106 @@ def mainfunction(bot, trigger):
         execute_main(bot, trigger, triggerargsarray_part, botcom, instigator)
 
 
+rpg_game_dict = {
+                "users": {
+                },
+                "maps": {
+                }
+}
+
+
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
     osd(bot, trigger.sender, 'say', "This is deathbybandaid's test module")
 
     triggerargsarray = spicemanip(bot, trigger.group(2), 'create')
-    bot.say(str(triggerargsarray))
-
-    commands_array = spicemanip(bot, triggerargsarray, "split_&&")
-    bot.say(str(commands_array))
-
-    for command_split_partial in commands_array:
-        bot.say(str(command_split_partial))
-        triggerargsarray_part = spicemanip(bot, command_split_partial, 'create')
-        bot.say(str(triggerargsarray_part))
-
-    return
-
-    return
-
-    testsgood = [
-                    0, 1, 2, 3, 4, 5, 6, 7,
-                    "'reverse'", "'count'", "'dedupe'", "'sort'",
-                    "'lower'", "'upper'", "'title'",
-                    "'list'", "'andlist'", "'orlist'", "'last'", "'random'"
-                    ]
-    triggerargstest = [
-                        8, 9,
-                        "'2^6'", "'4!'", "'4+'", "'4-'", "'4<'", "'4>'"
-                        ]
-    argtypetest = ["spicemanip_old", "spicemanip"]
-
-    for tasktest in triggerargstest:
-
-        bot.say("                    Testing " + str(tasktest))
-
-        for testtype in argtypetest:
-
-            testevalstr = str(str(testtype) + "(bot, trigger.group(2), " + str(tasktest) + ")")
-            try:
-                testeval = eval(testevalstr)
-            except NameError:
-                testeval = "N/A"
-
-            if testtype == "spicemanip":
-                testtype = str("      " + testtype)
-            bot.say(str(testtype) + "     " + str(testeval))
 
 
-# Hub
-def spicemanip(bot, inputs, outputtask, output_type='default'):
+# Database Users
+def get_user_dict(bot, rpg, nick, dictkey):
 
-    # TODO 'this*that' or '1*that' replace either all strings matching, or an index value
-    # TODO reverse sort z.sort(reverse = True)
-    # list.extend adds lists to eachother
+    # check that db list is there
+    if not hasattr(rpg, 'userdb'):
+        rpg.userdb = class_create('userdblist')
+    if not hasattr(rpg.userdb, 'list'):
+        rpg.userdb.list = []
 
-    mainoutputtask, suboutputtask = None, None
+    returnvalue = 0
 
-    # Input needs to be a list, but don't split a word into letters
-    if not inputs:
-        inputs = []
-    if not isinstance(inputs, list):
-        inputs = list(inputs.split(" "))
-        inputs = [x for x in inputs if x and x not in ['', ' ']]
-        inputs = [inputspart.strip() for inputspart in inputs]
-
-    # Create return
-    if outputtask == 'create':
-        return inputs
-
-    # Make temparray to preserve original order
-    temparray = []
-    for inputpart in inputs:
-        temparray.append(inputpart)
-    inputs = temparray
-
-    # Convert outputtask to standard
-    if outputtask in [0, 'complete']:
-        outputtask = 'string'
-    elif outputtask == 'index':
-        mainoutputtask = inputs[1]
-        suboutputtask = inputs[2]
-        inputs = inputs[0]
-    elif str(outputtask).isdigit():
-        mainoutputtask, outputtask = int(outputtask), 'number'
-    elif "^" in str(outputtask):
-        mainoutputtask = str(outputtask).split("^", 1)[0]
-        suboutputtask = str(outputtask).split("^", 1)[1]
-        outputtask = 'rangebetween'
-    elif str(outputtask).startswith("split_"):
-        mainoutputtask = str(outputtask).replace("split_", "")
-        outputtask = 'split'
-    elif str(outputtask).endswith(tuple(["!", "+", "-", "<", ">"])):
-        mainoutputtask = str(outputtask)
-        if str(outputtask).endswith("!"):
-            outputtask = 'exclude'
-        if str(outputtask).endswith("+"):
-            outputtask = 'incrange_plus'
-        if str(outputtask).endswith("-"):
-            outputtask = 'incrange_minus'
-        if str(outputtask).endswith(">"):
-            outputtask = 'excrange_plus'
-        if str(outputtask).endswith("<"):
-            outputtask = 'excrange_minus'
-        for r in (("!", ""), ("+", ""), ("-", ""), ("<", ""), (">", "")):
-            mainoutputtask = mainoutputtask.replace(*r)
-
-    if outputtask == 'string':
-        returnvalue = inputs
+    # check if nick has been pulled from db already
+    if nick not in rpg.userdb.list:
+        rpg.userdb.list.append(nick)
+        nickdict = get_database_value(bot, nick, rpg.default) or dict()
+        createuserdict = str("rpg.userdb." + nick + " = nickdict")
+        exec(createuserdict)
     else:
-        try:
-            returnvalue = eval('spicemanip_' + outputtask + '(bot, inputs, outputtask, mainoutputtask, suboutputtask)')
-        except NameError:
-            returnvalue = ''
+        if not hasattr(rpg.userdb, nick):
+            nickdict = dict()
+        else:
+            nickdict = eval('rpg.userdb.' + nick)
 
-    # default return if not specified
-    if output_type == 'default':
-        if outputtask in [
-                            'string', 'number', 'rangebetween', 'exclude', 'random',
-                            'incrange_plus', 'incrange_minus', 'excrange_plus', 'excrange_minus'
-                            ]:
-            output_type = 'string'
-        elif outputtask in ['count']:
-            output_type = 'dict'
+    if dictkey in nickdict.keys():
+        returnvalue = nickdict[dictkey]
+    else:
+        nickdict[dictkey] = 0
+        returnvalue = 0
 
-    # verify output is correct
-    if output_type == 'string':
-        if isinstance(returnvalue, list):
-            returnvalue = ' '.join(returnvalue)
-    elif output_type in ['list', 'array']:
-        if not isinstance(returnvalue, list):
-            returnvalue = list(returnvalue.split(" "))
-            returnvalue = [x for x in returnvalue if x and x not in ['', ' ']]
-            returnvalue = [inputspart.strip() for inputspart in returnvalue]
     return returnvalue
 
 
-# compare 2 lists, based on the location of an index item, passthrough needs to be [indexitem, arraytoindex, arraytocompare]
-def spicemanip_index(bot, indexitem, outputtask, arraytoindex, arraytocompare):
-    item = ''
-    for x, y in zip(arraytoindex, arraytocompare):
-        if x == indexitem:
-            item = y
-    return item
+# set a value
+def set_user_dict(bot, rpg, nick, dictkey, value):
+    currentvalue = get_user_dict(bot, rpg, nick, dictkey)
+    nickdict = eval('rpg.userdb.' + nick)
+    nickdict[dictkey] = value
 
 
-# split list by string
-def spicemanip_split(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    split_array = []
-    restring = ' '.join(inputs)
-    if mainoutputtask not in inputs:
-        split_array = [restring]
-    else:
-        split_array = restring.split(mainoutputtask)
-    split_array = [x for x in split_array if x and x not in ['', ' ']]
-    split_array = [inputspart.strip() for inputspart in split_array]
-    if split_array == []:
-        split_array = [[]]
-    return split_array
+# reset a value
+def reset_user_dict(bot, rpg, nick, dictkey):
+    currentvalue = get_user_dict(bot, rpg, nick, dictkey)
+    nickdict = eval('rpg.userdb.' + nick)
+    if dictkey in nickdict:
+        del nickdict[dictkey]
 
 
-# dedupe list
-def spicemanip_dedupe(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    newlist = []
-    for inputspart in inputs:
-        if inputspart not in newlist:
-            newlist.append(inputspart)
-    return newlist
+# add or subtract from current value
+def adjust_user_dict(bot, rpg, nick, dictkey, value):
+    oldvalue = get_user_dict(bot, rpg, nick, dictkey)
+    if not str(oldvalue).isdigit():
+        oldvalue = 0
+    nickdict = eval('rpg.userdb.' + nick)
+    nickdict[dictkey] = oldvalue + value
 
 
-# Sort list
-def spicemanip_sort(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    return sorted(inputs)
+# Save all database users in list
+def save_user_dicts(bot, rpg):
+
+    # check that db list is there
+    if not hasattr(rpg, 'userdb'):
+        rpg.userdb = class_create('userdblist')
+    if not hasattr(rpg.userdb, 'list'):
+        rpg.userdb.list = []
+
+    for nick in rpg.userdb.list:
+        if not hasattr(rpg.userdb, nick):
+            nickdict = dict()
+        else:
+            nickdict = eval('rpg.userdb.' + nick)
+        set_database_value(bot, nick, rpg.default, nickdict)
 
 
-# count items in list, return dictionary
-def spicemanip_count(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    returndict = dict()
-    if inputs == []:
-        return returndict
-    uniqueinputitems, uniquecount = [], []
-    for inputspart in inputs:
-        if inputspart not in uniqueinputitems:
-            uniqueinputitems.append(inputspart)
-    for uniqueinputspart in uniqueinputitems:
-        count = 0
-        for ele in inputs:
-            if (ele == uniqueinputspart):
-                count += 1
-        uniquecount.append(count)
-    for inputsitem, unumber in zip(uniqueinputitems, uniquecount):
-        returndict[inputsitem] = unumber
-    return returndict
-
-
-# random item from list
-def spicemanip_random(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    randomselectlist = []
-    for temppart in inputs:
-        randomselectlist.append(temppart)
-    while len(randomselectlist) > 1:
-        random.shuffle(randomselectlist)
-        randomselect = randomselectlist[random.randint(0, len(randomselectlist) - 1)]
-        randomselectlist.remove(randomselect)
-    randomselect = randomselectlist[0]
-    return randomselect
-
-
-# remove random item from list
-def spicemanip_exrandom(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return []
-    randremove = spicemanip_random(bot, inputs, outputtask, mainoutputtask, suboutputtask)
-    inputs.remove(randremove)
-    return inputs
-
-
-# Convert list into lowercase
-def spicemanip_lower(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return [inputspart.lower() for inputspart in inputs]
-
-
-# Convert list to uppercase
-def spicemanip_upper(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return [inputspart.upper() for inputspart in inputs]
-
-
-# Convert list to uppercase
-def spicemanip_title(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return [inputspart.title() for inputspart in inputs]
-
-
-# Reverse List Order
-def spicemanip_reverse(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return []
-    return inputs[::-1]
-
-
-# comma seperated list
-def spicemanip_list(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return ', '.join(str(x) for x in inputs)
-
-
-# comma seperated list with and
-def spicemanip_andlist(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    if len(inputs) < 2:
-        return ' '.join(inputs)
-    lastentry = str("and " + str(inputs[len(inputs) - 1]))
-    del inputs[-1]
-    inputs.append(lastentry)
-    if len(inputs) == 2:
-        return ' '.join(inputs)
-    return ', '.join(str(x) for x in inputs)
-
-
-# comma seperated list with or
-def spicemanip_orlist(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    if len(inputs) < 2:
-        return ' '.join(inputs)
-    lastentry = str("or " + str(inputs[len(inputs) - 1]))
-    del inputs[-1]
-    inputs.append(lastentry)
-    if len(inputs) == 2:
-        return ' '.join(inputs)
-    return ', '.join(str(x) for x in inputs)
-
-
-# exclude number
-def spicemanip_exclude(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    del inputs[int(mainoutputtask) - 1]
-    return ' '.join(inputs)
-
-
-# Convert list to string
-def spicemanip_string(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return ' '.join(inputs)
-
-
-# Get number item from list
-def spicemanip_number(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    elif len(inputs) == 1:
-        return inputs[0]
-    elif int(mainoutputtask) > len(inputs) or int(mainoutputtask) < 0:
-        return ''
-    else:
-        return inputs[int(mainoutputtask) - 1]
-
-
-# Get Last item from list
-def spicemanip_last(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return inputs[len(inputs) - 1]
-
-
-# range between items in list
-def spicemanip_rangebetween(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    if not str(mainoutputtask).isdigit() or not str(suboutputtask).isdigit():
-        return ''
-    mainoutputtask, suboutputtask = int(mainoutputtask), int(suboutputtask)
-    if suboutputtask == mainoutputtask:
-        return spicemanip_number(bot, inputs, outputtask, mainoutputtask, suboutputtask)
-    if suboutputtask < mainoutputtask:
-        mainoutputtask, suboutputtask = suboutputtask, mainoutputtask
-    if mainoutputtask < 0:
-        mainoutputtask = 1
-    if suboutputtask > len(inputs):
-        suboutputtask = len(inputs)
-    newlist = []
-    for i in range(mainoutputtask, suboutputtask + 1):
-        newlist.append(str(spicemanip_number(bot, inputs, outputtask, i, suboutputtask)))
-    if newlist == []:
-        return ''
-    return ' '.join(newlist)
-
-
-# Forward Range includes index number
-def spicemanip_incrange_plus(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return spicemanip_rangebetween(bot, inputs, outputtask, int(mainoutputtask), len(inputs))
-
-
-# Reverse Range includes index number
-def spicemanip_incrange_minus(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return spicemanip_rangebetween(bot, inputs, outputtask, 1, int(mainoutputtask))
-
-
-# Forward Range excludes index number
-def spicemanip_excrange_plus(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return spicemanip_rangebetween(bot, inputs, outputtask, int(mainoutputtask) + 1, len(inputs))
-
-
-# Reverse Range excludes index number
-def spicemanip_excrange_minus(bot, inputs, outputtask, mainoutputtask, suboutputtask):
-    if inputs == []:
-        return ''
-    return spicemanip_rangebetween(bot, inputs, outputtask, 1, int(mainoutputtask) - 1)
-
-
-def array_arrangesort(bot, sortbyarray, arrayb):
-    sortbyarray, arrayb = (list(x) for x in zip(*sorted(zip(sortbyarray, arrayb), key=itemgetter(0))))
-    return sortbyarray, arrayb
+# add or subtract from current value
+def adjust_user_dict_array(bot, rpg, nick, dictkey, entries, adjustmentdirection):
+    if not isinstance(entries, list):
+        entries = [entries]
+    oldvalue = get_user_dict(bot, rpg, nick, dictkey)
+    nickdict = eval('rpg.userdb.' + nick)
+    if not isinstance(oldvalue, list):
+        oldvalue = []
+    for x in entries:
+        if adjustmentdirection == 'add':
+            if x not in oldvalue:
+                oldvalue.append(x)
+        elif adjustmentdirection == 'del':
+            if x in oldvalue:
+                oldvalue.remove(x)
+    nickdict[dictkey] = oldvalue
