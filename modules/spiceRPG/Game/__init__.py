214e9cd6f305189d1cefbcae8dc8fd3b795775fd
deathbybandaid <sam@deathbybandaid.net>
2018-09-13 11:22:46 -0400

    test

--- modules/spiceRPG/Game/__init__.py
+++ modules/spiceRPG/Game/__init__.py
@@ -75,7 +75,7 @@ def execute_start(bot, trigger, triggerargsarray, command_type):
     global rpg_game_dict
 
     bot.say(str(rpg_game_dict))
-    return
+    # return
 
     if 'game_loaded' not in rpg_game_dict.keys():
         bot.say("not loaded")
