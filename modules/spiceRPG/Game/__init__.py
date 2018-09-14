8b2ec2194e04db0c41faae9e3d0f8d5cf447e2c5
deathbybandaid <sam@deathbybandaid.net>
2018-09-14 12:21:46 -0400

    test

--- modules/spiceRPG/Game/__init__.py
+++ modules/spiceRPG/Game/__init__.py
@@ -882,7 +882,8 @@ Bot Startup Monologue
 # @sopel.module.interval(1)  # TODO make this progress with the game
 @sopel.module.interval(rpg_game_dict['tempvals']['monologuecheck'])  # TODO make this progress with the game
 def rpg_bot_start_script(bot):
-    bot.say("testing")
+
+    bot.msg('deathbybandaid', "testing")
 
     # Create rp class
     rpg = class_create('rpg')
