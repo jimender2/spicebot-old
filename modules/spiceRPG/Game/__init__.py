11c5236759de8bac404d196833513718ff08b538
deathbybandaid <sam@deathbybandaid.net>
2018-09-14 16:26:08 -0400

    test

--- modules/spiceRPG/Game/__init__.py
+++ modules/spiceRPG/Game/__init__.py
@@ -80,7 +80,7 @@ def execute_start(bot, trigger, triggerargsarray, command_type):
     # Bacic User List
     rpg = rpg_command_users(bot, rpg)
 
-    bot.say(str(bot.channels))
+    bot.say(str(bot.channels[rpg.channel_current].privileges))
     return
 
     # Error Display System Create
