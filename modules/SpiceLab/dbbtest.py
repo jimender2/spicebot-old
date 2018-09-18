ba52c77efea66a7d619b99276caacdc2f9bfea04
deathbybandaid <sam@deathbybandaid.net>
2018-09-18 15:03:32 -0400

    test

--- modules/SpiceLab/dbbtest.py
+++ modules/SpiceLab/dbbtest.py
@@ -30,7 +30,7 @@ def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
     cmd = '/bin/systemctl status %s.service' % "SpiceBot"
     proc = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     # stdout_list = proc.communicate()[0].split('\n')
-    stdout_list = proc.communicate()[0]
+    stdout_list = proc.communicate()[1].split('\n')
     for line in stdout_list:
         bot.say(str(line))
         # if 'Active:' in line:
