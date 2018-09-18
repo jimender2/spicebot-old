4f01371c90101e238b24e31a0f795fed3e4474a3
deathbybandaid <sam@deathbybandaid.net>
2018-09-18 12:42:19 -0400

    test

--- modules/SpiceLab/dbbtest.py
+++ modules/SpiceLab/dbbtest.py
@@ -35,6 +35,7 @@ def main(bot, debug):
     j = systemd.journal.Reader()
     j.seek_tail()
     j.get_previous()
+    bot.say(str(j))
     while True:
         event = j.wait(-1)
         if event == systemd.journal.APPEND:
