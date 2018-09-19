5dd18f7b7d7e9a728db6702d972cef1a0ae72c81
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-09-18 23:11:04 -0400

    rock paper scissors start

---
+++ modules/SpiceBot/development/rockpaperscissors.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+import random
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+# author jimender2
+# contributers none
+
+
+@sopel.module.commands('rps', 'rockpaperscissors')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'rps')
+    if not enablestatus:
+        # IF "&&" is in the full input, it is treated as multiple commands, and is split
+        commands_array = spicemanip(bot, triggerargsarray, "split_&&")
+        if commands_array == []:
+            commands_array = [[]]
+        for command_split_partial in commands_array:
+            triggerargsarray_part = spicemanip(bot, command_split_partial, 'create')
+            execute_main(bot, trigger, triggerargsarray_part, botcom, instigator)
+
+
+def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+    osd(bot, trigger.sender, 'say', "do the thing")
+    selection = spicemanip(bot, triggerargsarray, 1)
+    selection = selection.lower()
+    if selection == "rock":
+        test
+    elif selection == "rock":
+        test
+    elif selection == "rock":
+        test
+    else:
+        test
