787805ed4064082e9b630f25ca3842072026cf94
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-09-19 06:44:11 -0400

    sudo

---
+++ modules/SpiceBot/development/sudo.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+# author jimender2
+# contributers none
+
+
+@sopel.module.commands('sudo')
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, trigger.group(1))
+    if not enablestatus:
+        # IF "&&" is in the full input, it is treated as multiple commands, and is split
+        commands_array = spicemanip(bot, triggerargsarray, "split_&&")
+        if commands_array == []:
+            commands_array = [[]]
+        for command_split_partial in commands_array:
+            triggerargsarray_part = spicemanip(bot, command_split_partial, 'create')
+            execute_main(bot, trigger, triggerargsarray_part, botcom, instigator)
+
+
+def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+    command = spicemanip(bot, triggerargsarray, 0)
+    if not command:
+        command = "Go make me a sandwich"
+    output = "Sudo " + command
+    osd(bot, trigger.sender, 'say', output)
