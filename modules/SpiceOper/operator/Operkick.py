f2fee847b28233bfc811fd3b90e47a82a8cd99e5
deathbybandaid <sam@deathbybandaid.net>
2018-09-21 11:20:39 -0400

    test

--- modules/SpiceOper/operator/Operkick.py
+++ modules/SpiceOper/operator/Operkick.py
@@ -6,13 +6,19 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from BotShared import *
 
-allowedusers = ['under_score', 'DoubleD', 'deathbybandaid', 'dysonparkes']
-
 
 @event('JOIN')
 @rule('.*')
 @sopel.module.thread(True)
 def joindetect(bot, trigger):
 
-    if not bot.privileges[trigger.sender.lower()][trigger.nick.lower()] >= module.OP and bot.privileges[trigger.sender.lower()][bot.nick.lower()] >= module.OP:
-        bot.write(['KICK', trigger.sender, trigger.nick], "You are not authorized to join " + trigger.sender)
+    # give chanserv time to OP
+    time.sleep(5)
+
+    # If new person not OP, kick
+    if not bot.privileges[trigger.sender.lower()][trigger.nick.lower()] >= module.OP:
+        # make sure bot is OP
+        if bot.privileges[trigger.sender.lower()][bot.nick.lower()] >= module.OP:
+            bot.write(['KICK', trigger.sender, trigger.nick], "You are not authorized to join " + trigger.sender)
+        else:
+            bot.say("I need to be OP to kick unauthorized users such as " + trigger.nick + " from " + trigger.sender)
