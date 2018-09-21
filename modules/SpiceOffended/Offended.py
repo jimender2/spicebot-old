a18de6a54ead1176a3ef5a7d3d6fd5f1c4a6dc9d
deathbybandaid <sam@deathbybandaid.net>
2018-09-21 14:01:39 -0400

    test

--- modules/SpiceOffended/Offended.py
+++ modules/SpiceOffended/Offended.py
@@ -6,20 +6,28 @@ shareddir = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(shareddir)
 from BotShared import *
 
+dontsaylist = ['offended', 'offense', 'offensive']
+
 
 @rule('(.*)')
 @sopel.module.thread(True)
 def offensedetect(bot, trigger):
 
-    # does not apply to bot
-    if trigger.nick.lower() == bot.nick.lower():
+    # does not apply to bots
+    if trigger.nick.lower() in bot_config_names(bot):
         return
 
     triggerargsarray = spicemanip(bot, trigger, 'create')
-    if [x for x in triggerargsarray if x in ['offended', 'offense']]:
+    for x in triggerargsarray:
+        stringx = x
 
-        # make sure bot is OP
-        if bot.privileges[trigger.sender.lower()][bot.nick.lower()] >= module.OP:
-            bot.write(['KICK', trigger.sender, trigger.nick], "You can't talk like that in " + trigger.sender)
-        else:
-            bot.say("I need to be OP to kick unauthorized users such as " + trigger.nick + " from " + trigger.sender + " for being offended.")
+        for r in (("?", ""), ("!", ""), (".", "")):
+            stringx = stringx.replace(*r)
+
+        if stringx in dontsaylist:
+
+            # make sure bot is OP
+            if bot.privileges[trigger.sender.lower()][bot.nick.lower()] >= module.OP:
+                bot.write(['KICK', trigger.sender, trigger.nick], "You can't talk like that in " + trigger.sender)
+            else:
+                bot.say("I need to be OP to kick unauthorized users such as " + trigger.nick + " from " + trigger.sender + " for being offended.")
