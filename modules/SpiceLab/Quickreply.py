7bce8999ddfe3ae1683be2c91367238198deca55
deathbybandaid <sam@deathbybandaid.net>
2018-09-25 15:40:41 -0400

    test

--- modules/SpiceLab/Quickreply.py
+++ modules/SpiceLab/Quickreply.py
@@ -9,22 +9,23 @@ from BotShared import *
 # author deathbybandaid
 
 
-argslist = ['testa', 'testb', 'testc', 'testd']
-
-
 commandsdict = {
-                "testa": "Testing alpha",
-                "testb": "testing beta",
-                "testc": "testing gamma",
-                "testd": "testering delta",
+                "testa": {
+                            "type": "simple",
+                            "reply": "Testing Alpha",
+                            },
+                "testb": {
+                            "type": "target"
+                            "reply": "Testing Beta on $target",
+                            }
                 }
 
 
 @rule('(.*)')
 @sopel.module.thread(True)
-def watchallthethings(bot, trigger):
+def watchallthethings(bot, trigger, commandsdict):
 
-    global commandsdict
+    # global commandsdict
 
     # does not apply to bots
     if trigger.nick.lower() in bot_config_names(bot):
@@ -33,9 +34,26 @@ def watchallthethings(bot, trigger):
     if not str(spicemanip(bot, trigger, 1)).startswith("."):
         return
 
-    dotcommand = str(spicemanip(bot, trigger, 1).lower().replace(".", ""))
+    triggerargsarray = spicemanip(bot, trigger, 'create')
 
-    if dotcommand in commandsdict.keys():
-        bot.say(str(commandsdict[dotcommand]))
-    else:
-        bot.say("I don't seem to have a command for " + str(spicemanip(bot, trigger, 1)))
+    dotcommand = str(spicemanip(bot, triggerargsarray, 1).lower().replace(".", ""))
+    triggerargsarray = spicemanip(bot, triggerargsarray, '2+')
+
+    if dotcommand not in commandsdict.keys():
+        osd(bot, trigger.sender, 'say', "I don't seem to have a command for " + dotcommand)
+        return
+
+    commandtype = str(commandsdict[dotcommand]["type"])
+
+    command_function_run = str('botfunction_' + commandtype + '(bot, trigger, triggerargsarray, commandsdict)')
+    eval(command_function_run)
+
+
+def botfunction_simple(bot, trigger, triggerargsarray, commandsdict):
+    osd(bot, trigger.sender, 'say', str(commandsdict[dotcommand]["reply"]))
+    return
+
+
+def botfunction_target(bot, trigger, triggerargsarray, commandsdict):
+    osd(bot, trigger.sender, 'say', spicemanip(bot, triggerargsarray, 0).replace("$target", spicemanip(bot, triggerargsarray, 1)))
+    return
