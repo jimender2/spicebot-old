71b6da56a8816895c989e163a331cf41642f0efe
Dyson Parkes <dyson@berserkir.net>
2018-10-02 11:00:50 +1300

    Add case for 'because' in blame

--- modules/SpiceBot/Main/Blame.py
+++ modules/SpiceBot/Main/Blame.py
@@ -47,5 +47,7 @@ def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
             osd(bot, trigger.sender, 'say', "It's " + whotoblame + "'s fault.")
         elif forwhat.startswith('for'):
             osd(bot, trigger.sender, 'say', "I blame " + whotoblame + " " + str(forwhat) + ".")
+        elif forw.startswith('because'):
+            osd(bot, trigger.sender, 'say', "I blame " + whotoblame + " " + str(forwhat) + ".")
         else:
             osd(bot, trigger.sender, 'say', "I blame " + whotoblame + " for " + str(forwhat) + ".")
