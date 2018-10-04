631f7d2d526b80827d7a78933996121fe4e0dabf
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-10-03 23:09:41 -0400

    warn

--- modules/SpiceBot/Bot/Warn.py
+++ modules/SpiceBot/Bot/Warn.py
@@ -20,6 +20,6 @@ def mainfunction(bot, trigger):
     target = spicemanip(bot, triggerargsarray, 2) or ''
     rand = random.randint(1, 10)
     if rand < 5:
-        osd(bot, inchannel, 'say', target + "This is just a warning. Overuse of the bot can get you kicked or banned by an operator. If you want to purely play with the bot, go to #Spicebot or #Spicebottest, or send Spicebot a PrivateMessage.")
+        osd(bot, inchannel, 'say', target + "This is just a warning. Overuse of the bot can get you kicked or banned by an operator. If you want to purely play with the bot, go to #Spicebottest, or send Spicebot a PrivateMessage.")
     else:
         osd(bot, inchannel, 'say', target + "This is just a warning. Underuse of the bot can get you kicked or banned by an operator. If you want to not play with the bot, go to Canada or some other place that no one cares about.")
