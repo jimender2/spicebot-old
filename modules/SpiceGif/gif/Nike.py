a3331a17b3487388899c388f9ae70b1019e72703
Dyson Parkes <dyson@berserkir.net>
2018-10-09 11:02:00 +1300

    Remove unused imports from Nike.

--- modules/SpiceGif/gif/Nike.py
+++ modules/SpiceGif/gif/Nike.py
@@ -2,10 +2,6 @@
 # coding=utf-8
 from __future__ import unicode_literals, absolute_import, print_function, division
 import sopel.module
-import urllib2
-import json
-from BeautifulSoup import BeautifulSoup
-from random import randint
 import sys
 import os
 moduledir = os.path.dirname(__file__)
