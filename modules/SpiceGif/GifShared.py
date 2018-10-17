788c3a5687df4d7d5da31fedaba394b180bf1a6b
deathbybandaid <sam@deathbybandaid.net>
2018-10-16 20:40:04 -0400

    test

--- modules/SpiceGif/GifShared.py
+++ modules/SpiceGif/GifShared.py
@@ -466,6 +466,7 @@ def getGif(bot, searchdict):
         data = json.loads(urllib2.urlopen(url).read())
 
         results = data[valid_gif_api_dict[currentapi]['results']]
+        osd(bot, trigger.sender, 'say', str(results))
         resultsarray = []
         for result in results:
             if currentapi == 'giphy':
