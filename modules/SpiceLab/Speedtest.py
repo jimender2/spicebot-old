69138b468d8a55345474d5b69f887db4efe827e3
deathbybandaid <sam@deathbybandaid.net>
2018-10-23 09:48:00 -0400

    test

--- modules/SpiceLab/Speedtest.py
+++ modules/SpiceLab/Speedtest.py
@@ -27,44 +27,4 @@ def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
     s.upload()
     s.results.share()
     results_dict = s.results.dict()
-    # osd(bot, trigger.sender, 'say', str(results_dict['client']['share']))
-    for key in results_dict.keys():
-        osd(bot, trigger.sender, 'say', str(key))
-
-
-"""
-{
-'client': {
-            'rating': '0',
-            'loggedin': '0',
-            'isprating': '3.7',
-            'ispdlavg': '0',
-            'ip': '131.93.141.62',
-            'isp': 'Spectrum',
-            'lon': '-87.4545',
-            'ispulavg': '0',
-            'country': 'US',
-            'lat': '46.5786'},
-            'bytes_sent': 27918336,
-            'download': 159917730.11941049,
-            'timestamp': '2018-10-23T13:39:28.056531Z',
-            'share': u'http://www.speedtest.net/result/7740317974.png',
-            'bytes_received': 200888412,
-            'ping': 61.299,
-            'upload': 22144728.947812572,
-            'server': {
-                        'latency': 61.299,
-                        'name': 'Houghton, MI',
-                        'url': 'http://208.68.24.149/speedtest/upload.php',
-                        'country': 'United States',
-                        'lon': '-88.5689',
-                        'cc': 'US',
-                        'host': 'speedtest.remc1.net:8080',
-                        'sponsor': 'REMC1',
-                        'url2': 'http://speedtest.remc1.net/speedtest/upload.php',
-                        'lat': '47.1219',
-                        'id': '5207',
-                        'd': 104.07353745240147
-                        }
-            }
-"""
+    osd(bot, trigger.sender, 'say', str(results_dict['share']))
