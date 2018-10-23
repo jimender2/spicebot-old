6f0a18172ffeaf0cee41f1070df2a57203bd8abb
deathbybandaid <sam@deathbybandaid.net>
2018-10-23 14:17:24 -0400

    test

--- modules/spiceRPG/Game/__init__.py
+++ modules/spiceRPG/Game/__init__.py
@@ -610,7 +610,7 @@ def rpg_bot_start_script(bot):
     trigger.sender = "SpiceRealm"
 
     startup_monologue = []
-    startup_monologue.append("The Spice Realms are vast; full of wonder, loot, monsters, and peril!")
+    startup_monologue.append("\x0309,01The Spice Realms are vast; full of wonder, loot, monsters, and peril!\x03")
     startup_monologue.append("\x0309,01Will you, Brave Adventurers, be triumphant over the challenges that await?\x03")
 
     if not rpg.gamedict["tempvals"]['versionnumber']:
