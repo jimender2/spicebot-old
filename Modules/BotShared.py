c904ca93e7d7a279a936f6ace682a9bfe018b2f4
deathbybandaid <sam@deathbybandaid.net>
2018-10-31 14:36:56 -0400

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -5,6 +5,16 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 # sopel imports
 import sopel.module
 
+# imports for system and OS access, directories
+import sys
+import os
+
+# imports based on THIS file
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from BotShared import *
+
 # Ensure Encoding
 reload(sys)
 sys.setdefaultencoding('utf-8')
