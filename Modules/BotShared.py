c896b1b4dc973a88ff750bf907294aace685faaf
deathbybandaid <sam@deathbybandaid.net>
2018-10-31 14:45:21 -0400

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -10,11 +10,8 @@ from sopel.module import commands, nickname_commands
 import os
 import sys
 
-# imports based on THIS file
-moduledir = os.path.dirname(__file__)
-shareddir = os.path.dirname(os.path.dirname(__file__))
-sys.path.append(shareddir)
-from BotShared import *
+# Additional imports
+import datetime
 
 # Ensure Encoding
 reload(sys)
