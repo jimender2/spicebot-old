964a45940fffdcb16dbf74ff02733c5cebcfb783
deathbybandaid <sam@deathbybandaid.net>
2018-10-31 19:04:57 -0400

    remove dummy code

--- Modules/Bot_Management/setup.py
+++ Modules/Bot_Management/setup.py
@@ -5,7 +5,6 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 # sopel imports
 import sopel.module
 
-testcom = 23
 # imports for system and OS access, directories
 import os
 import sys
