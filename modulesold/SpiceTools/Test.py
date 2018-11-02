f8986fb65a4721f15028e4fd2d843fc04d961960
Dyson Parkes <dyson@berserkir.net>
2018-11-02 16:22:32 +1300

    Comment code for readability

--- modulesold/SpiceTools/Test.py
+++ modulesold/SpiceTools/Test.py
@@ -25,21 +25,26 @@ def mainfunction(bot, trigger):
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
     """Run requested command with all possible triggers."""
-    commandtotest = spicemanip(bot, triggerargsarray, 1)
+    commandtotest = spicemanip(bot, triggerargsarray, 1)  # This is the module being tested, entered in chat.
+    randomnumber = random.randint(1, 27)
+    # The things used to test.
     instigator = trigger.nick
     channel = trigger.sender
     command = "." + commandtotest
     randombot = "SpiceBotdevold"
-    randomnumber = random.randint(1, 27)
     randomnumberstring = str(randomnumber)
     randomnumberword = Number2Words(randomnumber).convert()
-    randomadmin = ""
     blank = ""
+
+    # List of strings to test with.
     args = [instigator, channel, randombot, randomnumberstring, randomnumberword, blank]
     if not commandtotest:
+        # Handle if nothing was entered to test.
         osd(bot, trigger.sender, 'say', "What command did you want me to test, " + instigator + "?")
     else:
+        # Use the entered command to build strings to test with, and test.
         for arg in args:
+            # Use each arg in the list of args individually.
             message = command + " " + arg
             osd(bot, trigger.sender, 'say', message)
-            time.sleep(0.100)  # Wait for 100 milliseconds
+            time.sleep(0.250)  # Wait for 100 milliseconds
