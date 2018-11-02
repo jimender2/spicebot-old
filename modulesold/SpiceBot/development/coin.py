97e027578c5c52d3777f330c66f8abff92d0d526
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-11-01 22:57:25 -0400

    test

--- modulesold/SpiceBot/development/coin.py
+++ modulesold/SpiceBot/development/coin.py
@@ -5,6 +5,7 @@ import sopel.module
 import sys
 import os
 import random
+from Bot import *
 moduledir = os.path.dirname(__file__)
 shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
@@ -14,16 +15,7 @@ from BotShared import *
 
 
 @sopel.module.commands('coin', 'die')
-def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'coin')
-    if not enablestatus:
-        # IF "&&" is in the full input, it is treated as multiple commands, and is split
-        commands_array = spicemanip(bot, triggerargsarray, "split_&&")
-        if commands_array == []:
-            commands_array = [[]]
-        for command_split_partial in commands_array:
-            triggerargsarray_part = spicemanip(bot, command_split_partial, 'create')
-            execute_main(bot, trigger, triggerargsarray_part, botcom, instigator)
+mainfunction(bot, trigger)
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
