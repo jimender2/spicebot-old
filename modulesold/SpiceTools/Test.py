2fe62c23edba20b826dee48dc5a8ecbad163b130
Dyson Parkes <dyson@berserkir.net>
2018-11-02 16:08:17 +1300

    Add word2number

--- modulesold/SpiceTools/Test.py
+++ modulesold/SpiceTools/Test.py
@@ -6,6 +6,7 @@ import sys
 import os
 import random
 from word2number import w2n
+from number2word import n2w
 moduledir = os.path.dirname(__file__)
 shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
@@ -30,7 +31,7 @@ def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
     command = "." + commandtotest
     randombot = "SpiceBotdevold"
     randomnumber = random.randint(1, 10)
-    randomnumberword = w2n(randomnumber)
+    randomnumberword = n2w(randomnumber)
     randomadmin = ""
     blank = ""
     args = [instigator, channel, randombot, randomnumber, blank]
