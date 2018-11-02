c4ddef00f548258983be2588cf87165a77e1ecc2
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-11-01 22:37:36 -0400

    makeitso

--- modulesold/SpiceBot/development/makeitso.py
+++ modulesold/SpiceBot/development/makeitso.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 import sopel.module
 import sys
 import os
+import random
 moduledir = os.path.dirname(__file__)
 shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 sys.path.append(shareddir)
