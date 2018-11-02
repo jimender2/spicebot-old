96952fa5dbc68fe63238602f26f2dd254b453f1d
jimender2 <35303639+jimender2@users.noreply.github.com>
2018-11-01 23:00:21 -0400

    coin

--- modulesold/SpiceBot/development/bot.py
+++ modulesold/SpiceBot/development/bot.py
@@ -1,10 +1,5 @@
-def mainfunction(bot, trigger):
-    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'coin')
-    if not enablestatus:
-        # IF "&&" is in the full input, it is treated as multiple commands, and is split
-        commands_array = spicemanip(bot, triggerargsarray, "split_&&")
-        if commands_array == []:
-            commands_array = [[]]
-        for command_split_partial in commands_array:
-            triggerargsarray_part = spicemanip(bot, command_split_partial, 'create')
-            execute_main(bot, trigger, triggerargsarray_part, botcom, instigator)
+
+
+def test(bot, trigger.sender, 'say', msg) {
+    osd(bot, trigger.sender, 'say', msg)
+}
--- modulesold/SpiceBot/development/coin.py
+++ modulesold/SpiceBot/development/coin.py
@@ -15,12 +15,23 @@ from BotShared import *
 
 
 @sopel.module.commands('coin', 'die')
-mainfunction(bot, trigger)
+def mainfunction(bot, trigger):
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'coin')
+    if not enablestatus:
+        # IF "&&" is in the full input, it is treated as multiple commands, and is split
+        commands_array = spicemanip(bot, triggerargsarray, "split_&&")
+        if commands_array == []:
+            commands_array = [[]]
+        for command_split_partial in commands_array:
+            triggerargsarray_part = spicemanip(bot, command_split_partial, 'create')
+            execute_main(bot, trigger, triggerargsarray_part, botcom, instigator)
 
 
 def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
     sides = int( spicemanip(bot, triggerargsarray, 1) )
     rand = random.randint(1, sides)
+    msg = "you suck"
+    test(bot, trigger.sender, 'say', msg)
     if sides > 2:
         msg = trigger.nick + " you rolled a " + rand + " on a " + str(sides) + " sided die."
     else:
