62cf5b6bbcd3b938d3edfeb862944ebc58057e27
Dyson Parkes <dyson@berserkir.net>
2018-11-02 15:58:46 +1300

    Create test module

---
+++ modulesold/SpiceTools/Test.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+import sopel.module
+import sys
+import os
+import random
+from word2number import w2n
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+# author dysonparkes
+
+
+@sopel.module.commands('test')
+def mainfunction(bot, trigger):
+    """Check to see if module is enabled. """
+    enablestatus, triggerargsarray, botcom, instigator = spicebot_prerun(bot, trigger, 'test')
+    if not enablestatus:
+        execute_main(bot, trigger, triggerargsarray, botcom, instigator)
+
+
+def execute_main(bot, trigger, triggerargsarray, botcom, instigator):
+    """Run requested command with all possible triggers."""
+    commandtotest = spicemanip(bot, triggerargsarray, 1)
+    instigator = trigger.nick
+    channel = trigger.sender
+    command = "." + commandtotest
+    randombot = "SpiceBotdevold"
+    randomnumber = randint(1, 10)
+    randomnumberword = w2n(randomnumber)
+    randomadmin = ""
+    blank = ""
+    args = [instigator, channel, randombot, randomnumber, blank]
+    if not commandtotest:
+        osd(bot, trigger.sender, 'say', "What command did you want me to test, " + instigator + "?")
+    else:
+        for arg in args:
+            message = command + " " + string(arg)
+            osd(bot, trigger.sender, 'say', message)
