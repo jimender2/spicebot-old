2b1df114c7c7a4198b56f02397a5ad515ce8ddf9
deathbybandaid <sam@deathbybandaid.net>
2018-11-03 21:58:26 -0400

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -1086,7 +1086,10 @@ def bot_dictcom_simple(bot, botcom):
     reply = reply.replace("$instigator", botcom.instigator)
     if not isinstance(reply, list):
         reply = [reply]
+        bot.say("not list")
+    bot.say(str(reply))
     for realreply in reply:
+        bot.say(str(realreply))
         realreply = realreply.replace("$instigator", botcom.instigator)
         realreply = realreply.replace("$channel", botcom.channel_current)
         osd(bot, botcom.channel_current, 'say', realreply)
