8af103c8761834ea0628224b14412949617c7c23
deathbybandaid <sam@deathbybandaid.net>
2018-11-08 13:28:18 -0500

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -1964,7 +1964,7 @@ def bot_dictcom_simple(bot, botcom):
             if command not in ["?default"]:
                 nonstockoptions.append(command)
         nonstockoptions = spicemanip(bot, nonstockoptions, "andlist")
-        return osd(bot, botcom.channel_current, 'say', "The options for " + str(botcom.maincom) + " command include: " + str(nonstockoptions) + ".")
+        return osd(bot, botcom.channel_current, 'say', "The special options for " + str(botcom.maincom) + " command include: " + str(nonstockoptions) + ".")
     elif botcom.specified == 'count':
         return osd(bot, botcom.channel_current, 'say', "The " + str(botcom.maincom) + " " + str(botcom.responsekey or '') + " command has " + str(len(botcom.dotcommand_dict[botcom.responsekey]["responses"])) + " entries.")
     elif botcom.specified == 'author':
