e7ce6364893036b16d35cf9fb55fc5e8396ce1af
deathbybandaid <sam@deathbybandaid.net>
2018-11-09 10:51:42 -0500

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -1607,7 +1607,8 @@ def bot_dictcom_target(bot, botcom):
 
         if not botcom.target:
             if botcom.dotcommand_dict[botcom.responsekey]["target_fail"]:
-                return osd(bot, botcom.instigator, 'notice', botcom.dotcommand_dict[botcom.responsekey]["target_fail"])
+                botcom.replies = botcom.dotcommand_dict[botcom.responsekey]["target_fail"]
+                return bot_dictcom_reply_shared(bot, botcom)
             else:
                 return osd(bot, botcom.instigator, 'notice', "This command requires a target.")
 
@@ -1618,9 +1619,11 @@ def bot_dictcom_target(bot, botcom):
             targetchecking = bot_target_check(bot, botcom, botcom.target, botcom.dotcommand_dict[botcom.responsekey]["target_self"])
             if not targetchecking["targetgood"]:
                 if targetchecking["reason"] == "bot" and botcom.dotcommand_dict[botcom.responsekey]["react_bot"]:
-                    return osd(bot, botcom.instigator, 'notice', botcom.dotcommand_dict[botcom.responsekey]["react_bot"])
+                    botcom.replies = botcom.dotcommand_dict[botcom.responsekey]["react_bot"]
+                    return bot_dictcom_reply_shared(bot, botcom)
                 elif targetchecking["reason"] == "self" and botcom.dotcommand_dict[botcom.responsekey]["react_self"]:
-                    return osd(bot, botcom.instigator, 'notice', botcom.dotcommand_dict[botcom.responsekey]["react_self"])
+                    botcom.replies = botcom.dotcommand_dict[botcom.responsekey]["react_self"]
+                    return bot_dictcom_reply_shared(bot, botcom)
                 else:
                     return osd(bot, botcom.instigator, 'notice', targetchecking["error"])
 
