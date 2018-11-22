8cc9388cae11b53428460178233c68e8907956bd
deathbybandaid <sam@deathbybandaid.net>
2018-11-22 09:15:11 -0500

    trying again

--- Modules/BotCore/RPL_Welcome.py
+++ Modules/BotCore/RPL_Welcome.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals, absolute_import, print_function, divisi
 
 # sopel imports
 import sopel.module
-from sopel import module, tools
 
 # imports for system and OS access, directories
 import os
@@ -21,7 +20,8 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-@module.event(tools.events.RPL_WELCOME)
-@module.rule('.*')
+@event('RPL_WELCOME')
+@rule('.*')
+@sopel.module.thread(True)
 def real_startup(bot, trigger):
     bot.msg("#spicebottest", "loading test")
