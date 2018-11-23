6f8fff58d0eafe437311914c73df998f1cecfdf9
deathbybandaid <sam@deathbybandaid.net>
2018-11-23 18:34:07 -0500

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -420,10 +420,10 @@ def bot_api_socket_handler(conn, bot):
     if "botdict_loaded" not in bot.memory:
         stderr("[API] Not ready to process requests.")
         return
-    stderr("[API] test.")
 
     data = conn.recv(2048)
     if not data:
+        stderr("[API] test.")
         conn.close()
     else:
 
