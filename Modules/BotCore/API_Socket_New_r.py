190f6abeb4d9beb7722588c314efe08c6d971050
deathbybandaid <sam@deathbybandaid.net>
2018-11-24 11:38:43 -0500

    newline

--- Modules/BotCore/API_Socket_New_r.py
+++ Modules/BotCore/API_Socket_New_r.py
@@ -71,7 +71,7 @@ def listener(bot, trigger):
                         # Possibly add a api key
 
                         bot.msg("#spicebottest", "[R] sending data back to the client")
-                        connection.sendall(str("helloworld"))
+                        connection.sendall(str(str(bot.memory["botdict"]) + "\n"))
                         bot.msg("#spicebottest", "[R] sent data back to the client")
                         break
 
