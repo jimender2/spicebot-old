4df257ae8df7a22035b6e884078bf6c487e30449
deathbybandaid <sam@deathbybandaid.net>
2018-11-24 10:01:13 -0500

    test

---
+++ Modules/BotCore/API_New.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+# sopel imports
+import sopel.module
+
+# imports for system and OS access, directories
+import os
+import sys
+
+
+# imports based on THIS file
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from BotShared import *
+
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+
+
+# Ensure Encoding
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+# Start listener on welcome RPL, which should only ever be received once
+@event('001')
+@rule('.*')
+def listener(bot, trigger):
+
+    beguinelisten = False
+    while not beguinelisten:
+        if "botdict_loaded" in bot.memory:
+            beguinelisten = True
+        else:
+            time.sleep(1)
+
+    bot.msg("#spicebottest", "api test started")
+
+    api.add_resource(User, "/user/<string:name>")
+
+    app.run(debug=True)
+
+    users = [
+            {
+                "name": "Nicholas",
+                "age": 42,
+                "occupation": "Network Engineer"
+            },
+            {
+                "name": "Elvin",
+                "age": 32,
+                "occupation": "Doctor"
+            },
+            {
+                "name": "Jass",
+                "age": 22,
+                "occupation": "Web Developer"
+            }
+        ]
+
+
+class User(Resource):
+    def get(self, name):
+        for user in users:
+            if(name == user["name"]):
+                return user, 200
+        return "User not found", 404
+
+    def post(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if(name == user["name"]):
+                return "User with name {} already exists".format(name), 400
+
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+        }
+        users.append(user)
+        return user, 201
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument("age")
+        parser.add_argument("occupation")
+        args = parser.parse_args()
+
+        for user in users:
+            if(name == user["name"]):
+                user["age"] = args["age"]
+                user["occupation"] = args["occupation"]
+                return user, 200
+
+        user = {
+            "name": name,
+            "age": args["age"],
+            "occupation": args["occupation"]
+        }
+        users.append(user)
+        return user, 201
+
+    def delete(self, name):
+        global users
+        users = [user for user in users if user["name"] != name]
+        return "{} is deleted.".format(name), 200
