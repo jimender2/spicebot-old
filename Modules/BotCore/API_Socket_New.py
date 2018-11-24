be923c77dbfae0804e256d9c6cf5544ebcca1117
deathbybandaid <sam@deathbybandaid.net>
2018-11-24 10:25:00 -0500

    test

--- Modules/BotCore/API_Socket_New.py
+++ Modules/BotCore/API_Socket_New.py
@@ -42,6 +42,7 @@ def listener(bot, trigger):
     server_address = ('0.0.0.0', 10000)
     bot.msg("#spicebottest", "starting up on " + str(server_address))
     sock.bind(server_address)
+    sock.connect(server_address)
 
     # Listen for incoming connections
     sock.listen(1)
