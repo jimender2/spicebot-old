d375c64cdf0d60fb6d5d3909cb2c7045fd705b07
deathbybandaid <sam@deathbybandaid.net>
2018-11-26 11:30:17 -0500

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -632,7 +632,7 @@ def modules_command_listing(bot):
     for modules in bot.command_groups.items():
         modulecount += 1
         for commands in modules:
-            bot.msg("#spicebottest", str(commands))
+            # bot.msg("#spicebottest", str(commands))
             filename = commands[0]
             # validcoms = commands[1]
             # for com in validcoms:
