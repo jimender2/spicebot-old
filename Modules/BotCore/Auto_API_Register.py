4369f4da2e81c3187786ced49d0de1b78421688d
deathbybandaid <sam@deathbybandaid.net>
2018-11-26 19:02:09 -0500

    check register

---
+++ Modules/BotCore/Auto_API_Register.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+# sopel imports
+import sopel.module
+
+# imports for system and OS access, directories
+import os
+import sys
+
+# imports based on THIS file
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from BotShared import *
+
+# Ensure Encoding
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+@sopel.module.interval(1800)
+def savingitall(bot):
+    # don't run jobs if not ready
+    if "botdict_loaded" not in bot.memory:
+        return
+
+    bot_register_handler_startup(bot)
