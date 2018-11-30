da2b976db4f96ab025ae5aba2a5feb2095cda744
deathbybandaid <sam@deathbybandaid.net>
2018-11-29 21:37:06 -0500

    test

--- Modules/Tools/SpiceLab/Watch.py
+++ Modules/Tools/SpiceLab/Watch.py
@@ -23,7 +23,7 @@ sys.setdefaultencoding('utf-8')
 
 
 # Start listener on welcome RPL, which should only ever be received once
-@event('RPL_WELCOME')
+@event(RPL_WELCOME)
 @rule('.*')
 @sopel.module.thread(True)
 def watch_events_test(bot, trigger):
