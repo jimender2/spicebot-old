cd938aecf813f5bd4a37d28746eae8497e01de33
deathbybandaid <sam@deathbybandaid.net>
2018-12-02 12:01:50 -0500

    test

--- SpiceMother/Modules/BotCore/Bot_Startup_Permissions.py
+++ SpiceMother/Modules/BotCore/Bot_Startup_Permissions.py
@@ -26,4 +26,36 @@ sys.setdefaultencoding('utf-8')
 def bot_startup_main(bot, trigger):
 
     # os.system("sudo chown -R spicebot:sudo /home/spicebot/.sopel/")
-    shutil.chown("/home/spicebot/.sopel/" + str(bot.nick) + "/", "spicebot", "sudo")
+    chown("/home/spicebot/.sopel/" + str(bot.nick) + "/", "spicebot", "sudo")
+
+
+def chown(path, user=None, group=None):
+    """Change owner user and group of the given path.
+
+    user and group can be the uid/gid or the user/group names, and in that case,
+    they are converted to their respective uid/gid.
+    """
+
+    if user is None and group is None:
+        raise ValueError("user and/or group must be set")
+
+    _user = user
+    _group = group
+
+    # -1 means don't change it
+    if user is None:
+        _user = -1
+    # user can either be an int (the uid) or a string (the system username)
+    elif isinstance(user, basestring):
+        _user = _get_uid(user)
+        if _user is None:
+            raise LookupError("no such user: {!r}".format(user))
+
+    if group is None:
+        _group = -1
+    elif not isinstance(group, int):
+        _group = _get_gid(group)
+        if _group is None:
+            raise LookupError("no such group: {!r}".format(group))
+
+    os.chown(path, _user, _group)
