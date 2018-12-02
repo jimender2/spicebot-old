0b2100e16c5434f6e3215202ba02eb98ef7e9e74
deathbybandaid <sam@deathbybandaid.net>
2018-12-02 16:54:47 -0500

    test

--- SpiceMother/Modules/BotCore/Bot_Startup_Channels.py
+++ SpiceMother/Modules/BotCore/Bot_Startup_Channels.py
@@ -45,7 +45,6 @@ def bot_setup_channels(bot, trigger):
 
     for channel in bot.privileges.keys():
         channel = str(channel)
-        bot.msg("#spicemotherdev", str(bot.privileges[channel]))
 
         # permanent listing of the channel
         if channel not in bot.memory["botdict"]['servers_list'][currentservername]["channels_list"].keys():
@@ -70,35 +69,5 @@ def bot_setup_channels(bot, trigger):
 
         if 'all_current_users' not in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]['channels_list'][channel].keys():
             bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]['channels_list'][channel]['all_current_users'] = []
-
-        userprivdict = dict()
-        for user in bot.privileges[channel].keys():
-            user = str(user)
-
-            if user not in bot.memory["botdict"]["users"].keys():
-                bot.memory["botdict"]["users"][user] = dict()
-
-            if user not in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]['channels_list'][channel]['all_current_users']:
-                bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]['channels_list'][channel]['all_current_users'].append(user)
-
-            if user not in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]['channels_list'][channel]['current_users'] and user not in bot.memory["botdict"]["tempvals"]['bots_list'].keys():
-                bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]['channels_list'][channel]['current_users'].append(str(user))
-
-            try:
-                userprivdict[user] = bot.privileges[channel][str(user)] or 0
-            except KeyError:
-                userprivdict[str(user)] = 0
-
-        for privtype in ['VOICE', 'HALFOP', 'OP', 'ADMIN', 'OWNER']:
-            privstring = str("chan" + privtype.lower() + "s")
-            if userprivdict[user] == eval(privtype):
-                if user not in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]['channels_list'][channel][privstring]:
-                    bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]['channels_list'][channel][privstring].append(user)
-            elif userprivdict[user] >= eval(privtype) and privtype == 'OWNER':
-                if user not in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]['channels_list'][channel][privstring]:
-                    bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]['channels_list'][channel][privstring].append(user)
-            else:
-                if user in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]['channels_list'][channel][privstring]:
-                    bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]['channels_list'][channel][privstring].remove(user)
 
     bot_startup_requirements_set(bot, "channels")
