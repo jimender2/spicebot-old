d5c5ec7b8a58436c04bd66970a3165f1865c4d59
deathbybandaid <sam@deathbybandaid.net>
2018-12-01 20:37:11 -0500

    test

---
+++ SpiceMother/Modules/Nick_Commands/BotNick_Update.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+# sopel imports
+import sopel.module
+
+# imports for system and OS access, directories
+import os
+import sys
+
+# imports based on THIS file
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from BotShared import *
+
+# Ensure Encoding
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+@nickname_commands('update')
+@sopel.module.thread(True)
+def bot_command_hub(bot, trigger):
+
+    stderr("Recieved Command to update.")
+    osd(bot, bot.privileges.keys(), 'say', "Recived command from " + trigger.nick + " to update from Github and restart. Be Back Soon!")
+
+    # Pull directory from github
+    stderr("Pulling From Github.")
+    g = git.cmd.Git("/home/spicebot/.sopel/" + str(bot.nick))
+    g.pull()
+
+    # close connection
+    # stderr("[API] Closing Connection.")
+    # connection.close()
+
+    # restart systemd service
+    stderr("Restarting Service.")
+    os.system("sudo service " + str(bot.nick) + " restart")
