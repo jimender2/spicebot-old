2df6164288c136cbbebde8d605964e8615a0f018
deathbybandaid <sam@deathbybandaid.net>
2018-12-03 14:28:18 -0500

    test

--- SpiceMother/Modules/BotCore/Watch/Watch_Quit.py
+++ SpiceMother/Modules/BotCore/Watch/Watch_Quit.py
@@ -47,7 +47,6 @@ def botcom_player_leave(bot, trigger):
 
     # server
     botcom.server = bot.memory["botdict"]["tempvals"]['server']
-    bot.msg("#spicemotherdev", str(botcom.server))
 
     # channel
     botcom.channel_current = str(trigger.sender)
@@ -58,6 +57,7 @@ def botcom_player_leave(bot, trigger):
         if not botcom.channel_priv:
             privstring = str("chan" + privtype.lower() + "s")
             evalstring = str("bot.memory['botdict']['tempvals']['servers_list'][" + str(botcom.server) + "]['channels_list']['" + botcom.channel_current + "']['" + privstring + "']")
+            bot.msg("#spicemotherdev", str(evalstring))
             grouplist = eval(evalstring)
         else:
             grouplist = []
