fea0862b87f0ccb7b601746606b4d0eacc284dd3
deathbybandaid <sam@deathbybandaid.net>
2018-12-05 10:36:23 -0500

    test

--- Modules/BotCore/Nick_Commands/BotNick_debug_systemd.py
+++ Modules/BotCore/Nick_Commands/BotNick_debug_systemd.py
@@ -41,7 +41,8 @@ def bot_command_hub(bot, trigger):
     osd(bot, botcom.channel_current, 'action', "Is Examining systemd Log(s).")
 
     servicestarttime = str(os.popen("systemctl show " + str(bot.nick) + " --property=ActiveEnterTimestamp").read()).split("=")[-1]
-    parser.parse(str(servicestarttime))
+    servicestarttime = parser.parse(str(servicestarttime))
+    bot.say(str(servicestarttime))
 
     return
 
