60d9a2512cd4a17a167d5de1e1b95c4f1746ade4
deathbybandaid <sam@deathbybandaid.net>
2018-12-05 21:35:43 -0500

    test

--- Modules/Tools/Speedtest.py
+++ Modules/Tools/Speedtest.py
@@ -46,7 +46,7 @@ def mainfunctionnobeguine(bot, trigger):
 def execute_main(bot, trigger, botcom):
     osd(bot, trigger.sender, 'say', "Starting Speedtest...")
 
-    servers = []
+    servers = [14084]
     s = speedtest.Speedtest()
     s.get_servers(servers)
     s.get_best_server()
