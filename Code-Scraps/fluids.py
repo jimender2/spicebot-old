3743b4c7ac2baf78d7481f02c3a368c6076b56ff
Dyson Parkes <dyson@berserkir.net>
2018-12-07 13:37:12 +1300

    Add 'fluids' scrap

---
+++ Code-Scraps/fluids.py
@@ -0,0 +1,53 @@
+<deathbybandaid> .poop DoubleD
+<SpiceBot> deathbybandaid overestimated their capabilities and poops themselves. DoubleD is not impressed.
+<deathbybandaid> I never seem to get that right
+<deathbybandaid> dysonparkes might make poop moduler again, and apply some of the new claims logic for bladder to bowel capacity
+<under_score> oh my that is crazy
+<deathbybandaid> dysonparkes could write a whole game that implements the full body?
+<deathbybandaid> .spit
+<deathbybandaid> That could be a thing
+<deathbybandaid> Ooohhhhh like a virtual person
+<deathbybandaid> Could merge in .pint
+<under_score> that is a lot
+<deathbybandaid> If anybody is funny enough to write that code, it's dysonparkes
+<under_score> for sure
+<deathbybandaid> .tap dysonparkes
+<SpiceBot> Hey dysonparkes,,,, you there?
+<deathbybandaid> .echo dysonparkes
+<SpiceBot>  dysonparkes
+<SpiceBot>  dysonparkes
+<SpiceBot>  dysonparkes
+<deathbybandaid> Saying name 3 times is a summons, right?
+<deathbybandaid> .beetlejuice
+<under_score> doesn't seem to work
+<under_score> maybe you have to say it backwards
+<deathbybandaid> .fr .beetlejuice
+<SpiceBot> Successfully created Feature Request: .beetlejuice   https://github.com/SpiceBot/SpiceBot/issues/3086
+<deathbybandaid> Oh yeah, I updated GitHub commands to do that
+<under_score> .chuck deathbybandaid
+<deathbybandaid> Not yet
+<under_score> k
+<deathbybandaid> .fr SpiceBot what time is it $location
+<SpiceBot> Successfully created Feature Request   https://github.com/SpiceBot/SpiceBot/issues/3087
+-SpiceDuels- dysonparkes,    You have been awarded a mysterypotion!
+* dysonparkes appears through the mirror
+<deathbybandaid> Scrollback
+<dysonparkes> You want a poop game.
+<dysonparkes> A literal whole game of poop.
+<dysonparkes> That is the shittiest idea I have heard in a while.
+<dysonparkes> .rimshot
+* SpiceBot uses drumsticks to hit both the head and the rim of the drum, then the cymbal.
+<SpiceBot> *Ba Dum Tss!!!*
+<deathbybandaid> .points dysonparkes
+<SpiceBotold> deathbybandaid awards 527 points to dysonparkes.
+<dysonparkes> How are you envisaging this?
+<deathbybandaid> dysonparkes I was thinking a full game of bodily fluids?
+<dysonparkes> In what sense?
+* dysonparkes thumps deathbybandaid, causing anal leakage. 50 points!
+<deathbybandaid> Bowel capacity determines if you poop your pants, or fart
+* dysonparkes knees deathbybandaid in the crotch, causing bladder voiding. 20 points!
+<deathbybandaid> Spit requires saliva?
+<deathbybandaid> Idk
+<dysonparkes> You want me to 'gamify' shitting/spitting/bleeding?
+<deathbybandaid> Along with peeing in claims
+<dysonparkes> Have no limit on bleeding, just make it randomly have you die from bleeding out (causing loss of points)
