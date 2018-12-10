ff1d3d27d091038be4021bb6fd6b93f768e6fb31
deathbybandaid <sam@deathbybandaid.net>
2018-12-10 17:46:45 -0500

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -828,8 +828,8 @@ def bot_api_fetch(bot, TCP_PORT, TCP_IP):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect((TCP_IP, TCP_PORT))
-        s.send("GET")
-        data = s.recv(999999999)
+        s.send("GET /")
+        data = s.recv(4096)
         osd(bot, "#spicebottest", 'say', str(data))
         s.close()
         botdict_return = json.loads(data, object_hook=json_util.object_hook)
