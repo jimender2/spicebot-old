336399b7250f1e1a2cfebaeb54a7b2c188745d08
deathbybandaid <sam@deathbybandaid.net>
2018-12-11 16:30:28 -0500

    test

--- Modules/BotCore/Startup/Bot_Startup_Feeds.py
+++ Modules/BotCore/Startup/Bot_Startup_Feeds.py
@@ -47,7 +47,7 @@ def feed_configs(bot):
     bot.memory["botdict"]["tempvals"]['feeds'] = dict()
     bot.memory["botdict"]["tempvals"]['feeds_loaded'] = []
 
-    quick_coms_path = bot.memory["botdict"]["tempvals"]["bot_info"][str(bot.nick)]["directory_main"] + "/Modules/feeds/" + str(bot.nick) + "/"
+    quick_coms_path = bot.memory["botdict"]["tempvals"]["bot_info"][str(bot.nick)]["directory_main"] + "/Modules/Feeds/" + str(bot.nick) + "/"
     if os.path.exists(quick_coms_path) and os.path.isdir(quick_coms_path):
         if not os.path.isfile(quick_coms_path) and len(os.listdir(quick_coms_path)) > 0:
             filescan.append(quick_coms_path)
@@ -59,8 +59,7 @@ def feed_configs(bot):
             else:
                 extradirs = str(bot.memory["botdict"]["tempvals"]["bot_info"][str(bot.nick)]['configuration']["feeds"]["extra"]).split(",")
             for extra in extradirs:
-                quick_coms_path_extra = bot.memory["botdict"]["tempvals"]["bot_info"][str(bot.nick)]["directory_main"] + "/Modules/feeds/" + str(extra) + "/"
-                bot.msg("#spicebottest", str(quick_coms_path_extra))
+                quick_coms_path_extra = bot.memory["botdict"]["tempvals"]["bot_info"][str(bot.nick)]["directory_main"] + "/Modules/Feeds/" + str(extra) + "/"
                 if os.path.exists(quick_coms_path_extra) and os.path.isdir(quick_coms_path_extra):
                     if not os.path.isfile(quick_coms_path_extra) and len(os.listdir(quick_coms_path_extra)) > 0:
                         filescan.append(quick_coms_path_extra)
