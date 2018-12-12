625328b3c8eef2b945e87f204a5c13826e316230
deathbybandaid <sam@deathbybandaid.net>
2018-12-12 15:48:25 -0500

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -1267,16 +1267,7 @@ def bot_dictcom_feeds_handler(bot, feed, displayifnotnew):
             http_auth = bot.memory["botdict"]["tempvals"]['google'].authorize(httplib2.Http())
             service = build('calendar', 'v3', http=http_auth, cache_discovery=False)
 
-            page_token = None
-            calendar_ids = []
-            while True:
-                calendar_list = service.calendarList().list(pageToken=page_token).execute()
-                for calendar_list_entry in calendar_list['items']:
-                    # if '@qxf2.com' in calendar_list_entry['id']:
-                    calendar_ids.append(calendar_list_entry['id'])
-                page_token = calendar_list.get('nextPageToken')
-                if not page_token:
-                    break
+            calendar_list = service.calendarList().list(pageToken=None).execute()
 
             # events_result = service.events().list(calendarId='primary', maxResults=10).execute()
             # events = events_result.get('items', [])
@@ -1292,7 +1283,7 @@ def bot_dictcom_feeds_handler(bot, feed, displayifnotnew):
             #    else:
             #        return []
 
-            bot.msg("#spicebottest", str(calendar_ids))
+            bot.msg("#spicebottest", str(calendar_list))
             return []
 
             lastbuildtime = get_nick_value(bot, str(bot.nick), 'long', 'feeds', feed + '_lastbuildtime') or datetime.datetime(1999, 1, 1, 1, 1, 1, 1).replace(tzinfo=pytz.UTC)
