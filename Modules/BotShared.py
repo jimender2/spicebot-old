50ee64970c55a8f90a79a65eeab589059eb3aac2
deathbybandaid <sam@deathbybandaid.net>
2018-12-12 16:52:08 -0500

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -1269,7 +1269,7 @@ def bot_dictcom_feeds_handler(bot, feed, displayifnotnew):
 
             # calendar = service.calendars().get(calendarId='primary').execute()
 
-            events_result = service.events().list(calendarId=currentcalendar, maxResults=10, orderBy='startTime').execute()
+            events_result = service.events().list(calendarId=currentcalendar, maxResults=10, timeMin=datetime.datetime.utcnow()).execute()
             events = events_result.get('items', [])
 
             # try:
