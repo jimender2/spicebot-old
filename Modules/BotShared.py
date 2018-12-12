529995bdc7e2954c9fa44513efebc3d67df346d6
deathbybandaid <sam@deathbybandaid.net>
2018-12-12 12:01:53 -0500

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -1178,7 +1178,7 @@ def bot_dictcom_feeds_handler(bot, feed, displayifnotnew):
             lastbuildtime = get_nick_value(bot, str(bot.nick), 'long', 'feeds', feed + '_lastbuildtime') or datetime.datetime(1999, 1, 1, 1, 1, 1, 1).replace(tzinfo=pytz.UTC)
             lastbuildtime = parser.parse(str(lastbuildtime))
 
-            submissions = api.GetUserTimeline(currenttweetat)
+            submissions = bot.memory["botdict"]["tempvals"]['twitter'].GetUserTimeline(screen_name=currenttweetat, count=1, exclude_replies=True, include_rts=False)
             bot.msg("#spicebottest", str(submissions))
             return []
             submissions = bot.memory["botdict"]["tempvals"]['twitter'].GetUserTimeline(screen_name=currenttweetat, count=1, exclude_replies=True, include_rts=False)
