7cd9362e466ece2e62d69ede4ae99e9523beec80
deathbybandaid <sam@deathbybandaid.net>
2018-12-12 16:17:09 -0500

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -1267,9 +1267,25 @@ def bot_dictcom_feeds_handler(bot, feed, displayifnotnew):
             http_auth = bot.memory["botdict"]["tempvals"]['google'].authorize(httplib2.Http())
             service = build('calendar', 'v3', http=http_auth, cache_discovery=False)
 
-            # try:
-            events_result = service.events().list(calendarId=currentcalendar).execute()
+            # calendar = service.calendars().get(calendarId='primary').execute()
+
+            events_result = service.events().list(calendarId=currentcalendar, maxResults=10).execute()
             events = events_result.get('items', [])
+
+            # try:
+            # events_result = service.events().list(calendarId=currentcalendar).execute()
+            # events = events_result.get('items', [])
+            # except Exception as e:
+            #    if displayifnotnew:
+            #        return ["No Content Usable."]
+            #    else:
+            #        return []
+
+            # try:
+            # events_result = service.events().list(calendarId=currentcalendar, timeMin=now,
+            #                                      maxResults=1, singleEvents=True,
+            #                                      orderBy='startTime').execute()
+            # events = events_result.get('items', [])
             # except Exception as e:
             #    if displayifnotnew:
             #        return ["No Content Usable."]
