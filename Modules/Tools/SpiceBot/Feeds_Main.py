f40daf437df2eb09bbe35f4f4297f48cf9de6b0a
deathbybandaid <sam@deathbybandaid.net>
2018-12-11 20:37:49 -0500

    test

--- Modules/Tools/SpiceBot/Feeds_Main.py
+++ Modules/Tools/SpiceBot/Feeds_Main.py
@@ -178,7 +178,7 @@ def bot_dictcom_feeds_handler(bot, botcom, feed, displayifnotnew=True):
 
             feedjson = feedparser.parse(url)
 
-            rssentrytime = feedjson.entries[0].published_parsed
+            rssentrytime = feedjson.entries[0].pubdate
             rssentrytime = parser.parse(str(rssentrytime))
 
             xml = page.text
