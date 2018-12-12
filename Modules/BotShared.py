5b425f22166322e8e670482722c9d41d11040547
deathbybandaid <sam@deathbybandaid.net>
2018-12-12 18:58:07 -0500

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -1268,9 +1268,10 @@ def bot_dictcom_feeds_handler(bot, feed, displayifnotnew):
             service = build('calendar', 'v3', http=http_auth, cache_discovery=False)
 
             # nowtime = '2008-03-07T17:06:02.000Z'
-            nowtime = '2018-12-11T00:00:00.000Z'
-            nowtimesa = datetime.datetime.now()
-            bot.msg("#spicebottest", str(str(now.year) + " " + str(now.month) + " " + str(now.day) + " " + str(now.hour) + " " + str(now.minute) + " " + str(now.second)))
+            # nowtime = '2018-12-11T00:00:00.000Z'
+            nowtime = datetime.datetime.now()
+            nowtime = str(str(nowtime.year) + "-" + str(nowtime.month) + "-" + str(nowtime.day) + "T" + str(now.hour) + ":" + str(now.minute) + ":00.000Z")
+            # bot.msg("#spicebottest", str(str(now.year) + " " + str(now.month) + " " + str(now.day) + " " + str(now.hour) + " " + str(now.minute) + " " + str(now.second)))
 
             events_result = service.events().list(calendarId=currentcalendar, maxResults=1, singleEvents=True, orderBy='startTime', timeMin=nowtime).execute()
             events = events_result.get('items', [])
