c0b5c24e2902adf810015fc2f2aece5202e43009
deathbybandaid <sam@deathbybandaid.net>
2018-12-12 22:50:05 -0500

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -1314,7 +1314,6 @@ def bot_dictcom_feeds_handler(bot, feed, forcedisplay):
         dispmsg.insert(0, "[" + displayname + "]")
 
     botdict_save(bot)
-    osd(bot, "#spicebottest", 'say', dispmsg)
     return dispmsg
 
 
