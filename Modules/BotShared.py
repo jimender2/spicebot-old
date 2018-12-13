77883675a89bdbdc8f4b2ed72c48958421494223
deathbybandaid <sam@deathbybandaid.net>
2018-12-12 20:23:37 -0500

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -1276,7 +1276,7 @@ def bot_dictcom_feeds_handler(bot, feed, displayifnotnew):
                 return ["No upcoming events on this calendar"]
             nextevent = events[0]
 
-            entrytime = nextevent["start"][0]
+            entrytime = nextevent["start"]["dateTime"]
             osd(bot, "#spicebottest", 'say', str(entrytime))
             return []
 
