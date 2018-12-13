ffc36cf4d1b8018315b04ae59f11f5f80c8bb34a
deathbybandaid <sam@deathbybandaid.net>
2018-12-12 22:49:13 -0500

    test

--- Modules/BotCore/Automatic/Feeds_Auto.py
+++ Modules/BotCore/Automatic/Feeds_Auto.py
@@ -29,22 +29,23 @@ def auto_feeds(bot, trigger):
     while not bot_startup_requirements_met(bot, ["botdict", "monologue"]):
         pass
 
-    while True:
-        time.sleep(60)
-        for feed in bot.memory["botdict"]["tempvals"]['feeds'].keys():
-            # Thread(target=feeds_thread, args=(bot, feed,)).start()
-            feeds_thread(bot, feed)
+    for feed in bot.memory["botdict"]["tempvals"]['feeds'].keys():
+        Thread(target=feeds_thread, args=(bot, feed,)).start()
+        # feeds_thread(bot, feed)
 
 
 def feeds_thread(bot, feed):
-    dispmsg = bot_dictcom_feeds_handler(bot, feed, False)
-    if dispmsg != []:
-        for channel in bot.privileges.keys():
-            if 'feed' not in bot.memory["botdict"]['servers_list'][str(bot.memory["botdict"]["tempvals"]['server'])]['channels_list'][str(channel)]["disabled_commands"]:
-                feed_enabled = get_nick_value(bot, str(channel), "long", "feeds", "enabled") or []
+    while True:
+        time.sleep(60)
+        bot.msg("#spicebottest", feed)
+        dispmsg = bot_dictcom_feeds_handler(bot, feed, False)
+        if dispmsg != []:
+            for channel in bot.privileges.keys():
+                if 'feed' not in bot.memory["botdict"]['servers_list'][str(bot.memory["botdict"]["tempvals"]['server'])]['channels_list'][str(channel)]["disabled_commands"]:
+                    feed_enabled = get_nick_value(bot, str(channel), "long", "feeds", "enabled") or []
+                    if feed in feed_enabled:
+                        osd(bot, str(channel), 'say', dispmsg)
+            for user in bot.memory["botdict"]["tempvals"]["servers_list"][str(bot.memory["botdict"]["tempvals"]['server'])]['all_current_users']:
+                feed_enabled = get_nick_value(bot, user, "long", "feeds", "enabled") or []
                 if feed in feed_enabled:
-                    osd(bot, str(channel), 'say', dispmsg)
-        for user in bot.memory["botdict"]["tempvals"]["servers_list"][str(bot.memory["botdict"]["tempvals"]['server'])]['all_current_users']:
-            feed_enabled = get_nick_value(bot, user, "long", "feeds", "enabled") or []
-            if feed in feed_enabled:
-                osd(bot, user, 'priv', dispmsg)
+                    osd(bot, user, 'priv', dispmsg)
