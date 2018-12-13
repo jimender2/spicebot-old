9d9f1cdb7f2ca0a54170932977c46394861d61fd
deathbybandaid <sam@deathbybandaid.net>
2018-12-13 12:19:09 -0500

    fixed time

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -1331,18 +1331,6 @@ def bot_dictcom_feeds_handler(bot, feed, forcedisplay):
             scrapetime = feed_dict["scrapetime"]
             scrapetimezone = feed_dict["scrapetimezone"]
 
-            entrytime = tree.xpath(scrapetime)
-            if isinstance(entrytime, list):
-                entrytime = entrytime[0]
-            entrytime = str(entrytime)
-            for r in (("['", ""), ("']", ""), ("\\n", ""), ("\\t", ""), ("@ ", "")):
-                entrytime = entrytime.replace(*r)
-            entrytime = parser.parse(entrytime)
-            if not tz_aware(entrytime):
-                feedtimezone = pytz.timezone(feed_dict["scrapetimezone"])
-                entrytime = feedtimezone.localize(entrytime)
-
-            """
             try:
                 entrytime = tree.xpath(scrapetime)
                 if isinstance(entrytime, list):
@@ -1350,14 +1338,12 @@ def bot_dictcom_feeds_handler(bot, feed, forcedisplay):
                 entrytime = str(entrytime)
                 for r in (("['", ""), ("']", ""), ("\\n", ""), ("\\t", ""), ("@ ", "")):
                     entrytime = entrytime.replace(*r)
+                entrytime = parser.parse(entrytime)
                 if not tz_aware(entrytime):
-                    entrytime = parser.parse(entrytime)
-                    entrytime = parser.parse(entrytime).replace(tzinfo=None)
                     feedtimezone = pytz.timezone(feed_dict["scrapetimezone"])
                     entrytime = feedtimezone.localize(entrytime)
             except Exception as e:
                 entrytime = datetime.datetime(1999, 1, 1, 1, 1, 1, 1).replace(tzinfo=pytz.UTC)
-                """
 
             timeuntil = (entrytime - now).total_seconds()
             timecompare = arrow_time(now, entrytime)
