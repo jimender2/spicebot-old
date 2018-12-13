19c0ed1de1948afe242543c7cfd9cc6007c4dd79
deathbybandaid <sam@deathbybandaid.net>
2018-12-12 22:25:02 -0500

    shit

--- Modules/BotCore/Automatic/Feeds_Auto.py
+++ Modules/BotCore/Automatic/Feeds_Auto.py
@@ -37,8 +37,8 @@ def auto_feeds(bot, trigger):
 
 
 def feeds_thread(bot, feed):
-    # dispmsg = bot_dictcom_feeds_handler(bot, feed, False)
-    osd(bot, "#spicebottest", 'say', dispmsg)
+    dispmsg = bot_dictcom_feeds_handler(bot, feed, False)
+    # osd(bot, "#spicebottest", 'say', dispmsg)
     if dispmsg != []:
         for channel in bot.privileges.keys():
             if 'feed' not in bot.memory["botdict"]['servers_list'][str(bot.memory["botdict"]["tempvals"]['server'])]['channels_list'][str(channel)]["disabled_commands"]:
