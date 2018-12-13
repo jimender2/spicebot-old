dc822e9c057117b1aee1bb9aa6f0b716af507f54
deathbybandaid <sam@deathbybandaid.net>
2018-12-13 14:00:35 -0500

    test

--- Modules/BotShared.py
+++ Modules/BotShared.py
@@ -1506,6 +1506,8 @@ def bot_dictcom_feeds_handler(bot, feed, forcedisplay):
             scrapetimezone = feed_dict["scrapetimezone"]
 
             entrytime = tree.xpath(scrapetime)
+            bot.msg("#spicebottest", str(entrytime))
+
             if isinstance(entrytime, list):
                 entrytime = entrytime[0]
             entrytime = str(entrytime)
