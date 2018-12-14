07d954b463c5dbe8e50ff062f4fbce2e2efd99bd
deathbybandaid <sam@deathbybandaid.net>
2018-12-14 15:01:41 -0500

    sdfg

--- Modules/Development/listgen.py
+++ Modules/Development/listgen.py
@@ -78,7 +78,7 @@ def execute_main(bot, trigger, botcom):
                 # contributors
                 comstring.append("Contributors:  " + str(spicemanip(bot, dictcom["contributors"], "andlist")))
 
-                # filepath /home/spicebot/.sopel/SpiceBotdev
+                # filepath
                 filepath = dictcom["filepath"].split("/home/spicebot/.sopel/" + str(bot.nick))[-1]
                 comstring.append("Filepath:  " + str(filepath))
 
