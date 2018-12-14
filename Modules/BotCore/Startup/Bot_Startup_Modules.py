dbe6f60cbbba1b16ac4852cd24574b36f7e13b5b
deathbybandaid <sam@deathbybandaid.net>
2018-12-14 15:22:25 -0500

    test

--- Modules/BotCore/Startup/Bot_Startup_Modules.py
+++ Modules/BotCore/Startup/Bot_Startup_Modules.py
@@ -69,8 +69,7 @@ def bot_startup_modules(bot, trigger):
 
             if str(line).startswith("comdict") and not dict_from_file:
                 dict_from_file = str(line).replace("comdict = ", "")
-                bot.msg("#spicebottest", str(dict_from_file))
-                if str(line).endswith("}") and not dict_from_file_complete:
+                if str(dict_from_file).endswith("}") and not dict_from_file_complete:
                     dict_from_file_complete = True
 
             if dict_from_file and not dict_from_file_complete:
