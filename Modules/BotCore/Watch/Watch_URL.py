71159a94db478fbfba9635b8a40ea3239bab8921
deathbybandaid <sam@deathbybandaid.net>
2018-12-14 11:15:49 -0500

    test

---
+++ Modules/BotCore/Watch/Watch_URL.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+# sopel imports
+import sopel.module
+
+# imports for system and OS access, directories
+import os
+import sys
+
+# imports based on THIS file
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(shareddir)
+from BotShared import *
+
+# Ensure Encoding
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+@module.url('(.*)')
+@sopel.module.thread(True)
+def bot_join_hub(bot, trigger):
+
+    # don't run jobs if not ready
+    while not bot_startup_requirements_met(bot, ["connected", "botdict", "server", "channels", "users"]):
+        pass
+
+    bot.msg("#spicebottest", str(trigger))
+    bot.msg("#spicebottest", str(trigger.args))
