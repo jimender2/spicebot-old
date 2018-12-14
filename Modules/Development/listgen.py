2878cefe737687570b50071f41e27d15e2a3ed59
deathbybandaid <sam@deathbybandaid.net>
2018-12-14 14:31:54 -0500

    test

--- Modules/Development/listgen.py
+++ Modules/Development/listgen.py
@@ -53,6 +53,8 @@ def execute_main(bot, trigger, botcom):
         indexcount -= 1
         commandcount = len(mindex)
 
+        dispmsg.append([mtype + " commands:"])
+
         for command in mindex:
             commandcount -= 1
 
@@ -73,7 +75,8 @@ def execute_main(bot, trigger, botcom):
 
             # and to final
             dispmsg.append(comstring)
-            dispmsg.append(["     "])
+
+        dispmsg.append(["     "])
 
     for comstring in dispmsg:
         osd(bot, botcom.channel_current, 'say', comstring)
