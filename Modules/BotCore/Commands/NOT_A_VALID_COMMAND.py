f336c824ca182c1bed7e9573ca49d117f014a151
deathbybandaid <sam@deathbybandaid.net>
2018-12-16 18:34:54 -0500

    say

--- Modules/BotCore/Commands/NOT_A_VALID_COMMAND.py
+++ Modules/BotCore/Commands/NOT_A_VALID_COMMAND.py
@@ -52,4 +52,4 @@ def execute_main(bot, trigger, botcom):
     # command issued, check if valid
     botcom.dotcommand = spicemanip(bot, botcom.triggerargsarray, 1).lower()[1:]
     if botcom.dotcommand not in bot.memory["botdict"]["tempvals"]['dict_commands'].keys() and botcom.dotcommand not in bot.memory['botdict']['tempvals']['module_commands'].keys():
-        osd(bot, botcom.instigator, 'notice', ["I don't seem to have a command for " + str(botcom.dotcommand) + "!", "If you have a suggestion for this command, you can run .feature ." + str(botcom.dotcommand), " ADD DESCRIPTION HERE!"])
+        osd(bot, botcom.channel_current, 'say', ["I don't seem to have a command for " + str(botcom.dotcommand) + "!", "If you have a suggestion for this command, you can run .feature ." + str(botcom.dotcommand), " ADD DESCRIPTION HERE!"])
