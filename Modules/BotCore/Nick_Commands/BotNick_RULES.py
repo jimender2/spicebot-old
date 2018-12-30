81e6062b32d5d897a72e182b8ba6acfe0527359e
deathbybandaid <sam@deathbybandaid.net>
2018-12-30 13:08:34 -0500

    fuck you watch

---
+++ Modules/BotCore/Nick_Commands/BotNick_RULES.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals, absolute_import, print_function, division
+
+# sopel imports
+import sopel.module
+
+# imports for system and OS access, directories
+import os
+import sys
+
+# imports based on THIS file
+moduledir = os.path.dirname(__file__)
+shareddir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(shareddir)
+from BotShared import *
+
+# Ensure Encoding
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+
+comdict = {
+            "author": "deathbybandaid",
+            "contributors": [],
+            "description": "",
+            'privs': [],
+            "example": "",
+            "exampleresponse": "",
+            }
+
+
+@rule(r'(?i)(Fuck|Screw) you,? $nickname[ \t]*$')
+@sopel.module.thread(True)
+def bot_command_hub(bot, trigger):
+
+    botcom = botcom_nick(bot, trigger)
+
+    # Bots block
+    if bot_check_inlist(bot, botcom.instigator, [bot.nick]):
+        return
+
+    # does not apply to bots
+    if "altbots" in bot.memory:
+        if bot_check_inlist(bot, botcom.instigator, bot.memory["altbots"].keys()):
+            return
+
+    osd(bot, botcom.channel_current, 'say', "Watch your mouth, " + botcom.instigator + ", or I'll tell your mother!")
