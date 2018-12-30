a799eb19d05b2f4444e6cd7afad16e46c2f95bae
deathbybandaid <sam@deathbybandaid.net>
2018-12-30 17:54:47 -0500

    test

--- Modules/Main/SpiceOffended/IAmOffendedClone.py
+++ Modules/Main/SpiceOffended/IAmOffendedClone.py
@@ -55,7 +55,8 @@ def execute_main(bot, trigger, botcom):
 
     # make sure bot is OP
     if bot.privileges[botcom.channel_current][bot.nick.lower()] >= module.OP:
-        bot.write(['KICK', botcom.channel_current, botcom.instigator], "You can't talk like that in " + str(trigger.sender))
+        bot.write(['KICK', botcom.channel_current, botcom.instigator], "reason")
+        # bot.write(['KICK', botcom.channel_current, botcom.instigator], "You can't talk like that in " + str(trigger.sender))
         bot.say(trigger.nick + " had that coming. Anybody got a problem with that?")
     else:
         bot.say("I need to be OP to kick unauthorized users such as " + trigger.nick + " from " + trigger.sender + " for being offended.")
