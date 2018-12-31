a8f7797465323e9520e8b3948440376916afb155
deathbybandaid <sam@deathbybandaid.net>
2018-12-31 09:10:34 -0500

    try this

--- Modules/BotCore/Startup/Bot_Startup_ALLCOMS.py
+++ Modules/BotCore/Startup/Bot_Startup_ALLCOMS.py
@@ -42,40 +42,42 @@ def bot_startup_dict_coms(bot, trigger):
         comtypedict = str(comtype + "_commands")
         bot.memory["botdict"]["tempvals"]['all_coms'].extend(bot.memory[comtypedict].keys())
 
-    channeldict = dict()
-
-    for channel in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"].keys():
-        channeldict[str(channel).lower()] = dict()
-        channeldict[str(channel).lower()]['allcoms'] = []
-        channeldict[str(channel).lower()]['allcoms'].extend(bot.memory["botdict"]["tempvals"]['all_coms'])
-        channeldict[str(channel).lower()]['altdibs'] = []
-
-    for botchannel in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"].keys():
-        if "com_dibs" not in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"][str(botchannel).lower()].keys():
-            bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"][str(botchannel).lower()]["com_dibs"] = []
-        bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"][str(botchannel).lower()]["com_dibs"].extend(bot.memory["botdict"]["tempvals"]['all_coms'])
-
-    if "altbots" in bot.memory:
-        for botname in bot.memory["altbots"].keys():
-            if "tempvals" in bot.memory["altbots"][botname].keys():
-                if "server" in bot.memory["altbots"][botname]["tempvals"].keys():
-                    altbotserver = bot.memory["altbots"][botname]["tempvals"]['server']
-                    if altbotserver == currentservername:
-                        if "channels_list" in bot.memory["altbots"][botname]["tempvals"]["servers_list"][currentservername].keys():
-                                for altbotchan in bot.memory["altbots"][botname]["tempvals"]["servers_list"][currentservername]["channels_list"].keys():
-                                    if str(altbotchan).lower() in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"].keys():
-                                        if "com_dibs" in bot.memory["altbots"][botname]["tempvals"]["servers_list"][currentservername]["channels_list"][str(altbotchan).lower()].keys():
-                                            for valcom in bot.memory["botdict"]["tempvals"]['all_coms']:
-                                                if valcom in bot.memory["altbots"][botname]["tempvals"]["servers_list"][currentservername]["channels_list"][str(altbotchan).lower()]["com_dibs"]:
-                                                    channeldict[str(altbotchan).lower()]['altdibs'].append(valcom)
-
-    for channel in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"].keys():
-        for valcom in channeldict[str(channel).lower()]['allcoms']:
-            if valcom in channeldict[str(channel).lower()]['altdibs']:
-                if valcom in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"][str(channel).lower()]["com_dibs"]:
-                    bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"][str(channel).lower()]["com_dibs"].remove(valcom)
-            else:
-                if valcom not in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"][str(channel).lower()]["com_dibs"]:
-                    bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"][str(channel).lower()]["com_dibs"].append(valcom)
-
     bot_startup_requirements_set(bot, "all_coms")
+
+    while True:
+
+        channeldict = dict()
+
+        for channel in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"].keys():
+            channeldict[str(channel).lower()] = dict()
+            channeldict[str(channel).lower()]['allcoms'] = []
+            channeldict[str(channel).lower()]['allcoms'].extend(bot.memory["botdict"]["tempvals"]['all_coms'])
+            channeldict[str(channel).lower()]['altdibs'] = []
+
+        for botchannel in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"].keys():
+            if "com_dibs" not in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"][str(botchannel).lower()].keys():
+                bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"][str(botchannel).lower()]["com_dibs"] = []
+            bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"][str(botchannel).lower()]["com_dibs"].extend(bot.memory["botdict"]["tempvals"]['all_coms'])
+
+        if "altbots" in bot.memory:
+            for botname in bot.memory["altbots"].keys():
+                if "tempvals" in bot.memory["altbots"][botname].keys():
+                    if "server" in bot.memory["altbots"][botname]["tempvals"].keys():
+                        altbotserver = bot.memory["altbots"][botname]["tempvals"]['server']
+                        if altbotserver == currentservername:
+                            if "channels_list" in bot.memory["altbots"][botname]["tempvals"]["servers_list"][currentservername].keys():
+                                    for altbotchan in bot.memory["altbots"][botname]["tempvals"]["servers_list"][currentservername]["channels_list"].keys():
+                                        if str(altbotchan).lower() in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"].keys():
+                                            if "com_dibs" in bot.memory["altbots"][botname]["tempvals"]["servers_list"][currentservername]["channels_list"][str(altbotchan).lower()].keys():
+                                                for valcom in bot.memory["botdict"]["tempvals"]['all_coms']:
+                                                    if valcom in bot.memory["altbots"][botname]["tempvals"]["servers_list"][currentservername]["channels_list"][str(altbotchan).lower()]["com_dibs"]:
+                                                        channeldict[str(altbotchan).lower()]['altdibs'].append(valcom)
+
+        for channel in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"].keys():
+            for valcom in channeldict[str(channel).lower()]['allcoms']:
+                if valcom in channeldict[str(channel).lower()]['altdibs']:
+                    if valcom in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"][str(channel).lower()]["com_dibs"]:
+                        bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"][str(channel).lower()]["com_dibs"].remove(valcom)
+                else:
+                    if valcom not in bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"][str(channel).lower()]["com_dibs"]:
+                        bot.memory["botdict"]["tempvals"]["servers_list"][currentservername]["channels_list"][str(channel).lower()]["com_dibs"].append(valcom)
