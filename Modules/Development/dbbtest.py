fa92ad07690a6c38e26be32cff08c4ad6b956397
deathbybandaid <sam@deathbybandaid.net>
2019-03-31 21:07:43 -0400

    test

--- Modules/Development/dbbtest.py
+++ Modules/Development/dbbtest.py
@@ -39,7 +39,10 @@ def mainfunction(bot, trigger):
 
     # bot.say(str(trigger.hostmask))
 
-    # return
+    bot.say(str(bot.users.get("deathbybandaid").host))
+    bot.say(str(bot.users.get(bot.nick).host))
+
+    return
 
     # triggerargs = spicemanip.main(trigger.args[1], '2+', 'string')
     triggerargs = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum"
